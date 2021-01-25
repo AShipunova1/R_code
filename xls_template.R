354c7717288f2a90ed210256c257c0871529086c
AShipunova1 <anna.shipunova@noaa.gov>
2021-01-25 14:21:41 -0500

    Update xls_template.R
    add functions

--- xls_template.R
+++ xls_template.R
@@ -1,8 +1,29 @@
 library(openxlsx)
 
+create_new_workbook <- function() {
+  wb <- createWorkbook()
+  wb
+}
+
+load_exist_workbook <- function(template_file_full_name) {
+  wb <- loadWorkbook(template_file_full_name)
+  wb
+}
+
+add_data_to_new_sheet <- function(workbook_name, new_sheetName, my_data) {
+  addWorksheet(workbook_name, sheetName = new_sheetName) 
+  
+  writeData(workbook_name, sheet = new_sheetName, x = my_data) 
+}
+
+save_workbook <- function(workbook_name, template_file_full_name) {
+  template_file_name <- basename(template_file_full_name)
+  saveWorkbook(workbook_name, template_file_name, overwrite = TRUE)
+}
+
+# __main__
 getwd()
 template_file_full_name <- "~/examples_and_manuals/results_template.xlsx"
-template_file_name <- basename(template_file_full_name)
 
 # create new:
 # wb <- createWorkbook() 
