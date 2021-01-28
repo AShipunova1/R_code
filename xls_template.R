4416a5324d17cbd13c4f4abb186b7f42c6e223da
AShipunova1 <anna.shipunova@noaa.gov>
2021-01-28 14:14:57 -0500

    Update xls_template.R
    remove client

--- xls_template.R
+++ xls_template.R
@@ -23,48 +23,4 @@ save_workbook <- function(workbook_name, template_file_full_name) {
 
 # __main__
 getwd()
-template_file_full_name <- "~/examples_and_manuals/results_template.xlsx"
-wb <- load_exist_workbook(template_file_full_name)
-q_mass_r <- "select 
-YEAR,
-MONTH,
-NEGEAR,
-target_comnames TARGSPEC1_2,
-COMNAME,
-anmlcond,
-GIS_LATHBEG,
-GIS_LONHBEG,
-GIS_LATHEND,
-GIS_LONHEND
-FROM
-    REQUEST_INC_ALL
-    JOIN LAT_LON_DATA_RESULT_MASS_R ON ( ROUND(gis_lathbeg,6) = ROUND(lat, 6)
-                                      AND ROUND(gis_lonhbeg, 6) = ROUND(lon, 6) )
-                                    OR ( ROUND(gis_lathend, 6) = ROUND(lat, 6)
-                                         AND ROUND(gis_lonhend, 6) = ROUND(lon, 6) );"
-mass_r_res <- dbGetQuery(con_nova, q_mass_r)
-new_sheetName <- "mass_restr1"
-add_data_to_new_sheet(wb, new_sheetName, mass_r_res)
 
-q_great_s_ch_r <- "select 
-YEAR,
-MONTH,
-NEGEAR,
-target_comnames TARGSPEC1_2,
-COMNAME,
-anmlcond,
-GIS_LATHBEG,
-GIS_LONHBEG,
-GIS_LATHEND,
-GIS_LONHEND
-FROM
-    REQUEST_INC_ALL
-    JOIN lat_lon_data_result_great_s_ch ON ( ROUND(gis_lathbeg,6) = ROUND(lat, 6)
-                                      AND ROUND(gis_lonhbeg, 6) = ROUND(lon, 6) )
-                                    OR ( ROUND(gis_lathend, 6) = ROUND(lat, 6)
-                                         AND ROUND(gis_lonhend, 6) = ROUND(lon, 6) );"
-
-great_s_ch_r_res <- dbGetQuery(con_nova, q_great_s_ch_r)
-new_sheetName <- "great_s_ch_r"
-add_data_to_new_sheet(wb, new_sheetName, great_s_ch_r_res)
-save_workbook(wb, template_file_full_name)
