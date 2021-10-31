7127334c65bb5cac34f3056db06fd31b9dfb73fc
AShipunova1 <anna.shipunova@noaa.gov>
2021-10-31 11:53:50 -0400

    tryCatch

--- get_subset_f.R
+++ get_subset_f.R
@@ -248,7 +248,51 @@ my_test <- function() {
   # lat_lon_data <- stack_lat_lon_mid(lat_lon_mid_data)
   #
 
-  lat_lon_data_list <- lat_lon_data_to_spf(lat_lon_data, shapefile_data)
+  # lat_lon_data_list <- lat_lon_data_to_spf(lat_lon_data, shapefile_data)
+  
+  lat_lon_data_list <- tryCatch(
+    {
+      # Just to highlight: if you want to use more than one 
+      # R expression in the "try" part then you'll have to 
+      # use curly brackets.
+      # 'tryCatch()' will return the last evaluated expression 
+      # in case the "try" part was completed successfully
+      
+      message("This is the 'try' part")
+      
+      lat_lon_data_to_spf(lat_lon_data, shapefile_data)
+      # The return value is the actual value 
+      # that will be returned in case there is no condition 
+      # (e.g. warning or error). 
+      # You don't need to state the return value via `return()` as code 
+      # in the "try" part is not wrapped inside a function (unlike that
+      # for the condition handlers for warnings and error below)
+    },
+    error=function(cond) {
+      message(paste("No trips inside the shapefile area: ", filenames[2]))
+      message("Here's the original error message:")
+      message(cond)
+      # Choose a return value in case of error
+      return(NA)
+    },
+    warning=function(cond) {
+      message(paste("Coordinates caused a warning:", filenames[2]))
+      message("Here's the original warning message:")
+      message(cond)
+      # Choose a return value in case of warning
+      return(NULL)
+    },
+    finally={
+      # NOTE:
+      # Here goes everything that should be executed at the end,
+      # regardless of success or error.
+      # If you want more than one expression to be executed, then you 
+      # need to wrap them in curly brackets ({...}); otherwise you could
+      # just have written 'finally=<expression>' 
+      message(paste("Processed coords:", filenames[2]))
+      message("Some other message at the end")
+    }
+  )    
   
   view_maps(shapefile_data, lat_lon_data_list)
   write_result_to_csv(lat_lon_data_list[2], filenames)
