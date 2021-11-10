c5da7e571f847c620369cbb0210742cb51e45e5d
AShipunova1 <anna.shipunova@noaa.gov>
2021-11-10 10:42:20 -0500

    union query

--- one_link1.R
+++ one_link1.R
@@ -10,16 +10,33 @@ clean_where_part <- function(where_part) {
   return(where_part)
 }
 
-get_data_from_db <- function(table_name, where_part = "") {
-  where_part <- clean_where_part(where_part)
-  print(where_part)
-  q <- paste("select distinct 
-  haulnum,
-  GIS_LATHBEG,
-    GIS_LATHEND,
-    GIS_LONHBEG,
-    GIS_LONHEND
-             FROM ", table_name, where_part, sep = " ")
+get_hdata_from_db <- function(link1) {
+  
+  q <- paste("SELECT DISTINCT
+    TO_NUMBER(haulnum) || '_beg' as haulnum,
+    gis_lathbeg,
+    gis_lonhbeg
+FROM
+    obhau
+WHERE
+    link1 = '", link1, "'
+union
+SELECT DISTINCT
+    TO_NUMBER(haulnum)  || '_end' as haulnum,
+    gis_lathend,
+    gis_lonhend
+FROM
+    obhau
+WHERE
+    link1 = '", link1, "'", sep = "")
+
+  # q <- paste("select distinct 
+  # haulnum,
+  # GIS_LATHBEG,
+  #   GIS_LATHEND,
+  #   GIS_LONHBEG,
+  #   GIS_LONHEND
+  #            FROM ", table_name, where_part, sep = " ")
   print(q)
   dbGetQuery(con_nova, q)
 }
@@ -36,33 +53,19 @@ clean_data <- function(lat_lon_data_all) {
 }
 
 clean_dat_w_hnum <- function(lat_lon_data_w_hnum) {
-  #   SELECT
-  # DISTINCT
-  # haulnum,
-  #     gis_lathbeg,
-  #     gis_lathend,
-  #     gis_lonhbeg,
-  #     gis_lonhend
-  # FROM
-  #     obhau
-  # WHERE
-  #     link1 = '000201706M93025'
-  #     order by haulnum
-  #     ;
   # c:\Users\anna.shipunova\work_dir\today\requests\temp_results\export_w_hnum.csv
 
   stacked_lat_lon <- cbind(
     stack(lat_lon_data_w_hnum[2:3]),
     stack(lat_lon_data_w_hnum[4:5])
   )
-  rep_data <- lat_lon_data_w_hnum[rep(lat_lon_data_w_hnum$HAULNUM, each = 2), ]
+  rep_data <- as.data.frame(rep(lat_lon_data_w_hnum$HAULNUM, each = 2))
   row_odd <- seq_len(nrow(rep_data)) %% 2
   rep_data[row_odd == 1, 1] <- paste0(rep_data[row_odd == 1, 1], "_beg")
   rep_data[row_odd == 0, 1] <- paste0(rep_data[row_odd == 0, 1], "_end")
 
   stacked_lat_lon$haulnum <- rep_data[, 1]
-
-  colnames(stacked_lat_lon) <- c("lat", "i1", "lon", "i2", "haulnum")
+  colnames(stacked_lat_lon) <- c("latitude", "i1", "longitude", "i2", "haulnum")
 
   res2 <- dplyr::select(stacked_lat_lon, !starts_with("i"))
 
@@ -70,23 +73,23 @@ clean_dat_w_hnum <- function(lat_lon_data_w_hnum) {
   res2[complete.cases(res2), ]
 }
 
-
 show_dots <- function(lat_lon) {
   lat_lon %>%
     leaflet() %>%
     addTiles() %>%
     addMarkers(
-      label = paste(lat_lon$haulnum, " ", lat_lon$lat, " ", lat_lon$lon),
+      label = paste(lat_lon$haulnum, " ", lat_lon$latitude, " ", lat_lon$longitude),
       labelOptions = labelOptions(noHide = T),
       clusterOptions = markerClusterOptions()
     )
 }
 
 # __main__
-table_name <- "OBHAU"
-where_part <- "link1 = '000201805Q47012'"
-export1 <- get_data_from_db(table_name, where_part)
+link1 <- '000201706M93025'
+export1 <- get_hdata_from_db(link1)
+colnames(export1) <- c("haulnum", "latitude", "longitude")
+
 # or
 # export1 <- read.csv("C:/Users/anna.shipunova/work_dir/today/temp/1.csv")
-dots1 <- clean_dat_w_hnum(export1)
-show_dots(dots1)
+# dots1 <- clean_dat_w_hnum(export1)
+show_dots(export1)
