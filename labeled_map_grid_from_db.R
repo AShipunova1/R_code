9181931444458d5f0dc09532ae3a7204f0983605
AShipunova1 <anna.shipunova@noaa.gov>
2022-01-10 19:47:06 -0500

    make_map funcs

--- labeled_map_grid_from_db.R
+++ labeled_map_grid_from_db.R
@@ -4,12 +4,26 @@ prepare_data <- function(db_data) {
     db_data <- db_data[complete.cases(db_data), ]
     num_columns <- c("lat", "lon")
     db_data[, num_columns] <- lapply(num_columns, function(x) as.numeric(db_data[[x]]))
+}
 
-    leaflet(db_data) %>% addTiles() %>%
+make_map1 <- function(db_data) {
+        leaflet(db_data) %>% addTiles() %>%
         # setView(-70, 40, zoom = 1) %>%
         addCircleMarkers(~lon, ~lat,
-            radius = ~ ifelse(coord_name == "ten_min", 7, 4),
-            color = ~ ifelse(coord_name == "ten_min", "green", "red"),
+            radius = 4,
+            color = "red",
+            stroke = FALSE, fillOpacity = 0.5,
+            label = paste(db_data$coord_name, round(db_data$lat, 3), round(db_data$lon, 3), sep = "_")
+        ) -> m
+    m %>%
+        addSimpleGraticule(interval = 1)
+}
+
+make_map2 <- function(db_data1, db_data2) {
+        leaflet(db_data) %>% addTiles() %>%
+        addCircleMarkers(~lon, ~lat,
+            radius = ~ ifelse(coord_name == "coords2", 4, 4),
+            color = ~ ifelse(coord_name == "coords2", "green", "red"),
             stroke = FALSE, fillOpacity = 0.5,
             label = paste(db_data$coord_name, round(db_data$lat, 3), round(db_data$lon, 3), sep = "_")
         ) -> m
@@ -19,7 +33,7 @@ prepare_data <- function(db_data) {
 
 run_example <- function() {
     q1 <- paste("SELECT DISTINCT
-    link3 || '_beg_1' AS haul,
+    'coords1',
     lat_beg,
     lon_beg
 FROM
@@ -31,7 +45,7 @@ WHERE
          AND lon_end BETWEEN - 80 AND - 70 )
 UNION
 SELECT DISTINCT
-    link3 || '_end1' AS haul,
+    'coords1',
     lat_end,
     lon_end
 FROM
@@ -43,7 +57,7 @@ WHERE
          AND lon_end BETWEEN - 80 AND - 70 )")
 
 q2 <- paste("SELECT DISTINCT
-    link3 || '_beg2' AS haul,
+    'coords2',
     lat_beg,
     lon_beg
 FROM
@@ -55,7 +69,7 @@ WHERE
          AND lon_end BETWEEN - 70 AND - 66 )
 UNION
 SELECT DISTINCT
-    link3 || '_end2' AS haul,
+    'coords2',
     lat_end,
     lon_end
 FROM
@@ -65,8 +79,10 @@ WHERE
       AND lon_beg BETWEEN - 70 AND - 66 )
     OR ( lat_end BETWEEN 40 AND 42.5
          AND lon_end BETWEEN - 70 AND - 66 )")
-    
+
     db_data1 <- dbGetQuery(con_nova, q1)
     db_data2 <- dbGetQuery(con_nova, q2)
-    prepare_data(db_data1)
+    dat1 <- prepare_data(db_data1)
+    dat2 <- prepare_data(db_data2)
+    make_map2(dat1, dat2)
 }
