48523169e0748075d3a3406730b461b74fe23d2a
AShipunova1 <anna.shipunova@noaa.gov>
2022-01-24 14:10:50 -0500

    init

---
+++ convert_coords.R
@@ -0,0 +1,52 @@
+main <- function(q) {
+  db_data <- dbGetQuery(con_nova, q)  
+  all_coords <- get_all_clean_coords(db_data)
+  all_coords <- all_coords[complete.cases(all_coords), ]
+  convert_to_decimal_degree(dm_num)
+}
+
+get_all_clean_coords <- function(db_data){
+  all_coords <- data.frame(NA, NA, NA)
+  names(all_coords) <- c("port_name", "lat", "lon")
+  for (i in 1:nrow(db_data)) {
+    l_row <- db_data[i, ]
+    lat_1 <- clean_coord(l_row[3])
+    lon_1 <- clean_coord(l_row[4])
+    temp_df <- data.frame(l_row[2], lat_1, lon_1)
+    all_coords[nrow(all_coords) + 1,] <- temp_df
+  }
+  all_coords
+}
+
+clean_coord <- function(str_val){
+  pattern <- "[^0-9]+"
+  replacement <- "."
+  res1 <- str_replace_all(str_val, pattern, replacement)
+  res <- str_replace_all(res1, "\\.$", "")
+  res
+}
+
+convert_to_decimal_degree(dm_num)
+{
+  deg_min_sec <- str_split(db_num, "\\.", simplify = TRUE)
+  
+  degree <- as.numeric(deg_min_sec[1,1])
+  min <- as.numeric(deg_min_sec[1,2])
+  sec <- as.numeric(deg_min_sec[1,3])
+    
+  gis_coord <- degree + min/60 + sec/3600
+  gis_coord
+}
+
+convert_to_gis <- function(all_coords){
+  gis_coords <- data.frame(NA, NA, NA, NA, NA)
+  names(gis_coords) <- c("port_name", "DDMM_LAT", "DDMM_LON", "LON", "LAT")
+  for (i in 1:nrow(all_coords)) {
+    l_row <- all_coords[i, ]
+    lat_1 <- clean_coord(l_row[3])
+    lon_1 <- clean_coord(l_row[4])
+    temp_df <- data.frame(l_row[2], lat_1, lon_1)
+    gis_coords[nrow(gis_coords) + 1,] <- temp_df
+  }
+  gis_coords
+}
