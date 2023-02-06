52070fc9c2f759c681ab752986783a5b11ab5bbc
Anna Shipunova <anna.shipunova@nmfs.local>
2023-02-06 09:35:17 -0500

    correct path

--- start_module.R
+++ start_module.R
@@ -1,3 +1,11 @@
+#---
+# How to use:
+# source("~/GitHub/R_code/start_module.R")
+# my_paths <- set_work_dir()
+# csv_names_list = list("report1".csv", "report2.csv")
+# csv_content_1 <- load_csv_names(my_paths, csv_names_list)[[1]]
+#---
+
 library(dplyr)
 #install.packages("tidyverse")
 library(tidyverse)
@@ -5,20 +13,20 @@ library(stringr)
 library(magrittr)
 
 set_work_dir <- function() {
-  main_dir <- "~/Documents/R_files"
+  setwd("~/")
+  base_dir <- getwd()
+  main_r_dir <- "R_files_local"
   in_dir <- "my_inputs"
-  full_path_to_in_dir <- file.path(main_dir, in_dir)
+  full_path_to_in_dir <- file.path(base_dir, main_r_dir, in_dir)
   out_dir <- "my_outputs"
-  full_path_to_out_dir <- file.path(main_dir, out_dir)
+  full_path_to_out_dir <- file.path(base_dir, main_r_dir, out_dir)
   # dir.create(full_path_to_out_dir)
-  setwd(full_path_to_in_dir)
+  setwd(file.path(base_dir, main_r_dir))
 
   my_paths <- list("inputs" = full_path_to_in_dir,
     "outputs" = full_path_to_out_dir)
   return(my_paths)
 }
-
-# csv_names_list = list("report1".csv", "report2.csv")
 
 load_csv_names <- function(my_paths, csv_names_list) {
   my_inputs <- my_paths$inputs
