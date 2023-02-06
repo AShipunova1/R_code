2696afdbac4d6f1195606fe6f61352c0c8eae27f
Anna Shipunova <anna.shipunova@nmfs.local>
2023-02-06 12:48:06 -0500

    +load_xls_names <- function

--- start_module.R
+++ start_module.R
@@ -1,9 +1,13 @@
+# nolint: commented_code_linter.
 #---
 # How to use:
 # source("~/GitHub/R_code/start_module.R")
 # my_paths <- set_work_dir()
 # csv_names_list = list("report1".csv", "report2.csv")
+# xls_names_list = list("report1a".xls", "report2a.xls")
 # csv_content_1 <- load_csv_names(my_paths, csv_names_list)[[1]]
+# xls_content_1 <- load_xls_names(my_paths, xls_names_list)[[1]]
+
 #---
 
 library(dplyr)
@@ -11,6 +15,7 @@ library(dplyr)
 library(tidyverse)
 library(stringr)
 library(magrittr)
+library(readxl)  # reading in .xlsx
 
 set_work_dir <- function() {
   setwd("~/")
@@ -33,6 +38,20 @@ load_csv_names <- function(my_paths, csv_names_list) {
   myfiles <- sapply(csv_names_list, function(x) file.path(my_inputs, x))
 
   contents <- sapply(myfiles, read.csv, header = TRUE, simplify = FALSE)
+
+  return(contents)
+}
+
+load_xls_names <- function(my_paths, xls_names_list, sheet_num = 1) {
+  my_inputs <- my_paths$inputs
+
+  myfiles <- sapply(xls_names_list, function(x) file.path(my_inputs, x))
+
+  # xls_content_1 <- read_excel(paste(my_paths$inputs,
+  # xsl_names_list[[1]],
+  # sep = "/"), 1)  # nolint: commented_code_linter.
+
+  contents <- sapply(myfiles, read_excel, sheet_num)
 
   return(contents)
 }
