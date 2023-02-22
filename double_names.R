73f260ece2f87b63ca5992e626c629b49aa0e8b9
AShipunova1 <anna.shipunova@noaa.gov>
2023-02-22 11:38:35 -0500

    mutate(across

--- double_names.R
+++ double_names.R
@@ -1,17 +1,67 @@
-# find some of the vessels that have more than one vessel ID associated with them over time
+# Find vessels that have more than one vessel ID associated with them over time
+
 # Get common functions
 source("~/R_code_github/useful_functions_module.r")
 
-# ----set up----
+## ---- set up ----
 my_paths <- set_work_dir()
 
-## ---- get csv data into variables ----
-temp_var <- get_compl_and_corresp_data(my_paths)
-compl_clean <- temp_var[[1]]
-corresp_clean <- temp_var[[2]]
+## ----- get csv data into variables -----
+# temp_var <- get_compl_and_corresp_data(my_paths)
+# compl_clean <- temp_var[[1]]
+# corresp_clean <- temp_var[[2]]
+## ---- get safis data ----
+# safis_clean
+# "C:\Users\anna.shipunova\Documents\R_files_local\my_inputs\all_vessels_safis.csv"
+csv_names_list = list("all_vessels_safis.csv")
+# read all csv files
+csv_contents <- load_csv_names(my_paths, csv_names_list)
+csvs_clean <- lapply(csv_contents, clean_headers)
+dim(csvs_clean[[1]])
 
-# data_overview(compl_clean)
-# vesselofficialnumber      3715
+date_fields <- c("entrydate", "updatedate", "de", "dc")
+# csvs_clean %>% 
+#   for (i in seq_along(csvs_clean)){
+#     csvs_clean[[i]]$vesselofficialnumber <-
+#       trimws(csvs_clean[[i]]$vesselofficialnumber)
+#   }
+
+# data_overview(csvs_clean[[1]])
+date_format = "%m/%d/%Y"
+
+# csvs_clean[[1]] %>%
+#   # change_to_dates(date_fields[1], date_format) %>%
+#   # change_to_dates("updatedate", date_format) ->
+#   change_to_dates("entrydate", date_format) %>%
+#   change_to_dates("updatedate", date_format) %>%
+#   change_to_dates("de", date_format) %>%
+#   change_to_dates("dc", date_format) ->
+#   safis_clean
+
+# data_overview(safis_clean)
+# gdf %>% mutate(across(v1:v2, ~ .x + n))
+
+
+fun1 <- function(x) {
+  out <- as.POSIXct(x,
+             format = date_format)
+  # as.POSIXct(pull(csvs_clean[[1]][x]),
+  #   format = date_format) %>% str()
+  out
+}
+
+csvs_clean[[1]] %>%
+  mutate(across(date_fields, fun1)) %>% str()
+# as.POSIXct(pull(my_df[field_name]),
+# format = date_format))
+  
+
+csvs_clean[[1]] %>%
+  change_to_dates(date_fields, date_format) %>% str()
+
+data_overview(safis_clean)
+# vesselid         130666
+data_overview(safis_clean)
 
 # df1 %>%
 #   group_by_(.dots = names(df1)[3:6]) %>%
@@ -23,5 +73,6 @@ compl_clean %>%
   add_count(name, vesselofficialnumber, name = "id_freq") %>% 
   filter(id_freq > 1) %>% str()
     # filter(n_distinct(vesselofficialnumber) > 1)
-  0
-  
+  # 0
+
+
