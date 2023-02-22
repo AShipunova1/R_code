2b0ea04d2fcf11a0dec3222c5979893a895783cc
AShipunova1 <anna.shipunova@noaa.gov>
2023-02-22 16:14:34 -0500

    used_doube_pairs, writes a txt

--- double_names.R
+++ double_names.R
@@ -57,3 +57,95 @@ temp_var <- get_compl_and_corresp_data(my_paths)
 compl_clean <- temp_var[[1]]
 corresp_clean <- temp_var[[2]]
 
+
+used_doube_pairs <- double_names_pairs %>%
+  filter(coastguard %in% compl_clean$vesselofficialnumber &
+           statereg != "-") 
+# 94
+
+used_doube_pairs <- double_names_pairs %>%
+  filter(statereg %in% compl_clean$vesselofficialnumber &
+            coastguard != "-") %>% add_row(used_doube_pairs)
+# 6
+
+used_doube_pairs <- double_names_pairs %>%
+  filter(coastguard %in% corresp_clean$vesselofficialnumber &
+           statereg != "-") %>% add_row(used_doube_pairs)
+# 74
+used_doube_pairs <- double_names_pairs %>%
+  filter(statereg %in% corresp_clean$vesselofficialnumber &
+           coastguard != "-") %>% add_row(used_doube_pairs)
+# 20
+
+used_doube_pairs_u <- unique(used_doube_pairs)
+# 100
+
+# write.csv(used_doube_pairs_u, "used_doube_pairs_u.csv")
+
+# check
+corresp_clean %>% 
+  filter(grepl("1303026|FL1815RG", vesselofficialnumber)) %>%
+  select(vesselofficialnumber) %>% unique()
+  # select(-starts_with("week")) %>% unique()
+
+compl_clean %>% 
+  filter(grepl("1303026|FL1815RG", vesselofficialnumber)) %>%
+  select(vesselofficialnumber) %>% unique()
+
+corresp_clean %>% 
+  filter(grepl("1303026|FL1815RG", vesselofficialnumber)) %>%
+  select(vesselofficialnumber) %>% unique()
+
+# slow
+# TODO add munbers to the output
+foo <- function(x, output = "myfile.txt", num) {
+  # browser()
+  
+  pair <- paste(x[[1]],
+                x[[2]], sep = "|")
+  
+  in_corr <- corresp_clean %>% 
+    filter(grepl(pair, vesselofficialnumber)) %>%
+    select(vesselofficialnumber) %>% 
+    # select(vesselofficialnumber, contact_freq) %>% 
+    unique() 
+  # %>%
+    # lapply(write, file = output, append = TRUE)
+  
+  in_compl <- compl_clean %>% 
+    filter(grepl(pair, vesselofficialnumber)) %>%
+    select(vesselofficialnumber) %>% 
+    unique() 
+  # %>%
+    # lapply(write, file = output, append = TRUE)
+  if (nrow(in_corr) > 2 | nrow(in_compl) > 2) {
+    # browser()
+    print(pair)
+  }
+  else if (!identical(in_corr, in_compl)) {
+    my_out <- paste0(num <- num + 1, "\n")
+    # write(num, file = output, append = TRUE)
+    my_out <- paste0(my_out, pair, "\n")
+    # write(pair, file = output, append = TRUE)
+    my_out <- paste0(my_out, "correspondence", "\n")
+    # write("correspondence", file = output, append = TRUE)
+    # browser()
+    
+    my_out <- paste0(my_out, paste(in_corr), "\n")
+      # lapply(in_corr, function(x) paste0(my_out, x)) %>%
+    # lapply(in_corr, paste0, my_out)
+    # lapply(in_corr, write, file = output, append = TRUE)
+    my_out <- paste0(my_out, "compliance", "\n")
+    # write("compliance", file = output, append = TRUE)
+    # lapply(in_compl, write, file = output, append = TRUE)
+    my_out <- paste0(my_out, paste(in_compl), "\n")
+    my_out <- paste0(my_out, "\n")
+    
+    write(my_out, file = output, append = TRUE)
+  }
+  
+}
+
+num = 0
+apply(used_doube_pairs_u, 1, foo, output = 'outputfile.txt', num)
+
