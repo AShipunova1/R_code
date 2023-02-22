88084124a6cd68a3f0a2dec9b7f18aafc133693f
AShipunova1 <anna.shipunova@noaa.gov>
2023-02-22 12:03:23 -0500

    works, clean up

--- double_names.R
+++ double_names.R
@@ -6,15 +6,9 @@ source("~/R_code_github/useful_functions_module.r")
 ## ---- set up ----
 my_paths <- set_work_dir()
 
-## ----- get csv data into variables -----
-# temp_var <- get_compl_and_corresp_data(my_paths)
-# compl_clean <- temp_var[[1]]
-# corresp_clean <- temp_var[[2]]
 ## ---- get safis data ----
-# safis_clean
-# "C:\Users\anna.shipunova\Documents\R_files_local\my_inputs\all_vessels_safis.csv"
 csv_names_list = list("all_vessels_safis.csv")
-# read all csv files
+# read csv file
 csv_contents <- load_csv_names(my_paths, csv_names_list)
 csvs_clean <- lapply(csv_contents, clean_headers)
 dim(csvs_clean[[1]])
@@ -25,19 +19,13 @@ safis_clean <-
   change_fields_arr_to_dates(csvs_clean[[1]], date_fields, date_format)
   
 data_overview(safis_clean)
-# vesselid         130666
 
+names(safis_clean)
 
-# df1 %>%
-#   group_by_(.dots = names(df1)[3:6]) %>%
-#   filter(n_distinct(c1) > 1)
-compl_clean %>%
-  select(vesselofficialnumber, name) %>%
-  unique() %>%
-  # group_by(name) %>%
-  add_count(name, vesselofficialnumber, name = "id_freq") %>% 
-  filter(id_freq > 1) %>% str()
-    # filter(n_distinct(vesselofficialnumber) > 1)
-  # 0
-
+safis_clean %>%
+  filter(coastguard != statereg) %>% 
+  select(coastguard, statereg) %>%
+  unique() %>% dim()
+# 16 ==
+# 130649 !=
 
