b241b5c857a55cd3a635f97052f283d22d175a01
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-03 14:58:35 -0500

    plot catch by species

--- compare_catch/compare_catch.R
+++ compare_catch/compare_catch.R
@@ -185,13 +185,51 @@ mrip_and_fhier %>%
   filter(mrip_estimate_catch_by_species <= fhier_quantity_by_species) %>% str()
 # 15 
 
+# Graph mrip
 plot(mrip_and_fhier$mrip_estimate_catch_by_species, 
-     # type="o", 
-     col="blue", ylim=c(0, 100000))
+     col="blue", ylim=c(0, 20000)) #100000
 
-# Graph trucks with red dashed line and square points
+# Graph fhier with red dashed line and square points
 lines(mrip_and_fhier$fhier_quantity_by_species, 
-      # type="o", 
       pch = 22, 
-      # lty = 2, 
       col="red")
+str(mrip_and_fhier_w_names)
+# ---- plot catch by species ----
+mrip_and_fhier_w_names <- inner_join(scientific_names_w_mrip, mrip_and_fhier, by = "SP_CODE")
+my_colors <- c("MRIP" = "blue", "FHIER" = "red")
+counts_plot <-
+  mrip_and_fhier_w_names %>%
+  mutate(SP_CODE = ifelse(SP_CODE >= 7000000000, SP_CODE, NA)) %>%
+  mutate(fhier_quantity_by_species = ifelse(fhier_quantity_by_species <= 10000, fhier_quantity_by_species, NA)) %>%
+  mutate(mrip_estimate_catch_by_species = ifelse(mrip_estimate_catch_by_species <= 100000, mrip_estimate_catch_by_species, NA)) %>%
+  mutate(order = fct_reorder(as.factor(SP_CODE), COMMON_NAME.x)) %>% 
+  # str()
+  ggplot(aes(x = order,
+             y = fhier_quantity_by_species,
+             colour = "FHIER"
+             ),
+         size = 2,
+         alpha = 0.1
+         ) + 
+  geom_point(aes(x = order,
+                   # ,
+                 y = mrip_estimate_catch_by_species, 
+                 colour = "MRIP"
+                ),
+             size = 1
+             ) +
+  labs(title = "catch by species",
+     x = "common_name", 
+     y = "quantity_by_species"
+) +
+  theme(axis.text.x = element_text(angle = 45)
+        # ,
+        # axis.text.y = element_blank()
+        ) +
+  scale_colour_manual(values = my_colors) +
+  geom_point()
+
+counts_plot
+# Warning messages:
+#   1: Removed 305 rows containing missing values (`geom_point()`). 
+# 2: Removed 154 rows containing missing values (`geom_point()`). 
