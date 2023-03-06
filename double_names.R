3191dee6296b64e0d8319cbbf66da24fbe3c6f98
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-06 09:44:22 -0500

    rename csv files

--- double_names.R
+++ double_names.R
@@ -17,17 +17,17 @@ csv_names_list = list("all_vessels_safis.csv")
 
 # read csv file with EOF within quoted strings
 read_csv_w_eofs <- function(my_paths, csv_names_list) {
-    my_inputs <- my_paths$inputs
-    # add input directory path in front of each file name.
-    myfiles <- sapply(csv_names_list, function(x) file.path(my_inputs, add_csv_path, x))
-
-    # read csv files
-    contents <- sapply(myfiles, fread, header = TRUE)
-    # convert the first one into a data frame
-    # TODO change this function to deal with multiple files
-    contents[, 1] %>%
-      as.data.frame() %>%
-      return()
+  my_inputs <- my_paths$inputs
+  # add input directory path in front of each file name.
+  myfiles <- sapply(csv_names_list, function(x) file.path(my_inputs, add_csv_path, x))
+  
+  # read csv files
+  contents <- sapply(myfiles, fread, header = TRUE)
+  # convert the first one into a data frame
+  # TODO change this function to deal with multiple files
+  contents[, 1] %>%
+    as.data.frame() %>%
+    return()
 }
 
 csv_contents <- read_csv_w_eofs(my_paths, csv_names_list)
@@ -44,17 +44,17 @@ safis_clean <-
 
 double_names_pairs <-
   safis_clean %>%
-    filter(coastguard != statereg) %>%
-    select(coastguard, statereg) %>%
-    unique()
+  filter(coastguard != statereg) %>%
+  select(coastguard, statereg) %>%
+  unique()
 # dim(double_names_pairs)
 # [1] 138507      2
 
 ## ---- find FHIER correspondence and compliance data using both ----
 ## ----- get compliance and correspondence csv data into variables -----
 csv_names_list_21_23 = c("Correspondence__2_24_23.csv",
-                         "FHIER_Compliance_22__2_24_23.csv",
-                         "FHIER_Compliance_23__2_24_23.csv")
+                         "FHIER_Compliance_22__02_24_23.csv",
+                         "FHIER_Compliance_23__02_24_23.csv")
 temp_var <- get_compl_and_corresp_data(my_paths, csv_names_list_21_23)
 compl_clean <- temp_var[[1]]
 corresp_clean <- temp_var[[2]]
@@ -66,7 +66,7 @@ used_doube_pairs <- double_names_pairs %>%
 
 used_doube_pairs <- double_names_pairs %>%
   filter(statereg %in% compl_clean$vesselofficialnumber &
-            coastguard != "-") %>% add_row(used_doube_pairs)
+           coastguard != "-") %>% add_row(used_doube_pairs)
 # 103
 
 used_doube_pairs <- double_names_pairs %>%
@@ -89,26 +89,27 @@ df_out <- tibble(pair = character(),
                  correspondence = character(),
                  compliance = character())
 
+# head(used_double_pairs_u)
 # loop over pairs
 for (i in 1:nrow(used_double_pairs_u)) {
   # browser()
   # combine each pair in a string for search
   pair <- paste(used_double_pairs_u[i, ][1],
                 used_double_pairs_u[i, ][2], sep = "|")
-
+  
   # look up in correspondence
   in_corr <- corresp_clean %>%
     filter(grepl(pair, vesselofficialnumber)) %>%
     select(vesselofficialnumber) %>%
     # select(vesselofficialnumber, contact_freq) %>%
     unique()
-
+  
   # look up in compliance
   in_compl <- compl_clean %>%
     filter(grepl(pair, vesselofficialnumber)) %>%
     select(vesselofficialnumber) %>%
     unique()
-
+  
   # some combinations are too general
   if (nrow(in_corr) > 2 | nrow(in_compl) > 2) {
     print(pair)
