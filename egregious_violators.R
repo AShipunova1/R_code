601a00b9ff19b1c4c1889db35cceb27a07a34f13
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-06 09:44:39 -0500

    rename files

--- egregious_violators.R
+++ egregious_violators.R
@@ -55,8 +55,8 @@ source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
 
 csv_names_list_22_23 = c("Correspondence__2_24_23.csv",
-                         "FHIER_Compliance_22__2_24_23.csv",
-                         "FHIER_Compliance_23__2_24_23.csv")
+                         "FHIER_Compliance_22__02_24_23.csv",
+                         "FHIER_Compliance_23__02_24_23.csv")
 
 ## ---- get csv data into variables ----
 temp_var <- get_compl_and_corresp_data(my_paths, csv_names_list_22_23)
