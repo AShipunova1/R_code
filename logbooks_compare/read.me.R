1e9e704a83b52b97ca87752f7ea2c7faaae973ea
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-07 08:57:55 -0500

    start read me for logbooks compare

---
+++ logbooks_compare/read.me.R
@@ -0,0 +1,8 @@
+# Comparing the logbook to logbook data between the Validation Survey and the logbooks in FHIER
+# From Michelle:
+# For the Survey to FHIER logbook comparison, we want to compare the accuracy/inaccuracy, at a trip level. So, are they sending us the same information that they reported to the Surveyor? We'll need to estimate any differences at trip-level reporting, and quantify those differences (if any) across all trips. Just use 2022 data for now.
+
+# the Survey data are in: 
+# https://drive.google.com/drive/folders/1D1ksBarjJzvbmqWa5cEE-k0s6ZJi0VAM
+
+## ---- Workflow ----
