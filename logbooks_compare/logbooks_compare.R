2bb9278541555ae7e1bdc1ba72e990d7a614a94a
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-08 12:47:42 -0500

    start

---
+++ logbooks_compare/logbooks_compare.R
@@ -0,0 +1,9 @@
+# Compare catch in survey vs logbook
+# see read.me
+
+## ---- set up ----
+source("~/R_code_github/useful_functions_module.r")
+my_paths <- set_work_dir()
+# turn off the scientific notation
+options(scipen=999)
+source("~/R_code_github/logbooks_compare/get_data.R")
