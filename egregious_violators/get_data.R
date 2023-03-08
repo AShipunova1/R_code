f62fef3e36a92df11e8162fc76a6dee5ca8c7c1b
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-08 09:21:55 -0500

    get data from egr v

---
+++ egregious_violators/get_data.R
@@ -0,0 +1,8 @@
+csv_names_list_22_23 = c("Correspondence__2_24_23.csv",
+                         "FHIER_Compliance_22__02_24_23.csv",
+                         "FHIER_Compliance_23__02_24_23.csv")
+
+## ---- get csv data into variables ----
+temp_var <- get_compl_and_corresp_data(my_paths, csv_names_list_22_23)
+compl_clean <- temp_var[[1]]
+corresp_contact_cnts_clean0 <- temp_var[[2]]
