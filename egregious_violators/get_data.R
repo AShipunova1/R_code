7e1c9f1348760448c242770807c5fde6c0dbb740
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-08 12:47:28 -0500

    comment

--- egregious_violators/get_data.R
+++ egregious_violators/get_data.R
@@ -1,3 +1,4 @@
+# get data for egregious violators
 csv_names_list_22_23 = c("Correspondence__2_24_23.csv",
                          "FHIER_Compliance_22__02_24_23.csv",
                          "FHIER_Compliance_23__02_24_23.csv")
