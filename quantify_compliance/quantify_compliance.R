289243ca302a2c56dc6a9326d44fcb17d5ec02be
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-13 16:52:31 -0400

    glimpse

--- quantify_compliance/quantify_compliance.R
+++ quantify_compliance/quantify_compliance.R
@@ -3,3 +3,8 @@ source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
 
 source("~/R_code_github/quantify_compliance/get_data.R")
+
+## ---- looking at the proportion of reports submitted vs flag ----
+glimpse(compl_clean)
+# Rows: 167,607
+# Columns: 21
