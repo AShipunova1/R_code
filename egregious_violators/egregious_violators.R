8ea719e59ab9aa7b4696bad2a728bafa59726583
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-15 11:54:11 -0400

    dynamic names in reframe, works

--- egregious_violators/egregious_violators.R
+++ egregious_violators/egregious_violators.R
@@ -78,16 +78,28 @@ compl_w_non_compliant_weeks <-
 compliant_field_name <- find_col_name(compl_clean_sa, ".*comp", "liant.*")[1]
 
 get_all_weeks_not_compliance_id <- function(compl_clean_sa) {
+  browser()
+  
   compl_clean_sa %>% 
     group_by(!!vessel_id_field_name) %>% 
-    
+    # group_by(!!vessel_id_field_name, compliant_) %>% 
+    # summarise(my_freq = n()) %>% str()
     # add a new column with TRUE if all weeks are non compliant and FALSE otherwise
-  #   reframe(all_weeks_non_compl = all(tolower(compliant_) == "no")) %>%  str()
+    # reframe(all_weeks_non_compl = all(tolower(compliant_) == "no")) %>%
+    # arrange(desc(all_weeks_non_compl)) %>% str()
+  # T
+  
   # tibble [2,223 × 2] (S3: tbl_df/tbl/data.frame)
-  # 
-    summarise(all_weeks_non_compl = count(compliant_field_name)) %>%
-    str()
-    reframe(all_weeks_non_compl = all(compliant_field_name == "no")) %>%  str()
+  # 1020057
+    reframe(all_weeks_non_compl = all(tolower(!!sym(compliant_field_name)) == "no")) %>% 
+    arrange(desc(all_weeks_non_compl)) %>% str()
+    # reframe(all_weeks_non_compl = all({.data[[compliant_field_name]]} == "no")) %>% 
+  mutate(all_weeks_non_compl = all(tolower(!!sym(compliant_field_name)) == "no")) %>%
+    # arrange(desc(all_weeks_non_compl)) %>% str()
+    ungroup() %>%
+    select(all_weeks_non_compl) %>% 
+    arrange(desc(all_weeks_non_compl)) %>% str()
+    
   # [1] 2223    2
   
     # leave only those with all weeks are non compliant
