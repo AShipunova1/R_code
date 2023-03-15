1228791dc6123c93e05a0aaebdbd52e2876119c7
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-15 14:37:41 -0400

    dynamic col names

--- egregious_violators/need_an_email.R
+++ egregious_violators/need_an_email.R
@@ -2,10 +2,6 @@
 # at least 2 correspondences & no direct contact
 # keep only 2 or more correspondence with no direct contact, check manually?
 
-# rename for short
-compliance_clean <- compl_w_non_compliant_weeks
-# glimpse(compliance_clean)
-
 # correspondence with contact frequency and direct_contact column
 corresp_clean <- corresp_contact_cnts_clean_direct_cnt
 # glimpse(corresp_clean)
@@ -17,47 +13,51 @@ get_2_plus_contacts <- function(corresp_clean) {
 }
 corr_2_plus_contact <- get_2_plus_contacts(corresp_clean)
 
-# TODO
 # called twice w no direct communications &
 # sent an email w. no answer &
 # those with no contact information
-# TODO?
-# -- for email needed --
+# 
+# TODO
+# separate permit expired
 # all contacts outgoing
 
 get_all_not_direct_contact_id <- function(corr_2_plus_contact) {
   corr_2_plus_contact %>%
-    group_by(vesselofficialnumber) %>%
+    group_by(!!vessel_id_corr_field_name) %>%
     # add a new column all_dc with TRUE if all are not direct contacts
     reframe(all_dc = all(tolower(direct_contact) == "no")) %>%
     # keep these only
-    filter(all_dc) %>% 
-    select(vesselofficialnumber) %>%
+    filter(all_dc) %>%
+    select(vessel_id_corr_field_name) %>%
     unique() %>%
     return()
 }
 all_not_direct_contact_id <- get_all_not_direct_contact_id(corr_2_plus_contact)
 # str(all_not_direct_contact_id)
 # 93
+# 6
 
 ## ---- filter for "email's needed" ----
 # no direct contact or no phone number or all are voicemails
 #TODO: and exclude if they had an email and if they had at least one phone call!
 email_s_needed_short <- corr_2_plus_contact %>%
-  filter(is.na(contactphonenumber) |
-           contactphonenumber == "" |
-           vesselofficialnumber %in% all_vm_ids$vesselofficialnumber |
-           vesselofficialnumber %in% all_not_direct_contact_id$vesselofficialnumber
+  filter(is.na(!!contactphonenumber_field_name) |
+           !!contactphonenumber_field_name == "" |
+           !!vessel_id_corr_field_name %in%
+           all_vm_ids[[vessel_id_corr_field_name]] |
+           !!vessel_id_corr_field_name %in%
+           all_not_direct_contact_id[[vessel_id_corr_field_name]]
   ) 
 
 data_overview(email_s_needed_short) %>% head(1)
 # vesselofficialnumber 176
+# 93
 
 # sorted:
 email_s_needed_to_csv_short_sorted <-
-  combine_rows_based_on_multiple_columns_and_keep_all_unique_sorted_values(email_s_needed_short, c("vesselofficialnumber"))
+  combine_rows_based_on_multiple_columns_and_keep_all_unique_sorted_values(email_s_needed_short, c(as.character(vessel_id_corr_field_name)))
 
-# str(email_s_needed_to_csv_short_sorted)
+str(email_s_needed_to_csv_short_sorted)
 
 ## ---- output to csv ----
 # this script results
