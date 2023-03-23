14cf71481d37ab48a77248711290a10898ba1622
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-23 13:17:06 -0400

    add num_of_weeks

--- quantify_compliance/non_compliant.R
+++ quantify_compliance/non_compliant.R
@@ -23,11 +23,14 @@ compl_clean_sa_vs_gom_plus_dual_non_compl_rep <-
 #   glimpse()
 # $ report_cnts <int> 0, 1, 6, 14, 2, 3, 7, 21, 5, 18, 17, 8, 16, 9, 10, 4, 51, 22, 1…
 
-## ---- gom + dual ----
-# compl_clean_sa_vs_gom_plus_dual %>% select(captainreports__, negativereports__, gom_permitteddeclarations__) %>% unique()
 dim(compl_clean_sa_vs_gom_plus_dual_non_compl_rep)
 # 42162
-gom_non_compl <-
+compl_clean_sa_vs_gom_plus_dual %>% select(captainreports__,
+                                           negativereports__,
+                                           gom_permitteddeclarations__) %>% unique() %>% head()
+
+## ---- gom + dual ----
+short_non_compl <-
   compl_clean_sa_vs_gom_plus_dual_non_compl_rep %>%
   select(
     vessel_official_number,
@@ -37,7 +40,10 @@ gom_non_compl <-
     year_quarter,
     year,
     report_cnts
-  ) %>%
+  )
+
+gom_non_compl <-
+  short_non_compl %>%
   filter(permit == "gom")
 
 names(gom_non_compl)
@@ -63,3 +69,46 @@ compl_clean_sa_vs_gom_plus_dual %>%
   filter(vessel_official_number == "1000042") %>%
   glimpse()
 #   dim()
+
+## ---- SA ----
+sa_non_compl <-
+  short_non_compl %>%
+  filter(permit == "sa_only")
+
+data_overview(sa_non_compl)
+# vessel_official_number 1849
+
+num_of_weeks_per_vessel <-
+  sa_non_compl %>%
+  select(vessel_official_number, week_start) %>%
+  group_by(vessel_official_number) %>%
+  summarise(num_of_weeks = n()) 
+# %>%
+#   ungroup() %>% str()
+
+# A tibble: 1,849 × 2
+
+
+# num_of_weeks_per_vessel <-
+sa_non_compl %>%
+  select(vessel_official_number, week_start) %>%
+  add_count(vessel_official_number, name = "num_of_weeks") %>% 
+  filter(vessel_official_number == "1024180") %>%
+  glimpse()
+
+  # group_by(vessel_official_number) %>%
+  # summarise(num_of_weeks = n()) 
+
+
+## check ----
+sa_non_compl %>%
+  filter(vessel_official_number == "1024180")
+
+## expected vs. given
+left_join(sa_non_compl,
+          num_of_weeks_per_vessel) %>% 
+  arrange(vessel_official_number) %>% data_overview()
+glimpse()
+# Joining with `by = join_by(vessel_official_number)`
+# Rows: 39,863
+# Columns: 8
