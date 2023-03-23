b37f56985ac37724cee0352ef2d4fa2c052de13c
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-23 12:55:51 -0400

    count reports, test gom

--- quantify_compliance/non_compliant.R
+++ quantify_compliance/non_compliant.R
@@ -7,7 +7,7 @@ source(r"(~\R_code_github\quantify_compliance\quantify_compliance_start.R)")
 ## ---- how many reports are missing ----
 
 ## ---- count reports ----
-compl_clean_sa_vs_gom_plus_dual_rep <-
+compl_clean_sa_vs_gom_plus_dual_non_compl_rep <-
   compl_clean_sa_vs_gom_plus_dual %>%
   filter(tolower(compliant_) == "no") %>%
   # glimpse()
@@ -23,6 +23,43 @@ compl_clean_sa_vs_gom_plus_dual_rep <-
 #   glimpse()
 # $ report_cnts <int> 0, 1, 6, 14, 2, 3, 7, 21, 5, 18, 17, 8, 16, 9, 10, 4, 51, 22, 1…
 
+## ---- gom + dual ----
 # compl_clean_sa_vs_gom_plus_dual %>% select(captainreports__, negativereports__, gom_permitteddeclarations__) %>% unique()
+dim(compl_clean_sa_vs_gom_plus_dual_non_compl_rep)
+# 42162
+gom_non_compl <-
+  compl_clean_sa_vs_gom_plus_dual_non_compl_rep %>%
+  select(
+    vessel_official_number,
+    permit,
+    week_start,
+    year_month,
+    year_quarter,
+    year,
+    report_cnts
+  ) %>%
+  filter(permit == "gom")
 
-compl_clean_sa_vs_gom_plus_dual_rep
+names(gom_non_compl)
+
+gom_non_compl %>%
+  data_overview()
+
+gom_non_compl %>%
+  select(vessel_official_number, week_start) %>%
+  group_by(vessel_official_number) %>%
+  summarise(n = n())
+
+# 1000042                    2?
+# Groups:   vessel_official_number [493]
+
+compl_clean_sa_vs_gom_plus_dual_non_compl_rep %>%
+  filter(vessel_official_number == "1000042") %>%
+  glimpse()
+#   dim()
+# 2299    6
+
+compl_clean_sa_vs_gom_plus_dual %>%
+  filter(vessel_official_number == "1000042") %>%
+  glimpse()
+#   dim()
