ca94724f1836c67a026ac156691672e3a2edcb39
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-25 10:23:53 -0400

    separate gom and sa

--- quantify_compliance/missing_reports.R
+++ quantify_compliance/missing_reports.R
@@ -5,3 +5,39 @@
 #
 library(grid)
 source(r"(~\R_code_github\quantify_compliance\quantify_compliance_start.R)")
+
+
+compl_clean_sa_vs_gom_plus_dual_short <-
+  compl_clean_sa_vs_gom_plus_dual %>%
+  select(vessel_official_number,
+         year,
+         week,
+         gom_permitteddeclarations__,
+         captainreports__,
+         negativereports__,
+         complianceerrors__,
+         compliant_,
+         week_start,
+         permit,
+         year_month,
+         year_quarter
+  )
+  
+  # filter(permit == "sa_only")
+# %>% names() %>% cat()
+# select(-report2, -permit) # don't need' gom_permitteddeclarations__,
+# )
+  
+## separate by permit
+permit_dfs <- split(compl_clean_sa_vs_gom_plus_dual_short,
+                    compl_clean_sa_vs_gom_plus_dual_short$permit)
+setNames(permit_dfs, c("gom", "sa"))
+
+str(permit_dfs)
+
+# gom_compl_clean_sa_vs_gom_plus_dual_short <-
+#   compl_clean_sa_vs_gom_plus_dual_short %>%
+#   filter(permit == "gom")
+# 
+# identical(permit_dfs[[1]], gom_compl_clean_sa_vs_gom_plus_dual_short)
+# T
