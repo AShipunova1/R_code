69ab82898d48eac1de1a7259db0c6dce67c8694e
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-25 10:10:01 -0400

    new file

---
+++ quantify_compliance/missing_reports.R
@@ -0,0 +1,7 @@
+# GOM - should be equal # of decl and logb  
+# For SA, they need either a no fishing report weekly (due Tuesday following the fishing week, which is Mon-Sun) or they need a logbook for every fishing trip (so at least 1 per week if no fishing report is submitted)
+## non-compliant vessels for the GOM, if 65 vessels are missing 1 report (1) is that a missing declaration or logbook?, (2) how many did they submit correctly (e.g. this month in question maybe they are missing 1 logbook, but they submitted 20. So, 1/20 = 5% missing). That is a vessel level, so then perhaps take the average of all for the month, and then the year, by region, and plot it that way.
+
+#
+library(grid)
+source(r"(~\R_code_github\quantify_compliance\quantify_compliance_start.R)")
