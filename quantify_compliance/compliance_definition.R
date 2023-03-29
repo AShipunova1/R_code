640f6b7bb056dd830cc9e9bce92131cc717ffe11
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-29 16:34:20 -0400

    vessel is sa_only in one week and gom another

--- quantify_compliance/compliance_definition.R
+++ quantify_compliance/compliance_definition.R
@@ -60,6 +60,37 @@ compl_clean_short_sa_vs_gom %>%
   filter(permit == "gom_only" &
            !grepl("G", permitgroup)) %>% head()
 # 0
+# ===
+compl_clean_short_sa_vs_gom %>%
+  filter(permit == "sa_only") %>%
+  filter(tolower(compliant_) == "no",
+         (captainreports__) > 0,
+        tolower(overridden_) == "no") %>% View()
+# 1 Feb 2023
+# ==== check if the same vessel can be sa_only in one week and gom or dual the other ====
+
+sa_only_vessel_id <-
+compl_clean_short_sa_vs_gom %>%
+  filter(permit == "sa_only") %>%
+  select(vessel_official_number) %>%
+  unique()
+# 2223
+
+gom_or_dual_vessel_id <-
+  compl_clean_short_sa_vs_gom %>%
+  filter(!(permit == "sa_only")) %>%
+  select(vessel_official_number) %>%
+  unique()
+# 1518
+
+intersect(sa_only_vessel_id, gom_or_dual_vessel_id) %>% glimpse()
+# 0
+
+### SA compliant & no reports:
+# Leeanne Delrosario - NOAA Affiliate
+# to me, Chris, Michelle
+# 
+# Hey Anna, Chris and I finished taking a look at that report you sent over. Majority of those vessels had week 8 of 2023 as the listed week that was compliant when no reports were submitted for the SA vessels. If the report was run on 2/24/23 (during week 8), then it makes sense that those vessels are marked compliant with no reports as the deadline to submit reports for that week had not passed yet (the following Tuesday after the reporting week ends).
 # === different permits in the report and the dashboard ====
 # (!) has a gom permit in the "vessel dashboard" and
 # no gom repots in the "permitgroup" column of the FHIER compliance report
