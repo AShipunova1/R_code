8ae00f19f0b78d5985307aba7008ac626b0c7e0b
AShipunova1 <anna.shipunova@noaa.gov>
2023-03-29 13:31:32 -0400

    separate_permits_into_3_groups <- function(compl_clean)

--- quantify_compliance/quantify_compliance_start.R
+++ quantify_compliance/quantify_compliance_start.R
@@ -23,15 +23,18 @@ source("~/R_code_github/quantify_compliance/get_data.R")
 # SA:  CDW, CHS, SC
 
 ### ---- separate into 3 groups ----
-compl_clean_sa_vs_gom <-
+separate_permits_into_3_groups <- function(compl_clean) {
   compl_clean %>%
   mutate(permit =
            case_when(
              !grepl("RCG|HRCG|CHG|HCHG", permitgroup) ~ "sa_only",
              !grepl("CDW|CHS|SC", permitgroup) ~ "gom_only",
              .default = "both"
-           ))
-
+           )) %>%
+    return()
+}
+compl_clean_sa_vs_gom <- separate_permits_into_3_groups(compl_clean)
+    
 # names(compl_clean_sa_vs_gom)
 # dim(compl_clean_sa_vs_gom)
 # [1] 167607     22
