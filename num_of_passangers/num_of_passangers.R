5836b75780dbd4fd8de26833b7a8fd2a75bc3338
ashipunova1 <anna.shipunova@noaa.gov>
2023-04-28 13:04:46 -0400

    get trips to csvs

--- num_of_passangers/num_of_passangers.R
+++ num_of_passangers/num_of_passangers.R
@@ -45,5 +45,26 @@ paste0(names(trip_id_vessel_from_db), collapse = ", ")
 names(trip_id_vessel_from_db) <- c("trip_id", "vessel_official_number")
 
 unique(trip_id_vessel_from_db) %>% dim()
+# 87145     
 
-setdiff()
+# trip_id_vessel_from_logbooks
+# 81594
+str(trip_id_vessel_from_logbooks)
+trips_logbooks <- as.numeric(trip_id_vessel_from_logbooks$trip_id) %>%
+  unique()
+trips_db_trips <- as.numeric(trip_id_vessel_from_db$trip_id) %>%
+  unique()
+# str(trips_db_trips)
+in_logbooks_only <-
+  setdiff(trips_logbooks, trips_db_trips)
+length(in_logbooks_only)
+# 1522
+
+in_db_only <-
+  setdiff(trips_db_trips, trips_logbooks)
+length(in_db_only)
+# 7073
+
+out_dir <- file.path(my_paths$inputs, "fhier_vs_db")
+write_csv(as.data.frame(in_logbooks_only),
+          file.path(out_dir, "trips_in_logbooks_only.csv"))
