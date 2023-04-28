258c70bc8d2a0489ec6f2f400bd61341469957bb
ashipunova1 <anna.shipunova@noaa.gov>
2023-04-28 13:48:02 -0400

    get logbooks.csv and db

--- num_of_passangers/num_of_passangers_get_data.R
+++ num_of_passangers/num_of_passangers_get_data.R
@@ -1,32 +1,3 @@
-source("~/R_code_github/useful_functions_module.r")
-library(ROracle)
-
-con = dbConnect(
-  dbDriver("Oracle"),
-  username = keyring::key_list("SECPR")[1, 2],
-  password = keyring::key_get("SECPR", keyring::key_list("SECPR")[1, 2]),
-  dbname = "SECPR"
-)
-
-## From DB ====
-
-num_of_pass_db = dbGetQuery(
-  con,
-  "SELECT
-  *
-FROM
-       srh.v_val_srfh_pending@secapxdv_dblk.sfsc.noaa.gov
-  JOIN srh.val_param@secapxdv_dblk USING (
-  VAL_PARAM_ID,
-  VAL_PARAM_TABLE,
-  VAL_PARAM_NAME
-  )
-WHERE
-  departure_date >= '01-JAN-2022'"
-)
-
-str(dat_pending)
-
 ##| echo: false
 library(zoo)
 library(gridExtra)
@@ -41,3 +12,41 @@ my_paths <- set_work_dir()
 ##| echo: false
 
 source("~/R_code_github/compare_catch/compare_catch_data_preparation.R")
+
+source("~/R_code_github/useful_functions_module.r")
+library(ROracle)
+
+con = dbConnect(
+  dbDriver("Oracle"),
+  username = keyring::key_list("SECPR")[1, 2],
+  password = keyring::key_get("SECPR", keyring::key_list("SECPR")[1, 2]),
+  dbname = "SECPR"
+)
+
+## From DB ====
+
+# from db ----
+query1 <- "SELECT DISTINCT
+  trip_id,
+  coalesce(state_reg_nbr, coast_guard_nbr)
+FROM
+       safis.trips@secapxdv_dblk.sfsc.noaa.gov t
+  JOIN safis.vessels@secapxdv_dblk.sfsc.noaa.gov v
+  USING ( vessel_id )
+WHERE
+--  t.sero_vessel_permit IS NOT NULL
+--  AND 
+    t.vendor_app_name <> 'VMS'
+  AND t.de <= '27-FEB-23'
+  AND t.de >= '01-JAN-22'
+"
+# t.sero_vessel_permit IS NOT NULL
+# AND t.de <= '26-FEB-23' - doesn't pick up 26-FEB-23
+
+trip_id_vessel_from_db <- dbGetQuery(con, query1)
+
+### rename columns to be the same ----
+paste0(names(trip_id_vessel_from_db), collapse = ", ")
+# "TRIP_ID, COALESCE(STATE_REG_NBR,COAST_GUARD_NBR)"
+names(trip_id_vessel_from_db) <- c("trip_id", "vessel_official_number")
+
