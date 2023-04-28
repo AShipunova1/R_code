b195593305e21795bffc91a777bc1e7227afff88
ashipunova1 <anna.shipunova@noaa.gov>
2023-04-28 10:15:11 -0400

    readme

--- num_of_passangers/read.me.R
+++ num_of_passangers/read.me.R
@@ -1,1 +1,5 @@
-# (needs code) compare the actual number of passengers to what they are allowed to have on board - they need to be the smaller of the permit baseline or the vessel capacityI
+# (needs code) compare the actual number of passengers to what they are allowed to have on board - they need to be the smaller of the permit baseline or the vessel capacity
+# the actual number of passenger
+# what they are allowed to have on board
+# the permit baseline
+# the vessel capacity
