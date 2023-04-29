49cfdfae40080e943e12cf41bb56c1e13a584beb
ashipunova1 <anna.shipunova@noaa.gov>
2023-04-28 20:01:57 -0400

    compare downloaded from Fhier and from db

--- num_of_passangers/num_of_passangers.R
+++ num_of_passangers/num_of_passangers.R
@@ -1,4 +1,4 @@
-# check diff between logbook csv and db
+# check diff between logbook csv and db ----
 View(fhier_logbooks_content)
 fhier_logbooks_content %>%
   filter(trip_de >= "2022-01-01 00:00:00") %>%
@@ -57,7 +57,6 @@ length(in_db_only)
 # write_csv(as.data.frame(in_db_only),
 #           file.path(out_dir, "trips_in_db_only.csv"))
 
-# ----
 trip_id_vessel_from_logbooks %>%
   filter(trip_id == "61479063")
 
@@ -80,9 +79,10 @@ trip_id_vessel_from_logbooks %>%
 # 1 59403264          <NA>          556499
 # in FHIER trips are all in 2021
 # names(trip_id_vessel_st_from_db)
+# [1] "TRIP_ID"          "SUPPLIER_TRIP_ID" "STATE_REG_NBR"   
+# [4] "COAST_GUARD_NBR"  "DE"               "VENDOR_APP_NAME" 
+# [7] "STATE_NAME"      
 str(trip_id_vessel_st_from_db)
-# [1] "TRIP_ID"         "STATE_REG_NBR"   "COAST_GUARD_NBR"
-# [4] "DE"              "VENDOR_APP_NAME" "STATE_NAME"     
 
 str(in_db_only)
 # trip_id_vessel_st_from_db %<>%
@@ -108,15 +108,163 @@ head(in_db_only)
 # vessel_nbr ----
 vessel_nbr_trips_indb_only <-
   trip_id_vessel_st_from_db %>%
-  filter(TRIP_ID %in% in_db_only) %>%
-  mutate(vessel_off_num = coalesce(STATE_REG_NBR, COAST_GUARD_NBR)) %>% 
+  filter(trip_id %in% in_db_only) %>%
+  mutate(vessel_off_num = coalesce(state_reg_nbr, coast_guard_nbr)) %>% 
   # select(STATE_REG_NBR, COAST_GUARD_NBR) %>% 
   select(vessel_off_num) %>%
     unique() 
-# %>% 
-#   dim()
+# %>%
+  # dim()
 # 691   
+0
 
 str(vessel_nbr_trips_indb_only)
 fhier_logbooks_content %>%
   filter(vessel_official_nbr %in% vessel_nbr_trips_indb_only$vessel_off_num) %>% View()
+
+# === compare downloaded from Fhier and from db ----
+logbooks_downloaded_from_fhier_trip_id_only <-
+  logbooks_downloaded_from_fhier_trip_vsl %>%
+  select(trip_id) %>% unique() 
+# %>% 
+  # dim()
+  
+trip_id_vessel_st_from_db_trip_id_only <-
+  trip_id_vessel_st_from_db %>%
+  select(trip_id) %>% unique()
+# %>% dim()
+
+setdiff(logbooks_downloaded_from_fhier_trip_id_only$trip_id, 
+        trip_id_vessel_st_from_db_trip_id_only$trip_id) %>% length()
+# 17535
+# 2
+
+setdiff(trip_id_vessel_st_from_db_trip_id_only$trip_id,
+        logbooks_downloaded_from_fhier_trip_id_only$trip_id) %>%
+  # head()
+  length()
+# 7167
+# 5348
+
+names(trip_id_vessel_st_from_db)
+# [1] "trip_id"          "supplier_trip_id" "state_reg_nbr"   
+# [4] "coast_guard_nbr"  "de"               "vendor_app_name" 
+# [7] "state_name" 
+
+names(logbooks_downloaded_from_fhier)
+#  [1] "view"                      "supplier_trip_id"         
+#  [3] "trip_type"                 "vessel_official_number"   
+#  [5] "vesselname"                "activitystart_date"       
+#  [7] "activitystart_time"        "activityend_date"         
+#  [9] "activityend_time"          "portname"                 
+# [11] "portcounty"                "portstate"                
+# [13] "de"                        "ue"                       
+# [15] "submissionlag_time__mins_"
+
+logbooks_downloaded_from_fhier_sup_trip_id_only <-
+  logbooks_downloaded_from_fhier %>%
+  select(supplier_trip_id) %>% unique() 
+
+# names(logbooks_downloaded_from_fhier_trip_vsl)
+  
+trip_id_vessel_st_from_db_sup_trip_id_only <-
+  trip_id_vessel_st_from_db %>%
+  select(supplier_trip_id) %>% unique()
+
+# str(logbooks_downloaded_from_fhier_sup_trip_id_only)
+
+in_db_only <-
+setdiff(trip_id_vessel_st_from_db_sup_trip_id_only$supplier_trip_id,
+        logbooks_downloaded_from_fhier_sup_trip_id_only$supplier_trip_id) 
+# str(logbooks_downloaded_from_fhier_trip_id_only)
+
+length(in_db_only)
+# 7057
+# with VMS 8958
+# with VMS and correct dates 9148
+# 5259
+
+in_fhier_only <-
+  setdiff(
+    logbooks_downloaded_from_fhier_sup_trip_id_only$supplier_trip_id,
+    trip_id_vessel_st_from_db_sup_trip_id_only$supplier_trip_id
+  )
+
+length(in_fhier_only)
+# 17586
+# with VMS 2380
+# 2
+
+# head(in_fhier_only)
+
+# what is in logbooks only: ----
+# VMS
+logbooks_downloaded_from_fhier %>%
+  filter(supplier_trip_id %in% in_fhier_only) %>%
+  glimpse()
+# $ activitystart_date        <chr> "06/23/2022", "06/03/2022"
+# $ activityend_date          <chr> "06/24/2022", "06/03/2021"
+# $ ue                        <chr> "VMS", "VMS"
+# $ submissionlag_time__mins_ <dbl> -525174, 469
+
+# what is in db only: ----
+in_db_only
+
+# not downloaded
+
+vessel_num_in_db_only <-
+  trip_id_vessel_st_from_db %>%
+  filter(supplier_trip_id %in% in_db_only) %>%
+  # glimpse()
+  mutate(vessel_num = coalesce(state_reg_nbr, coast_guard_nbr)) %>%
+  select(vessel_num)
+
+str(vessel_num_in_db_only)
+vessel_num_in_db_only$vessel_num %>% head(100) %>% 
+  paste0(collapse = ", ")
+
+# == make a filter supplier_trip_id ----
+
+supplier_trip_id_in_db_only <-
+  trip_id_vessel_st_from_db %>%
+  filter(supplier_trip_id %in% in_db_only) %>%
+  select(supplier_trip_id)
+  
+str(supplier_trip_id_in_db_only)
+
+supplier_trip_id_in_db_only$supplier_trip_id %>%
+  head(1000) %>%
+  paste0(collapse = ", ")
+# csv2
+
+supplier_trip_id_in_db_only$supplier_trip_id %>%
+  head(2000) %>%
+  tail(1000) %>%
+  paste0(collapse = ", ")
+
+supplier_trip_id_in_db_only$supplier_trip_id %>%
+  head(5500) %>%
+  tail(1000) %>%
+  paste0(collapse = ", ")
+
+names(new_logb)
+in_db_only <-
+setdiff(new_logb$suppliertripid,
+        logbooks_downloaded_from_fhier_sup_trip_id_only$supplier_trip_id) 
+# str(logbooks_downloaded_from_fhier_trip_id_only)
+
+length(in_db_only)
+# 24473
+
+# ---
+names(new_logb)
+new_logb_supplier_ids <-
+  new_logb %>%
+  filter(suppliertripid %in% in_db_only) %>%
+  select(suppliertripid) %>% unique()
+
+new_logb_supplier_ids %>%
+  head()
+
+# 42023030444943, 31423124614460, 22723072211510, 22823061120428, 31823115219825, 42523113314349
+# not in fhier
--- num_of_passangers/num_of_passangers_get_data.R
+++ num_of_passangers/num_of_passangers_get_data.R
@@ -36,7 +36,7 @@ FROM
   USING ( vessel_id )
 WHERE
 --  t.sero_vessel_permit IS NOT NULL
---  AND 
+--  AND
     t.vendor_app_name <> 'VMS'
   AND t.de <= '27-FEB-23'
   AND t.de >= '01-JAN-22'
@@ -53,11 +53,14 @@ paste0(names(trip_id_vessel_from_db), collapse = ", ")
 
 # names(trip_id_vessel_from_db) <- c("trip_id", "vessel_official_number")
 
-names(trip_id_vessel_from_db) <- c("trip_id", "state_reg_nbr", "coast_guard_nbr")
+names(trip_id_vessel_from_db) <-
+  c("trip_id", "state_reg_nbr", "coast_guard_nbr")
 
+# with state names and supplier_trip_id
 query_w_state <- "SELECT
 --  *
   trip_id,
+  supplier_trip_id,
   state_reg_nbr,
   coast_guard_nbr,
   t.de,
@@ -74,11 +77,67 @@ FROM
   ON ( s.state_code = t.state )
 WHERE
 --    coast_guard_nbr = '556499'
-    t.vendor_app_name <> 'VMS'
-  AND t.de <= '26-FEB-23'
-  AND t.de >= '01-JAN-22'
+--    t.vendor_app_name <> 'VMS'
+--  AND 
+--  t.de <= '27-FEB-23'
+--  AND 
+  t.de >= '31-DEC-21'
+  and t.trip_start_date <= '27-FEB-23'
+  AND t.trip_start_date >= '31-DEC-21'
 ORDER BY
   t.de DESC
 "
 
 trip_id_vessel_st_from_db <- dbGetQuery(con, query_w_state)
+trip_id_vessel_st_from_db <- clean_headers(trip_id_vessel_st_from_db)
+
+# download Reports / FHIER All Logbooks 01/01/2022-02/26/2023
+
+logbooks_downloaded_from_fhier_22_23_feb_file_name <-
+  file.path(my_paths$inputs,
+            r"(logbooks_from_fhier\SRFH All Logbooks.csv)")
+
+logbooks_downloaded_from_fhier <- read_csv(logbooks_downloaded_from_fhier_22_23_feb_file_name)
+
+names(logbooks_downloaded_from_fhier)
+
+logbooks_downloaded_from_fhier_trip_vsl <-
+  logbooks_downloaded_from_fhier %>%
+  select(View, `Vessel Official Number`)
+
+names(logbooks_downloaded_from_fhier_trip_vsl) <-
+  c("trip_id", "vessel_official_nbr")
+
+str(logbooks_downloaded_from_fhier_trip_vsl)
+
+logbooks_downloaded_from_fhier <- clean_headers(logbooks_downloaded_from_fhier)
+
+# === additional downloads from fhier ----
+
+logbooks_downloaded_from_fhier_22_23_feb_file_name <-
+  file.path(my_paths$inputs,
+            r"(logbooks_from_fhier\SRFH All Logbooks.csv)")
+
+logbooks_downloaded_from_fhier <- read_csv(logbooks_downloaded_from_fhier_22_23_feb_file_name)
+
+file_list <-
+  list(
+    "SRFH All Logbooks.csv",
+    "SRFH All Logbooks2.csv",
+    "SRFH All Logbooks3.csv",
+    "SRFH All Logbooks4.csv",
+    "SRFH All Logbooks5.csv",
+    "SRFH All Logbooks6.csv"
+  )
+
+new_logb <-
+  bind_rows(lapply(file_list, function(f) {
+    f_name <- file.path(my_paths$inputs,
+                        "logbooks_from_fhier", f)
+    read.csv(f_name)
+  }))
+
+# dim(new_logb)
+# 100824     
+
+new_logb <- clean_headers(new_logb)
