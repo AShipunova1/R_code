14c1d58189d6a636f47f313585600654ce0715c8
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-08 09:06:05 -0400

    add info to geo err

--- fishing_effort_location/geo_err.R
+++ fishing_effort_location/geo_err.R
@@ -1,12 +1,19 @@
 # errors in geo data ----
-# LATITUDE      NA's   :1198            
-# LATITUDE        LONGITUDE  
-# Min.   :-87.30   Min.   :-117.25
-# Max.   : 90.00   Max.   : 137.59  
-
 # 1) a sign
 # 2) on land
 # 3) outside boundaries
 # 4) wrong depth
 # 5) missing coords
   # filter(complete.cases(.)) %>%
+
+# ===
+# fathom, old English measure of length, now standardized at 6 feet (1.83 metre), which has long been used as a nautical unit of depth.
+
+# ===
+# LATITUDE      NA's   :1198            
+# LATITUDE        LONGITUDE  
+# Min.   :-87.30   Min.   :-117.25
+# Max.   : 90.00   Max.   : 137.59  
+
+# ===
+data_overview(db_data)
