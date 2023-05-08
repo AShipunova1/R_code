621094a560e3bfd0a8b86c9c4122c965c08060b3
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-08 08:55:12 -0400

    geo err +

--- fishing_effort_location/geo_err.R
+++ fishing_effort_location/geo_err.R
@@ -1,5 +1,12 @@
-# errors in geo data
+# errors in geo data ----
+# LATITUDE      NA's   :1198            
+# LATITUDE        LONGITUDE  
+# Min.   :-87.30   Min.   :-117.25
+# Max.   : 90.00   Max.   : 137.59  
+
 # 1) a sign
 # 2) on land
 # 3) outside boundaries
 # 4) wrong depth
+# 5) missing coords
+  # filter(complete.cases(.)) %>%
