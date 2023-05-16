0dfbb26ba69005a2c818a31fd42c3dee5336af37
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-16 10:26:12 -0400

    write xls w_r_sedar gom

--- compare_catch/compare_catch_report.R
+++ compare_catch/compare_catch_report.R
@@ -1,3 +1,4 @@
+library(xlsx)
 # 3 sets of spp:
 # 1a) SEDAR lists;
 # 2b) MRIP / Recreational ACL tops;
@@ -8,19 +9,25 @@
 # 3) By year and region
 # 4) By year and state
 
-# 1) By wave and region
-write_excel_csv(fhier_acl_catch_by_species_state_region_waves_list_for_plot$gom, "w_r_gom.csv")
-
-report_file_path <- file.path(my_paths$outputs, r"(\compare_catch\report\12_categories_tables.xlsx)")
+# write_excel_csv(fhier_acl_catch_by_species_state_region_waves_list_for_plot$gom, "w_r_gom.csv")
 
 # add to an existing excel
-add_to_report_xls <- function(my_df, report_file_path, current_sheet_name) {
-  write.xlsx(
+add_to_report_xls <- function(my_df, current_sheet_name) {
+  report_file_path <-
+    file.path(my_paths$outputs,
+              r"(\compare_catch\report\12_categories_tables.xlsx)")
+  
+  xlsx::write.xlsx(
     as.data.frame(my_df),
     file = report_file_path,
     sheetName = current_sheet_name,
-    # rowNames = TRUE,
-    colNames = TRUE,
+    # row.names = TRUE,
+    col.names = TRUE,
     append = TRUE
   )
 }
+
+# 1) By wave and region 1a) SEDAR ----
+## keep only entries for spp. in the top ten list, separately for each region ----
+
+add_to_report_xls(fhier_acl_catch_by_species_state_region_waves_list_for_plot_gom_top_sedar, "1.1a wave and region SEDAR GOM")
