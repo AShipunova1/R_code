58fa21ceb64c29858064aab6e0aaf47b03d22ef9
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-16 09:13:59 -0400

    new file

---
+++ compare_catch/compare_catch_report.R
@@ -0,0 +1,12 @@
+# 3 sets of spp: 
+# 1a) SEDAR; 
+# 2b) Recreational ACL tops; 
+# 3c) All FHIER spp
+
+# Plots:
+# 1) By wave and region
+# 2) By wave and state
+# 3) By year and region
+# 4) By year and state
+# ===
+# 1) By wave and region ----
