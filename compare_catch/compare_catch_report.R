152b6c7fca82c1c87bf02adca6d24ac10deb92ee
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-16 12:39:24 -0400

    By wave and stat sedar xls

--- compare_catch/compare_catch_report.R
+++ compare_catch/compare_catch_report.R
@@ -12,10 +12,13 @@ library(xlsx)
 # write_excel_csv(fhier_acl_catch_by_species_state_region_waves_list_for_plot$gom, "w_r_gom.csv")
 
 # add to an existing excel
-add_to_report_xls <- function(my_df, current_sheet_name) {
-  report_file_path <-
-    file.path(my_paths$outputs,
-              r"(\compare_catch\report\12_categories_tables.xlsx)")
+add_to_report_xls <- function(my_df, current_sheet_name, report_file_path = NULL) {
+  # default path
+  if (is.null(report_file_path)) {
+    report_file_path <-
+      file.path(my_paths$outputs,
+                r"(\compare_catch\report\12_categories_tables_reg.xlsx)")
+  }
   
   xlsx::write.xlsx(
     as.data.frame(my_df),
@@ -60,4 +63,22 @@ fhier_acl_catch_by_species_state_region_waves_list_for_plot$gom %>%
 fhier_acl_catch_by_species_state_region_waves_list_for_plot$sa %>%
   add_to_report_xls("1.3c wave region all FHIER SA")
 
-state_wave_has_rec_acl_data_list_new
+# 2) By wave and state
+View(state_wave_has_rec_acl_data_list_new)
+# 3 sets of spp:
+# 1a) SEDAR lists;
+# 2b) MRIP / Recreational ACL tops;
+# 3c) All FHIER spp
+
+report_file_path2 <- file.path(my_paths$outputs,
+                                    r"(\compare_catch\report\12_categories_tables_states.xlsx)")
+
+# 1a) SEDAR lists;
+map(names(state_wave_has_rec_acl_data_list_state_sedar),
+    function(current_state_abbr) {
+      sheet_name = paste0("2.1a wave state SEDAR lists ", current_state_abbr)
+      # browser()
+      state_wave_has_rec_acl_data_list_state_sedar[[current_state_abbr]] %>%
+        add_to_report_xls(sheet_name, report_file_path = report_file_path2)
+    })
+    
