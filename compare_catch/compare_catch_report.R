9419435cea1759e16729115e8f17bd7264f36981
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-16 12:39:10 -0400

    acl_estimate_catch_by_4 to rec_acl... report

--- compare_catch/compare_catch_report.R
+++ compare_catch/compare_catch_report.R
@@ -29,9 +29,6 @@ add_to_report_xls <- function(my_df, current_sheet_name) {
 
 # all ----
 fhier_acl_catch_by_species_state_region_waves %>%
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("counts by species_state_region_waves")
 
 # 1) By wave region 1a) SEDAR ----
@@ -39,46 +36,28 @@ fhier_acl_catch_by_species_state_region_waves %>%
 
 ## GOM ----
 fhier_acl_catch_by_species_state_region_waves_list_for_plot_gom_top_sedar
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("1.1a wave region SEDAR GOM")
 
 ## SA ----
 fhier_acl_catch_by_species_state_region_waves_list_for_plot_sa_sedar %>%
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("1.1a wave region SEDAR SA")
 
 # 1) By wave region 2b) Recreational ACL tops; ----
 ## GOM ----
 gom_acl_top_to_plot %>%
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("1.2b wave region top MRIP GOM")
 
 ## SA ----
 sa_acl_top_to_plot %>%
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("1.2b wave region top MRIP SA")
 
 # 1) By wave region 3c) All FHIER spp ----
 ## GOM ----
 fhier_acl_catch_by_species_state_region_waves_list_for_plot$gom %>%
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("1.3c wave region all FHIER GOM")
 
 ## SA ----
 fhier_acl_catch_by_species_state_region_waves_list_for_plot$sa %>%
-  # rename the field
-  rename(rec_acl_estimate_catch_by_4 =
-           acl_estimate_catch_by_4) %>%
   add_to_report_xls("1.3c wave region all FHIER SA")
 
 state_wave_has_rec_acl_data_list_new
