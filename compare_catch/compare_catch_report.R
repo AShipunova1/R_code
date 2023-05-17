e16391e2861ae83e39d163d81bd185cd8ad95848
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-17 17:27:28 -0400

    reg state sedar to xls

--- compare_catch/compare_catch_report.R
+++ compare_catch/compare_catch_report.R
@@ -17,9 +17,9 @@ add_to_report_xls <- function(my_df, current_sheet_name, report_file_path = NULL
   if (is.null(report_file_path)) {
     report_file_path <-
       file.path(my_paths$outputs,
-                r"(\compare_catch\report\12_categories_tables_reg.xlsx)")
+                r"(compare_catch\report\12_categories_tables_reg.xlsx)")
   }
-  
+  # browser()
   xlsx::write.xlsx(
     as.data.frame(my_df),
     file = report_file_path,
@@ -38,7 +38,7 @@ fhier_acl_catch_by_species_state_region_waves %>%
 ## keep only entries for spp. in the top ten list, separately for each region ----
 
 ## GOM ----
-fhier_acl_catch_by_species_state_region_waves_list_for_plot_gom_top_sedar
+fhier_acl_catch_by_species_state_region_waves_list_for_plot_gom_top_sedar %>% 
   add_to_report_xls("1.1a wave region SEDAR GOM")
 
 ## SA ----
@@ -63,22 +63,33 @@ fhier_acl_catch_by_species_state_region_waves_list_for_plot$gom %>%
 fhier_acl_catch_by_species_state_region_waves_list_for_plot$sa %>%
   add_to_report_xls("1.3c wave region all FHIER SA")
 
-# 2) By wave and state
-View(state_wave_has_rec_acl_data_list_new)
+# 2) By wave and state ----
+
 # 3 sets of spp:
 # 1a) SEDAR lists;
 # 2b) MRIP / Recreational ACL tops;
 # 3c) All FHIER spp
 
 report_file_path2 <- file.path(my_paths$outputs,
-                                    r"(\compare_catch\report\12_categories_tables_states.xlsx)")
+                                    r"(compare_catch\report\12_categories_tables_states.xlsx)")
 
-# 1a) SEDAR lists;
+# 1a) SEDAR lists ----
+# c("sa", "gom")
+View(state_wave_has_rec_acl_data_list_state_sedar)
+
 map(names(state_wave_has_rec_acl_data_list_state_sedar),
-    function(current_state_abbr) {
-      sheet_name = paste0("2.1a wave state SEDAR lists ", current_state_abbr)
+    function(sa_or_gom) {
       # browser()
-      state_wave_has_rec_acl_data_list_state_sedar[[current_state_abbr]] %>%
-        add_to_report_xls(sheet_name, report_file_path = report_file_path2)
+      current_df_list <-
+        state_wave_has_rec_acl_data_list_state_sedar[[sa_or_gom]]
+      map(names(current_df_list),
+          function(current_state_abbr) {
+            sheet_name = paste("2.1a wave state SEDAR", sa_or_gom, current_state_abbr)
+            # browser()
+            current_df_list[[current_state_abbr]] %>%
+              add_to_report_xls(sheet_name, report_file_path = report_file_path2)
+          })
     })
-    
+# if file exists gives an error:
+# ! no field, method or inner class called 'use_cli_format' 
+
