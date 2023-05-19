cd32e55978552fafd1a175a4645df91422444aed
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-19 15:39:12 -0400

    rm maps

--- fishing_effort_location/fishing_effort_location.R
+++ fishing_effort_location/fishing_effort_location.R
@@ -8,73 +8,22 @@
 # 83 west (federal waters) / 24'35 N, 24'33 N (state waters)
 
 library(zoo) #date manipulations
-# maps
-library(sf)
-library(mapview)
-library(leaflet)
 
 source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
 source(file.path(my_paths$git_r, "fishing_effort_location", "fishing_effort_locations_get_data.R"))
 
-# SA only:
+# to get SA only:
 # filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary. 
-names(db_data)
-# get 
+# ? how to get the boundary?
+
+# fields to get 
 # Trip start and end date
 # Start Port: Code, Name, County and/or State, State Code, Postal Code - no
 # End Port: Code,
 # Longitude and Latitude fields:  Lat and long as specific as possible
 # Fishing Area Code, Sub Area Code, Distance Code and Distance Code Name?
 # depth
-
-db_data %>%
-  select(AREA_CODE) %>%
-# 
-#     select(AREA_CODE, SUB_AREA_CODE, LOCAL_AREA_CODE, DISTANCE_CODE_NAME) %>%
-  unique()
-  # View()
-
-# VENDOR_APP_NAME ----
-a <- db_data %>% 
-  count(VENDOR_APP_NAME)
-# 1 BLUEFIN DATA ACCSP SDK  33560
-# 2          ETRIPS ONLINE   1185
-# 3        ETRIPS/MOBILE 2  82207
-# 4                   VESL 186854
-# 5                   <NA>   2455
-sum(a$n)
-# 306261
-# 253941
-
-lat_long <- db_data %>%
-  select(LATITUDE, LONGITUDE, TRIP_START_DATE)
-
-# plot(sa_shp)
-# plot(gom_shp)
-# str(sa_shp)
-
-plot(sa_shp$geometry)
-
-lat_long_sf <-
-  lat_long %>%
-  filter(complete.cases(.)) %>%
-    st_as_sf(coords = c("LONGITUDE",
-                        "LATITUDE"),
-             crs = 4326)
-
-# str(lat_long_sf)
-# plot(lat_long_sf)
-# plot(lat_long_sf$geometry)
-
-# shape files maps ----
-m_s <- mapview(sa_shp,
-               layer.name = "South Altlantic",
-               legend = FALSE)
-# m_g <- mapview(gom_shp,
-#                layer.name = "Gulf of Mexico",
-#                legend = FALSE)
-
 
 # OK boundaries ----
 # lat 23 : 28
@@ -94,40 +43,6 @@ clean_lat_long <- function(my_lat_long_df, my_limit = 1000) {
     return()
 }
 
-# to_sf <- function(my_df) {
-#   my_df %>%
-#     st_as_sf(coords = c("LONGITUDE",
-#                         "LATITUDE"),
-#              crs = 4326) %>%
-#     return()
-# }
-
-to_sf <- function(my_df) {
-  my_df %>%
-    st_as_sf(coords = c("LONGITUDE",
-                        "LATITUDE"),
-             crs = st_crs(sa_shp)) %>%
-    return()
-}
-
-# --- with labels ----
-
-# my_colors <-
-#   # number of colors = number of unique points
-#   viridisLite::turbo(n = dim(clean_lat_long_subset)[1],
-#                      # reverse direction
-#                      direction = -1)
-
-# n_map <-
-#   clean_lat_long_subset %>%
-#   mutate(point = paste(LATITUDE, LONGITUDE)) %>%
-#   to_sf() %>%
-#   mapview(zcol = "point",
-#           # col.regions = my_colors,
-#           col.regions = viridisLite::turbo,
-#           legend = FALSE)
-
-# n_map + m_g + m_s
 
 # --- with dates ----
 # View(db_data)
@@ -139,29 +54,7 @@ lat_long3 <- db_data %>%
 
 # str(lat_long3)
 
-points_num <- 100
-clean_lat_long_subset3 <-
-  lat_long3 %>%
-  clean_lat_long(points_num)
-
-n_map <-
-  clean_lat_long_subset3 %>%
-  # mutate(point = paste(LATITUDE, LONGITUDE, TRIP_START_DAY_M)) %>%
-  mutate(point = TRIP_START_DAY_M) %>%
-  to_sf() %>%
-  mapview(zcol = "point",
-          col.regions = viridisLite::turbo,
-          layer.name = 'Month',
-          cex = "CATCH_CNT",
-          alpha = 0.3,
-          legend = T)
-
-### show my map + shape files ----
-n_map + m_s
-
 ## with depth ----
-
-# db_data %>% glimpse()
 
 lat_long_dat_dep <-
   db_data %>%
@@ -183,43 +76,11 @@ lat_long_dat_dep <-
   # 0 instead of NA
   mutate(AVG_DEPTH = replace_na(AVG_DEPTH, 0))
 
-points_num <- 1000
-
 clean_lat_long_subset <-
   lat_long_dat_dep %>%
   select(LATITUDE, LONGITUDE, TRIP_START_M, AVG_DEPTH) %>%
   clean_lat_long(points_num)
 
-n_map <-
-  clean_lat_long_subset %>%
-  # save info to show on the map
-  mutate(point = paste(LATITUDE, LONGITUDE, sep = ", ")) %>%
-  # convert to sf
-  # an sf object is a collection of simple features that includes attributes and geometries in the form of a data frame.
-  to_sf() %>%
-  mapview(
-    # colors
-    zcol = "TRIP_START_M",
-    # color palette
-    col.regions = viridisLite::turbo,
-    layer.name = 'Month',
-    # size
-    cex = "AVG_DEPTH",
-    # transparency
-    alpha = 0.3,
-    legend = TRUE
-  )
-
-n_map + m_s
-
-
-# for quarters ----
-
-lat_long_dat_dep_q <-
-  lat_long_dat_dep %>%
-  # add quarter
-  mutate(YEAR_QUARTER = as.yearqtr(TRIP_START_DATE)) %>%
-  mutate(QUARTER = format(YEAR_QUARTER, "%q"))
 
 lat_long_dat_dep_q_list <-
   split(lat_long_dat_dep_q,
@@ -259,62 +120,6 @@ mapview_q <- function(my_df, points_num, q_name) {
   return(n_map)
 }
 
-points_num <- 1000
-
-maps_q <-
-  map(names(lat_long_dat_dep_q_list),
-      function(q_name) {
-        # browser()
-        m_n <- mapview_q(lat_long_dat_dep_q_list[[q_name]],
-                         points_num,
-                         q_name)
-        return(m_n + m_s)
-      })
-
-maps_q[[4]]
-
-# map(lat_long_dat_dep_q_list,
-#     function(x) {dim(x)})
-# $`2022 Q1`
-# [1] 35207    24
-# 
-# $`2022 Q2`
-# [1] 91786    24
-# 
-# $`2022 Q3`
-# [1] 89717    24
-# 
-# $`2022 Q4`
-# [1] 37148    24
-
-map(lat_long_dat_dep_q_list,
-    function(x) {
-      x %>%
-        select(LATITUDE, LONGITUDE) %>%
-        filter(complete.cases(.)) %>%
-        unique() %>%
-        dim()
-    })
-# $`2022 Q1`
-# [1] 9219    2
-# 
-# $`2022 Q2`
-# [1] 23792     2
-# 
-# $`2022 Q3`
-# [1] 22052     2
-# 
-# $`2022 Q4`
-# [1] 8950    2
-
-# View(lat_long_dat_dep_q)
-# check depth ----
-lat_long_dat_dep_q %>% 
- filter(!(AVG_BOTTOM_DEPTH == AVG_DEPTH)) %>% dim()
-# 0
-# TODO: remove AVG_DEPTH
-# TODO: create lat_long_dat_dep_q independently
-
 # lat long and area ----
 dim(db_area_data)[1]
 # 83 FL
@@ -342,19 +147,29 @@ lat_long_area <-
     AREA_CODE,
     DISTANCE_CODE_NAME,
     AREA_NAME,
-    SUB_AREA_NAME
+    SUB_AREA_NAME, 
+    REGION
   )
-         # , SUB_AREA_CODE, LOCAL_AREA_CODE, DISTANCE_CODE_NAME)
 
 all_points <- dim(lat_long_area)[1]
 # 254444
 # 254689
 
 lat_long_area_clean <- clean_lat_long(lat_long_area, all_points)
-View(lat_long_area_clean)
+dim(lat_long_area_clean)[1]
+# 6359
+
+lat_long_area_clean %>%
+  select(AREA_NAME, REGION) %>% 
+  filter(grepl("MEX", AREA_NAME) | grepl("GOM", AREA_NAME)) %>% 
+  unique() 
+
+lat_long_area_clean_no_gom <-
+  lat_long_area_clean %>%
+  filter(!REGION %in% c("GULF OF MEXICO"))
 
 lat_long_area_clean_map <-
-  lat_long_area_clean %>%
+  lat_long_area_clean_no_gom %>%
   mutate(
     POINT = paste(
       LATITUDE,
@@ -364,6 +179,7 @@ lat_long_area_clean_map <-
       SUB_AREA_NAME,
       AREA_CODE,
       DISTANCE_CODE_NAME,
+      REGION,
       sep = ", "
     )
   ) %>%
@@ -377,6 +193,8 @@ lat_long_area_clean_map <-
     legend = F,
     clusterOptions = markerClusterOptions()
   )
+
+lat_long_area_clean_map
 
 # SpatialPointsDataFrame(coordinates(gadmCHE), 
 #                                       data = gadmCHE@data, 
