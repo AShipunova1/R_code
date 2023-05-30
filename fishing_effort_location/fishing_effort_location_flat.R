e4254330e1c6ebfc08ad56077ac21dc343a7acae
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-30 09:48:55 -0400

    clean up spaces

--- fishing_effort_location/fishing_effort_location_flat.R
+++ fishing_effort_location/fishing_effort_location_flat.R
@@ -15,31 +15,31 @@ library(mapview) #View spatial objects interactively
 # Start Port: Code, Name, County and/or State, State Code, Postal Code (all separate fields):  all
 # End Port: Code,
 # Longitude and Latitude fields:  Lat and long as specific as possible
-# Fishing Area Code, Sub Area Code, Distance Code and Distance Code Name 
+# Fishing Area Code, Sub Area Code, Distance Code and Distance Code Name
 # ---
 
-# I was wondering if it would be possible to get access to the for hire reports.  We are working on a management strategy evaluation and need information on location of relative fishing effort.  The relative would be looking by depth, area, and seasonally. 
-# 
+# I was wondering if it would be possible to get access to the for hire reports.  We are working on a management strategy evaluation and need information on location of relative fishing effort.  The relative would be looking by depth, area, and seasonally.
+#
 # I got approval to send you the data, per your request. To ensure I get what you need, can you please respond to the following:
-# 
-# You are looking for just depth, area fished (lats/longs?) and dates of those trips (start and end fields), correct? 
-# The lawyer deemed these non-confidential data fields, but just making sure you weren't expecting anything else.  No problem.  I do have access to confidential data through the SEFSC and ACCSP, and adrain has been approved by the SEFSC as well.  Are there additional steps I need to take for the future to be able to access landings data?  I don't think so, but we can cross that bridge when needed.  
+#
+# You are looking for just depth, area fished (lats/longs?) and dates of those trips (start and end fields), correct?
+# The lawyer deemed these non-confidential data fields, but just making sure you weren't expecting anything else.  No problem.  I do have access to confidential data through the SEFSC and ACCSP, and adrain has been approved by the SEFSC as well.  Are there additional steps I need to take for the future to be able to access landings data?  I don't think so, but we can cross that bridge when needed.
 # Do you want just for-hire intended fishing trips that have recorded an "area fished" on their trip report that falls within SA waters?
-# Are there any lat/long or state limits to apply? 
+# Are there any lat/long or state limits to apply?
 # I think filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary.  ok
-# 
-# The other possibilities are instead sending any for-hire trip taken by a SA federal for-hire permitted vessel (either for just SA or dual Gulf/SA permitted vessels), and/or filtering by start and/or end port of the trip. 
-# Is there a date range? 
+#
+# The other possibilities are instead sending any for-hire trip taken by a SA federal for-hire permitted vessel (either for just SA or dual Gulf/SA permitted vessels), and/or filtering by start and/or end port of the trip.
+# Is there a date range?
 # I would not suggest using 2021 data, given the program was so new. We tend to run any analyses from Jan 1, 2022+
 # All of 2022 would work for us ok
-# 
+#
 # What is the deadline that you would need this data request fulfilled by?
 # June 1 ok
-# 
-# We collect 3 depth fields in the SEFHIER logbook: min and max depth, and primary fishing depth. 
+#
+# We collect 3 depth fields in the SEFHIER logbook: min and max depth, and primary fishing depth.
 # Do you want all 3 fields, or one in particular?
 # Primary depth is fine.  However if the other fields are more commonly filled out than primary depth, please provide those fields instead.  ok
-# 
+#
 # We also have lat/long data fields, collected in the logbook. A summary of logbook fields of potential interest are here:
 # Trip type (commercial, charter, recreational, headboat) - these would be for-hire permitted vessels, telling us which trip type they intend to make:  Trips filtered to charter trips only.  We have access to commercial and headboat  ok (here headboat is a for-hire vessel operating as a headboat, but that is not in the SRHS program). So, I will include charter and headboat (which excludes SRHS data)
 # Trip start and trip end date and time fields  :  Trip start and end date  ok
@@ -48,18 +48,18 @@ library(mapview) #View spatial objects interactively
 # Trip Activity Type: options are trips with effort; intended fishing but unable to fish (e.g. weather); trip with no intention of fishing (e.g. chartered sunset or dolphin cruise): fishing charter trips  ok
 # Longitude and Latitude fields:  Lat and long as specific as possible  ok
 # Fishing Area Code, Sub Area Code, Distance Code and Distance Code Name (these are ACCSP fields, so I or you would need to get the code definitions) all.  I have access in ACCSP  ok
-#  
-# 
+#
+#
 # **Some data caveats to be aware of:
-# 
+#
 # this is preliminary SEFHIER reported data, and the SEFHIER data is not yet calibrated or certified.  This information will only be used to consider relative effort.  Are there issues with potential bias by area?  This was more of a note, to just say please be careful when using it for relative effort as this may not be representative.  We have not looked into what the biases are, but based alone on the lack of reporting it just may not be representative
 # to-date; we still have only about 60% compliance month to month in the SA, for vessels reporting as required. Therefore, interpretation of the data should take that into consideration
 # SRHS data are not included in the SEFHIER data (I do not have access to that program's data).  I can access this data   ok
 # SEFHIER logbooks don’t collect depth at the set level. So, a species (or complex level) analysis probably wouldn’t be very useful given how the fishery operates. We understand.  It is a hard one to define especially with some of the captains drifting instead of anchoring.  Average for the trip is fine.      ok
-# We collect “average” gear depth (essentially fishing depth for the whole trip, where a trip could be across many depths - depending how and where they moved for that trip, and they could have targeted many species throughout the trip- depending on weather and what was biting). 
+# We collect “average” gear depth (essentially fishing depth for the whole trip, where a trip could be across many depths - depending how and where they moved for that trip, and they could have targeted many species throughout the trip- depending on weather and what was biting).
 # We also collect min and max depth in the logbooks, but again those are trip level fields
-#  
-# 
+#
+#
 
 # setup ----
 set_work_dir <- function() {
@@ -145,8 +145,8 @@ db_data = ROracle::dbGetQuery(con,
 
 
 # get area data ----
-area_data_query <- 
-  "select * from SAFIS.AREAS_FISHED@secapxdv_dblk.sfsc.noaa.gov 
+area_data_query <-
+  "select * from SAFIS.AREAS_FISHED@secapxdv_dblk.sfsc.noaa.gov
   where state in ('FL', 'US')
 "
 
@@ -178,8 +178,8 @@ fl_state_w_counties_shp <- read_shapefile(r"(GOVTUNIT_Florida_State_Shape\Shape\
 
 #### Current file: C:/Users/anna.shipunova/Documents/R_code_github/fishing_effort_location/fishing_effort_location.R ----
 
-# information on location of relative fishing effort.  The relative would be looking by depth, area, and seasonally. 
-# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary. 
+# information on location of relative fishing effort.  The relative would be looking by depth, area, and seasonally.
+# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary.
 # --- OK boundaries
 # lat 23 : 36
 # lon -71 : -98
@@ -188,9 +188,9 @@ fl_state_w_counties_shp <- read_shapefile(r"(GOVTUNIT_Florida_State_Shape\Shape\
 # 83 west (federal waters) / 24'35 N, 24'33 N (state waters)
 
 # to get SA only:
-# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary. 
+# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary.
 
-# fields to get 
+# fields to get
 # Trip start and end date
 # Start Port: Code, Name, County and/or State, State Code, Postal Code - no
 # End Port: Code,
@@ -203,7 +203,7 @@ fl_state_w_counties_shp <- read_shapefile(r"(GOVTUNIT_Florida_State_Shape\Shape\
 # lon -71 : -83
 
 clean_lat_long <- function(my_lat_long_df, my_limit = 1000) {
-  
+
   my_lat_long_df %>%
     unique() %>%
     # we can limit the amount of points to show on the map
@@ -370,4 +370,3 @@ res_map
 # mapview::mapshot(res_map, file = png_fl)
 # # open the file
 # utils::browseURL(png_fl)
-
