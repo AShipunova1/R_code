81224406e85c07c6ce45d33ee39058c4ae9ce254
ashipunova1 <anna.shipunova@noaa.gov>
2023-05-31 11:32:14 -0400

    ### ---- separate SA and GOM permits ----

--- quantify_compliance/quantify_compliance.R
+++ quantify_compliance/quantify_compliance.R
@@ -3,3 +3,21 @@ source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
 
 source("~/R_code_github/quantify_compliance/get_data.R")
+
+### ---- separate SA and GOM permits ----
+
+separate_permits_into_3_groups <- function(compl_clean) {
+  compl_clean %>%
+  mutate(permit =
+           case_when(
+             !grepl("RCG|HRCG|CHG|HCHG", permitgroup) ~ "sa_only",
+             !grepl("CDW|CHS|SC", permitgroup) ~ "gom_only",
+             .default = "both"
+           )) %>%
+    return()
+}
+
+compl_clean_sa_vs_gom <- separate_permits_into_3_groups(compl_clean)
+
+# View(compl_clean_sa_vs_gom)
+
