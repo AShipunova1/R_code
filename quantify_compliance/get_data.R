ccf115e31d08f38a19677522064f1091ea5cb89e
ashipunova1 <anna.shipunova@noaa.gov>
2023-06-02 09:16:39 -0400

    Update get_data.R
    data from csvs to functions

--- quantify_compliance/get_data.R
+++ quantify_compliance/get_data.R
@@ -4,43 +4,58 @@ library(tictoc)
 
 project_dir_name <- "FHIER Compliance"
 
-filenames = c("FHIER_Compliance_2022__05_31_2023.csv",
-              "FHIER_Compliance_2023__05_31_2023.csv")
+# get data from csvs ----
 
-# "C:\Users\anna.shipunova\Documents\R_files_local\my_inputs\FHIER Compliance\05_31_2023\FHIER_Compliance_2023__05_31_2023.csv"
+get_data_from_FHIER_csvs <- function() {
+  filenames = c(
+    "FHIER_Compliance_2022__05_31_2023.csv",
+    "FHIER_Compliance_2023__05_31_2023.csv"
+  )
+  
+  # "C:\Users\anna.shipunova\Documents\R_files_local\my_inputs\FHIER Compliance\05_31_2023\FHIER_Compliance_2023__05_31_2023.csv"
+  
+  ## ---- get csv data into variables ----
+  csv_names_list <- prepare_csv_names(filenames)
+  
+  # View(csv_names_list)
+  # read all csv files
+  csv_contents <- load_csv_names(my_paths, csv_names_list)
+  # browser()
+  # unify headers, trim vesselofficialnumber, just in case
+  csvs_clean1 <- clean_all_csvs(csv_contents)
+  str(csvs_clean1)
+  # browser()
+  compl_clean <- compliance_cleaning(csvs_clean1)
+  
+  return(compl_clean)
+}
 
-## ---- get csv data into variables ----
-csv_names_list <- prepare_csv_names(filenames)
-
-# View(csv_names_list)
-# read all csv files
-csv_contents <- load_csv_names(my_paths, csv_names_list)
-# browser()
-# unify headers, trim vesselofficialnumber, just in case
-csvs_clean1 <- clean_all_csvs(csv_contents)
-str(csvs_clean1)
-# browser()
-compl_clean <- compliance_cleaning(csvs_clean1)
-
+# uncomment to run
+# compl_clean <- get_data_from_FHIER_csvs()
 # View(compl_clean)
-dim(compl_clean)
+# dim(compl_clean)
 # 208893     21
 
-## ---- get compliance error definitions ----
+## get compliance error definitions from csvs ----
+get_compliance_error_definitions <- function() {
+  err_desc_filenames = c(file.path(project_dir_name, "Compliance_Error_Types_03_29_2023.csv"))
+  
+  err_desc_csv_contents <-
+    load_csv_names(my_paths, err_desc_filenames)
+  
+  err_desc_clean_headers_csv_content <-
+    clean_headers(err_desc_csv_contents[[1]])
+  err_desc <-
+    change_to_dates(err_desc_clean_headers_csv_content,
+                    "last_updated",
+                    "%m/%d/%Y %I:%M:%S %p")
+  
+  return(err_desc)
+}
 
-err_desc_filenames = c(file.path(project_dir_name, "Compliance_Error_Types_03_29_2023.csv"))
+err_desc <- get_compliance_error_definitions()
 
-err_desc_csv_contents <-
-  load_csv_names(my_paths, err_desc_filenames)
-
-err_desc_clean_headers_csv_content <-
-  clean_headers(err_desc_csv_contents[[1]])
-err_desc <-
-  change_to_dates(err_desc_clean_headers_csv_content,
-                  "last_updated",
-                  "%m/%d/%Y %I:%M:%S %p")
-
-## ---- get permit data from PIMS ----
+## get permit data from PIMS csv ----
 get_permit_data_from_PIMS_csv <- function() {
   permit_names_list = r"(other\Permits_2023-03-29_1611_active.csv)"
   
@@ -94,7 +109,9 @@ get_permit_data_from_PIMS_csv <- function() {
 }
 
 # uncomment to run
-active_permits_from_pims <- get_permit_data_from_PIMS_csv()
+# active_permits_from_pims <- get_permit_data_from_PIMS_csv()
+
+# get data from db ----
 
 ## get permit data from db ----
 
@@ -128,9 +145,9 @@ WHERE
 }
 
 # to run
-permit_db_data <- get_permit_data_from_db()
+# permit_db_data <- get_permit_data_from_db()
 
-str(permit_db_data)
+# str(permit_db_data)
 # 37187 
 # old csv 23888
 
@@ -176,15 +193,22 @@ WHERE
 tic("get_compl_err_data_from_db()")
 compl_err_db_data_raw <- get_compl_err_data_from_db()
 toc()
+
 # 16.46 sec
 # get_compl_err_data_from_db(): 47.5 sec elapsed
 # get_compl_err_data_from_db(): 22.23 sec elapsed
 
-names(compl_err_db_data_raw) %>% 
-  unique() %>%
+# test for unique() fields
+all_names_len <- names(compl_err_db_data_raw) %>% length()
+uniq_names_len <-
+  names(compl_err_db_data_raw) %>% unique() %>% length()
+identical(all_names_len, uniq_names_len)
+
+# names(compl_err_db_data_raw) %>% 
+  # unique() %>%
 #   # 42
   # 38
-  length()
+  # length()
 # 46
 # 38
 
