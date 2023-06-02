d14bc5d26c414bfac8968ef6fcf36f161e505366
ashipunova1 <anna.shipunova@noaa.gov>
2023-06-02 19:33:14 -0400

    Update create_area_name_map.R
    fewer points

--- fishing_effort_location/create_area_name_map.R
+++ fishing_effort_location/create_area_name_map.R
@@ -19,39 +19,38 @@ with_st_difference <- function(points_sf, polygons_sf) {
   return(res)
 }
 
-# db_data_w_area %>% 
-#   select(AREA_NAME) %>% 
-#   unique() %>% arrange(AREA_NAME) %>% 
-#   head(15)
-# 
-# add TRIP_START_M and select fewer columns ----
-# lat_long_area <-
-#   db_data_w_area %>%
-#   # labels are a month only
-#   mutate(TRIP_START_M =
-#            format(TRIP_START_DATE, "%m")) %>%
-#   select(
-#     LATITUDE,
-#     LONGITUDE,
-#     TRIP_START_M,
-#     AREA_CODE,
-#     DISTANCE_CODE_NAME,
-#     AREA_NAME,
-#     SUB_AREA_NAME, 
-#     REGION
-#   )
+m_s <- mapview(
+  sa_shp,
+  col.regions = "#F4E3FF",
+  alpha.regions = 0.2,
+  layer.name = "South Altlantic",
+  legend = FALSE
+)
+
+m_g_r <- mapview(
+  gom_reef_shp,
+  col.regions = "lightblue",
+  alpha.regions = 0.2,
+  layer.name = "GOM Reef Fish EFH",
+  legend = FALSE
+)
+
 lat_long_area <- db_data_w_area %>%
   mutate(TRIP_START_M =
            format(TRIP_START_DATE, "%m")) %>%
   unique()
 
-all_points <- dim(lat_long_area)[1]
+# all_points <- dim(lat_long_area)[1]
 # 75536
 
 # clean coordinates and cut by coordinate boundaries ----
-lat_long_area_clean <- clean_lat_long(lat_long_area, all_points)
+how_many_points_to_draw <- 10000
+
+lat_long_area_clean <- clean_lat_long(lat_long_area, how_many_points_to_draw)
 dim(lat_long_area_clean)[1]
 # 28008
+# 397 (1000)
+# 5250 (10000)
 
 # exclude GOM "region" ----
 lat_long_area_clean_no_gom <-
@@ -61,7 +60,6 @@ lat_long_area_clean_no_gom <-
 # create sf ----
 lat_long_area_clean_sf <-
   lat_long_area_clean_no_gom %>%
-  filter(!is.na(AREA_NAME)) %>%
   mutate(
     POINT = paste(
       LATITUDE,
@@ -80,30 +78,14 @@ lat_long_area_clean_sf <-
 lat_long_area_clean_map <-
   lat_long_area_clean_sf %>%
   mapview(
-    zcol = "END_PORT_NAME",
-    # zcol = "AREA_NAME",
+    # zcol = "END_PORT_NAME",
+    zcol = "AREA_NAME",
     col.regions = viridisLite::turbo,
     layer.name = 'AREA_NAME',
     # cex = "DISTANCE_CODE_NAME",
     alpha = 0.3,
     legend = F
   )
-
-m_s <- mapview(
-  sa_shp,
-  col.regions = "#F4E3FF",
-  alpha.regions = 0.2,
-  layer.name = "South Altlantic",
-  legend = FALSE
-)
-
-m_g_r <- mapview(
-  gom_reef_shp,
-  col.regions = "lightblue",
-  alpha.regions = 0.2,
-  layer.name = "GOM Reef Fish EFH",
-  legend = FALSE
-)
 
 m_s + m_g_r + lat_long_area_clean_map
 
@@ -113,10 +95,12 @@ lat_long_area_clean_sf_no_gom_reef_sf <-
   with_st_difference(lat_long_area_clean_sf, gom_reef_shp)
 toc()
 # 540.17 sec elapsed
+# fewer points
+# 90.67 sec elapsed
 
 ### or read csv ----
 lat_long_area_clean_sf_no_gom_reef_sf_file_name <- 
-file.path(my_paths$outputs, current_project_name, "lat_long_area_clean_sf_no_gom_reef_sf.csv")
+file.path(my_paths$outputs, current_project_name, "lat_long_area_clean_sf_no_gom_reef_sf_short.csv")
 
 lat_long_area_clean_sf_no_gom_reef_sf <-
   read_sf(lat_long_area_clean_sf_no_gom_reef_sf_file_name) %>%
@@ -129,10 +113,6 @@ write_csv(
 
 lat_long_area_clean_sf_no_gom_reef_sf_map <-
   lat_long_area_clean_sf_no_gom_reef_sf %>%
-  filter(!is.na(AREA_NAME)) %>%
-  select(AREA_NAME) %>% 
-  unique() %>% 
-  head(15)
   mapview(
     # zcol = "END_PORT_NAME",
     zcol = "AREA_NAME",
