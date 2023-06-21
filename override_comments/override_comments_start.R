b56d6a27c5f8e5f4b65ad442076eaaf66702496f
ashipunova1 <anna.shipunova@noaa.gov>
2023-06-21 15:45:07 -0400

    Create override_comments_start.R

---
+++ override_comments/override_comments_start.R
@@ -0,0 +1,85 @@
+source("~/R_code_github/useful_functions_module.r")
+my_paths <- set_work_dir()
+
+# get data ----
+con <- connect_to_secpr()
+
+override_cmts_query <-
+  "select * from
+srh.srfh_vessel_comp@secapxdv_dblk.sfsc.noaa.gov
+where comp_override_cmt is not NULL
+"
+
+override_cmts_data =
+  ROracle::dbGetQuery(con,
+                      override_cmts_query)
+
+ROracle::dbDisconnect(con)
+
+# look at data ----
+dim(override_cmts_data)
+# [1] 17132    23
+
+data_overview(override_cmts_data)
+# SAFIS_VESSEL_ID        1538
+# VESSEL_OFFICIAL_NBR    1536
+# COMP_YEAR                 3
+# COMP_OVERRIDE_CMT      1492
+#   COMP_YEAR      COMP_WEEK_START_DT              
+# Min.   :2021   Min.   :2021-01-03 23:00:00.00  
+# Max.   :2023   Max.   :2023-05-29 00:00:00.00  
+
+# View(override_cmts_data)
+
+# process comments ----
+
+vesl_override_cmts <-
+  grep("vesl", override_cmts_data$COMP_OVERRIDE_CMT, 
+     ignore.case = T, value = T)
+# length(vesl_override_cmts)
+# 7521
+
+# View(vesl_override_cmts)
+
+override_cmts_data %>%
+  mutate(vesl_comment = case_when(
+    grepl("vesl", COMP_OVERRIDE_CMT,
+          ignore.case = T) ~ COMP_OVERRIDE_CMT,
+    .default = ""
+  )) %>%
+  mutate(etrips_comment = case_when(
+    grepl("ETRIPS", COMP_OVERRIDE_CMT,
+          ignore.case = T) ~ COMP_OVERRIDE_CMT,
+    .default = ""
+  )) %>%
+  mutate(scdnr_comment = case_when(
+    grepl("SCDNR", COMP_OVERRIDE_CMT,
+          ignore.case = T) ~ COMP_OVERRIDE_CMT,
+    .default = ""
+  )) %>%
+  mutate(safis_comment = case_when(
+    grepl("SAFIS", COMP_OVERRIDE_CMT,
+          ignore.case = T) ~ COMP_OVERRIDE_CMT,
+    .default = ""
+  )) %>%
+  mutate(submitted_comment = case_when(
+    grepl("SUBMITTED", COMP_OVERRIDE_CMT,
+          ignore.case = T) ~ COMP_OVERRIDE_CMT,
+    .default = ""
+  )) %>%
+  mutate(gom_comment = case_when(
+    grepl("dual|gom|feb 23|lawsuit", COMP_OVERRIDE_CMT,
+          ignore.case = T) ~ COMP_OVERRIDE_CMT,
+    .default = ""
+  )) %>%
+  arrange(desc(COMP_WEEK_END_DT)) %>% 
+  filter(vesl_comment == "" & 
+           etrips_comment == "" &
+           scdnr_comment == "" &
+           safis_comment == "" &
+           submitted_comment == "" &
+           gom_comment == "") %>% 
+  select(COMP_OVERRIDE_CMT, COMP_WEEK_END_DT) %>% 
+  View()
+
+# before gom_comment 2,932 entries
