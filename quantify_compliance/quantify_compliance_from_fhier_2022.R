1f2910b32c728e9ac9a61e613bd3eaf425efa1f9
ashipunova1 <anna.shipunova@noaa.gov>
2023-06-22 14:56:21 -0400

    Update quantify_compliance_from_fhier_2022.R
    # compare vessels per region with Jenny's ----

--- quantify_compliance/quantify_compliance_from_fhier_2022.R
+++ quantify_compliance/quantify_compliance_from_fhier_2022.R
@@ -1313,3 +1313,74 @@ files_to_combine <-
 # run as needed
 make_a_flat_file(file.path(dir_to_comb, "flat_file_quantify_compliance.R"), files_to_combine)
 
+# compare vessels per region with Jenny's ----
+# Download the permit data from the FHIER data management web application. Use the FHIER Compliance Count report to create a list of SA only permitted vessels. Set the start and end fields to the date range you are interested in. (You may have to download data in smaller chunks if it times out on you.) Only display the columns of data you need. Download the file as csv and put it in the same directory that has this R file. Rename the file PermitInfo.
+
+## get data ----
+j_file_name = r"(my_inputs\quantify_compliance\PermitInfo.csv)"
+
+vessels_per_permit_from_j <-
+  read_csv(j_file_name)
+
+# print_df_names(vessels_per_permit_from_j)
+vessels_per_permit_from_j_vsl <-
+  vessels_per_permit_from_j %>% 
+  select(`Vessel Official Number`) %>% 
+  arrange(`Vessel Official Number`) %>% 
+  unique()
+#   dim()
+# 2205    
+
+names(vessels_per_permit_from_j_vsl) <- "vessel_official_number"
+
+compl_clean_sa_vs_gom_m_int_22_sa <-
+  compl_clean_sa_vs_gom_m_int %>%
+  filter(year == "2022" &
+           permit_sa_gom == "sa_only")
+
+compl_clean_sa_vs_gom_m_int_22_sa_vsl <-
+  compl_clean_sa_vs_gom_m_int_22_sa %>% 
+  select(vessel_official_number) %>% 
+  arrange(vessel_official_number) %>%
+  unique()
+
+# dim(compl_clean_sa_vs_gom_m_int_22_sa_vsl)
+# 2178    
+
+## compare ----
+identical(vessels_per_permit_from_j_vsl$vessel_official_number,
+          compl_clean_sa_vs_gom_m_int_22_sa_vsl$vessel_official_number)
+# F
+
+in_j_only <-
+  setdiff(
+    vessels_per_permit_from_j_vsl$vessel_official_number,
+    compl_clean_sa_vs_gom_m_int_22_sa_vsl$vessel_official_number
+  )
+# 137
+
+in_a_only <-
+  setdiff(
+    compl_clean_sa_vs_gom_m_int_22_sa_vsl$vessel_official_number,
+    vessels_per_permit_from_j_vsl$vessel_official_number
+  )
+# 110
+
+## Search for in_j_only in a ----
+head(in_j_only)
+# [1] "1000051" "1020792" "1021103" "1035832" "1041705" "1041927"
+
+grep("1000051",
+     compl_clean_sa_vs_gom_m_int$vessel_official_number,
+     value = T)
+# 0
+
+compl_clean_sa_vs_gom_m_int %>% 
+  filter(vessel_official_number %in% in_j_only) %>% 
+  select(vessel_official_number, permitgroup) %>% 
+  unique() %>% 
+  # dim()
+# 75  
+  # View()
+  filter(!grepl("RCG|HRCG|CHG|HCHG", permitgroup, ignore.case = T))
+# 1 548359                 (CDW)CDW,(CHS)CHS,(SC)SC
