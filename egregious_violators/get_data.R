4dddcc14f54e236ccb29cb6e4cce891d177ab628
ashipunova1 <anna.shipunova@noaa.gov>
2023-06-23 13:02:23 -0400

    Update get_data.R
    new input files

--- egregious_violators/get_data.R
+++ egregious_violators/get_data.R
@@ -1,11 +1,8 @@
 # get data for egregious violators
-# csv_names_list_22_23 = c("Correspondence__2_24_23.csv",
-#                          "FHIER_Compliance_22__02_24_23.csv",
-#                          "FHIER_Compliance_23__02_24_23.csv")
-
-csv_names_list_22_23 = c("Correspondence_oct22_04_04_2023.csv",
-                         r"(04_04_2023\FHIER Compliance_22_04_04_2023.csv)",
-                         r"(04_04_2023\FHIER Compliance_23_04_04_2023.csv)")
+# Download from FHIER first
+csv_names_list_22_23 = c("Correspondence_22_23__06_22_2023.csv",
+                         r"(FHIER_Compliance_2022__06_22_2023.csv)",
+                         r"(FHIER_Compliance_2023__06_22_2023.csv)")
 
 ## ---- get csv data into variables ----
 temp_var <- get_compl_and_corresp_data(my_paths, csv_names_list_22_23)
