a52661575ab9bf9bde125f3c770353825166b3ae
ashipunova1 <anna.shipunova@noaa.gov>
2023-06-26 16:16:32 -0400

    Create db_compliance.R

---
+++ db_compliance/db_compliance.R
@@ -0,0 +1,65 @@
+# db_compliance
+# It would be really interesting to see an after the fact analysis done for compliance with another point of view, you can query the source of the activity from the tables we download from ACCSP and the permit materialized view mv_sero_fh_permits_his and then apply the latest compliance rules (all the fields needed for compliance are in these tables), it is very different to write an analysis report after the fact than build something for day-to-day activity that it has to be assigned to multiple users step by step.
+# CATCHES
+# EFFORTS
+# TRIPS
+# TRIPS_NEG
+# TRIP_NOTIFICATIONS
+# VESSELS
+
+# setup ----
+
+source("~/R_code_github/useful_functions_module.r")
+my_paths <- set_work_dir()
+con <- connect_to_secpr()
+
+# get data ----
+## permit ----
+file_name_permits <- r"(my_outputs\from_db\mv_sero_fh_permits_his.csv)"
+
+mv_sero_fh_permits_his <- 
+  read_csv(file_name_permits)
+# Rows: 181032 Columns: 22                                                                        
+# ── Column specification ─────────────────────────────────────
+# Delimiter: ","
+# chr (14): VESSEL_ID, EXPIRATION_DATE, TOP, PERMIT, EFFECT...
+
+# View(mv_sero_fh_permits_his)
+
+### the same from db ----
+
+mv_sero_fh_permits_his_query <-
+  "select * from
+srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov
+"
+
+permit_info <- dbGetQuery(con,
+                          mv_sero_fh_permits_his_query)
+
+View(permit_info)
+
+permit_vessel_query_exp21_query <-
+"select * from srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
+join safis.vessels@secapxdv_dblk.sfsc.noaa.gov v
+ON (v.sero_official_number = p.vessel_id)
+where expiration_date > TO_DATE('01-JAN-21')
+"
+# and top in ('CDW', 'CHS', 'SC')
+
+permit_vessel_query_exp21 <- dbGetQuery(con,
+                          permit_vessel_query_exp21_query)
+
+# View(permit_vessel_query_exp21)
+
+# add sa gom field
+
+print_df_names(permit_vessel_query_exp21)
+
+permit_vessel_query_exp21_reg <- 
+permit_vessel_query_exp21 %>% 
+  separate_permits_into_3_groups(permit_group_field_name = "top")
+
+names(permit_vessel_query_exp21) <-
+  make.unique(names(permit_vessel_query_exp21))
+
+  # dplyr::rename(iris, id_name_v = vessel_id.1)
