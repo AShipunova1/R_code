c8897a4a31df4d9bfb845ee961132b453c76f2b6
ashipunova1 <anna.shipunova@noaa.gov>
2023-07-03 15:43:15 -0400

    Update get_data_db_compliance.R
    write all csvs, get vessels

--- db_compliance/get_data_db_compliance.R
+++ db_compliance/get_data_db_compliance.R
@@ -1,4 +1,6 @@
 # get data ----
+input_path <- file.path(my_paths$inputs, current_project_name)
+
 ## permit ----
 file_name_permits <- r"(my_outputs\from_db\mv_sero_fh_permits_his.csv)"
 
@@ -22,6 +24,7 @@ permit_info <- dbGetQuery(con,
                           mv_sero_fh_permits_his_query)
 
 # View(permit_info)
+write_csv(permit_info, file.path(input_path, "permit_info.csv"))
 
 ### the same from another db table (to compare) ----
 
@@ -33,31 +36,32 @@ udp_v_sero_oth_prm_period_his_query <-
 permit_info_udp <- dbGetQuery(con,
                           udp_v_sero_oth_prm_period_his_query)
 
+write_csv(permit_info_udp, file.path(input_path, "permit_info.csv"))
 
 ### permit + vessel from db ----
-permit_vessel_query_exp21_query <-
-"select * from srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
-join safis.vessels@secapxdv_dblk.sfsc.noaa.gov v
-ON (v.sero_official_number = p.vessel_id)
-where expiration_date > TO_DATE('01-JAN-21')
-"
+# permit_vessel_query_exp21_query <-
+# "select * from srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
+# join safis.vessels@secapxdv_dblk.sfsc.noaa.gov v
+# ON (v.sero_official_number = p.vessel_id)
+# where expiration_date > TO_DATE('01-JAN-21')
+# "
 # and top in ('CDW', 'CHS', 'SC')
 
-permit_vessel_query_exp21 <- dbGetQuery(con,
-                          permit_vessel_query_exp21_query)
+# permit_vessel_query_exp21 <- dbGetQuery(con,
+                          # permit_vessel_query_exp21_query)
 
-View(permit_vessel_query_exp21)
+# View(permit_vessel_query_exp21)
 
 # add sa gom field
 
-names(permit_vessel_query_exp21) <-
-  make.unique(names(permit_vessel_query_exp21), sep = "_")
+# names(permit_vessel_query_exp21) <-
+  # make.unique(names(permit_vessel_query_exp21), sep = "_")
 
 # print_df_names(permit_vessel_query_exp21)
 
-permit_vessel_query_exp21 %>%
-  filter(!(VESSEL_ID == SERO_OFFICIAL_NUMBER)) %>%
-  dim()
+# permit_vessel_query_exp21 %>%
+  # filter(!(VESSEL_ID == SERO_OFFICIAL_NUMBER)) %>%
+  # dim()
 # 0
 
 # get trips info for 2022 ----
@@ -80,7 +84,6 @@ toc()
 # glimpse(trips_info_2022)
 # Rows: 205,772
 
-input_path <- file.path(my_paths$inputs, current_project_name)
 write_csv(trips_info_2022, file.path(input_path, "trips_info_2022.csv"))
 
 # trip_notifications ----
@@ -120,6 +123,48 @@ trip_neg_query_2022 <-
 tic("trip_neg_query_2022")
 trip_neg_2022 <- dbGetQuery(con,
                           trip_neg_query_2022)
+toc()
+# trip_neg_query_2022: 201.21 sec elapsed
+
+write_csv(trip_neg_2022, 
+          file.path(input_path, "trip_neg_2022.csv"))
+
+# glimpse(trip_neg_2022)
+# Rows: 1,495,929
+
+# trip_notifications ----
+trip_notifications_query <-
+  "SELECT
+ *
+FROM
+  safis.trip_notifications@secapxdv_dblk.sfsc.noaa.gov
+WHERE
+  trip_start_date >= TO_DATE('01-JAN-22', 'dd-mon-yy')
+  OR trip_end_date <= TO_DATE('31-DEC-22', 'dd-mon-yy')
+"
+# 97279
+
+tic("trip_notifications_query")
+trip_notifications_2022 <- dbGetQuery(con,
+                          trip_notifications_query)
+toc()
+# trip_notifications_query: 52.08 sec elapsed
+
+# glimpse(trip_notifications_2022)
+# Rows: 129,701
+
+write_csv(trip_notifications_2022, 
+          file.path(input_path, "trip_notifications_2022.csv"))
+
+# get vessels from db ----
+vessels_query <-
+  "SELECT *
+  FROM
+    safis.vessels@secapxdv_dblk.sfsc.noaa.gov"
+
+tic("vessels_all")
+vessels_all <- dbGetQuery(con,
+                          vessels_query)
 toc()
 # trip_neg_query_2022: 201.21 sec elapsed
 
