17137a8bef8019324e6f90d90e9b6942ef5aaf0e
ashipunova1 <anna.shipunova@noaa.gov>
2023-07-03 14:57:36 -0400

    Update db_compliance.R
    get_data in a separate file

--- db_compliance/db_compliance.R
+++ db_compliance/db_compliance.R
@@ -11,70 +11,18 @@
 library(tictoc)
 source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
+current_project_name <- "db_compliance"
 con <- connect_to_secpr()
 
 # get data ----
-## permit ----
-file_name_permits <- r"(my_outputs\from_db\mv_sero_fh_permits_his.csv)"
-
-mv_sero_fh_permits_his <-
-  read_csv(file_name_permits)
-# Rows: 181032 Columns: 22
-# ── Column specification ─────────────────────────────────────
-# Delimiter: ","
-# chr (14): VESSEL_ID, EXPIRATION_DATE, TOP, PERMIT, EFFECT...
-
-# View(mv_sero_fh_permits_his)
-
-### the same from db ----
-
-mv_sero_fh_permits_his_query <-
-  "select * from
-srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov
-"
-
-permit_info <- dbGetQuery(con,
-                          mv_sero_fh_permits_his_query)
-
-# View(permit_info)
-
-### the same from another db table (to compare) ----
-
-udp_v_sero_oth_prm_period_his_query <-
-  "select * from
-  udp.v_sero_oth_prm_period_his@secpr_dblk
-"
-
-permit_info_udp <- dbGetQuery(con,
-                          udp_v_sero_oth_prm_period_his_query)
-
-
-### permit + vessel from db ----
-permit_vessel_query_exp21_query <-
-"select * from srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
-join safis.vessels@secapxdv_dblk.sfsc.noaa.gov v
-ON (v.sero_official_number = p.vessel_id)
-where expiration_date > TO_DATE('01-JAN-21')
-"
-# and top in ('CDW', 'CHS', 'SC')
-
-permit_vessel_query_exp21 <- dbGetQuery(con,
-                          permit_vessel_query_exp21_query)
-
-View(permit_vessel_query_exp21)
-
-# add sa gom field
-
-names(permit_vessel_query_exp21) <-
-  make.unique(names(permit_vessel_query_exp21), sep = "_")
-
-# print_df_names(permit_vessel_query_exp21)
-
-permit_vessel_query_exp21 %>%
-  filter(!(VESSEL_ID == SERO_OFFICIAL_NUMBER)) %>%
-  dim()
-# 0
-
+get_data_file_path <- file.path(
+  my_paths$git_r,
+  current_project_name,
+  paste0("get_data_",
+         current_project_name,
+         ".R")
+)
+source(get_data_file_path)
 
 # separate_permits_into_3_groups ----
 #repeat for permit only
@@ -215,3 +163,4 @@ permit_info_r_l_overlap_join1_w_dual_22
 # 361
 # end here permits
 
+# 
