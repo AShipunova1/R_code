1202043e69942f6e09d4e1d8a6670d15ef0bbc37
ashipunova1 <anna.shipunova@noaa.gov>
2023-07-03 14:57:24 -0400

    Create get_data_db_compliance.R

---
+++ db_compliance/get_data_db_compliance.R
@@ -0,0 +1,73 @@
+# get data ----
+## permit ----
+file_name_permits <- r"(my_outputs\from_db\mv_sero_fh_permits_his.csv)"
+
+mv_sero_fh_permits_his <-
+  read_csv(file_name_permits)
+# Rows: 181032 Columns: 22
+# ── Column specification ─────────────────────────────────────
+# Delimiter: ","
+# chr (14): VESSEL_ID, EXPIRATION_DATE, TOP, PERMIT, EFFECT...
+
+# View(mv_sero_fh_permits_his)
+
+### the same from db ----
+
+mv_sero_fh_permits_his_query <-
+  "select * from
+srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov
+"
+
+permit_info <- dbGetQuery(con,
+                          mv_sero_fh_permits_his_query)
+
+# View(permit_info)
+
+### the same from another db table (to compare) ----
+
+udp_v_sero_oth_prm_period_his_query <-
+  "select * from
+  udp.v_sero_oth_prm_period_his@secpr_dblk
+"
+
+permit_info_udp <- dbGetQuery(con,
+                          udp_v_sero_oth_prm_period_his_query)
+
+
+### permit + vessel from db ----
+permit_vessel_query_exp21_query <-
+"select * from srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
+join safis.vessels@secapxdv_dblk.sfsc.noaa.gov v
+ON (v.sero_official_number = p.vessel_id)
+where expiration_date > TO_DATE('01-JAN-21')
+"
+# and top in ('CDW', 'CHS', 'SC')
+
+permit_vessel_query_exp21 <- dbGetQuery(con,
+                          permit_vessel_query_exp21_query)
+
+View(permit_vessel_query_exp21)
+
+# add sa gom field
+
+names(permit_vessel_query_exp21) <-
+  make.unique(names(permit_vessel_query_exp21), sep = "_")
+
+# print_df_names(permit_vessel_query_exp21)
+
+permit_vessel_query_exp21 %>%
+  filter(!(VESSEL_ID == SERO_OFFICIAL_NUMBER)) %>%
+  dim()
+# 0
+
+# get trips info for 2022 ----
+
+trips_22_query <-
+  "select * from
+safis.trips@secapxdv_dblk.sfsc.noaa.gov
+where year = '2022'
+"
+
+trips_info_2022 <- dbGetQuery(con,
+                          trips_22_query)
+
