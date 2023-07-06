effad20845a07783833a6a47ad2f62caac0895a8
ashipunova1 <anna.shipunova@noaa.gov>
2023-07-06 12:00:00 -0400

    Update db_compliance.R
    vessels__trip_neg_22_l_sa_short

--- db_compliance/db_compliance.R
+++ db_compliance/db_compliance.R
@@ -686,11 +686,20 @@ vessels__trips_22_l %>%
 # vessels__trip_notif_22_l
 # vessels__trip_neg_22_l
 
-## neg
+## neg trips per week per vessel ----
 vessels__trip_neg_22_l_sa_short <-
   vessels__trip_neg_22_l$sa_only %>%
-  select(contains("vessel"), starts_with("TRIP")) %>%
+  filter(!lubridate::floor_date(TRIP_DATE,
+                                unit = "day") ==
+           "2021-12-31") %>%
+  select(permit_vessel_id,
+         SUPPLIER_VESSEL_ID,
+         TRIP_week_num,
+         TRIP_DATE_y) %>%
   unique()
+
+  # select(contains("vessel"), starts_with("TRIP")) %>%
+  # unique()
 
 View(vessels__trip_neg_22_l_sa_short)
 # 390,659 10
