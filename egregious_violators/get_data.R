8680b6a4a80f4277bde60a35bb31529703890012
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2023-08-01 09:08:26 -0400

    Update get_data.R
    change path from_Fhier

--- egregious_violators/get_data.R
+++ egregious_violators/get_data.R
@@ -7,6 +7,10 @@ csv_names_list_22_23 = c("Correspondence_22_23__06_22_2023.csv",
 data_file_date <- lubridate::mdy("06_22_2023") 
 
 ## ---- get csv data into variables ----
+my_paths$inputs <- file.path(my_paths$inputs, "from_Fhier")
+# [1] "C:/Users/anna.shipunova/Documents/R_files_local/my_inputs"
+
+# "C:\Users\anna.shipunova\Documents\R_files_local\my_inputs\from_Fhier\Correspondence\Correspondence_22_23__06_22_2023.csv"
 temp_var <- get_compl_and_corresp_data(my_paths, csv_names_list_22_23)
 
 compl_clean <- temp_var[[1]]
