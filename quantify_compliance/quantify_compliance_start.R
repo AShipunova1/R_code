c7ce8329f2774b25065c03ac62a5c4352e17c21f
AShipunova1 <anna.shipunova@noaa.gov>
2023-08-26 10:27:19 -0400

    Update quantify_compliance_start.R
    metric_tracking_no_srhs.R

--- quantify_compliance/quantify_compliance_start.R
+++ quantify_compliance/quantify_compliance_start.R
@@ -9,4 +9,6 @@ my_paths <- set_work_dir()
 source("~/R_code_github/quantify_compliance/quantify_compliance_functions.R")
 source("~/R_code_github/quantify_compliance/get_data.R")
 
+source(r"(~\R_code_github\get_data_from_fhier\metric_tracking_no_srhs.R)")
+# fhier_reports_metrics_tracking_not_srhs_ids
 
