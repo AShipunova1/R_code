ea203be4510deaa81ec8cb8e9db57eb40028baae
AShipunova1 <anna.shipunova@noaa.gov>
2023-08-29 14:53:32 -0400

    Update part_time_fishery.R
    get counts for db numbers

--- part_time_fishery/part_time_fishery.R
+++ part_time_fishery/part_time_fishery.R
@@ -1,6 +1,7 @@
 # Determine what level of the fishery is part time via declarations and VMS positions (e.g., from Jessica - number of fishing trips vs non-fishing trips). I would look at # of gulf + dual permitted vessels who declared charter/headboat fishing intended trips vs total # of permitted vessels, for 2022.
 
 library(tictoc)
+library(zoo)
 
 source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
@@ -27,7 +28,6 @@ source(file.path(my_paths$git_r,
                  db_compliance_dir_name,
                  "clean_data_db_compliance.R"))
 
-
 # v_p_d_w_22_short
 # t_d_w_short
 # tne_d_w_short
@@ -38,5 +38,45 @@ source(file.path(my_paths$git_r,
 # gulf + dual permitted vessels 
 # who declared charter/headboat fishing intended trips
 
-View(v_p_d_w_22_short)
-# get gulf_dual 2022 ----
+# print_df_names(v_p_d_w_22_short)
+
+v_p_d_w_22_short_gom <-
+  v_p_d_w_22_short |> 
+  filter(permit_sa_gom_dual %in% c("dual", "gom_only"))
+
+dim(v_p_d_w_22_short_gom)
+# [1] 1348    5
+
+# total # of gulf + dual permitted vessels 
+v_p_d_w_22_short_gom_total_vsls <-
+  v_p_d_w_22_short_gom |> 
+  select(PERMIT_VESSEL_ID) |> 
+  distinct() |> 
+  dim()
+
+v_p_d_w_22_short_gom_total_vsls
+# 1348
+
+# gulf + dual permitted vessels who declared charter/headboat fishing intended trips
+# View(tn_d_w_short)
+
+tn_d_w_short_22 <-
+  tn_d_w_short |>
+  filter(YEAR == "2022")
+# print_df_names(v_p_d_w_22_short_gom)
+
+v_p__tn <-
+  inner_join(tn_d_w_short_22,
+            v_p_d_w_22_short_gom,
+            join_by(VESSEL_ID == VESSEL_VESSEL_ID))
+
+dim(v_p__tn)
+# [1] 64568    25
+
+v_p__tn |> 
+  select(PERMIT_VESSEL_ID) |> 
+  distinct() |> 
+  dim()
+# 865   
+
+# 1304 total permitted in the compliance from FHIER
