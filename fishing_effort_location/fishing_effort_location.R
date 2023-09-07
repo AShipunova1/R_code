e1a4a11cdfd096507920e95d55d87d1ddf1372c0
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2023-09-07 16:24:21 -0400

    Update fishing_effort_location.R
    gom fishing locations

--- fishing_effort_location/fishing_effort_location.R
+++ fishing_effort_location/fishing_effort_location.R
@@ -1,6 +1,6 @@
 # Requirements ----
-# information on location of relative fishing effort.  The relative would be looking by depth, area, and seasonally. 
-# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary. 
+# information on location of relative fishing effort.  The relative would be looking by depth, area, and seasonally.
+# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary.
 # --- OK boundaries
 # lat 23 : 36
 # lon -71 : -98
@@ -8,8 +8,8 @@
 # see https://myfwc.com/fishing/saltwater/recreational/maps/
 # 83 west (federal waters) / 24'35 N, 24'33 N (state waters)
 # to get SA only:
-# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary. 
-# fields to get 
+# filter out beyond state waters for trips north of 28N.  All charter trips south of 28N to the SAFMC/GMFMC boundary.
+# fields to get
 # Trip start and end date
 # Start Port: Code, Name, County and/or State, State Code, Postal Code - no
 # End Port: Code,
@@ -68,7 +68,7 @@ with_st_intersection <- function(points_sf, polygons_sf) {
   # get param names
   par1 <- rlang::enexpr(points_sf)
   par2 <- rlang::enexpr(polygons_sf)
-  
+
   # start time
   tic(paste0("sf::st_intersection(", par1, ", ", par2, ")"))
   res <- sf::st_intersection(points_sf, polygons_sf)
@@ -83,7 +83,7 @@ with_st_difference <- function(points_sf, polygons_sf) {
   # get param names
   par1 <- rlang::enexpr(points_sf)
   par2 <- rlang::enexpr(polygons_sf)
-  
+
   # start time
   tic(paste0("sf::st_difference(", par1, ", ", par2, ")"))
   res <- sf::st_difference(points_sf, polygons_sf)
@@ -105,7 +105,7 @@ all_points <- dim(db_data_w_area)[1]
 #     "fishing_effort_location_by_table.R"
 #   )
 # )
-fields_list <- 
+fields_list <-
   c("TRIP_START_DATE",
 "TRIP_END_DATE",
 "START_PORT",
@@ -125,58 +125,35 @@ fields_list <-
 "LONGITUDE",
 "FISHING_GEAR_DEPTH")
 
-# Filter out maximum by data ----
-db_data_w_area_no_mex <-
-  db_data_w_area %>%
-  # [1] 254689     32
-  dplyr::filter(!(grepl("MEX", AREA_NAME))) %>% 
-    # 254503     
-  dplyr::filter(!(grepl("GOM", AREA_NAME))) %>%
-  dplyr::filter(!REGION %in% c("GULF OF MEXICO")) %>%
-  # all LONG should be negative
-  dplyr::mutate(LONGITUDE = -abs(LONGITUDE)) %>% 
-  # keep only full sets of coordinates
-  dplyr::filter(!is.na(LONGITUDE) | !is.na(LATITUDE)) %>% 
-  # [1] 253003     32
-  # south and north by SA shp
-  dplyr::filter(between(LATITUDE, 23.81794, 36.55028)) %>%
-  # [1] 241183     32
-  # 133889     
-  # west and east by SA shp
-  dplyr::filter(between(LONGITUDE, -83, -71.37133))
-  # [1] 140177     32
+# GOM ----
+dim(db_data_w_area)
+# [1] 75536    30
 
-# st_geometry(sa_shp)
-# Bounding box:  xmin: -83 ymin: 23.81794 xmax: -71.37133 ymax: 36.55028
+db_data_w_area_short <-
+  db_data_w_area |>
+  select(all_of(fields_list)) |>
+  unique()
 
-db_data_w_area_no_mex_uniq <-
-  db_data_w_area_no_mex %>% 
-  unique() 
+dim(db_data_w_area_short)
+# [1] 75536    18
 
-# keep fewer columns ----
-db_data_w_area_report_short <-
-  db_data_w_area_no_mex_uniq %>%
-  select(all_of(fields_list))
+print_df_names(db_data_w_area_short)
 
-dim(db_data_w_area_report_short)
-# 45315 10
-# [1] 45264    18
+db_data_w_area_short_good_coord <-
+  db_data_w_area_short %>%
+  distinct() %>%
+  filter(!is.na(LATITUDE) |
+           !is.na(LONGITUDE))
 
-# keep fewer rows by removing duplicates ----
-db_data_w_area_report <-
-  db_data_w_area_report_short %>% unique()
-# [1] 45261    10
+dim(db_data_w_area_short_good_coord)
+# [1] 74520    18
 
-# SA EEZ for all ----
-## get only the points inside the SA EEZ ----
-db_data_w_area_report_sf <-
-  db_data_w_area_report_short %>%
-  unique() %>%
-  # convert to sf, see fun above (use F2)
-  my_to_sf()
+# convert to sf
+db_data_w_area_short_good_coord_sf <-
+  my_to_sf(db_data_w_area_short_good_coord)
 
-dim(db_data_w_area_report_sf)
-# [1] 45264    19
+dim(db_data_w_area_short_good_coord_sf)
+# [1] 74520    19
 
 ### with st_intersection ----
 # get only the points inside the SA EEZ by intersection
@@ -184,7 +161,7 @@ db_data_w_area_report_sa_eez_sf <-
   with_st_intersection(db_data_w_area_report_sf, sa_shp)
 # 594.35 sec
 # 63.44 sec (uniq)
-# 65.1 sec 
+# 65.1 sec
 # 174.95 sec
 # 193.08 /60 ~3
 # 196.25 sec
@@ -232,27 +209,27 @@ get_state_waters_sa_sf <- function() {
     "Volusia",
     "Monroe"
   ) #has GOM too, remove separately
-  
+
   # mapview(fl_state_w_counties)
   # fl_state_w_counties$gnis_name %>% paste0(collapse = ", ")
-  
+
   fl_state_w_counties_names <- fl_state_w_counties_shp$gnis_name
-  
+
   # length(fl_state_w_counties_names)
   # 67
-  
+
   # grep("Monroe", fl_state_w_counties_names, value = T)
-  
+
   # length(fl_counties_sa)
   # 12 + Monroe
-  
+
   fl_state_w_counties_names_df <-
     as.data.frame(fl_state_w_counties_names)
   # str(fl_state_w_counties_names_df)
   # fl_state_w_counties_names) %>%
-  
+
   # View(as.data.frame(fl_counties_sa))
-  
+
   sa_fl_state_w_counties_names <-
     as.data.frame(fl_counties_sa)[[1]] %>%
     map_df(function(fl_county) {
@@ -263,15 +240,15 @@ get_state_waters_sa_sf <- function() {
           fl_county,
           fl_state_w_counties_names_df$fl_state_w_counties_names
         ))
-      
+
       return(sa_county)
     })
-  
+
   fl_state_w_counties_sa <- filter(
     fl_state_w_counties_shp,
     gnis_name %in% sa_fl_state_w_counties_names$fl_state_w_counties_names
   )
-  
+
   return(fl_state_w_counties_sa)
 }
 
@@ -283,7 +260,7 @@ fl_state_w_counties_sa_sf <- get_state_waters_sa_sf()
 db_data_w_area_report_28_s_sa_counties_sf <-
   with_st_intersection(db_data_w_area_report_28_s_sf,
                       fl_state_w_counties_sa_sf)
-# 0.37 sec 
+# 0.37 sec
 # 3.56 sec
 
 dim(db_data_w_area_report_28_s_sa_counties_sf)
@@ -292,7 +269,7 @@ dim(db_data_w_area_report_28_s_sa_counties_sf)
 # mapview(db_data_w_area_report_28_s_sa_counties_sf)
 
 ### or read csv ----
-db_data_w_area_report_28_s_sa_counties_file_name <- 
+db_data_w_area_report_28_s_sa_counties_file_name <-
 file.path(my_paths$outputs, current_project_name, "db_data_w_area_report_28_s_sa_counties_sf_u_more_fields.csv")
 
 db_data_w_area_report_28_s_sa_counties_sf <-
@@ -313,14 +290,14 @@ dim(db_data_w_area_report_28_s_sa_counties_sf)
 # View(fl_state_w_counties_monroe)
 
 ### using sf::st_difference ----
-# slow 
+# slow
 db_data_w_area_report_28_s_sa_counties_no_gom_sf <-
   with_st_difference(db_data_w_area_report_28_s_sa_counties_sf, gom_reef_shp)
 # 188.69 sec
 # 195.96 sec ~3 m
 
 
-# or read csv 
+# or read csv
 sa_counties_no_gom_sf_filename <- file.path(my_paths$outputs, current_project_name, "db_data_w_area_report_28_s_sa_counties_no_gom_sf_more_fields.csv")
 
 db_data_w_area_report_28_s_sa_counties_no_gom_sf <-
@@ -358,13 +335,13 @@ my_sf_to_df <- function(my_sf) {
     #   FISHING_GEAR_DEPTH
     ) %>%
     unique()
-  
+
   return(my_df)
 }
 
 my_sf_to_csv <- function(my_sf, file_name) {
   my_df <- my_sf_to_df(my_sf)
-  
+
   write_csv(
     my_df,
     file.path(my_paths$outputs,
@@ -379,13 +356,13 @@ dim(db_data_w_area_report_28_s_sa_counties_no_gom_sf)
 
 my_sf_to_csv(db_data_w_area_report_sa_eez_sf, "sa_eez_all")
 # names(db_data_w_area_report_sa_eez_sf)
-# 18989   
+# 18989
 # [1] 18970    21
 
 
-my_sf_to_csv(db_data_w_area_report_28_s_sa_counties_no_gom_sf, "south_of_28_state_w") 
+my_sf_to_csv(db_data_w_area_report_28_s_sa_counties_no_gom_sf, "south_of_28_state_w")
 # dim(db_data_w_area_report_28_s_sa_counties_no_gom_sf)
-# 8903   
+# 8903
 
 # all maps together ----
 ## south of 28 map ----
@@ -455,13 +432,357 @@ db_data_w_area_report_sa_eez_sf %>%
   count(FISHING_GEAR_DEPTH, START_PORT_STATE) %>%
   View()
 
-db_data_w_area_report_28_s_sa_counties_no_gom_sf %>% 
+db_data_w_area_report_28_s_sa_counties_no_gom_sf %>%
   my_sf_to_df() %>%
   count(FISHING_GEAR_DEPTH, START_PORT_STATE) %>%
   View()
 
 # by end_port, depth, month ---
-db_data_w_area_report %>% 
+db_data_w_area_report %>%
+    dplyr::mutate(TRIP_START_M =
+           format(TRIP_START_DATE, "%m")) %>%
+  count(FISHING_GEAR_DEPTH, END_PORT, TRIP_START_M) %>% glimpse()
+  # View()
+
+# SA only ----
+
+# Filter out maximum by data ----
+db_data_w_area_no_mex <-
+  db_data_w_area %>%
+  # [1] 254689     32
+  dplyr::filter(!(grepl("MEX", AREA_NAME))) %>%
+    # 254503
+  dplyr::filter(!(grepl("GOM", AREA_NAME))) %>%
+  dplyr::filter(!REGION %in% c("GULF OF MEXICO")) %>%
+  # all LONG should be negative
+  dplyr::mutate(LONGITUDE = -abs(LONGITUDE)) %>%
+  # keep only full sets of coordinates
+  dplyr::filter(!is.na(LONGITUDE) | !is.na(LATITUDE)) %>%
+  # [1] 253003     32
+  # south and north by SA shp
+  dplyr::filter(between(LATITUDE, 23.81794, 36.55028)) %>%
+  # [1] 241183     32
+  # 133889
+  # west and east by SA shp
+  dplyr::filter(between(LONGITUDE, -83, -71.37133))
+  # [1] 140177     32
+
+# st_geometry(sa_shp)
+# Bounding box:  xmin: -83 ymin: 23.81794 xmax: -71.37133 ymax: 36.55028
+
+db_data_w_area_no_mex_uniq <-
+  db_data_w_area_no_mex %>%
+  unique()
+
+# keep fewer columns ----
+db_data_w_area_report_short <-
+  db_data_w_area_no_mex_uniq %>%
+  select(all_of(fields_list))
+
+dim(db_data_w_area_report_short)
+# 45315 10
+# [1] 45264    18
+
+# keep fewer rows by removing duplicates ----
+db_data_w_area_report <-
+  db_data_w_area_report_short %>% unique()
+# [1] 45261    10
+
+# SA EEZ for all ----
+## get only the points inside the SA EEZ ----
+db_data_w_area_report_sf <-
+  db_data_w_area_report_short %>%
+  unique() %>%
+  # convert to sf, see fun above (use F2)
+  my_to_sf()
+
+dim(db_data_w_area_report_sf)
+# [1] 45264    19
+
+### with st_intersection ----
+# get only the points inside the SA EEZ by intersection
+db_data_w_area_report_sa_eez_sf <-
+  with_st_intersection(db_data_w_area_report_sf, sa_shp)
+# 594.35 sec
+# 63.44 sec (uniq)
+# 65.1 sec
+# 174.95 sec
+# 193.08 /60 ~3
+# 196.25 sec
+
+### or read it ----
+db_data_w_area_report_sa_eez_file_name <- file.path(my_paths$outputs, current_project_name, "db_data_w_area_report_sa_eez_sf_more_fields.csv")
+
+db_data_w_area_report_sa_eez_sf <-
+  read_sf(db_data_w_area_report_sa_eez_file_name) %>%
+  my_to_sf()
+
+#### save sa_eez_data ----
+write_csv(db_data_w_area_report_sa_eez_sf, db_data_w_area_report_sa_eez_file_name)
+
+dim(db_data_w_area_report_sa_eez_sf)
+# 18989 13
+# [1] 18967    13
+# [1] 18970    21
+
+# South of 28N - all SA ----
+db_data_w_area_report_28_s_sf <-
+  db_data_w_area_report %>%
+  filter(between(LATITUDE, 23, 28)) %>%
+  # convert to sf, see fun above (use F2)
+  my_to_sf()
+
+dim(db_data_w_area_report_28_s_sf)
+# 92882   11
+# 27979   19   unique
+
+## state waters sa ----
+get_state_waters_sa_sf <- function() {
+  fl_counties_sa <- c(
+    "Brevard",
+    "Broward",
+    "Duval",
+    "Flagler",
+    "Indian River",
+    "Martin",
+    "Miami-Dade",
+    "Nassau",
+    "Palm Beach",
+    "Saint Johns",
+    "Saint Lucie",
+    "Volusia",
+    "Monroe"
+  ) #has GOM too, remove separately
+
+  # mapview(fl_state_w_counties)
+  # fl_state_w_counties$gnis_name %>% paste0(collapse = ", ")
+
+  fl_state_w_counties_names <- fl_state_w_counties_shp$gnis_name
+
+  # length(fl_state_w_counties_names)
+  # 67
+
+  # grep("Monroe", fl_state_w_counties_names, value = T)
+
+  # length(fl_counties_sa)
+  # 12 + Monroe
+
+  fl_state_w_counties_names_df <-
+    as.data.frame(fl_state_w_counties_names)
+  # str(fl_state_w_counties_names_df)
+  # fl_state_w_counties_names) %>%
+
+  # View(as.data.frame(fl_counties_sa))
+
+  sa_fl_state_w_counties_names <-
+    as.data.frame(fl_counties_sa)[[1]] %>%
+    map_df(function(fl_county) {
+      # browser()
+      sa_county <-
+        fl_state_w_counties_names_df %>%
+        filter(grepl(
+          fl_county,
+          fl_state_w_counties_names_df$fl_state_w_counties_names
+        ))
+
+      return(sa_county)
+    })
+
+  fl_state_w_counties_sa <- filter(
+    fl_state_w_counties_shp,
+    gnis_name %in% sa_fl_state_w_counties_names$fl_state_w_counties_names
+  )
+
+  return(fl_state_w_counties_sa)
+}
+
+fl_state_w_counties_sa_sf <- get_state_waters_sa_sf()
+
+# mapview(db_data_w_area_report_28_s_sf)
+
+### get only state and inner waters by intersection ----
+db_data_w_area_report_28_s_sa_counties_sf <-
+  with_st_intersection(db_data_w_area_report_28_s_sf,
+                      fl_state_w_counties_sa_sf)
+# 0.37 sec
+# 3.56 sec
+
+dim(db_data_w_area_report_28_s_sa_counties_sf)
+# [1] 10761    38
+
+# mapview(db_data_w_area_report_28_s_sa_counties_sf)
+
+### or read csv ----
+db_data_w_area_report_28_s_sa_counties_file_name <-
+file.path(my_paths$outputs, current_project_name, "db_data_w_area_report_28_s_sa_counties_sf_u_more_fields.csv")
+
+db_data_w_area_report_28_s_sa_counties_sf <-
+  read_sf(db_data_w_area_report_28_s_sa_counties_file_name) %>%
+  my_to_sf()
+
+write_csv(
+  db_data_w_area_report_28_s_sa_counties_sf,
+  db_data_w_area_report_28_s_sa_counties_file_name
+)
+
+dim(db_data_w_area_report_28_s_sa_counties_sf)
+# 30392    30
+# 10761    38
+
+## For Monroe exclude GOM ----
+
+# View(fl_state_w_counties_monroe)
+
+### using sf::st_difference ----
+# slow
+db_data_w_area_report_28_s_sa_counties_no_gom_sf <-
+  with_st_difference(db_data_w_area_report_28_s_sa_counties_sf, gom_reef_shp)
+# 188.69 sec
+# 195.96 sec ~3 m
+
+
+# or read csv
+sa_counties_no_gom_sf_filename <- file.path(my_paths$outputs, current_project_name, "db_data_w_area_report_28_s_sa_counties_no_gom_sf_more_fields.csv")
+
+db_data_w_area_report_28_s_sa_counties_no_gom_sf <-
+  read_sf(sa_counties_no_gom_sf_filename) %>%
+  my_to_sf()
+
+dim(db_data_w_area_report_28_s_sa_counties_no_gom_sf)
+# 23519    32
+# 8903 40
+
+write_csv(
+  db_data_w_area_report_28_s_sa_counties_no_gom_sf,
+  sa_counties_no_gom_sf_filename
+)
+
+# mapview(db_data_w_area_report_28_s_sa_counties_no_gom_sf)
+
+# Report csv ----
+my_sf_to_df <- function(my_sf) {
+  my_df <-
+    my_sf %>%
+    sf::st_drop_geometry() %>%
+    select(all_of(fields_list)
+           # )
+    # select(
+    #   TRIP_START_DATE,
+    #   TRIP_END_DATE,
+    #   START_PORT,
+    #   START_PORT_NAME,
+    #   START_PORT_COUNTY,
+    #   START_PORT_STATE,
+    #   END_PORT,
+    #   LATITUDE,
+    #   LONGITUDE,
+    #   FISHING_GEAR_DEPTH
+    ) %>%
+    unique()
+
+  return(my_df)
+}
+
+my_sf_to_csv <- function(my_sf, file_name) {
+  my_df <- my_sf_to_df(my_sf)
+
+  write_csv(
+    my_df,
+    file.path(my_paths$outputs,
+              current_project_name,
+              "report",
+              paste0(file_name, ".csv"))
+  )
+}
+
+dim(db_data_w_area_report_28_s_sa_counties_no_gom_sf)
+# 8903   40
+
+my_sf_to_csv(db_data_w_area_report_sa_eez_sf, "sa_eez_all")
+# names(db_data_w_area_report_sa_eez_sf)
+# 18989
+# [1] 18970    21
+
+
+my_sf_to_csv(db_data_w_area_report_28_s_sa_counties_no_gom_sf, "south_of_28_state_w")
+# dim(db_data_w_area_report_28_s_sa_counties_no_gom_sf)
+# 8903
+
+# all maps together ----
+## south of 28 map ----
+m_db_data_w_area_report_28_s_sa_counties_no_gom_sf <-
+  mapview(
+  db_data_w_area_report_28_s_sa_counties_no_gom_sf,
+  col.regions = "green",
+  layer.name = 'State and inner waters'
+)
+
+m_s <- mapview(
+  sa_shp,
+  col.regions = "#F4E3FF",
+  alpha.regions = 0.2,
+  layer.name = "South Altlantic",
+  legend = FALSE
+)
+
+m_g_r <- mapview(
+  gom_reef_shp,
+  col.regions = "lightblue",
+  alpha.regions = 0.2,
+  layer.name = "GOM Reef Fish EFH",
+  legend = FALSE
+)
+
+m_sa_eez <-
+  mapview(
+    db_data_w_area_report_sa_eez_sf,
+    layer.name = 'SA EEZ'
+  )
+
+all_maps <-
+  m_s +
+  m_g_r +
+  m_sa_eez +
+  m_db_data_w_area_report_28_s_sa_counties_no_gom_sf
+
+
+# make a flat file ----
+dir_to_comb <- file.path(my_paths$git_r, current_project_name)
+
+files_to_combine_list <-
+  c(
+    file.path(my_paths$git_r, "useful_functions_module.r"),
+    file.path(dir_to_comb, "read.me.R"),
+    file.path(dir_to_comb, "fishing_effort_locations_get_data.R"),
+    file.path(dir_to_comb, "fishing_effort_location.R")
+    # ,
+    # file.path(dir_to_comb, "fishing_effort_location_viz.R")
+  )
+
+flat_file_name = file.path(dir_to_comb, "fishing_effort_location_flat_05_30.R")
+
+# run as needed
+# make_a_flat_file(flat_file_name,
+                 # files_to_combine_list)
+
+# The relative would be looking by depth, area, and seasonally. ----
+## by area: ----
+# db_data_w_area_report_sa_eez_sf
+# db_data_w_area_report_28_s_sa_counties_no_gom_sf
+
+## by depth, state ----
+db_data_w_area_report_sa_eez_sf %>%
+  my_sf_to_df() %>%
+  count(FISHING_GEAR_DEPTH, START_PORT_STATE) %>%
+  View()
+
+db_data_w_area_report_28_s_sa_counties_no_gom_sf %>%
+  my_sf_to_df() %>%
+  count(FISHING_GEAR_DEPTH, START_PORT_STATE) %>%
+  View()
+
+# by end_port, depth, month ---
+db_data_w_area_report %>%
     dplyr::mutate(TRIP_START_M =
            format(TRIP_START_DATE, "%m")) %>%
   count(FISHING_GEAR_DEPTH, END_PORT, TRIP_START_M) %>% glimpse()
