2cbff25b5f40f705936228d73ecb7ca9ad7b53ed
AShipunova1 <anna.shipunova@noaa.gov>
2023-09-15 18:00:34 -0400

    by zone todo

--- fishing_effort_location/fishing_effort_location_by_permit.R
+++ fishing_effort_location/fishing_effort_location_by_permit.R
@@ -888,7 +888,6 @@ toc()
 
 ## heatmap data ----
 
-
 # short_example_3_cnts_short |> glimpse()
 glimpse(safis_efforts_extended_2022_short_good_sf_crop_big_short_df_permits_sa_gom_ten_min_perm_list$gom_dual)
 
@@ -912,6 +911,7 @@ for_heatmap_lat_lon_trips_vessels_only <-
 
 #### assuming data is dataframe with variables LATITUDE, LONGITUDE, and trips ####
 
+# do by zone from here
 df_join_grid <-
   function(my_df) {
     my_df |>
@@ -954,6 +954,17 @@ toc()
 
 # class(effort_cropped)
 
+# print_df_names(effort_vsl_cropped)
+## count trip ids and vessels by statZone ----
+effort_vsl_cropped_cnt_stat_zone <-
+  effort_vsl_cropped |>
+  # sf::st_drop_geometry() |>
+  group_by(StatZone) |>
+  mutate(vsl_cnt_stat_zone = n_distinct(VESSEL_OFFICIAL_NBR),
+         trip_id_cnt_stat_zone = n_distinct(TRIP_ID)) |>
+  ungroup()
+
+
 ## count trip ids and vessels by grid cell ----
 effort_vsl_cropped_cnt2 <-
   effort_vsl_cropped |>
@@ -983,9 +994,15 @@ effort_cropped_short_cnt_rule3 <-
   effort_vsl_cropped_cnt2 |>
   filter(vsl_cnt > 2)
 
+# check if there are xzzones with fewer than 3 vessels
+effort_vsl_cropped_cnt_stat_zone |>
+  filter(vsl_cnt_stat_zone < 2)
+# 0
+
 dim(effort_cropped_short_cnt_rule3)
 # [1] 31981     9
 
+## remove extra columns ----
 print_df_names(effort_cropped_short_cnt_rule3)
 
 effort_cropped_short_cnt_rule3_short <-
@@ -995,6 +1012,11 @@ effort_cropped_short_cnt_rule3_short <-
 dim(effort_cropped_short_cnt_rule3_short)
 # [1] 31981     5
 
+effort_vsl_cropped_cnt_stat_zone_short <-
+  effort_vsl_cropped_cnt_stat_zone |>
+  select(-c(LATITUDE, LONGITUDE, TRIP_ID, VESSEL_OFFICIAL_NBR))
+
+## join with min grid ----
 heat.plt <-
   effort_cropped_short_cnt_rule3_short |>
   # have to use data.frame, to avoid
@@ -1008,32 +1030,44 @@ dim(heat.plt)
 # [1] 33883     6 (rule3)
 # [1] 31981     6
 
+# heat.plt_zone <-
+  effort_vsl_cropped_cnt_stat_zone_short |>
+    glimpse()
+  # have to use data.frame, to avoid
+  # Error: y should not have class sf; for spatial joins, use st_join
+  # inner_join(data.frame(grid))
+
+# View(heat.plt_zone)
+
 ## make a plot ----
 make_map_trips <-
   function(map_trip_base_data,
            shape_data,
-           total_trips_title) {
+           total_trips_title,
+           trip_cnt_name,
+           caption_text = "Heat map of SEFHIER trips (5 min. resolution).") {
+
+    max_num <- max(map_trip_base_data[[trip_cnt_name]])
     map_trips <-
       ggplot() +
       geom_sf(data = map_trip_base_data,
               aes(geometry = x,
-                  fill = trip_id_cnt),
+              # aes(geometry = x,
+                  fill = !!sym(trip_cnt_name)),
               colour = NA) +
       geom_sf(data = shape_data, fill = NA) +
-      # geom_sf_text(data = GOMsf,
-      #              aes(geometry = geometry,
-      #                  label = StatZone),
-      #              size = 3.5) +
+      geom_sf_text(data = shape_data,
+                   aes(geometry = geometry,
+                       label = StatZone),
+                   size = 3.5) +
       labs(
         x = "",
         y = "",
         fill = "",
-        caption = "Heat map of SEFHIER trips (5 min. resolution)."
+        caption = caption_text
       ) +
       theme_bw() +
       scale_fill_gradient2(
-        # name = "total trips",
-        # name = "total trips (if more than 3)",
         name = total_trips_title,
         labels = scales::comma,
         low = "red",
@@ -1041,14 +1075,14 @@ make_map_trips <-
         high = "blue",
         # trans = "log2",
         trans = "log1p",
-        limits = c(1, max(heat.plt$trip_id_cnt))
+        limits = c(1, max_num)
         # ,
         # oob = scales::oob_keep
       ) +
       theme(
         legend.position = "top",
         legend.justification = "left",
-        legend.key.width = unit(1.3, "npc"),
+        legend.key.width = unit(0.9, "npc"),
         # legend.key.width = unit(3, "cm"),
         plot.caption = element_text(hjust = 0)
       ) +
@@ -1057,12 +1091,15 @@ make_map_trips <-
     return(map_trips)
   }
 
-map_trips_rule_3 <-
-  make_map_trips(heat.plt,
-           st_union_GOMsf,
-           "total trips (if more than 3)")
+# max_num3 <- max(heat.plt$trip_id_cnt)
+# map_trips_rule_3 <-
+#   make_map_trips(heat.plt,
+#            st_union_GOMsf,
+#            "total trips",
+#            max_num = max_num3,
+#            caption_text = "Heat map of SEFHIER trips (5 min. resolution). 2022. GoM permitted vessels. Only squares with more than 3 reporting vessels are shown. ")
 
-map_trips_rule_3
+# map_trips_rule_3
 
 # map_trips_no_rule_3 <-
 #   make_map_trips(heat.plt_no_rule3_all_dots,
@@ -1070,43 +1107,27 @@ map_trips_rule_3
 #            "total trips")
 
 
-map_trips <-
-  # rule of 3
-  # map_trips_base +
-  # all points
-  map_trips_no_3_base +
-  # use GOMsf to show StatZones
-  geom_sf(data = st_union_GOMsf, fill = NA) +
-  # geom_sf_text(data = GOMsf,
-  #              aes(geometry = geometry,
-  #                  label = StatZone),
-  #              size = 3.5) +
-  labs(
-    x = "",
-    y = "",
-    fill = "",
-    caption = "Heat map of SEFHIER trips (5 min. resolution)."
-  ) +
-  theme_bw() +
-  scale_fill_gradient2(
-    # name = "total trips",
-    name = "total trips (if more than 3)",
-    labels = scales::comma,
-    low = "red", mid = "purple", high = "blue",
-    # trans = "log2",
-    trans = "log1p",
-    limits = c(1, max(heat.plt$trip_id_cnt))
-    # ,
-    # oob = scales::oob_keep
-  ) +
-  theme(
-    legend.position = "top",
-    legend.justification = "left",
-    legend.key.width = unit(1, "npc"),
-    # legend.key.width = unit(3, "cm"),
-    plot.caption = element_text(hjust = 0)
-  ) +
-  guides(fill = guide_colourbar(title.position = "top"))
+map_trips_stat_zone <-
+  make_map_trips(heat.plt_zone,
+           GOMsf,
+           "total trips",
+           "trip_id_cnt_stat_zone",
+           caption_text = "Heat map of SEFHIER trips. 2022. GoM permitted vessels. Only areas with more than 3 reporting vessels are shown. ")
 
-map_trips
+map_trips_stat_zone
 
+glimpse(heat.plt_zone)
+# [1] "geometry, cell_id, StatZone, vsl_cnt_stat_zone, trip_id_cnt_stat_zone, x"
+
+heat.plt_zone_zones_only_df <-
+  heat.plt_zone |>
+  sf::st_drop_geometry() |>
+  select(-cell_id) |>
+  as.data.frame()
+
+ggplot() +
+  geom_sf(data = heat.plt_zone_zones_only_df,
+          aes(geometry = x,
+              # aes(geometry = x,
+              fill = trip_id_cnt_stat_zone,
+          colour = "blue"))
