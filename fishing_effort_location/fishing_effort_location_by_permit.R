4dc716e4fa9672104ee7d0d7360324a6a91a574c
AShipunova1 <anna.shipunova@noaa.gov>
2023-09-16 10:39:19 -0400

    vsl count by zone, works

--- fishing_effort_location/fishing_effort_location_by_permit.R
+++ fishing_effort_location/fishing_effort_location_by_permit.R
@@ -197,7 +197,7 @@ safis_efforts_extended_2022_short_good_sf_crop_big_df <-
 dim(safis_efforts_extended_2022_short_good_sf_crop_big_df)
 # [1] 95720     17
 
-# use metriks only vessels not in SRHS ----
+# use metrics only vessels not in SRHS ----
 source(r"(~\R_code_github\get_data_from_fhier\metric_tracking_no_srhs.R)")
 # fhier_reports_metrics_tracking_not_srhs_ids
 
@@ -911,7 +911,7 @@ for_heatmap_lat_lon_trips_vessels_only <-
 
 #### assuming data is dataframe with variables LATITUDE, LONGITUDE, and trips ####
 
-# do by zone from here
+## by n min grid ----
 df_join_grid <-
   function(my_df) {
     my_df |>
@@ -955,15 +955,6 @@ toc()
 # class(effort_cropped)
 
 # print_df_names(effort_vsl_cropped)
-## count trip ids and vessels by statZone ----
-effort_vsl_cropped_cnt_stat_zone <-
-  effort_vsl_cropped |>
-  # sf::st_drop_geometry() |>
-  group_by(StatZone) |>
-  mutate(vsl_cnt_stat_zone = n_distinct(VESSEL_OFFICIAL_NBR),
-         trip_id_cnt_stat_zone = n_distinct(TRIP_ID)) |>
-  ungroup()
-
 
 ## count trip ids and vessels by grid cell ----
 effort_vsl_cropped_cnt2 <-
@@ -994,11 +985,6 @@ effort_cropped_short_cnt_rule3 <-
   effort_vsl_cropped_cnt2 |>
   filter(vsl_cnt > 2)
 
-# check if there are xzzones with fewer than 3 vessels
-effort_vsl_cropped_cnt_stat_zone |>
-  filter(vsl_cnt_stat_zone < 2)
-# 0
-
 dim(effort_cropped_short_cnt_rule3)
 # [1] 31981     9
 
@@ -1011,10 +997,6 @@ effort_cropped_short_cnt_rule3_short <-
 
 dim(effort_cropped_short_cnt_rule3_short)
 # [1] 31981     5
-
-effort_vsl_cropped_cnt_stat_zone_short <-
-  effort_vsl_cropped_cnt_stat_zone |>
-  select(-c(LATITUDE, LONGITUDE, TRIP_ID, VESSEL_OFFICIAL_NBR))
 
 ## join with min grid ----
 heat.plt <-
@@ -1030,15 +1012,6 @@ dim(heat.plt)
 # [1] 33883     6 (rule3)
 # [1] 31981     6
 
-# heat.plt_zone <-
-  effort_vsl_cropped_cnt_stat_zone_short |>
-    glimpse()
-  # have to use data.frame, to avoid
-  # Error: y should not have class sf; for spatial joins, use st_join
-  # inner_join(data.frame(grid))
-
-# View(heat.plt_zone)
-
 ## make a plot ----
 make_map_trips <-
   function(map_trip_base_data,
@@ -1052,7 +1025,6 @@ make_map_trips <-
       ggplot() +
       geom_sf(data = map_trip_base_data,
               aes(geometry = x,
-              # aes(geometry = x,
                   fill = !!sym(trip_cnt_name)),
               colour = NA) +
       geom_sf(data = shape_data, fill = NA) +
@@ -1091,43 +1063,204 @@ make_map_trips <-
     return(map_trips)
   }
 
-# max_num3 <- max(heat.plt$trip_id_cnt)
-# map_trips_rule_3 <-
-#   make_map_trips(heat.plt,
-#            st_union_GOMsf,
-#            "total trips",
-#            max_num = max_num3,
-#            caption_text = "Heat map of SEFHIER trips (5 min. resolution). 2022. GoM permitted vessels. Only squares with more than 3 reporting vessels are shown. ")
+max_num3 <- max(heat.plt$trip_id_cnt)
+map_trips_rule_3 <-
+  make_map_trips(heat.plt,
+           st_union_GOMsf,
+           "total trips",
+           max_num = max_num3,
+           caption_text = "Heat map of SEFHIER trips (5 min. resolution). 2022. GoM permitted vessels. Only squares with more than 3 reporting vessels are shown. ")
 
 # map_trips_rule_3
 
-# map_trips_no_rule_3 <-
-#   make_map_trips(heat.plt_no_rule3_all_dots,
-#            st_union_GOMsf,
-#            "total trips")
+map_trips_no_rule_3 <-
+  make_map_trips(heat.plt_no_rule3_all_dots,
+           st_union_GOMsf,
+           "total trips")
+
+
+## by zone ----
+
+### join data and gomsf ----
+tic("for_heatmap_lat_lon_trips_vessels_only_join_gomsf")
+for_heatmap_lat_lon_trips_vessels_only_join_gomsf <-
+  for_heatmap_lat_lon_trips_vessels_only |>
+        st_as_sf(
+        coords = c("LONGITUDE", "LATITUDE"),
+        crs = st_crs(GOMsf)) |>
+        # ,
+        # remove = FALSE) %>%
+        st_join(GOMsf, join = st_nearest_feature)
+toc()
+# for_heatmap_lat_lon_trips_vessels_only_join_gomsf: 75.5 sec elapsed
+
+# all points for GOM permitted
+# mapview(for_heatmap_lat_lon_trips_vessels_only_join_gomsf)
+# [1] 41455     4
+
+tic("for_heatmap_lat_lon_trips_vessels_only_inters_gomsf")
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf <-
+  for_heatmap_lat_lon_trips_vessels_only |>
+        st_as_sf(
+        coords = c("LONGITUDE", "LATITUDE"),
+        crs = st_crs(GOMsf)) |>
+  st_join(GOMsf, left = FALSE) %>%
+  mutate(LONGITUDE = st_coordinates(.)[, 1],
+         LATITUDE = st_coordinates(.)[, 2])
+toc()
+# for_heatmap_lat_lon_trips_vessels_only_join_gomsf_1: 0.5 sec elapsed
+
+# only points in GOM
+dim(for_heatmap_lat_lon_trips_vessels_only_inters_gomsf)
+# [1] 35822     6
+# [1] "TRIP_ID, VESSEL_OFFICIAL_NBR, geometry, StatZone, LONGITUDE, LATITUDE"
+
+### count trip ids and vessels by statZone ----
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt <-
+  for_heatmap_lat_lon_trips_vessels_only_inters_gomsf |>
+  # sf::st_drop_geometry() |>
+  group_by(StatZone) |>
+  mutate(vsl_cnt_stat_zone = n_distinct(VESSEL_OFFICIAL_NBR),
+         trip_id_cnt_stat_zone = n_distinct(TRIP_ID)) |>
+  ungroup()
+
+View(for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt)
+
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt |>
+  filter(vsl_cnt_stat_zone < 3) |>
+  write_csv("state_zone_less_2_vsl.csv")
+
+# check
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt |>
+    sf::st_drop_geometry() |>
+    select(vsl_cnt_stat_zone) |>
+    distinct() |>
+    arrange(vsl_cnt_stat_zone)
+
+### remove extra columns ----
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short <-
+  for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt |>
+  select(-c(LATITUDE, LONGITUDE, TRIP_ID, VESSEL_OFFICIAL_NBR)) |>
+  distinct() |>
+  ungroup()
+
+# View(for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short)
+# [1] 35822     4
+
+# dots
+# for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short |>
+  # filter(vsl_cnt_stat_zone < 10) |>
+  # mapview()
+
+#### only vsl > 3 ----
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short_rule3 <-
+  for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short |>
+  filter(vsl_cnt_stat_zone > 2)
+
+### combine cnts with statzone geometry ----
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short_df <-
+  for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short |>
+  sf::st_drop_geometry() |>
+  distinct()
+# 21
+
+# class(for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short_df)
+
+gomshp_zone_cnt <-
+left_join(GOMsf,
+          for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short_df,
+         join_by("StatZone"))
+
+dim(gomshp_zone_cnt)
+# [1] 21  4
+
+shape_data <- gomshp_zone_cnt
+
+
+### plot by zone ----
+# ggplot(map.df,aes(x=long,y=lat,group=group))+
+#   geom_polygon(aes(fill=delta),color="grey20")+
+
+# glimpse(gomshp_zone_cnt)
+# mapview(for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short)
+
+plot_zone_cnt <-
+    function(shape_data,
+           total_cnt_title,
+           cnts_col_name,
+           caption_text) {
+      # browser()
+      max_num <-
+        max(shape_data[[cnts_col_name]])
+      # max(gomshp_zone_cnt$vsl_cnt_stat_zone)
+      # 148
+      min_num <-
+        min(shape_data[[cnts_col_name]])
+      # 2
+
+      map_vsl_zone <-
+        ggplot() +
+        geom_sf(data = shape_data,
+                aes(geometry = geometry,
+                    #                   fill = !!sym(trip_cnt_name)),
+                    fill = !!sym(cnts_col_name)),
+                colour = NA) +
+        geom_sf_text(data = shape_data,
+                     aes(geometry = geometry,
+                         label = StatZone),
+                     size = 3.5) +
+        labs(
+          x = "",
+          y = "",
+          fill = "",
+          caption = caption_text
+        ) +
+        theme_bw() +
+        scale_fill_gradient2(
+          name = total_cnt_title,
+          labels = scales::comma,
+          low = "red",
+          mid = "purple",
+          high = "blue",
+          # trans = "log2",
+          trans = "log1p",
+          limits = c(1, max_num)
+          # ,
+          # oob = scales::oob_keep
+        ) +
+        theme(
+          legend.position = "top",
+          legend.justification = "left",
+          legend.key.width = unit(0.9, "npc"),
+          # legend.key.width = unit(3, "cm"),
+          plot.caption = element_text(hjust = 0)
+        ) +
+        guides(fill = guide_colourbar(title.position = "top"))
+    }
+
+for_heatmap_lat_lon_trips_vessels_only_inters_gomsf_cnt_short |>
+  # dim()
+# [1] 35822     4
+
+  distinct() |>
+  dim()
+  # [1] 29230     4
+
+
+shape_data = gomshp_zone_cnt
+total_cnt_title = "total reporting vessels"
+cnts_col_name = "vsl_cnt_stat_zone"
+caption_text = "Heat map of SEFHIER trips. 2022. GoM permitted vessels."
 
 
 map_trips_stat_zone <-
-  make_map_trips(heat.plt_zone,
-           GOMsf,
-           "total trips",
-           "trip_id_cnt_stat_zone",
-           caption_text = "Heat map of SEFHIER trips. 2022. GoM permitted vessels. Only areas with more than 3 reporting vessels are shown. ")
+  plot_zone_cnt(
+    gomshp_zone_cnt,
+    "total reporting vessels",
+    "vsl_cnt_stat_zone",
+    caption_text = "Heat map of SEFHIER reporting vessels. 2022. GoM permitted only."
+  )
 
 map_trips_stat_zone
+#
 
-glimpse(heat.plt_zone)
-# [1] "geometry, cell_id, StatZone, vsl_cnt_stat_zone, trip_id_cnt_stat_zone, x"
-
-heat.plt_zone_zones_only_df <-
-  heat.plt_zone |>
-  sf::st_drop_geometry() |>
-  select(-cell_id) |>
-  as.data.frame()
-
-ggplot() +
-  geom_sf(data = heat.plt_zone_zones_only_df,
-          aes(geometry = x,
-              # aes(geometry = x,
-              fill = trip_id_cnt_stat_zone,
-          colour = "blue"))
