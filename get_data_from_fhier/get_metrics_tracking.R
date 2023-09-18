37328469b5474911b338b38b9f3b5e58bfefc1d6
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2023-09-18 09:52:36 -0400

    Update get_metrics_tracking.R
    rename csvs

--- get_data_from_fhier/get_metrics_tracking.R
+++ get_data_from_fhier/get_metrics_tracking.R
@@ -1,8 +1,8 @@
 ## fhier_reports_metrics_tracking ----
 
 fhier_reports_metrics_tracking_file_names <-
-  c("Detail_Report_12312021_12312022.csv",
-    "Detail_Report_12312022_12312023.csv")
+  c("Detail_Report_12312021_12312022__08_23_2023.csv",
+    "Detail_Report_12312022_12312023__08_23_2023")
 
 common_dir <-
   r"(~\R_files_local\my_inputs\from_Fhier\Detail Report - via Valid and Renewable Permits Filter (SERO_NEW Source))"
@@ -30,16 +30,16 @@ fhier_reports_metrics_tracking_list <-
 
 # check how many in diff years ----
 setdiff(fhier_reports_metrics_tracking_list[[1]]$vessel_official_number,
-         fhier_reports_metrics_tracking_list[[2]]$vessel_official_number) |> 
+         fhier_reports_metrics_tracking_list[[2]]$vessel_official_number) |>
   length()
 # [1] 669
 
 setdiff(fhier_reports_metrics_tracking_list[[2]]$vessel_official_number,
-         fhier_reports_metrics_tracking_list[[1]]$vessel_official_number) |> 
+         fhier_reports_metrics_tracking_list[[1]]$vessel_official_number) |>
   length()
 # [1] 493
 
 intersect(fhier_reports_metrics_tracking_list[[1]]$vessel_official_number,
-         fhier_reports_metrics_tracking_list[[2]]$vessel_official_number) |> 
+         fhier_reports_metrics_tracking_list[[2]]$vessel_official_number) |>
   length()
 # 2965
