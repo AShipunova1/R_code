02cb013164332764bfe4d2e9550215b5cbb9f7a7
AShipunova1 <anna.shipunova@noaa.gov>
2023-09-20 11:11:46 -0400

    use get_data module

--- fishing_effort_location/fishing_effort_location_db.R
+++ fishing_effort_location/fishing_effort_location_db.R
@@ -1,50 +1,12 @@
+# setup ----
+source("~/R_code_github/useful_functions_module.r")
+my_paths <- set_work_dir()
+current_project_dir_path <- get_current_file_directory()
+
 # get data ----
-get_trip_type_data_from_db <- function() {
-  # browser()
-  con = dbConnect(
-    dbDriver("Oracle"),
-    username = keyring::key_list("SECPR")[1, 2],
-    password = keyring::key_get("SECPR", keyring::key_list("SECPR")[1, 2]),
-    dbname = "SECPR"
-  )
+# "C:\Users\anna.shipunova\Documents\R_code_github\get_db_data\get_db_data.R"
+source(file.path(my_paths$git_r, r"(get_db_data\get_db_data.R)"))
 
-  request_query <-
-    paste0(
-    "SELECT DISTINCT
-    trip_id,
-    vessel_official_nbr,
-    trip_start_date,
-    trip_end_date,
-    trip_type_name,
-    latitude,
-    longitude
-FROM
-  srh.mv_safis_trip_download@secapxdv_dblk.sfsc.noaa.gov
-WHERE
-  trip_type in ('H', 'A')
-  AND trip_de >= TO_DATE('01-JAN-22', 'dd-mon-yy')
-  AND TRIP_START_DATE >= TO_DATE('01-JAN-22', 'dd-mon-yy')
-  AND TRIP_END_DATE <= TO_DATE('31-DEC-22', 'dd-mon-yy')
-  AND sero_vessel_permit IS NOT NULL"
-)
-
-  # nchar(request_query)
-  # [1] 523009
-
-  # cat(request_query)
-
-  db_data = dbGetQuery(con,
-                       request_query)
-
-  dbDisconnect(con)
-
-  return(db_data)
-}
-
-tic("get_trip_type_data_from_db")
-fishing_data_from_db <- get_trip_type_data_from_db()
-toc()
-# get_trip_type_data_from_db: 12.8 sec elapsed
 
 # prepare data ----
-fishing_data_from_db
+# fishing_data_from_db
