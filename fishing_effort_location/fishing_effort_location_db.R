b482eb57220aa90e192e2ec984c0682a5a340789
AShipunova1 <anna.shipunova@noaa.gov>
2023-09-20 13:21:05 -0400

    get data

--- fishing_effort_location/fishing_effort_location_db.R
+++ fishing_effort_location/fishing_effort_location_db.R
@@ -7,6 +7,13 @@ current_project_dir_path <- get_current_file_directory()
 # "C:\Users\anna.shipunova\Documents\R_code_github\get_db_data\get_db_data.R"
 source(file.path(my_paths$git_r, r"(get_db_data\get_db_data.R)"))
 
+tic("run_all_get_db_data()")
+all_get_db_data_result_l <- run_all_get_db_data()
+toc()
+# run_all_get_db_data(): 2.27 sec elapsed (from csv)
 
 # prepare data ----
-# fishing_data_from_db
+# print_df_names(all_get_db_data_result_l)
+# mv_sero_fh_permits_his, trips_info, trip_coord_info, trip_neg_2022, trips_notifications_2022, vessels_permits, dates_2022, compl_err_db_data
+
+# all_get_db_data_result_l[[df_name]]
