109293d76b5fe26699d829fd3d7b63d63f405ad5
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-12 11:52:44 -0400

    Update metric_tracking_no_srhs.R
    add comments

--- get_data_from_fhier/metric_tracking_no_srhs.R
+++ get_data_from_fhier/metric_tracking_no_srhs.R
@@ -8,17 +8,21 @@ source(file.path(my_paths$git_r,
 
 ## exclude srhs vessels from metric traking ----
 fhier_reports_metrics_tracking_not_srhs_ids <-
-  map_df(
+  purrr::map_df(
     fhier_reports_metrics_tracking_list,
+    # for each df
     ~ .x |>
-      filter(!vessel_official_number %in% srhs_vessels_2022_info$uscg__)
+      # exclude SRHS vessels
+      dplyr::filter(!vessel_official_number %in% srhs_vessels_2022_info$uscg__)
   ) |>
-  select(vessel_official_number) |>
+  # keep only the vessel_official_numbers, remove all other columns
+  dplyr::select(vessel_official_number) |>
   distinct()
 
 dim(fhier_reports_metrics_tracking_not_srhs_ids)
 # [1] 2981    1
-# browser()
+
+# the same, but result kept in a list
 fhier_reports_metrics_tracking_not_srhs_ids_list <-
   map(
     fhier_reports_metrics_tracking_list,
@@ -32,13 +36,13 @@ fhier_reports_metrics_tracking_not_srhs_ids_list <-
 map(fhier_reports_metrics_tracking_list, dim)
 # [[1]]
 # [1] 3634   13
-# 
+#
 # [[2]]
 # [1] 3460   13
 
 map(fhier_reports_metrics_tracking_not_srhs_ids_list, dim)
 # [[1]]
 # [1] 3571    1
-# 
+#
 # [[2]]
 # [1] 3399    1
