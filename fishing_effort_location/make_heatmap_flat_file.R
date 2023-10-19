5d470bec26f49e69f4d7cef01f69af3783e04536
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-19 14:07:01 -0400

    Update make_heatmap_flat_file.R
    add files

--- fishing_effort_location/make_heatmap_flat_file.R
+++ fishing_effort_location/make_heatmap_flat_file.R
@@ -1,8 +1,50 @@
 # heatmap_make_flat_file.R
 source("~/R_code_github/useful_functions_module.r")
+my_paths <- set_work_dir()
 
 dir_to_comb <- "~/R_code_github/fishing_effort_location"
 
 flat_file_name <-
   file.path(dir_to_comb, "flat_file_heatmap.R")
-sink(flat_file_name, append = TRUE)
+
+
+write_to_1_flat_file(flat_file_name,
+                     "~/R_code_github/useful_functions_module.r")
+
+cat(my_paths <- set_work_dir())
+
+write_to_1_flat_file(
+  flat_file_name,
+  file.path(
+    my_paths$git_r,
+    r"(fishing_effort_location\fishing_effort_location_by_permit.R)"
+  )
+)
+
+write_to_1_flat_file(
+  flat_file_name,
+  file.path(
+    my_paths$git_r,
+    r"(fishing_effort_location\fishing_effort_location_by_permit.R)"
+  )
+)
+
+write_to_1_flat_file(
+  flat_file_name,
+  file.path(
+    my_paths$git_r,
+    r"(fishing_effort_location\prepare_gom_heatmap_func.R)"
+  )
+)
+
+write_to_1_flat_file(
+  flat_file_name,
+  file.path(
+    my_paths$git_r,
+    r"(fishing_effort_location\fishing_effort_location_heatmap.R)"
+  )
+)
+
+
+
+sink()
