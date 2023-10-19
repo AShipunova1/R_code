655ce89f7d2a0e63a92f3e1e8303fabbb0d56c18
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-19 15:17:55 -0400

    Update vessel_permit_corrected_list.R
    compare data only

--- vessel_permit_list/vessel_permit_corrected_list.R
+++ vessel_permit_list/vessel_permit_corrected_list.R
@@ -1,8 +1,14 @@
+source("~/R_code_github/useful_functions_module.r")
+my_paths <- set_work_dir()
+
 # read Jeannette's file ----
 library(readxl)  # reading in .xlsx
 # the file is from Jeannette Oct 17 2023
 v_list_file_name <-
-  r"(C:\Users\anna.shipunova\Documents\R_files_local\vessels_permits\SA.Permitted.Vessels.Among_revised.Lists.xlsx)"
+  file.path(
+    my_paths$inputs,
+    r"(vessels_permits\SA.Permitted.Vessels.Among_revised.Lists.xlsx)"
+  )
 
 # Create a sequence of sheet numbers from 1 to 4. These sheet numbers will be used to specify which sheets to read from the Excel file.
 sheets <- seq(1:4)
@@ -12,7 +18,7 @@ sheets <- seq(1:4)
 all_sheets_l <-
   purrr::map(sheets,
              function(sheet_num) {
-               read_excel(
+               readxl::read_excel(
                  # The name of the Excel file.
                  v_list_file_name,
                  # Specify the sheet number to read.
@@ -21,14 +27,17 @@ all_sheets_l <-
                  guess_max = 21474836,
                  # Read all columns as text to preserve data integrity.
                  col_types = "text",
-                 # Use "universal" name repair for column names.
-                 .name_repair = "universal"
+                 # Use my function for name repair for column names.
+                 .name_repair = fix_names,
+                 # Do not use the first row as column names
+                 col_names = FALSE
                )
              })
 
-# map(all_sheets_l, dim)
+# Check what's inside ----
+map(all_sheets_l, dim)
 # [[1]]
-# [1] 2216    1
+# [1] 2215    1
 #
 # [[2]]
 # [1] 55  2
@@ -37,11 +46,90 @@ all_sheets_l <-
 # [1] 126   1
 #
 # [[4]]
-# [1] 131   4
+# [1] 130   2
 
-# names(all_sheets_l[[4]])
-# names(all_sheets_l[[1]])
+map(all_sheets_l, glimpse)
 
+# Clean up dfs ----
+
+## add sheet names to the df list ----
+# Extract the sheet names from an Excel file specified by 'v_list_file_name'.
+# - 'excel_sheets' function is used to get the sheet names.
+# - 'str_replace_all' replaces any periods with underscores in sheet names.
+# - 'tolower' converts all sheet names to lowercase.
+all_sheets_l_names <-
+  readxl::excel_sheets(v_list_file_name) |>
+  str_replace_all("\\.", "_") |>
+  tolower()
+
+# Rename the elements of the 'all_sheets_l' object to match the modified sheet names.
+# - 'names' function is used to access and modify the names of the object.
+# - 'all_sheets_l_names' is a character vector containing the modified sheet names.
+# - Use the sheet names for all dataframes in the 'all_sheets_l'  list.
+#   The names of 'all_sheets_l' are updated to match the corresponding sheet names.
+names(all_sheets_l) <- all_sheets_l_names[1:length(all_sheets_l)]
+
+## Remove old column names ----
+# like "Vessel id"
+# Define a function called 'remove_first_row_if_vessel'.
+# This function takes a data frame 'my_df' as its input parameter.
+remove_first_row_if_vessel <- function(my_df) {
+
+  # Check if the first cell (1,1) of 'my_df' contains the word "vessel"
+  # (case-insensitive search).
+  if (grepl("vessel", my_df[1, 1], ignore.case = TRUE)) {
+    # If the word "vessel" is found, remove the first row (-1) from 'my_df'.
+    my_df <- my_df[-1,]
+  }
+  # Return the modified 'my_df'. If no changes were made, the original
+  # 'my_df' is returned unchanged.
+  return(my_df)
+}
+
+# Use the 'map' function to apply the 'remove_first_row_if_vessel' function
+# to each data frame in the 'all_sheets_l' list.
+# The result is a modified 'all_sheets_l' list with the undesired header rows removed from each data frame.
+
+all_sheets_l <-
+  purrr::map(all_sheets_l, remove_first_row_if_vessel)
+
+# View(all_sheets_l)
+
+## add names to the columns ----
+
+### 1) ----
+# Set the names of the column in the first data frame in the list to "vessel_official_number".
+names(all_sheets_l[[1]]) <- c("vessel_official_number")
+
+### 2) ----
+# Set the names of the column in the second data frame in the list.
+names(all_sheets_l[[2]]) <- c("vessel_official_number",
+                              "comments")
+
+### 3) ----
+# Set the names of the column in the third data frame in the list.
+names(all_sheets_l[[3]]) <- c("vessel_official_number")
+
+### 4) ----
+# Set the temporary names of the column in the forth data frame in the list.
+names(all_sheets_l[[4]]) <- c("num",
+                              "vessel_official_number-comments")
+
+#### Split comments ----
+# Create a new data frame 'temp1' by splitting the 'vessel_official_number-comments'
+# column of the fourth data frame in 'all_sheets_l' using the 'separate_wider_delim'
+# function.
+temp1 <- all_sheets_l[[4]] |>
+  separate_wider_delim(
+    "vessel_official_number-comments",  # Column to split
+    delim = " - ",                     # Delimiter used for splitting
+    names = c("vessel_official_number", "comments"),  # Names of the new columns
+    too_few = "align_end"               # How to handle if fewer columns are created
+  )
+
+
+View(temp1)
+# vessels_22_sa ----
 # Create a new data frame 'vessels_22_sa' by combining data from two sheets.
 vessels_22_sa <-
 
@@ -61,163 +149,5 @@ vessels_22_sa <-
 dim(vessels_22_sa)
 # [1] 2321    1
 
-# compare with db_data ----
-## prepare_db_data ----
-use_df_names <- list("mv_sero_fh_permits_his",
-                     "vessels_permits")
-### add permit region groups ----
-
-# Create a list of data frames 'db_df_reg_l' by applying a function to each element of 'use_df_names'.
-db_df_reg_l <-
-
-  # 'use_df_names' is a character vector containing names of data frames.
-  use_df_names |>
-
-  # Use the 'map' function to apply a custom function to each data frame in 'all_get_db_data_result_l'.
-  map(function(use_df_name) {
-
-    # Inside the 'map' function, apply the 'separate_permits_into_3_groups' function
-    # to the corresponding data frame from 'all_get_db_data_result_l'.
-    separate_permits_into_3_groups(all_get_db_data_result_l[[use_df_name]],
-                                   permit_group_field_name = "TOP")
-  }) |>
-
-  # Set the names of the resulting list to match the names in 'use_df_names'.
-  rlang::set_names(use_df_names)
-
-# map(db_df_reg_l,
-#     print_df_names)
-
-### get 2022 sa_only ----
-# Create a list of data frames 'db_df_reg_2022_sa_only_l' by applying a function to each element of 'use_df_names'.
-db_df_reg_2022_sa_only_l <-
-
-  # 'use_df_names' is a character vector containing names of data frames.
-  use_df_names |>
-
-  # Use the 'map' function to apply a custom function to each data frame in 'db_df_reg_l'.
-  map(function(use_df_name) {
-
-    # Inside the 'map' function, access the data frame 'db_df_reg_l[[use_df_name]]'.
-    db_df_reg_l[[use_df_name]] |>
-
-      # Use the 'filter' function to filter rows based on specific conditions.
-      filter(
-        permit_sa_gom == "sa_only" &   # Filter rows with 'permit_sa_gom' equal to "sa_only"
-        EFFECTIVE_DATE >= '2022-01-01' &  # Filter rows with 'EFFECTIVE_DATE' on or after '2022-01-01'
-        END_DATE > '2022-12-31'          # Filter rows with 'END_DATE' after '2022-12-31'
-      )
-  }) |>
-
-  # Set the names of the resulting list to match the names in 'use_df_names'.
-  rlang::set_names(use_df_names)
-
-map(db_df_reg_2022_sa_only_l, dim)
-# $mv_sero_fh_permits_his
-# [1] 6189   23
-#
-# $vessels_permits
-# [1] 12381    52
-
-# compare Jeannettes's with mv_sero_fh_permits_his ----
-mv_sero_fh_permits_his_u_v_ids_all <-
-  unique(all_get_db_data_result_l$mv_sero_fh_permits_his$VESSEL_ID)
-
-mv_sero_fh_permits_his_u_v_ids_22_sa <-
-  unique(db_df_reg_2022_sa_only_l$mv_sero_fh_permits_his$VESSEL_ID)
-
-## intersection ----
-mv_sero_fh_permits_his_intersect <-
-  mv_sero_fh_permits_his_u_v_ids_22_sa |>
-  intersect(vessels_22_sa$permit_vessel_id)
-
-length(mv_sero_fh_permits_his_intersect)
-# [1] 1339
-
-## in J only ----
-### all ----
-not_in_mv_sero_fh_permits_his_all <-
-  setdiff(vessels_22_sa$permit_vessel_id,
-          mv_sero_fh_permits_his_u_v_ids_all)
-length(not_in_mv_sero_fh_permits_his_all)
-# 18
-
-# cat(not_in_mv_sero_fh_permits_his_all,
-#     sep = ", ")
-
-### 2022 SA ----
-not_in_mv_sero_fh_permits_his <-
-  setdiff(vessels_22_sa$permit_vessel_id,
-          mv_sero_fh_permits_his_u_v_ids_22_sa)
-length(not_in_mv_sero_fh_permits_his)
-# 982
-
-## in db only ----
-### all ----
-mv_sero_fh_permits_his_all__not_in_j <-
-  setdiff(mv_sero_fh_permits_his_u_v_ids_all,
-          vessels_22_sa$permit_vessel_id)
-length(mv_sero_fh_permits_his_all__not_in_j)
-# 11749
-
-### 2022 SA ----
-mv_sero_fh_permits_his__not_in_j <-
-  setdiff(mv_sero_fh_permits_his_u_v_ids_22_sa,
-          vessels_22_sa$permit_vessel_id)
-length(mv_sero_fh_permits_his__not_in_j)
-# 1357
-head(mv_sero_fh_permits_his__not_in_j)
-
-# compare Jeannettes's with vessels_permits ----
-vessels_permits_u_v_ids_all <-
-  unique(all_get_db_data_result_l$vessels_permits$PERMIT_VESSEL_ID)
-
-vessels_permits_u_v_ids_22_sa <-
-  unique(db_df_reg_2022_sa_only_l$vessels_permits$PERMIT_VESSEL_ID)
-
-## intersection ----
-### all ----
-vessels_permits_intersect_all <-
-  vessels_permits_u_v_ids_all |>
-  intersect(vessels_22_sa$permit_vessel_id)
-
-length(vessels_permits_intersect_all)
-# 2301
-
-### 2022 sa ----
-vessels_permits_intersect <-
-  vessels_permits_u_v_ids_22_sa |>
-  intersect(vessels_22_sa$permit_vessel_id)
-
-length(vessels_permits_intersect)
-# [1] 1339
-
-## in J only ----
-### all ----
-not_in_vessels_permits_all <-
-  setdiff(vessels_22_sa$permit_vessel_id,
-          vessels_permits_u_v_ids_all)
-length(not_in_vessels_permits_all)
-# 20
-
-### 2022 SA ----
-not_in_vessels_permits <-
-  setdiff(vessels_22_sa$permit_vessel_id,
-          vessels_permits_u_v_ids_22_sa)
-length(not_in_vessels_permits)
-# 982
-
-## in db only ----
-### all ----
-vessels_permits_all__not_in_j <-
-  setdiff(vessels_permits_u_v_ids_all,
-          vessels_22_sa$permit_vessel_id)
-length(vessels_permits_all__not_in_j)
-# 4450
-
-### 2022 SA ----
-vessels_permits__not_in_j <-
-  setdiff(vessels_permits_u_v_ids_22_sa,
-          vessels_22_sa$permit_vessel_id)
-length(vessels_permits__not_in_j)
-# 1356
+# remove from FHIER results ----
+names(all_sheets_l[[2]])
