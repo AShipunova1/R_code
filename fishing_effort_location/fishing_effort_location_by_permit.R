f8ad090c885d9ce802f4064df65321abc5dc36c4
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-20 16:07:55 -0400

    Update fishing_effort_location_by_permit.R
    comments

--- fishing_effort_location/fishing_effort_location_by_permit.R
+++ fishing_effort_location/fishing_effort_location_by_permit.R
@@ -132,57 +132,91 @@ with_st_difference <- function(points_sf, polygons_sf) {
 
 ## functions for ten_min ----
 
+# Define an R function 'get_degree' to extract degrees from geographical coordinates.
 get_degree <- function(gis_coord) {
+  # Take the absolute value and round down to the nearest integer to get degrees.
   floor(abs(gis_coord))
 }
 
+# These R functions are used for converting between different formats of geographical coordinates.
+# Define an R function 'get_minute' to extract minutes from geographical coordinates.
 get_minute <- function(gis_coord) {
+  # Take the absolute value, find the fractional part, and convert to minutes.
   dd <- abs(gis_coord) %% 1
   minute <- floor(dd * 60)
 }
 
+# Define an R function 'convert_to_ten_min' to convert minutes to the nearest multiple of ten.
 convert_to_ten_min <- function(minute) {
+  # Divide minutes by 10, round down to the nearest multiple of 10.
   floor(minute/10) * 10
 }
 
+# Define an R function 'convert_to_decimal_degree' to convert degrees and minutes to decimal degrees.
 convert_to_decimal_degree <- function(dm_num) {
+  # Extract the degrees part (first two positions) and convert it to a numeric value.
   degree <- as.numeric(substr(as.character(dm_num), 1, 2))
+
+  # Extract the minutes part (characters in positions 3 and 4), divide by 60, and convert to a numeric value.
   dd <- as.numeric(substr(as.character(dm_num), 3, 4)) / 60
+
+  # Calculate the decimal degrees by adding degrees and minutes.
   degree + dd
 }
 
+# Define an R function 'get_lat_ten_min' for rounding latitude coordinates to the nearest ten minutes and converting to decimal degrees.
 get_lat_ten_min <- function(gis_lat) {
+  # Get the degrees component from latitude
   deg <- get_degree(gis_lat)
+
+  # Get the minutes component from latitude
   minute <- get_minute(gis_lat)
+
+  # Round minutes to the nearest ten minutes
   ten_min_num <- convert_to_ten_min(minute)
-  dm_num <-
-    paste(deg, stringi::stri_pad_left(ten_min_num, 2, 0), sep = "")
+
+  # Create a string representing degrees and ten minutes (e.g., "DDMM")
+  dm_num <- paste(deg, stringi::stri_pad_left(ten_min_num, 2, 0), sep = "")
+
+  # Convert the string representation to decimal degrees
   convert_to_decimal_degree(dm_num)
 }
 
+# All lon should be negative, bc we know it is all in America
+# Define an R function 'get_ten_min_coords' for rounding coordinates to the nearest ten minutes.
 get_ten_min_coords <- function(my_df) {
+  # Create a new data frame 'ten_min_df' based on the input data frame 'my_df'
   ten_min_df <-
     my_df |>
-    mutate(
+    dplyr::mutate(
+      # Create a new column 'ten_min_lat' by calling 'get_lat_ten_min' on the 'LATITUDE' column
       ten_min_lat = get_lat_ten_min(as.numeric(my_df$LATITUDE)),
-      # All lon should be negative, bc we know it is all in America
-      ten_min_lon =
-        -1 * abs(get_lat_ten_min(as.numeric(my_df$LONGITUDE)))
+
+      # Create a new column 'ten_min_lon' by rounding and negating 'LONGITUDE' to ensure it's negative
+      ten_min_lon = -1 * abs(get_lat_ten_min(as.numeric(my_df$LONGITUDE)))
     )
+
+  # Return the resulting 'ten_min_df'
   return(ten_min_df)
-  # distinct(ten_min_df)
 }
 
 ## Data from db as in FHIER ----
 # print_df_names(all_logbooks_db_data_2022_short_p_region)
 # [1] 94471    73
 
+# Create a new data frame 'safis_efforts_extended_2022_short_good_all_coords' by processing the data.
+
 safis_efforts_extended_2022_short_good_all_coords <-
   all_logbooks_db_data_2022_short_p_region |>
+
+  # Convert 'LONGITUDE' and 'LATITUDE' columns to numeric data types
   dplyr::mutate(LONGITUDE = as.numeric(LONGITUDE),
                 LATITUDE = as.numeric(LATITUDE)) |>
-  # all LONG should be negative
+
+  # Ensure that all 'LONGITUDE' values are negative by taking the absolute value and negating them
   dplyr::mutate(LONGITUDE = -abs(LONGITUDE)) |>
+
+  # Keep only distinct rows in the data frame
   distinct()
 
 dim(safis_efforts_extended_2022_short_good_all_coords)
@@ -190,31 +224,41 @@ dim(safis_efforts_extended_2022_short_good_all_coords)
 
 ## From FHIER ----
 # View(safis_efforts_extended_2022_short)
-
-safis_efforts_extended_2022_short_good_all_coords_fhier <-
-  safis_efforts_extended_2022_short |>
-  dplyr::mutate(LONGITUDE = as.numeric(LONGITUDE),
-                LATITUDE = as.numeric(LATITUDE)) |>
-  # all LONG should be negative
-  dplyr::mutate(LONGITUDE = -abs(LONGITUDE)) |>
-  distinct()
-
-dim(safis_efforts_extended_2022_short_good_all_coords)
+#
+# safis_efforts_extended_2022_short_good_all_coords_fhier <-
+#   # Convert 'LONGITUDE' and 'LATITUDE' columns to numeric data types
+#   dplyr::mutate(LONGITUDE = as.numeric(LONGITUDE),
+#                 LATITUDE = as.numeric(LATITUDE)) |>
+#
+#   # Ensure that all 'LONGITUDE' values are negative by taking the absolute value and negating them
+#   dplyr::mutate(LONGITUDE = -abs(LONGITUDE)) |>
+#
+#   # Keep only distinct rows in the data frame
+#   distinct()
+#
+# dim(safis_efforts_extended_2022_short_good_all_coords)
 # [1] 97970    17
 
+# Keep only full sets of coordinates ----
+# Create a new data frame 'safis_efforts_extended_2022_short_good' by filtering and keeping only distinct rows.
 safis_efforts_extended_2022_short_good <-
   safis_efforts_extended_2022_short_good_all_coords |>
-  # keep only full sets of coordinates
+
+  # Use the filter function to keep rows where either 'LONGITUDE' or 'LATITUDE' is not NA
   dplyr::filter(!is.na(LONGITUDE) | !is.na(LATITUDE)) |>
+
+  # Keep only distinct rows in the data frame
   distinct()
 
 dim(safis_efforts_extended_2022_short)
-# [1] 97970    17
+# [1] 97970    17 FHIER
+# [1] 97943    16 db
 
 dim(safis_efforts_extended_2022_short_good)
 # [1] 97547    17
+# [1] 93450    73 db
 
-### convert to sf from FHIER ----
+### convert to sf ----
 safis_efforts_extended_2022_short_good_sf <-
   my_to_sf(safis_efforts_extended_2022_short_good)
 
@@ -224,24 +268,31 @@ safis_efforts_extended_2022_short_good_sf <-
 # Michelle: I think we need to allow trips that occur anywhere in the GOM, with the eastern lat border being like a big line down the Atlantic Ocean at Bermuda. Does that make sense? Southern Border could be at Cuba. The Northern Border needs to extend up through Maine - since we require reporting no matter where they fish. Basically just a big box, regardless of Council jurisdiction.
 # Jessica: I like the big box without council jurisdiction and then I am going to assume we will just plot those trips for vessels with GOM permits?  This should show the Council how many GOM vessels also fish in other regions as well as where they are fishing in the Gulf.
 
+# Define a bounding box represented as a vector named 'big_bounding_box'.
 big_bounding_box <- c(
-   xmin = -97.79954,
-   ymin = 21.521757, #Cuba
-   xmax = -64.790337, #Bermuda
-   ymax = 49 #Canada
- )
+   xmin = -97.79954,  # Minimum longitude (western boundary)
+   ymin = 21.521757,  # Minimum latitude (southern boundary, Cuba)
+   xmax = -64.790337,  # Maximum longitude (eastern boundary, Bermuda)
+   ymax = 49  # Maximum latitude (northern boundary, Canada)
+)
 
 tic("safis_efforts_extended_2022_short_good_sf_crop_big")
+# Create a new spatial object 'safis_efforts_extended_2022_short_good_sf_crop_big'
+# by cropping 'safis_efforts_extended_2022_short_good_sf' to the 'big_bounding_box'.
+
 safis_efforts_extended_2022_short_good_sf_crop_big <-
-  st_crop(safis_efforts_extended_2022_short_good_sf,
-          big_bounding_box)
+  sf::st_crop(safis_efforts_extended_2022_short_good_sf,  # Spatial object to be cropped
+              big_bounding_box)  # Bounding box used for cropping
 toc()
 # safis_efforts_extended_2022_short_good_sf_crop_big: 0.89 sec elapsed
 
 dim(safis_efforts_extended_2022_short_good_sf_crop_big)
 # [1] 95720    18
+# [1] 91735    74
 
 # convert back to df ----
+# The sf::st_drop_geometry function is applied to the spatial object to remove its geometry, resulting in a data frame that contains only the non-geometric attributes or columns.
+
 safis_efforts_extended_2022_short_good_sf_crop_big_df <-
   safis_efforts_extended_2022_short_good_sf_crop_big |>
   sf::st_drop_geometry()
@@ -253,15 +304,22 @@ dim(safis_efforts_extended_2022_short_good_sf_crop_big_df)
 source(r"(~\R_code_github\get_data_from_fhier\metric_tracking_no_srhs.R)")
 # fhier_reports_metrics_tracking_not_srhs_ids
 
-# remove ids not in fhier_reports_metrics_tracking_not_srhs_ids
+## remove ids not in fhier_reports_metrics_tracking_not_srhs_ids ----
+# Create a new data frame 'safis_efforts_extended_2022_short_good_sf_crop_big_df_in_metricks'
+# by filtering 'safis_efforts_extended_2022_short_good_sf_crop_big_df' based on a condition.
+
 safis_efforts_extended_2022_short_good_sf_crop_big_df_in_metricks <-
   safis_efforts_extended_2022_short_good_sf_crop_big_df |>
-  filter(
+
+  # Use the 'filter' function to retain rows meeting a specific condition.
+  dplyr::filter(
+    # Keep rows where the 'VESSEL_OFFICIAL_NBR' values are present in the fhier_reports_metrics_tracking_not_srhs_ids
     VESSEL_OFFICIAL_NBR %in% fhier_reports_metrics_tracking_not_srhs_ids$vessel_official_number
   )
 
 dim(safis_efforts_extended_2022_short_good_sf_crop_big_df_in_metricks)
 # [1] 93581    17
+# [1] 90838    73
 
 
 ### check names ----
