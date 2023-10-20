7396f64fb25287d2bf0e7733e32124ec88b94ace
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-20 08:26:33 -0400

    Update useful_functions_module.r
    fix .x in fnc

--- useful_functions_module.r
+++ useful_functions_module.r
@@ -472,7 +472,7 @@ change_fields_arr_to_dates <-
     result_df <- my_df %>%
       dplyr::mutate(dplyr::across(
         all_of(field_names_arr),
-        ~ as.POSIXct(x, format = date_format)  # Apply 'as.POSIXct' to each column with the provided 'date_format'
+        ~ as.POSIXct(.x, format = date_format)  # Apply 'as.POSIXct' to each column with the provided 'date_format'
       ))
 
     # Return the data frame with the specified columns converted to dates
