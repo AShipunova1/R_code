348889aa10b70312d39f90e49f3f1c30fad197a2
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-23 09:44:34 -0400

    Update get_data.R
    comments

--- quantify_compliance/get_data.R
+++ quantify_compliance/get_data.R
@@ -1,5 +1,6 @@
 # this file is called from quantify_compliance.R
 
+# Load the 'tictoc' library for timing operations.
 library(tictoc)
 
 project_dir_name <- "FHIER Compliance"
@@ -22,8 +23,6 @@ get_data_from_FHIER_csvs <- function() {
   # browser()
   # unify headers, trim vesselofficialnumber, just in case
   csvs_clean1 <- clean_all_csvs(csv_contents)
-#  str(csvs_clean1)
-  # browser()
   compl_clean <- compliance_cleaning(csvs_clean1)
 
   return(compl_clean)
@@ -59,29 +58,40 @@ get_permit_data_from_PIMS_csv <- function() {
     clean_headers()
 
   # separate columns
-  active_permits_from_pims_temp2 <-
-    active_permits_from_pims_temp1 %>%
+# Use the 'separate_wider_delim' function to split the 'permit__' column in the 'active_permits_from_pims_temp1' dataframe
+# based on the delimiter "-", creating new columns 'permit_code' and 'permit_num'.
+# The 'too_many' argument is set to "merge," which means any excess columns generated during the split will be merged.
+active_permits_from_pims_temp2 <- active_permits_from_pims_temp1 %>%
     separate_wider_delim(permit__,
-                         "-",
+                         "-",                # Delimiter used for splitting
                          names = c("permit_code", "permit_num"),
                          too_many = "merge") %>%
+
+    # Use the 'separate_wider_regex' function to split the 'vessel_or_dealer' column in the resulting dataframe.
+    # This function uses regular expressions to define patterns for creating new columns.
+    # In this case, it defines patterns for 'vessel_official_number' and 'vessel_name.'
+    # The 'too_few' argument is set to "align_start," which aligns any missing columns at the start.
     separate_wider_regex(
       cols = vessel_or_dealer,
       patterns = c(
-        vessel_official_number = "[A-Za-z0-9]+",
-        " */* ",
-        vessel_name = "[A-Za-z0-9]+"
+        vessel_official_number = "[A-Za-z0-9]+",  # Regular expression for vessel official number (more than one alphanumeric character)
+        " */* ",                                  # Pattern for separating columns with slashes
+        vessel_name = "[A-Za-z0-9]+"              # Regular expression for vessel name (more than one alphanumeric character)
       ),
       too_few = "align_start"
     )
 
   # correct dates format
 
-  # get a list of field names ends with "_date"
-  ends_with_date_fields <-
-    grep("_date", names(active_permits_from_pims_temp2), value = TRUE)
+  # get a list of field names with "_date"
+  # Use the 'grep' function to find and extract column names from the 'active_permits_from_pims_temp2' dataframe
+  # that has "_date".
+  ends_with_date_fields <- grep("_date",             # Pattern to search for in column names
+                                names(active_permits_from_pims_temp2),  # Names of columns to search within
+                                value = TRUE)         # Return matching column names as values in the result.
 
-  # convert to date
+
+  # convert to the date format
   active_permits_from_pims <-
     change_fields_arr_to_dates(active_permits_from_pims_temp2,
                                ends_with_date_fields,
@@ -89,8 +99,10 @@ get_permit_data_from_PIMS_csv <- function() {
 
   # test
   active_permits_from_pims %>%
-    select(status_date) %>%
-    arrange(desc(status_date)) %>% unique() %>% head()
+    dplyr::select(status_date) %>%                 # Select 'status_date' column
+    dplyr::arrange(dplyr::desc(status_date)) %>%   # Arrange in descending order
+    distinct() %>%                               # Remove duplicate rows
+    head()                                       # Retrieve the first few rows
   # correct
   # str(active_permits_from_pims)
 
@@ -98,23 +110,24 @@ get_permit_data_from_PIMS_csv <- function() {
 }
 
 get_data_from_csv <- function() {
-
-# uncomment to run
-compl_clean <- get_data_from_FHIER_csvs()
-# View(compl_clean)
-dim(compl_clean)
-# 208893     21
-
-## get compliance error definitions from csvs ----
-err_desc <- get_compliance_error_definitions()
-
-## get permit data from PIMS csv ----
-
-active_permits_from_pims <- get_permit_data_from_PIMS_csv()
-
-compl_clean1 <- additional_clean_up(compl_clean)
-
-return(compl_clean1)
+  
+  
+  # uncomment to run
+  compl_clean <- get_data_from_FHIER_csvs()
+  # View(compl_clean)
+  dim(compl_clean)
+  # 208893     21
+  
+  ## get compliance error definitions from csvs ----
+  err_desc <- get_compliance_error_definitions()
+  
+  ## get permit data from PIMS csv ----
+  
+  active_permits_from_pims <- get_permit_data_from_PIMS_csv()
+  
+  compl_clean1 <- additional_clean_up(compl_clean)
+  
+  return(compl_clean1)
 }
 
 additional_clean_up <- function(compl_clean) {
@@ -126,17 +139,16 @@ additional_clean_up <- function(compl_clean) {
   # View(compl_clean_sa_vs_gom)
 
   # ---- add columns for month and quarter ----
-  compl_clean_sa_vs_gom_m <-
-    compl_clean_sa_vs_gom %>%
-    # add month
-    mutate(year_month = as.yearmon(week_start)) %>%
-    # add quarter
-    mutate(year_quarter = as.yearqtr(week_start))
+compl_clean_sa_vs_gom_m <- compl_clean_sa_vs_gom %>%
+    # Add a new column 'year_month' by extracting the year and month from the 'week_start' column
+    dplyr::mutate(year_month = zoo::as.yearmon(week_start)) %>%
+    # Add another new column 'year_quarter' by extracting the year and quarter from the 'week_start' column
+    dplyr::mutate(year_quarter = zoo::as.yearqtr(week_start))
 
   # ---- convert report numbers to numeric ----
-  compl_clean_sa_vs_gom_m_int <-
-    compl_clean_sa_vs_gom_m %>%
-    mutate(
+# Create a new data frame 'compl_clean_sa_vs_gom_m_int' by applying integer conversion to selected columns in 'compl_clean_sa_vs_gom_m'.
+  compl_clean_sa_vs_gom_m_int <- compl_clean_sa_vs_gom_m %>%
+    dplyr::mutate(
       captainreports__ = as.integer(captainreports__),
       negativereports__ = as.integer(negativereports__),
       gom_permitteddeclarations__ = as.integer(gom_permitteddeclarations__)
@@ -145,9 +157,9 @@ additional_clean_up <- function(compl_clean) {
   # add year_permit column ----
   compl_clean_sa_vs_gom_m_int_c <-
     compl_clean_sa_vs_gom_m_int %>%
-    mutate(
+    dplyr::mutate(
       year_permit =
-        case_when(
+        dplyr::case_when(
           year == "2022" & (permit_sa_gom == "gom_only"
                             | permit_sa_gom =="dual") ~
             paste(year, "gom_dual"),
