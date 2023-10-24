4923532a63f7bb8b2da350afaf3d288e2c343a39
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-24 10:40:51 -0400

    Update make_heatmap_flat_file.R
    add vessel_permit_corrected_list

--- fishing_effort_location/make_heatmap_flat_file.R
+++ fishing_effort_location/make_heatmap_flat_file.R
@@ -1,4 +1,7 @@
 # heatmap_make_flat_file.R
+# Remove all "source" lines from the flat file when done.
+
+
 source("~/R_code_github/useful_functions_module.r")
 my_paths <- set_work_dir()
 
@@ -10,6 +13,12 @@ flat_file_name <-
 write_to_1_flat_file(flat_file_name,
                      "~/R_code_github/useful_functions_module.r")
 
+cat(flat_file_name,
+    'my_paths <- set_work_dir()',
+    sep = "\n")
+
+# data are from "by_permit"
+
 write_to_1_flat_file(
   flat_file_name,
   file.path(
@@ -18,11 +27,12 @@ write_to_1_flat_file(
   )
 )
 
-fishing_effort_location_by_permit_and_end_port_path <-
-  file.path(
-    my_paths$git_r,
-    r"(fishing_effort_location\fishing_effort_location_by_permit_and_end_port.R)"
-  )
+cat(
+  flat_file_name,
+  "library(ggplot2) # a visualization package",
+  "library(ggmap) # extends 'ggplot2' for creating maps and working with spatial data.",
+  sep = "\n"
+)
 
 write_to_1_flat_file(
   flat_file_name,
@@ -32,11 +42,19 @@ write_to_1_flat_file(
   )
 )
 
-file.exists(fishing_effort_location_by_permit_and_end_port_path)
+# Build the path to the R script 'vessel_permit_corrected_list.R' by
+# combining the base path 'my_paths$git_r' and the script name.
+script_path <-
+  file.path(my_paths$git_r,
+            "vessel_permit_list/vessel_permit_corrected_list.R")
+
+# Source (run) the R script using the constructed script path.
+# source(script_path)
+# Remove "source(script_path)" from the flat file
 
 write_to_1_flat_file(
   flat_file_name,
-  fishing_effort_location_by_permit_and_end_port_path
+  script_path
 )
 
 write_to_1_flat_file(
@@ -54,5 +72,5 @@ file.exists(flat_file_name)
 sink.number()
 # 0 - right
 # delete the file
-# unlink(flat_file_name)
+unlink(flat_file_name)
 #
