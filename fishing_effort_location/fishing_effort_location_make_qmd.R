eab84851e77b5de026365657319a34de3e36d27a
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-25 08:44:09 -0400

    Update fishing_effort_location_make_qmd.R
    add yaml in front

--- fishing_effort_location/fishing_effort_location_make_qmd.R
+++ fishing_effort_location/fishing_effort_location_make_qmd.R
@@ -1,15 +1,26 @@
+# setup ----
 source("~/R_code_github/useful_functions_module.r")
 # my_paths <- set_work_dir()
 
 dir_to_comb <- "~/R_code_github/fishing_effort_location"
 
-rmd_file_name <-
+r_file_name <-
   file.path(dir_to_comb, "flat_file_heatmap.R")
 
-knitr::spin(rmd_file_name, knit = FALSE)
+rmd_file_name <-
+  file.path(dir_to_comb, "flat_file_heatmap.Rmd")
 
 qmd_file_name <-
-  file.path(dir_to_comb, "flat_file_heatmap.Rmd")
+  file.path(dir_to_comb, "flat_file_heatmap.qmd")
+
+# The 'knitr::spin' function is used to create an R Markdown (Rmd) file, but the 'knit' argument is set to 'FALSE', indicating that the document should not be fully knitted. Instead, this function generates an Rmd file from the R script without executing the code chunks.
+knitr::spin(r_file_name, knit = FALSE)
+
+# prepare all pieces ----
+
+## read the new file ----
+# Read the lines of text from the specified file into a character vector called 'rmd_contents'.
+rmd_contents <- readLines(rmd_file_name)
 
 pre_text <- '
 ---
@@ -34,13 +45,6 @@ format:
 ---
 '
 
-cat(
-  pre_text,
-  file = qmd_file_name,
-  append = TRUE,
-  sep = "\n"
-)
-
 # Setup
 setup_text <- "
 ```{r no cache setup, results='hide', message=FALSE, warning=FALSE, cache=FALSE}
@@ -50,8 +54,24 @@ library(knitr)
 ```
 "
 
+# combine pieces into a Quarto file ----
+
 cat(
   pre_text,
+  file = qmd_file_name,
+  # append = TRUE,
+  sep = "\n"
+)
+
+cat(
+  setup_text,
+  file = qmd_file_name,
+  append = TRUE,
+  sep = "\n"
+)
+
+cat(
+  rmd_contents,
   file = qmd_file_name,
   append = TRUE,
   sep = "\n"
