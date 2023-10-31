9eb1d17f8fc27e199c00ffeb027828d30d275e75
AShipunova1 <anna.shipunova@noaa.gov>
2023-10-31 11:44:17 -0400

    Update check_custom_functions.R
    get a df with func name and if it is in the code after removing useful module

--- check_custom_functions.R
+++ check_custom_functions.R
@@ -22,10 +22,37 @@ functions_in_fish_eff_location_list <-
 # List of 82
 #  $ : chr "add_count_contacts"
 
+# functions_in_fish_eff_location_web <- foodweb()
 
-functions_in_fish_eff_location_web <- foodweb()
+result_func_list <-
+  data.frame(func_name = character(0),
+             is_in_code = character(0))
 
-map(functions_in_fish_eff_location_list, function(f_name) {
-  browser()
-  callers.of(f_name, functions_in_fish_eff_location_web)
-})
+res <-
+  map(functions_in_fish_eff_location_list, function(f_name) {
+    # browser()
+    # callers.of(f_name, functions_in_fish_eff_location_web)
+    cat(f_name, sep = "\n")
+    cat(toString((unique(grepl(f_name, current_file_text)))), sep = "\n")
+    toString(unique(grepl(f_name, current_file_text)))
+    # res_temp <-
+    #   result_func_list |>
+    #   mutate(func_name = f_name,
+    #          is_in_code = toString(unique(grepl(f_name, current_file_text))))
+     # = toString(unique(grepl(f_name, current_file_text)))
+    # cat(f_name, sep = "\n")
+    # grep(f_name, current_file_text, value = T)
+    # return(res_temp)
+  })
+
+rr <- as.data.frame(res)
+names(rr)
+names(rr) <- unlist(functions_in_fish_eff_location_list)
+rr2 <-
+  t(rr) |>
+  as.data.frame() |>
+  rownames_to_column()
+
+names(rr2) <- c("func_name", "is_in_code")
+
+View(rr2)
