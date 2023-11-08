ddca4b263d6cf0719e3f3561c54424736b5ee1e1
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-08 13:16:04 -0500

    Update ifq_landing_locations.R
    start # fill in empty ----

--- ifq_landing_locations/ifq_landing_locations.R
+++ ifq_landing_locations/ifq_landing_locations.R
@@ -14,9 +14,50 @@ input_data_file_path <-
 
 input_data <- readr::read_csv(input_data_file_path)
 
-# > problems(input_data)
+# problems(input_data)
 # # A tibble: 2 × 5
 #     row   col expected actual file                                                      
 #   <int> <int> <chr>    <chr>  <chr>                                                     
 # 1  1264    68 a double TX     C:/Users/anna.shipunova/Documents/R_files_local/my_inputs…
 # 2  1264    78 a double TX     C:/Users/anna.shipunova/Documents/R_files_local/my_inputs…
+
+# data_overview(input_data)
+# print_df_names(input_data)
+
+# fill in empty ----
+input_data |>
+  filter(USER_FK_LANDING_LOCATION_ID == "2128") |>
+  select(USER_LATITUDE,
+         USER_LONGITUDE) |> 
+  distinct() |>
+  glimpse()
+
+
+res <-
+  strsplit("27° 54.478'",
+           "\\D+")
+
+# str(res)
+# List of 1
+
+# North West coords only
+convert_dms_to_dd_nw <- 
+  function(one_dms_coord) {
+    res <-
+      strsplit("27° 54.478'",
+               "(\\d+)\D+(\\d+)\\.(\\d+)\D+")
+    
+  }
+
+USER_FK_LANDING_LOCATION_ID
+# 2128	27° 54.478' N	97° 07.991' W
+# 27° 54' 478"
+# = 27° + 54'/60 + 478"/3600
+# = 28.032778°
+# 
+# 97° 07' 991"
+# = 97° + 07'/60 + 991"/3600
+# = 97.391944°
+
+# check if given lat/lon is different from geocoded ----
+# USER_LATITUDE, USER_LONGITUDE, x, y, OID_, USER_FK_LANDING_LOCATION_ID
