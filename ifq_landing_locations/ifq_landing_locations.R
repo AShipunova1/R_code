95724059b09a2db6a5667f0f24c5e4ded26f1414
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-10 09:26:27 -0500

    Update ifq_landing_locations.R
    # try tidygeocoder

--- ifq_landing_locations/ifq_landing_locations.R
+++ ifq_landing_locations/ifq_landing_locations.R
@@ -509,3 +509,96 @@ input_data_convert_dms_short_clean |>
   remove_empty_cols() |>
   select(USER_NYEAR, USER_UseCount) |>
   head()
+
+# try tidygeocoder
+if (!require(tidygeocoder)) {
+  install.packages("tidygeocoder")
+  library(tidygeocoder)
+}
+help(tidygeocoder)
+
+raw_input_data_file_path <-
+  file.path(my_paths$inputs,
+            r"(ifq_landing_locations\IFQ_Landing_Location_Use.csv)")
+
+input_data_raw <-
+  read.csv(
+    raw_input_data_file_path,
+    header = TRUE,
+    stringsAsFactors = FALSE,
+    fileEncoding = "latin1"
+  )
+
+print_df_names(input_data_raw)
+# [1] 3418   10
+
+tic("geocode6")
+lat_longs <- input_data_raw %>%
+  head() |>
+  geocode(
+    street = "STREET",
+    city = "CITY",
+    state = "STATE",
+    postalcode = "ZIP"
+  )
+# ,
+  # country = NULL,
+  # lat = "lat",
+  # long = "long",
+  # return_input = TRUE,
+  # limit = 1,
+  # return_addresses = NULL,
+# 
+#     addr,
+# method = 'arcgis',
+          # method = 'osm',
+          # lat = latitude ,
+          # long = longitude)
+toc()
+
+# View(lat_longs)
+# print_df_names(input_data_convert_dms)
+
+lat_longs |>
+  left_join(
+    input_data_convert_dms,
+    by = join_by(
+      NYEAR == USER_NYEAR,
+      FK_LANDING_LOCATION_ID == USER_FK_LANDING_LOCATION_ID,
+      UseCount == USER_UseCount
+    )
+  ) |>
+  select(NYEAR, FK_LANDING_LOCATION_ID, LATITUDE, LONGITUDE,
+         lat, long, Y, X.y, OID_, Place_addr) |> 
+  View()
+# diff in 3rd digit
+
+# test with another method
+
+tic("geocode6_esri")
+lat_longs_esri <- input_data_raw %>%
+  mutate(address = paste(STREET,
+                         CITY,
+                         STATE,
+                         ZIP,
+                         sep = ", ")) |>
+  head() |>
+  geocode(address = address,
+          return_addresses = TRUE,
+          method = 'arcgis')
+toc()
+# 1.5 sec elapsed
+
+lat_longs_esri |>
+  left_join(
+    input_data_convert_dms,
+    by = join_by(
+      NYEAR == USER_NYEAR,
+      FK_LANDING_LOCATION_ID == USER_FK_LANDING_LOCATION_ID,
+      UseCount == USER_UseCount
+    )
+  ) |>
+  select(NYEAR, FK_LANDING_LOCATION_ID, LATITUDE, LONGITUDE,
+         lat, long, Y, X.y, OID_, Place_addr) |> 
+  View()
+# the diff in 4-5 digit
