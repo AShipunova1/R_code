ecd28c94cef1518832c2079f5438b73fbb5cb5d3
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-13 13:02:24 -0500

    Update ifq_landing_locations.R
    join_nominatim_n_arcgis

--- ifq_landing_locations/ifq_landing_locations.R
+++ ifq_landing_locations/ifq_landing_locations.R
@@ -143,9 +143,54 @@ test_data_path <- file.path(my_paths$git_r,
 # source(test_data_path)
 
 # add missing coords to input_data_raw_nominatim_converted ----
-input_data_raw_nominatim_converted |> 
+input_data_raw_nominatim_converted_no_coord <- 
+  input_data_raw_nominatim_converted |> 
   filter(is.na(X) | is.na(Y))
 
+# dim(input_data_raw_nominatim_converted_no_coord)
+# Rows: 1,307
+# Columns: 29
+
+input_data_raw_nominatim_converted_no_coord |> 
+  select(-USER_NYEAR) |> 
+  remove_empty_cols() |> 
+  distinct() |> 
+  dim()
+# [1] 1062   15
+
+input_data_raw_nominatim_converted_no_coord |> 
+  select(USER_FK_LANDING_LOCATION_ID) |> 
+  distinct() |> 
+  dim()
+# [1] 210   1
+
+input_data_raw_nominatim_converted_no_coord[1,]$USER_FK_LANDING_LOCATION_ID 
+   # int 1604
+
+arcgisdata_coord_only <- 
+  input_data |>
+  select(-c(USER_NYEAR, OID_, USER_UseCount)) |>
+  remove_empty_cols() |>
+  distinct()
+# dim(arcgisdata_coord_only)
+# [1] 585  61
+
+# arcgisdata_coord_only |>
+#   filter(USER_FK_LANDING_LOCATION_ID == 1604) |>
+#   remove_empty_cols() |>
+#   distinct() |>
+#   dim()
+# [1]  1 56
+
+join_nominatim_n_arcgis <-
+  left_join(
+    input_data_raw_nominatim_converted_no_coord,
+    arcgisdata_coord_only,
+    join_by(USER_FK_LANDING_LOCATION_ID),
+    suffix = c("_osm", "_arc")
+  )
+
+View(join_nominatim_n_arcgis)
 # shorten ----
 keep_fields_list_arcgis <-
   c(
