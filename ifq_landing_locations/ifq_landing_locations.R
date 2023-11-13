ff133f2b9435ef675d44d82283aca7d281c14abc
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-13 11:02:01 -0500

    separate get_shapes

--- ifq_landing_locations/ifq_landing_locations.R
+++ ifq_landing_locations/ifq_landing_locations.R
@@ -276,91 +276,6 @@ input_data_convert_dms_short_clean_short_cnt_sf <-
 
 # glimpse(input_data_convert_dms_short_clean_short_cnt_sf)
 
-# get shapes ----
-south_east_coast_states <- c(
-  "Alabama",
-  # "Connecticut",
-  # "Delaware",
-  "Florida",
-  "Georgia",
-  "Louisiana",
-  # "Maine",
-  # "Maryland",
-  # "Massachusetts",
-  "Mississippi",
-  # "New Hampshire",
-  # "New Jersey",
-  # "New York",
-  "North Carolina",
-  # "Pennsylvania",
-  # "Rhode Island",
-  "South Carolina",
-  "Texas"
-  # ,
-  # "Virginia",
-  # "Washington DC"
-)
-
-## us maps ----
-# The code loads U.S. state boundary shapefile data using the 'tigris' package, and the resulting spatial data is stored in the 'us_s_shp' variable as a simple feature (sf) object. The progress bar is disabled during the data loading process.
-# The 'cb = TRUE' parameter specifies that you want the U.S. state boundaries.
-
-us_s_shp <-
-  tigris::states(cb = TRUE, progress_bar = FALSE)
-
-## Rows are retained if the 'NAME' column (state name) matches any of the values in 'states_sa'.
-south_states_shp <-
-  us_s_shp |>
-  filter(NAME %in% south_east_coast_states)
-
-# View(south_states_shp)
-
-# str(south_east_coast_states)
-
-## read in GOM shp ----
-## Create a file path using 'file.path' by combining elements from 'my_paths' and specifying a shapefile path.
-GOM_400fm_path <-
-  file.path(my_paths$inputs,
-                      r"(shapefiles\GOM_400fm\GOM_400fm.shp)")
-
-# file.exists(GOM_400fm_path)
-# T
-
-## Read a shapefile from the specified file path using 'sf::read_sf'.
-## Then, group the resulting data by 'StatZone' and summarize it.
-GOMsf_all <-
-  sf::read_sf(GOM_400fm_path)
-
-# glimpse(GOMsf)
-
-my_file_path_local <- file.path(my_paths$outputs,
-                           "fishing_effort_location",
-                           "st_union_GOMsf.rds")
-my_file_path_out <- file.path(my_paths$outputs,
-                           "st_union_GOMsf.rds")
-
-# If the file exists, read the data from the RDS file (to speed it up).
-if (file.exists(my_file_path_local)) {
-  current_path <- my_file_path_local
-  st_union_GOMsf <- readr::read_rds(current_path)
-} else if (file.exists(my_file_path_out)) {
-  current_path <- my_file_path_out
-  st_union_GOMsf <- readr::read_rds(current_path)
-} else {
-  # Start measuring the time it takes to perform the operation and display a message.
-  tic("st_union(GOMsf)")
-  st_union_GOMsf <- sf::st_union(GOMsf)
-  # Stop measuring time and display the elapsed time.
-  toc()
-  
-  readr::write_rds(st_union_GOMsf,
-                   my_file_path_out)
-}
-
-
-# mapview::mapview(input_data_convert_dms_short_clean_short_cnt_sf) +
-#   south_states_shp
-
 ## plot_by_year (arcGis) ---- 
 
 plot_by_year <- ggplot() +  # Initialize a ggplot object.
@@ -624,7 +539,7 @@ toc()
 # in_address_sep_clean: 8.24 sec elapsed
 
 head(in_address_sep_clean, 2)
-state.abb |> head()
+
 # $ OID_              <int> 194, 664, 695, 1368, 2511, 2898, 3122, 3157
 # $ Place_addr        <chr> "", "", "", "", "", "", "", ""
 # $ corrected_addr    <chr> "", "", "", "", "", "", "", ""
