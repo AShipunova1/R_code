02b212b772c506527e1ed7e537aa9d359cfe5ce4
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-13 10:58:58 -0500

    Update ifq_landing_locations.R
    rm parts

--- ifq_landing_locations/ifq_landing_locations.R
+++ ifq_landing_locations/ifq_landing_locations.R
@@ -123,86 +123,6 @@ toc()
 
 glimpse(input_data_convert_dms)
 
-# check if given lat/lon is different from geocoded ----
-# USER_LATITUDE, USER_LONGITUDE, X, Y, OID_, USER_FK_LANDING_LOCATION_ID
-## compare user_coord with geocoded ----
-
-input_data_convert_dms |>
-  filter(
-    !round(abs(X), 2) == round(abs(converted_dms_lon), 2) |
-      !round(Y, 2) == round(converted_dms_lat, 2)
-  ) |>
-  select(X, converted_dms_lon,
-         Y, converted_dms_lat) |>
-  distinct() |>
-  dim()
-# 63  4
-# 58 if round to 1 digit
-
-input_data_convert_dms |>
-  filter(
-    !round(abs(DisplayX), 2) == round(abs(converted_dms_lon), 2) |
-      !round(DisplayY, 2) == round(converted_dms_lat, 2)
-  ) |>
-    select(X, DisplayX, converted_dms_lon,
-         Y, DisplayY, converted_dms_lat) |>
-  distinct() |>
-  dim()
-# [1] 65  6
-# View(input_data_convert_dms)
-# Fields:
-# https://pro.arcgis.com/en/pro-app/latest/help/data/geocoding/what-is-included-in-the-geocoded-results-.htm#:~:text=The%20DisplayX%20and%20DisplayY%20values,.%2C%20Redlands%2C%20CA%2092373.
-
-# DisplayX—The display x-coordinate of an address returned in the spatial reference of the locator. The display x-coordinate is returned in spatial reference WGS84 (WKID 4326) by the ArcGIS World Geocoding Service. For matches to PointAddress or Subddress as indicated in the Addr_type field and PointAddress role-based locators, this value represents the x-coordinate value of the building rooftop or parcel centroid for the address. It differs from the primary x-value, which represents the x-coordinate of the side of street location or the street entry for an address. However, there are exceptions, as some data sources used by the ArcGIS World Geocoding Service only provide the rooftop location of PointAddress and Subaddress features. In other cases, only the street-side location is available for some PointAddress and Subaddress features. For such cases, the X and DisplayX values are equivalent. For all other Addr_type matches and locators, this value is equal to the x-value.
-
-# compare geocoded coords
-input_data_convert_dms |>
-  filter(
-    !round(DisplayX, 2) == round(X, 2) |
-      !round(DisplayY, 2) == round(Y, 2)
-  ) |>
-    select(X, DisplayX, converted_dms_lon,
-         Y, DisplayY, converted_dms_lat) |>
-  distinct() |> 
-  dim()
-# [1] 13  6
-
-# check ExInfo and missing coords ----
-#   print_df_names(input_data_convert_dms)
-# [1] "OID_, Loc_name, Status, Score, Match_type, Match_addr, LongLabel, ShortLabel, Addr_type, PlaceName, Place_addr, Rank, AddNum, AddNumFrom, AddNumTo, AddRange, Side, StPreDir, StName, StType, StDir, StAddr, Nbrhd, City, MetroArea, Subregion, Region, RegionAbbr, Postal, PostalExt, Country, CntryName, LangCode, Distance, X, Y, DisplayX, DisplayY, Xmin, Xmax, Ymin, Ymax, ExInfo, IN_Address, IN_City, IN_Region, IN_Postal, USER_NYEAR, USER_FK_LANDING_LOCATION_ID, USER_LATITUDE, USER_LONGITUDE, USER_STREET, USER_CITY, USER_STATE, USER_ZIP, USER_UseCount, USER_Field10, converted_dms_lat, converted_dms_lon"
-
-# ExInfo — A collection of strings from the input that could not be matched to any part of an address and were used to score or penalize the result.
-input_data_convert_dms |>
-  filter(!is.na(ExInfo)) |> 
-  filter(!ExInfo == "") |>
-  remove_empty_cols() |>
-  distinct() |> 
-  # dim()
-# [1] 466  66
-  select(
-    Place_addr,
-    ExInfo,
-    IN_Address,
-    USER_STREET,
-    USER_CITY,
-    USER_STATE,
-    USER_ZIP,
-    Postal,
-    X,
-    converted_dms_lon,
-    Y,
-    converted_dms_lat
-  ) |>
-  distinct() |>
-  filter(!USER_ZIP == Postal) |> 
-  dim()
-# [1] 67 11
-
-input_data_convert_dms |>
-  filter(X == 0) |> 
-  dim()
-# 8
-
 # shorten ----
 keep_fields_list <-
   c(
@@ -221,7 +141,7 @@ keep_fields_list <-
 
 input_data_convert_dms_short <- 
   input_data_convert_dms |> 
-  select(all_of(keep_fields_list)) |> 
+  select(any_of(keep_fields_list)) |> 
   distinct()
   
 # dim(input_data_convert_dms_short)
@@ -240,7 +160,7 @@ input_data_convert_dms_short <-
 #     filter(USER_NYEAR == 1899) |> 
 #   glimpse()
 
-# clean df ----
+# clean ARCgis df ----
 # In summary, the code takes the 'input_data_convert_dms_short' data frame, makes several modifications to it using the 'mutate' function, and creates three new columns: 'use_lat', 'use_lon', and 'use_addr'. These new columns are generated based on specific conditions or by choosing the first non-NA value from a set of columns using 'coalesce'. The resulting data frame is stored in 'input_data_convert_dms_short_clean'.
 
 input_data_convert_dms_short_clean <-
@@ -321,7 +241,7 @@ input_data_convert_dms_short_clean_short_cnt |>
   filter(USER_NYEAR == 1899) |>
   glimpse()
 
-# leave cnts and unique ----
+# leave only cnts and unique ----
 input_data_convert_dms_short_clean_short_cnt_short <- 
   input_data_convert_dms_short_clean_short_cnt |>
   select(
@@ -441,8 +361,8 @@ if (file.exists(my_file_path_local)) {
 # mapview::mapview(input_data_convert_dms_short_clean_short_cnt_sf) +
 #   south_states_shp
 
-## plot_by_year ---- 
-# Create a new variable 'plot_by_year'.
+## plot_by_year (arcGis) ---- 
+
 plot_by_year <- ggplot() +  # Initialize a ggplot object.
   
   # Add a spatial feature layer to the plot using 'geom_sf' and 'input_data_convert_dms_short_clean_short_cnt_sf' as data.
@@ -499,7 +419,7 @@ input_data_convert_dms_short_clean_short_cnt_sf |>
     layer.name = 'Counts by year and place'
   )
 
-# check missing addresses ----
+# check missing addresses (arcgis) ----
 input_data_convert_dms_short_clean |>
   filter(is.na(X) | is.na(Y)) |>
   filter(is.na(converted_dms_lat) | is.na(converted_dms_lon)) |>
@@ -509,47 +429,35 @@ input_data_convert_dms_short_clean |>
 
 # 
 # using tidygeocoder ----
-input_data <- 
-  input_data_raw_esri |> 
-  select(-c(X, my_address)) |>
-  rename(USER_NYEAR = NYEAR,
-         USER_FK_LANDING_LOCATION_ID = FK_LANDING_LOCATION_ID,
-         USER_LATITUDE = LATITUDE, 
-         USER_LONGITUDE = LONGITUDE,
-         USER_UseCount = UseCount,
-         IN_Address = address,
-         Y = lat,
-         X = long
-         )
 
-## convert all input coord format ----
-tic("input_data_convert_dms")
-input_data_convert_dms <- 
-  input_data |>
-  dplyr::rowwise() |>
-  dplyr::mutate(
-    converted_dms_lat = convert_dms_to_dd_nw(USER_LATITUDE),
-    converted_dms_lon = convert_dms_to_dd_nw(USER_LONGITUDE)
-  ) |>
-  dplyr::ungroup()
-toc()
-# input_data_convert_dms: 0.2 sec elapsed
-
-glimpse(input_data_convert_dms)
+# ## convert all input coord format ----
+# tic("input_data_convert_dms")
+# input_data_convert_dms <- 
+#   input_data |>
+#   dplyr::rowwise() |>
+#   dplyr::mutate(
+#     converted_dms_lat = convert_dms_to_dd_nw(USER_LATITUDE),
+#     converted_dms_lon = convert_dms_to_dd_nw(USER_LONGITUDE)
+#   ) |>
+#   dplyr::ungroup()
+# toc()
+# # input_data_convert_dms: 0.2 sec elapsed
+# 
+# glimpse(input_data_convert_dms)
 
 # check if given lat/lon is different from geocoded ----
 # USER_LATITUDE, USER_LONGITUDE, X, Y, OID_, USER_FK_LANDING_LOCATION_ID
 ## compare user_coord with geocoded ----
 
-input_data_convert_dms |>
-  filter(
-    !round(abs(X), 2) == round(abs(converted_dms_lon), 2) |
-      !round(Y, 2) == round(converted_dms_lat, 2)
-  ) |>
-  select(X, converted_dms_lon,
-         Y, converted_dms_lat) |>
-  distinct() |>
-  dim()
+# input_data_convert_dms |>
+#   filter(
+#     !round(abs(X), 2) == round(abs(converted_dms_lon), 2) |
+#       !round(Y, 2) == round(converted_dms_lat, 2)
+#   ) |>
+#   select(X, converted_dms_lon,
+#          Y, converted_dms_lat) |>
+#   distinct() |>
+#   dim()
 # 63  4
 # 58 if round to 1 digit
 # from tidy
@@ -619,49 +527,7 @@ input_data_convert_dms |>
   dim()
 # 8
 
-# shorten ----
-keep_fields_list <-
-  c(
-    "OID_",
-    "Place_addr",
-    "IN_Address",
-    "corrected_addr",
-    "corrected_lat",
-    "corrected_long",
-    "Y",
-    "converted_dms_lat",
-    "X",
-    "converted_dms_lon",
-    "USER_NYEAR",
-    "USER_UseCount"
-  )
-
-input_data_convert_dms_short <- 
-  input_data_convert_dms |> 
-  select(any_of(keep_fields_list))
-# |> 
-#   distinct()
-  
-dim(input_data_convert_dms_short)
-  # [1] 3418    10
-# [1] 3400    7 from tidygeocoder, can't distinct yet!
-# [1] 3418    7
-
-# check
-input_data_convert_dms_short |>
-  filter(USER_NYEAR == 1899) |>
-  select(USER_UseCount, USER_NYEAR) |>
-  # select(OID_, USER_UseCount, USER_NYEAR) |>
-  # distinct() |>
-  count(wt = USER_UseCount)
-# 43
-
-# input_data_convert_dms_short |> 
-#     add_count(USER_NYEAR, wt = USER_UseCount, name = "total_use_count_y") |> 
-#     filter(USER_NYEAR == 1899) |> 
-#   glimpse()
-
-# clean df ----
+# clean df from tidygeo ----
 # In summary, the code takes the 'input_data_convert_dms_short' data frame, makes several modifications to it using the 'mutate' function, and creates three new columns: 'use_lat', 'use_lon', and 'use_addr'. These new columns are generated based on specific conditions or by choosing the first non-NA value from a set of columns using 'coalesce'. The resulting data frame is stored in 'input_data_convert_dms_short_clean'.
 
 input_data_convert_dms_short_clean <-
@@ -703,15 +569,14 @@ input_data_convert_dms_short |>
   glimpse()
 
 # compare missing values with ARCgis result ----
-# use arcgis_address vs my_address diff
+
+# check arcgis_address vs my_address diff ----
 # split to strit, city, state (FL in input vs Florida), zip and compare separately
 
 input_data_convert_dms |>
   filter(!tolower(IN_Address) == tolower(arcgis_address)) |> 
   View()
 
-
-stringr::str_split_fixed(before$type, "_and_", 2)
 
 in_address_sep <- 
   stringr::str_split(input_data_convert_dms$IN_Address, ",",
