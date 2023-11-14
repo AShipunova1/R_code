548ca84919a5d02c24721ad2cd2a6a1b8b420904
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-14 10:43:52 -0500

    Update boats_number.R
    # how many vessels have variable landing locations (i.e., in the winter they are in one state while in the summer they fish in another); ----

--- boats_number/boats_number.R
+++ boats_number/boats_number.R
@@ -44,10 +44,25 @@ all_logbooks_db_data_2022_short_p_region_short |>
 
 # how many vessels have variable landing locations (i.e., in the winter they are in one state while in the summer they fish in another); ----
 
-all_logbooks_db_data_2022_short_p_region |>
-  select(vessel_id,
-         vessel_official_nbr,
-         permit_region) |>
-  distinct() |>
+all_logbooks_db_data_2022_short_p_region_short_all_ports_by_vsl <-
+  all_logbooks_db_data_2022_short_p_region_short |>
+  group_by(vessel_id, vessel_official_nbr) |>
+  mutate(all_start_ports = toString(unique(start_port)),
+         all_end_ports   = toString(unique(end_port))) |>
+  mutate(all_start_ports_num = length(str_split(all_start_ports, ",")),
+         all_end_ports_num   = length(str_split(all_end_ports, ","))) |>
+  ungroup()
+
+
+all_logbooks_db_data_2022_short_p_region_short_all_ports_by_vsl |>
+  # View()
+  filter(all_start_ports_num > 1) |>
+  glimpse()
+
+  # filter(!start_port == end_port)
+  # select(vessel_id,
+  #        vessel_official_nbr,
+  #        permit_region) |>
+  # distinct() |>
 
 
