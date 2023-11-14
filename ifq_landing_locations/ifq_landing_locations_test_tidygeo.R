14fcbd5e36bc63468cd5a74f179fd0ec6d10cd5c
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-14 08:43:57 -0500

    Update ifq_landing_locations_test_tidygeo.R
    # compare missing values with ARCgis result ----

--- ifq_landing_locations/ifq_landing_locations_test_tidygeo.R
+++ ifq_landing_locations/ifq_landing_locations_test_tidygeo.R
@@ -69,3 +69,76 @@ lat_longs_esri |>
   View()
 # the diff in 4-5 digit
 
+# compare missing values with ARCgis result ----
+
+# check arcgis_address vs my_address diff ----
+# split to strit, city, state (FL in input vs Florida), zip and compare separately
+
+input_data_convert_dms |>
+  filter(!tolower(IN_Address) == tolower(arcgis_address)) |> 
+  View()
+
+
+in_address_sep <- 
+  stringr::str_split(input_data_convert_dms$IN_Address, ",",
+                     simplify = TRUE) |> 
+  as.data.frame()
+
+
+head(in_address_sep, 2)
+# names(in_address_sep) <- c("in_street",
+#                            "in_city",
+#                            "in_state",
+#                            "in_zip",
+#                            "V5")
+
+clean_addr <- 
+  function(addr_str) {
+    return(trimws(tolower(addr_str)))
+  }
+
+map(in_address_sep$V5, length) |> 
+  head()
+# 1
+
+names(state.abb) <- state.name
+names(state.name) <- state.abb
+state.name["FL"]
+
+tic("in_address_sep_clean")
+in_address_sep_clean <- 
+  in_address_sep |> 
+  rowwise() |> 
+  mutate(
+    # v5_len = length(trimws(V5))
+    in_city_sep = case_when(length(trimws(V5)) > 1 ~ clean_addr(V3),
+                                .default = clean_addr(V2)),
+    in_state_sep = case_when(length(trimws(V5)) > 1 ~ clean_addr(V4),
+                                .default = clean_addr(V3)),
+    in_zip_sep = case_when(length(trimws(V5)) > 1 ~ clean_addr(V5),
+                                .default = clean_addr(V4))
+  ) |> 
+  ungroup() |> 
+  rename(in_street_sep = V1) |> 
+  select(-starts_with("V"))
+toc()
+# in_address_sep_clean: 8.24 sec elapsed
+
+head(in_address_sep_clean, 2)
+
+# $ OID_              <int> 194, 664, 695, 1368, 2511, 2898, 3122, 3157
+# $ Place_addr        <chr> "", "", "", "", "", "", "", ""
+# $ corrected_addr    <chr> "", "", "", "", "", "", "", ""
+# $ corrected_lat     <dbl> NA, NA, NA, NA, NA, NA, NA, NA
+# $ corrected_long    <dbl> NA, NA, NA, NA, NA, NA, NA, NA
+# $ Y                 <dbl> 0, 0, 0, 0, 0, 0, 0, 0
+# $ converted_dms_lat <dbl> 28.03278, 28.03278, 28.03278, NA, NA, 28.03278, NA, NA
+# $ X                 <dbl> 0, 0, 0, 0, 0, 0, 0, 0
+# $ converted_dms_lon <dbl> 97.39194, 97.39194, 97.39194, NA, NA, 97.39194, NA, NA
+# $ USER_NYEAR        <int> 2015, 2016, 2018, 2014, 2020, 2020, 2013, 2015
+# $ USER_UseCount     <int> 4, 1, 3, 136, 1, 3, 40, 28
+
+# input_data_convert_dms_short_clean |>
+#   filter(OID_ == 194) |>
+#   glimpse()
+
