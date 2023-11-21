6e97e55abfa30fb75fbdd4100e85e3b0abde446e
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-21 11:53:39 -0500

    Update make_qmd.R
    add one group, to use for glimpse|dim etc

--- make_qmd.R
+++ make_qmd.R
@@ -108,8 +108,8 @@ flat_file_r_text <-
 # add "pretty" table output (add kable to glimpse)
 flat_file_r_text <-
   gsub(
-    "(^ *[^#] +)(glimpse\\(\\S*\\))", # was in the original .R
-    '\\1\\2 |>
+    "(^ *[^#] +)(glimpse)(\\(\\S*\\))", # was in the original .R
+    '\\1\\2\\3 |>
 \\1knitr::kable(caption = "")',
     flat_file_r_text
   )
