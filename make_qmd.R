8b2fe54fd98d0309289cfe3db4fb157954845275
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-21 12:59:42 -0500

    add print-df options

--- make_qmd.R
+++ make_qmd.R
@@ -144,6 +144,22 @@ library(knitr)
 library(styler)
 
 ```
+
+```{r df format setup}
+#| include: false
+knit_print.data.frame = function(x, ...) {
+  res = paste(c('', '',
+                knitr::kable(x, digits = 2) |>
+                  kableExtra::kable_styling()), collapse = '\n')
+  knitr::asis_output(res)
+}
+
+registerS3method(
+  'knit_print', 'data.frame', knit_print.data.frame,
+  envir = asNamespace('knitr')
+)
+```
+
 "
 
 # combine pieces into a Quarto file ----
