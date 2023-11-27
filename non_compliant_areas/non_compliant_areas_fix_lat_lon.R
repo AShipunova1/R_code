0d481b3f049f572837e5781120551eeff465bfc5
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-27 11:10:01 -0500

    Create non_compliant_areas_fix_lat_lon.R

---
+++ non_compliant_areas/non_compliant_areas_fix_lat_lon.R
@@ -0,0 +1,261 @@
+# run once to get lat lon and check names with no coords
+
+# add lat/lon ----
+
+my_file_path_lat_lon <- 
+  file.path(my_paths$outputs, 
+            current_project_dir_name,
+            paste0(current_project_dir_name, "_no_county.rds"))
+
+file.exists(my_file_path_lat_lon)
+
+get_lat_lon_no_county <-
+  function(vessels_permits_home_port) {
+    vessels_permits_home_port_lat_longs <-
+      vessels_permits_home_port_c_st_fixed |>
+      select(-SERO_HOME_PORT_COUNTY) |> 
+      # tidygeocoder::geocode(city = "SERO_HOME_PORT_CITY",
+      #                       state = "SERO_HOME_PORT_STATE")
+      tidygeocoder::geocode(city = "",
+                            state = "")
+    return(vessels_permits_home_port_lat_longs)
+  }
+
+vessels_permits_home_port_lat_longs_nc <-
+  read_rds_or_run(my_file_path_lat_lon,
+                  my_data =
+                    as.data.frame(vessels_permits_home_port),
+                  get_lat_lon)
+
+# check home port typos by lat/lon ----
+check_home_port_typos_by_lat_lon <- 
+  function(df_list_by_reg) {
+    compl_err_db_data_metrics_permit_reg_list_home_port_err <-
+      names(df_list_by_reg) |>
+      map(\(curr_permit_reg_name) {
+        df_list_by_reg[[curr_permit_reg_name]] |>
+          filter(is.na(long) |
+                   is.na(lat)) |>
+          select(
+            vessel_official_nbr,
+            SERO_HOME_PORT_CITY,
+            # SERO_HOME_PORT_COUNTY,
+            SERO_HOME_PORT_STATE
+          ) |>
+          distinct() |>
+          mutate(
+            SERO_HOME_PORT_CITY = trimws(SERO_HOME_PORT_CITY),
+            # SERO_HOME_PORT_COUNTY = trimws(SERO_HOME_PORT_COUNTY),
+            SERO_HOME_PORT_STATE = trimws(SERO_HOME_PORT_STATE)
+          )
+      })
+    
+    names(compl_err_db_data_metrics_permit_reg_list_home_port_err) <-
+      names(df_list_by_reg)
+    
+    return(compl_err_db_data_metrics_permit_reg_list_home_port_err)
+  }
+
+
+# vessels_permits_home_port_lat_longs_nc |>
+#   filter(is.na(long) |
+#            is.na(lat)) |>
+#   select(SERO_OFFICIAL_NUMBER,
+#          SERO_HOME_PORT_CITY,
+#          SERO_HOME_PORT_STATE) |>
+#   distinct() |>
+#   mutate(
+#     SERO_HOME_PORT_CITY = trimws(SERO_HOME_PORT_CITY),
+#     SERO_HOME_PORT_STATE = trimws(SERO_HOME_PORT_STATE)
+#   )
+# Rows: 80
+
+# View(compl_err_db_data_metrics_permit_reg_list_home_port_err)
+
+compl_err_db_data_metrics_permit_reg_list_home_port_err_county <- 
+  check_home_port_typos_by_lat_lon(compl_err_db_data_metrics_permit_reg_list_home_port)
+
+# Work with compl_err_db_data_metrics_permit_reg_list_home_port_err_county in excel ----
+
+vessels_permits_home_port_lat_longs_nc_err <-
+  vessels_permits_home_port_lat_longs_nc |>
+  filter(is.na(long) |
+           is.na(lat)) |>
+  select(SERO_OFFICIAL_NUMBER,
+         SERO_HOME_PORT_CITY,
+         SERO_HOME_PORT_STATE) |>
+  distinct() |>
+  mutate(
+    SERO_HOME_PORT_CITY = trimws(SERO_HOME_PORT_CITY),
+    SERO_HOME_PORT_STATE = trimws(SERO_HOME_PORT_STATE)
+  )
+
+dim(vessels_permits_home_port_lat_longs_nc_err)
+# [1] 80  3
+
+vessels_permits_home_port_lat_longs_nc_err_all <-
+  vessels_permits_home_port_lat_longs_nc |>
+  select(SERO_HOME_PORT_CITY,
+         SERO_HOME_PORT_STATE,
+         lat,
+         long) |>
+  mutate(
+    SERO_HOME_PORT_CITY = trimws(SERO_HOME_PORT_CITY),
+    SERO_HOME_PORT_STATE = trimws(SERO_HOME_PORT_STATE)
+  ) |> 
+  distinct()
+
+vessels_permits_home_port_lat_longs_nc_err_all |> 
+  dim()
+  # [1] 648   2
+
+csv_file_path <-
+  file.path(
+    my_paths$outputs,
+    current_project_dir_name,
+    stringr::str_glue(
+      "{current_project_dir_name}_vessels_permits_home_port_lat_longs_nc_err_all.csv"
+    )
+  )
+
+file.exists(csv_file_path)   
+
+vessels_permits_home_port_lat_longs_nc_err_all |>
+  write_csv(file = csv_file_path)
+
+# fix home port typos ----
+# the list is created manuall from the csv
+to_fix_list <- 
+  list(
+    c(
+      "BAYOU LABATRE#AL",
+      "BAYOU LA BATRE#AL"),
+    c("CAROLINA BEACH#UN",
+      "CAROLINA BEACH#NC"),
+    c("CHALESTON#SC",
+      "CHARLESTON#SC"),
+    c("CHAUVIN, LA#LA",
+      "CHAUVIN#LA"),
+    c("FERNADINA BCH#FL",
+      "FERNANDINA BEACH#FL"),
+    c("FORT MORGAN MARINA#AL",
+      "FORT MORGAN#AL"),
+    c("GALLINANO#LA",
+      "GALLIANO#LA"),
+    c("GEORGRTOWN#SC",
+      "GEORGETOWN#SC"),
+    c("GULFSHORES#AL",
+      "GULF SHORES#AL"),
+    c("HILISBORO INLET#FL",
+      "HILLSBORO INLET#FL"),
+    c("HOMOASSA#FL",
+      "HOMOSASSA#FL"),
+    c("HOUMA LA#LA",
+      "HOUMA#LA"),
+    c("INTERCOASTAL CITY#LA",
+      "INTRACOASTAL CITY#LA"),
+    c("ISLAMORADA#UN",
+      "ISLAMORADA#FL"),
+    c("KEYWEST#FL",
+      "KEY WEST#FL"),
+    c("LITTLE RIVERNHV1N4WH#SC",
+      "LITTLE RIVER#SC"),
+    c("LOXLEY AL#AL",
+      "LOXLEY#AL"),
+    c("MADIERA BEACH#FL",
+      "MADEIRA BEACH#FL"),
+    c("MAYPPORT#FL",
+      "MAYPORT#FL"),
+    c("MCLELLANVILLE#SC",
+      "MCCLELLANVILLE#SC"),
+    c("MURELLS INLET#SC",
+      "MURRELLS INLET#SC"),
+    c("MURRELS INLET#SC",
+      "MURRELLS INLET#SC"),
+    c("NEW SMYMA BEACH#FL",
+      "NEW SMYRNA BEACH#FL"),
+    c("NEW SYMRNA BEACH#FL",
+      "NEW SMYRNA BEACH#FL"),
+    c("OCEEAN CITY#MD",
+      "OCEAN CITY#MD"),
+    c("POINT PLEASANT NJ#NJ",
+      "POINT PLEASANT#NJ"),
+    c("PORT CANVERAL#FL",
+      "PORT CANAVERAL#FL"),
+    c("PORT O CANNOR#TX",
+      "PORT O CONNOR#TX"),
+    c("PORT OCONNOR#TX",
+      "PORT O'CONNOR#TX"),
+    c("PORT ST.LUICE#FL",
+      "PORT ST LUCIE#FL"),
+    c("PUNTA GORGA#FL",
+      "PUNTA GORDA#FL"),
+    c("RIVERIA BEACH#FL",
+      "RIVIERA BEACH#FL"),
+    c("S PADRE ISLE#TX",
+      "S. PADRE ISLAND#TX"),
+    c("SEBASTAIN#FL",
+      "SEBASTIAN#FL"),
+    c("ST AUGUSTIN#FL",
+      "ST AUGUSTINE#FL"),
+    c("ST PETERSBURG BEACH#FL",
+      "ST PETERSBURG#FL"),
+    c("STEINAHTCHEE#FL",
+      "STEINHATCHEE#FL"),
+    c("SUMMRLND KEY#FL",
+      "SUMMERLAND KEY#FL"),
+    c("TAVENIER#FL",
+      "TAVERNIER#FL"),
+    c("WANCHEESE#NC",
+      "WANCHESE#NC"),
+    c("ALEXANDER CITY, AL#AL",
+      "ALEXANDER CITY#AL")
+  )
+
+vessels_permits_home_port_c_st <- 
+  vessels_permits_home_port |> 
+  mutate(city_state = 
+           paste(SERO_HOME_PORT_CITY, SERO_HOME_PORT_STATE, sep = "#")) 
+
+wrong_port_addr <-
+  sapply(to_fix_list, "[", 1)
+
+get_correct_addr_by_wrong <-
+  function(wrong_addr) {
+    idx <- grep(wrong_addr, to_fix_list)
+    
+    names_pair <- 
+      tryCatch(to_fix_list[[idx]],
+               error = function(e) {
+                 print(e)
+                 print(str_glue("Index: {idx}"))
+                 })
+    good_addr <- names_pair[[2]]
+
+    return(good_addr)
+  }
+
+# Have to use "if". case_when will execute all the LHS and RHS, then keep based on conditions. So get_correct_addr_by_wrong is executed, one time by each row during the RHS evaluation and produces idx = 0 error. 
+
+vessels_permits_home_port_c_st_fixed <-
+  vessels_permits_home_port_c_st |>
+  rowwise() |>
+  mutate(city_state_fixed =
+           if (city_state %in% wrong_port_addr)
+             get_correct_addr_by_wrong(city_state)
+         else
+           city_state) |>
+  ungroup() |> 
+  tidyr::separate_wider_delim(city_state_fixed, 
+                              delim = "#", 
+                              names = c("city_fixed", "state_fixed"))
+
+dim(vessels_permits_home_port_c_st_fixed)
+# [1] 4729    7
+
+vessels_permits_home_port_c_st_fixed |> 
+  filter(!SERO_HOME_PORT_CITY == city_fixed) |> 
+  dim()
+# [1] 49  7
+
+# vessels_permits_home_port_c_st_fixed
