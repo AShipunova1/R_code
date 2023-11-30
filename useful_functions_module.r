5113216fccbfe3b38c53fb067b151d8b3c09bdb6
AShipunova1 <anna.shipunova@noaa.gov>
2023-11-30 12:56:47 -0500

    Update useful_functions_module.r
    add \n to cat output

--- useful_functions_module.r
+++ useful_functions_module.r
@@ -544,7 +544,7 @@ data_overview <- function(my_df) {
   summary(my_df) %>% print()
 
   # Print a header indicating the next section of the output.
-  cat("\nCount unique values in each column:")
+  cat("\nCount unique values in each column:\n")
 
   # Call the 'count_uniq_by_column' function to count unique values in each column of the data frame.
   count_uniq_by_column(my_df)
