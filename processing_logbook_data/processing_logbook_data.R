ec26b2876c7cf04f11225cd12531dc61ba745821
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2023-12-15 10:56:02 -0500

    Update processing_logbook_data.R
    research vessels added

--- processing_logbook_data/processing_logbook_data.R
+++ processing_logbook_data/processing_logbook_data.R
@@ -344,6 +344,13 @@ SEFHIER_metrics_tracking <-
   rename(PERMIT_REGION = `Permit.Grouping.Region`,
          VESSEL_OFFICIAL_NUMBER = `Vessel.Official.Number`)
 
+# Logbooks |>
+  # SEFHIER_logbooks_usable_p_regions |>
+SEFHIER_metrics_tracking |>
+  filter(VESSEL_OFFICIAL_NUMBER == "FL3610NF") |>
+  glimpse()
+
+
 # import the list of SRHS vessels
 # this is a single spreadsheet with all vessels listed, as opposed to the version where they are separated by region (bothregions_asSheets)
 SRHS_vessels <-
@@ -1038,4 +1045,105 @@ write_rds(
 )
 
 # check missing/added boats ----
-"FL4980LH"
+not_all_na <- function(x) any(!is.na(x))
+
+my_check <- function(my_df, my_vsl) {
+  my_df |>
+    filter(VESSEL_OFFICIAL_NUMBER == my_vsl) |>
+    select(contains("perm"),
+           contains("effect")) |>
+    distinct() |>
+    select(where(not_all_na))
+}
+
+vsls_to_check <-
+  list(
+    "FL3610NF",
+    "FL4980LH",
+    "TX8671AM",
+    "1262695",
+    "FL0404LF",
+    "934563",
+    "1075595",
+    "1168377",
+    "676198",
+    "900347",
+    "612531",
+    "1097071",
+    "981337",
+    "1056956",
+    "FL5345ML",
+    "572241",
+    "521095",
+    "TX6348EK",
+    "680069",
+    "1325496",
+    "621251",
+    "FL4004NG",
+    "FL0526TJ",
+    "946290",
+    "1045140",
+    "FL8519NA",
+    "979631",
+    "968261",
+    "FL0223KJ",
+    "FL6731ST",
+    "FL9124ST",
+    "1276869",
+    "1275799",
+    "GA0824VK",
+    "FL6313GU",
+    "1213107",
+    "1292808",
+    "FL6016NY",
+    "FL7085PL",
+    "FL9370JC",
+    "1192239",
+    "1233666",
+    "996948",
+    "1078789",
+    "944179",
+    "FL5386PZ",
+    "FL6786PB",
+    "FL4862MW",
+    "AL0220VC",
+    "1227341",
+    "FL1815PL",
+    "1061390",
+    "FL3510LY",
+    "FL1202MW",
+    "1300336"
+  )
+
+dfs_to_check <-
+  list(SEFHIER_logbooks_usable_p_regions,
+  SEFHIER_metrics_tracking)
+
+test_res <-
+  map(vsls_to_check,
+      \(vessel_official_num) {
+        map(dfs_to_check,
+            \(my_df) {
+              my_check(my_df,
+                       vessel_official_num)
+            })
+      })
+names(test_res) <- vsls_to_check
+
+View(test_res)
+
+library(data.tree)
+nodes <- FromListExplicit(test_res)
+View(nodes)
+any(nodes$Get(function(node) length(as.list(node))) == 0)
+# [1] TRUE
+
+remove_empty <- function(x){
+  if(is.list(x)) {
+    x %>%
+      purrr::discard(rlang::is_na) %>%
+      purrr::map(remove_empty)
+  } else {
+    x
+  }
+
