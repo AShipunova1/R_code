23a13749f8cc4f261842b0c74d885e1f5b4457ff
AShipunova1 <anna.shipunova@noaa.gov>
2023-12-26 16:40:09 -0500

    start ggOceanMaps

--- boats_number/boats_number.R
+++ boats_number/boats_number.R
@@ -814,3 +814,46 @@ lat_lon_gom_state_cnt_sf |>
     # legend = FALSE
     layer.name = "GOM home port trips"
   )
+# ---
+GOM_400fm_path <-
+  file.path(my_paths$inputs,
+                      r"(shapefiles\GOM_400fm\GOM_400fm.shp)")
+
+# file.exists(GOM_400fm_path)
+# T
+
+GOMsf <-
+  sf::read_sf(GOM_400fm_path)
+# mapview(GOMsf)
+
+# ---
+sa_path <-
+  file.path(my_paths$inputs,
+                      r"(shapefiles\shapefiles_sa_eez_off_states\SA_EEZ_off_states.shp)")
+
+# file.exists(sa_path)
+
+sa_shp <- sf::read_sf(sa_path)
+
+# mapview(sa_shp)
+
+# ---
+### fl_state_w_counties ----
+fl_state_w_counties_path <-
+  file.path(my_paths$inputs,
+                      r"(shapefiles\GOVTUNIT_Florida_State_Shape\Shape\GU_CountyOrEquivalent.shp)")
+
+# file.exists(fl_state_w_counties_path)
+
+fl_state_w_counties_shp <- sf::read_sf(fl_state_w_counties_path)
+
+mapview(fl_state_w_counties_shp)
+
+install.packages("ggOceanMaps")
+library(ggOceanMaps)
+
+shapefile_list(name = "all")
+
+basemap(data = lat_lon_gom_state_cnt_sf, bathymetry = TRUE)
+
+# (dd_rbathy)
