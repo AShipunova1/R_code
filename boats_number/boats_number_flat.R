349800057971127e64d4027b6851d466dcd65773
AShipunova1 <anna.shipunova@noaa.gov>
2024-01-18 11:42:16 -0500

    Update boats_number_flat.R
    create paths

--- boats_number/boats_number_flat.R
+++ boats_number/boats_number_flat.R
@@ -17,17 +17,27 @@ library(viridis)
 library(grid)
 library(gridExtra)
 
-# from my functions ----
-# Define a function named 'get_username' that retrieves the username of the current user.
-get_username <- function() {
-    # Use 'Sys.info()' to obtain system information and then extract the 'user' field.
-    # Convert the result to a character to ensure it's in a usable format.
+# help functions 1 ----
+# current user name
+get_username <- function(){
     return(as.character(Sys.info()["user"]))
+}
+
+# set working directories in useful functions ----
+
+# Define a function named 'get_current_file_directory',
+# to obtain the directory where the script is located.
+get_current_file_directory <- function() {
+
+  # Use 'rstudioapi::getSourceEditorContext()' to access information about the currently open script
+  # Extract the 'path' from the source editor context and obtain its directory using 'dirname'
+  dirname(rstudioapi::getSourceEditorContext()$path)
 }
 
 # change main_r_dir, in_dir, out_dir, git_r_dir to your local environment
   # then you can use it in the code like my_paths$input etc.
 set_work_dir <- function() {
+
   # Set the working directory to the user's home directory (~)
   setwd("~/")
   base_dir <- getwd()
@@ -71,11 +81,62 @@ set_work_dir <- function() {
   return(my_paths)
 }
 
+# Define a function named 'set_work_dir_local'
+# This function sets the working directory to the user's home directory, defines paths to 'my_inputs,' 'my_outputs,' and 'R_code_github' directories, and returns these directory paths as a list. The use of file.path ensures that the path construction is platform-independent.
+
+set_work_dir_local <- function() {
+
+  # Set the working directory to the user's home directory (~)
+  setwd("~/")
+  base_dir <- getwd()
+
+  # Define 'main_r_dir' as "R_files_local"
+  main_r_dir <- "R_files_local"
+
+  # Define 'in_dir' as "my_inputs"
+  in_dir <- "my_inputs"
+
+  # Construct the full path to 'my_inputs' directory
+  full_path_to_in_dir <- file.path(base_dir, main_r_dir, in_dir)
+
+  # Define 'out_dir' as "my_outputs"
+  out_dir <- "my_outputs"
+
+  # Construct the full path to 'my_outputs' directory
+  full_path_to_out_dir <- file.path(base_dir, main_r_dir, out_dir)
+
+  # Define 'git_r_dir' as "R_code_github"
+  git_r_dir <- "R_code_github"
+
+  # Construct the full path to 'R_code_github' directory
+  full_path_to_r_git_dir <- file.path(base_dir, git_r_dir)
+
+  # Change the working directory to 'R_files_local'
+  setwd(file.path(base_dir, main_r_dir))
+
+  # Create a list of directory paths for 'inputs,' 'outputs,' and 'git_r'
+  my_paths <- list("inputs" = full_path_to_in_dir,
+                   "outputs" = full_path_to_out_dir,
+                   "git_r" = full_path_to_r_git_dir)
+
+  # Return the list of directory paths
+  return(my_paths)
+}
+
+# ===
+# Change the behavior of the set_work_dir function based on the username. If the username matches "anna.shipunova," it reassigns set_work_dir to the set_work_dir_local function, effectively using a different directory structure for Anna compared to other users.
+
+# Check if the current username is "anna.shipunova"
+if (get_username() == "anna.shipunova") {
+  # If the condition is true, assign the 'set_work_dir_local' function to 'set_work_dir'
+  set_work_dir <- set_work_dir_local
+}
+
 #### add-ons 2 ----
 
 
 my_paths <- set_work_dir()
-get_username()
+
 # Get the current project directory name using the 'this.path' package.
 current_project_dir_name <- this.path::this.dir()
 
