e1077103f6b804573a5aa82842aef8180cfa9dda
AShipunova1 <anna.shipunova@noaa.gov>
2024-01-22 09:27:40 -0500

    Update compare_4_files.R
    use joined permit and vessel table

--- compare_4_files/compare_4_files.R
+++ compare_4_files/compare_4_files.R
@@ -2,10 +2,12 @@
 # 1) compliance report downloaded from FHIER (= complaince module)
 # 2) logbooks from the Oracle db all_logbooks... (has 3 or 4 letters coded permit types)
 # 3) Metrics tracking from FHIER
-# 4) permit table from the Oracle db
+# 4) joined permit and vessel tables from the Oracle db
 # check transformed permits
 
 # setup ----
+source("~/R_code_github/useful_functions_module.r")
+
 library(tidyverse)
 
 # Determine the path of the executing script
@@ -27,7 +29,7 @@ curr_proj_input_path <- file.path(my_paths$inputs,
                          current_project_basename)
 
 # get data ----
-# 1) compliance report downloaded from FHIER (= complaince module)
+## 1) compliance report downloaded from FHIER (= complaince module) ----
 
 compliance_file_name <- "FHIER Compliance.csv"
 
@@ -41,7 +43,7 @@ compliance_from_fhier <-
 dim(compliance_from_fhier)
 # [1] 148375     17
 
-# 2) logbooks from the Oracle db all_logbooks... (has 3 or 4 letters coded permit types)
+## 2) logbooks from the Oracle db all_logbooks... (has 3 or 4 letters coded permit types) ----
 
 db_logbooks_query <-
   "SELECT
@@ -79,7 +81,7 @@ db_logbooks <- get_db_logbooks()
 dim(db_logbooks)
 # [1] 327869    149
 
-# 3) Metrics tracking from FHIER
+## 3) Metrics tracking from FHIER ----
 metrics_report_file_name <-
   r"(Detail Report - via Valid and Renewable Permits Filter (SERO_NEW Source).csv)"
 
@@ -95,32 +97,91 @@ metrics_report <- read_csv(metrics_report_file_path)
 dim(metrics_report)
 # [1] 3606   13
 
-# 4) permit table from the Oracle db
+## 4) joined permit and vessel table from the Oracle db ----
+# Need both tables to get the vessel official numbers
 
-mv_sero_fh_permits_his_query_file_path <-
-  file.path(my_paths$inputs, "get_db_data", "permit_info.rds")
+# get_vessels with permits 2021+ ----
 
-# file.exists(file_name_permits)
-# T
-
-mv_sero_fh_permits_his_query <-
-  "select * from
-srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov
+dates_filter <- " (end_date >= TO_DATE('01-JAN-21', 'dd-mon-yy')
+    OR expiration_date >= TO_DATE('01-JAN-21', 'dd-mon-yy') )
+  AND effective_date <= CURRENT_DATE
 "
+# Use that "dates_filter" in all parts of the union below.
 
-mv_sero_fh_permits_his_query_fun <- function(mv_sero_fh_permits_his_query) {
-  result <- dbGetQuery(con, mv_sero_fh_permits_his_query)
-  return(result)
-}
+# The 3 part union is needed because while the permit table has only one vessel id, the vessel table has 3 different columns for that (sero_official_number, coast_guard_nbr and state_reg_nbr) and we want to join tables by all 3 in turn.
+# stringr::str_glue is a function that allows you to create strings with placeholders for variable values. It works by using curly braces {} to enclose variable names within a string.
+vessels_permits_query <-
+  stringr::str_glue("SELECT
+  *
+FROM
+       srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
+  JOIN safis.vessels@secapxdv_dblk.sfsc.noaa.gov
+  ON ( p.vessel_id = sero_official_number )
+WHERE {dates_filter}
+UNION ALL
+SELECT
+  *
+FROM
+       srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
+  JOIN safis.vessels@secapxdv_dblk.sfsc.noaa.gov
+  ON ( p.vessel_id = coast_guard_nbr )
+WHERE
+  {dates_filter}
+UNION ALL
+SELECT
+  *
+FROM
+       srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov p
+  JOIN safis.vessels@secapxdv_dblk.sfsc.noaa.gov
+  ON ( p.vessel_id = state_reg_nbr )
+WHERE
+{dates_filter}
+")
 
-get_permit_info <-
-  function() {
-    read_rds_or_run(mv_sero_fh_permits_his_query_file_path,
-                    mv_sero_fh_permits_his_query,
-                    mv_sero_fh_permits_his_query_fun)
+vessels_permits_file_path <-
+  file.path(my_paths$inputs, "get_db_data", "vessels_permits.rds")
+
+vessels_permits_fun <-
+  function(vessels_permits_query) {
+    return(dbGetQuery(con,
+                      vessels_permits_query))
   }
 
-permit_info <- get_permit_info()
+get_vessels_permits <-
+  function() {
+    read_rds_or_run(vessels_permits_file_path,
+                    vessels_permits_query,
+                    vessels_permits_fun) |>
+      vessels_permits_id_clean()
+  }
+
+vessels_permits <- get_vessels_permits()
+# File: vessels_permits.rds modified 2024-01-02 10:27:22.824263
+
+# mv_sero_fh_permits_his_query_file_path <-
+#   file.path(my_paths$inputs, "get_db_data", "permit_info.rds")
+#
+# # file.exists(file_name_permits)
+# # T
+#
+# mv_sero_fh_permits_his_query <-
+#   "select * from
+# srh.mv_sero_fh_permits_his@secapxdv_dblk.sfsc.noaa.gov
+# "
+#
+# mv_sero_fh_permits_his_query_fun <- function(mv_sero_fh_permits_his_query) {
+#   result <- dbGetQuery(con, mv_sero_fh_permits_his_query)
+#   return(result)
+# }
+#
+# get_permit_info <-
+#   function() {
+#     read_rds_or_run(mv_sero_fh_permits_his_query_file_path,
+#                     mv_sero_fh_permits_his_query,
+#                     mv_sero_fh_permits_his_query_fun)
+#   }
+#
+# permit_info <- get_permit_info()
 # File: permit_info.rds modified 2023-10-19 09:34:47.729631
 
 dim(permit_info)
@@ -137,9 +198,18 @@ all_4_dfs <-
 all_4_df_names <- names(all_4_dfs)
 
 # prepare data for comparison ----
+## clean_headers ----
+
+all_4_dfs1 <- map(all_4_dfs, clean_headers)
+View(all_4_dfs1)
+
+
+# 1)
+print_df_names(all_4_dfs[[1]])
+# [1] "Vessel Official Number, Name, PermitGroup, Permit GroupExpiration, Year, Week, # GOM PermittedDeclarations, # CaptainReports, # NegativeReports, # ComplianceErrors, Compliant?, Set Permits On Hold?, Overridden?, Override Date, Override By, Contactedwithin 48 Hours?, SubmittedPower Down?"
+
 
 # get pairs ----
 
 # combn(my_col_names, 3) |>
-str(all_4_dfs)
-get_df_name()
+# str(all_4_dfs)
