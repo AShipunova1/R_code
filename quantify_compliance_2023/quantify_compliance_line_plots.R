9ec3c30d04e6be8727c5e89a0ffc49756477d6b3
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-01-26 15:53:54 -0500

    Update quantify_compliance_line_plots.R
    22 vs. 23

--- quantify_compliance_2023/quantify_compliance_line_plots.R
+++ quantify_compliance_2023/quantify_compliance_line_plots.R
@@ -2,16 +2,16 @@
 # run after quantify_compliance_from_fhier_month.R
 
 ## Month, line plots with dots ----
-# line_df_22_gom <- count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2$`2022 gom_dual`
+# line_df_23_gom <- count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2$`2023 gom_only`
 
-# line_df_22_sa <- count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2$`2022 sa_only`
+# line_df_23_sa <- count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2$`2023 sa_dual`
 
 percent_names <- paste0(seq(0, 100, by = 10), "%")
 
-# dim(line_df_22_gom)
+# dim(line_df_23_gom)
 # [1] 24 10
 
-# dim(line_df_22_sa)
+# dim(line_df_23_sa)
 # [1] 24 10
 
 geom_text_size = text_sizes[["geom_text_size"]]
@@ -19,7 +19,7 @@ geom_text_size <- 5
 axis_title_size <- text_sizes[["axis_text_x_size"]]
 axis_title_size <- 12
 
-line_df_22_gom_good_plot_l <-
+line_df_23_gom_good_plot_l <-
   names(count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2) |> 
   purrr::map(function(line_df_permit) {
     line_df <- count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2[[line_df_permit]]
@@ -58,22 +58,22 @@ line_df_22_gom_good_plot_l <-
           element_text(size = axis_title_size)
       ) +
       labs(size = "Groups of percentage",
-           x = "Months (2022)",
+           x = "Months (2023)",
            y = "Number of Vessels") +
-      labs(title = stringr::str_glue("The Number of Non-Compliant Vessels ({line_df_permit}) Each Month\nThat Were Compliant More Than 50% of a Month in 2022")) +
+      labs(title = stringr::str_glue("The Number of Non-Compliant Vessels ({line_df_permit}) Each Month\nThat Were Compliant More Than 50% of a Month in 2023")) +
       # theme(plot.title = element_text(lineheight = 0.9)) +
       labs(caption = "(The blue number is a total number of non-compliant vessels per month.)")
     
     return(one_plot)
   })
 
-line_df_22_gom_good_plot <- line_df_22_gom_good_plot_l[[1]]
-line_df_22_sa_good_plot <- line_df_22_gom_good_plot_l[[2]]
+line_df_23_gom_good_plot <- line_df_23_gom_good_plot_l[[1]]
+line_df_23_sa_good_plot <- line_df_23_gom_good_plot_l[[2]]
 
 # non compliant by month ----
-line_df_22_monthly_nc_plot_l <-
+line_df_23_monthly_nc_plot_l <-
   names(count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2) |>
-  # count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2$`2022 gom_dual` |>
+  # count_weeks_per_vsl_permit_year_compl_m_p_nc_b_cnt_in_b_p_short_y_r2$`2023 sa_dual` |>
   # filter(percent_non_compl_2_buckets == "< 50%") |>
   purrr::map(
     function(line_df_permit) {
@@ -114,57 +114,57 @@ line_df_22_monthly_nc_plot_l <-
           axis.text.y =
             element_text(size = axis_title_size)
         ) +
-        labs(x = "Months (2022)",
+        labs(x = "Months (2023)",
              y = "Number of Vessels") +
         labs(
           title = str_glue(
-            "The Number of Non-Compliant {curr_title_permit} Permitted Vessels Each Month in 2022"
+            "The Number of Non-Compliant {curr_title_permit} Permitted Vessels Each Month in 2023"
           )
         )
     })
 
-gom_line_df_22_monthly_nc_plot_l <- line_df_22_monthly_nc_plot_l[[1]]
-sa_line_df_22_monthly_nc_plot_l <- line_df_22_monthly_nc_plot_l[[2]]
+gom_line_df_23_monthly_nc_plot_l <- line_df_23_monthly_nc_plot_l[[1]]
+sa_line_df_23_monthly_nc_plot_l <- line_df_23_monthly_nc_plot_l[[2]]
 
-sa_line_df_22_monthly_nc_plot_l
+sa_line_df_23_monthly_nc_plot_l
 
 # GOM non compliant by month percent of total ----
 
-count_weeks_per_vsl_permit_year_compl_m_p_2022_gom <-
+count_weeks_per_vsl_permit_year_compl_m_p_2023_gom <-
   count_weeks_per_vsl_permit_year_compl_m_p |>
-  filter(year_permit == "2022 gom_dual")
+  filter(year_permit == "2023 sa_dual")
 
-count_weeks_per_vsl_permit_year_compl_m_p_2022_gom_c_cnts_short <-
-  count_weeks_per_vsl_permit_year_compl_m_p_2022_gom |>
+count_weeks_per_vsl_permit_year_compl_m_p_2023_gom_c_cnts_short <-
+  count_weeks_per_vsl_permit_year_compl_m_p_2023_gom |>
   select(year_month,
          total_vsl_m,
          cnt_vsl_m_compl,
          compliant_) |>
   dplyr::distinct()
 
-# View(count_weeks_per_vsl_permit_year_compl_m_p_2022_gom_c_cnts_short)
+# View(count_weeks_per_vsl_permit_year_compl_m_p_2023_gom_c_cnts_short)
 # [1] 24  4
 
-count_weeks_per_vsl_permit_year_compl_m_p_2022_gom_c_cnts_short_percent <-
-  count_weeks_per_vsl_permit_year_compl_m_p_2022_gom_c_cnts_short |>
+count_weeks_per_vsl_permit_year_compl_m_p_2023_gom_c_cnts_short_percent <-
+  count_weeks_per_vsl_permit_year_compl_m_p_2023_gom_c_cnts_short |>
   dplyr::group_by(year_month) |>
   mutate(percent_of_total = 100 * cnt_vsl_m_compl / total_vsl_m)
 
-glimpse(count_weeks_per_vsl_permit_year_compl_m_p_2022_gom_c_cnts_short_percent)
+glimpse(count_weeks_per_vsl_permit_year_compl_m_p_2023_gom_c_cnts_short_percent)
 
-line_df_22_gom_monthly_nc_percent_plot_color = plot_colors$non_compliant_by_month
+line_df_23_gom_monthly_nc_percent_plot_color = plot_colors$non_compliant_by_month
 
-line_df_22_gom_monthly_nc_percent_plot <-
-  count_weeks_per_vsl_permit_year_compl_m_p_2022_gom_c_cnts_short_percent |>
+line_df_23_gom_monthly_nc_percent_plot <-
+  count_weeks_per_vsl_permit_year_compl_m_p_2023_gom_c_cnts_short_percent |>
   filter(compliant_ == "NO") |> 
   ggplot(aes(
     x = as.Date(year_month),
     y = percent_of_total,
-    color = line_df_22_gom_monthly_nc_percent_plot_color
+    color = line_df_23_gom_monthly_nc_percent_plot_color
   )) +
-  geom_point(color = line_df_22_gom_monthly_nc_percent_plot_color,
+  geom_point(color = line_df_23_gom_monthly_nc_percent_plot_color,
              size = 4) +
-  geom_line(color = line_df_22_gom_monthly_nc_percent_plot_color,
+  geom_line(color = line_df_23_gom_monthly_nc_percent_plot_color,
             linewidth = 1) +
   theme_bw() +
   # text under the dot
@@ -172,7 +172,7 @@ line_df_22_gom_monthly_nc_percent_plot <-
     aes(label = paste0(round(percent_of_total, 1), "%")),
     # vjust = -0.4,
     hjust = -0.3,
-    color = line_df_22_gom_monthly_nc_percent_plot_color,
+    color = line_df_23_gom_monthly_nc_percent_plot_color,
     size = 6
   ) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b") +
@@ -184,10 +184,10 @@ line_df_22_gom_monthly_nc_percent_plot <-
       element_text(size = axis_title_size)
   ) +
   ylim(0, 50) +
-  labs(x = "Months (2022)",
+  labs(x = "Months (2023)",
        y = "Proportion of Non-Compliant Vessels") +
-  # labs(title = "The Percent of Non-Compliant GOM + Dual Permitted Vessels Each Month in 2022") +
-  expand_limits(x = as.Date("12/31/22", "%m/%d/%y"))
+  # labs(title = "The Percent of Non-Compliant GOM + Dual Permitted Vessels Each Month in 2023") +
+  expand_limits(x = as.Date("12/31/23", "%m/%d/%y"))
 # 
 #   coord_cartesian(xlim = c(as.Date(year_month), NA))
 
@@ -195,7 +195,7 @@ line_df_22_gom_monthly_nc_percent_plot <-
 # as.Date(dates, "%m/%d/%y")
 
 
-line_df_22_gom_monthly_nc_percent_plot
+line_df_23_gom_monthly_nc_percent_plot
 
 # save to files ----
 
@@ -208,20 +208,20 @@ plot_file_path_lines <-
 create_dir_if_not(plot_file_path_lines)
 
 file_full_name <- file.path(plot_file_path_lines,
-                            "line_df_22_sa_good_plot.png")
+                            "line_df_23_sa_good_plot.png")
 
 # see the function definition F2
 save_plots_list_to_files(file_full_name,
                          # plots
-                         line_df_22_sa_good_plot)
+                         line_df_23_sa_good_plot)
 
 # test_df |> 
-#   filter(year_month == "Jan 2022") |> 
+#   filter(year_month == "Jan 2023") |> 
 #   View()
 
 count_weeks_per_vsl_permit_year_compl_m_p_nc |> 
-  filter(year_month == "Jun 2022" &
-             year_permit == "2022 gom_dual" &
+  filter(year_month == "Jun 2023" &
+             year_permit == "2023 sa_dual" &
              percent_compl_m < 50) |> 
   summarise(n_distinct(vessel_official_number))
 # print_df_names(test_df)
@@ -295,12 +295,11 @@ test_plot +
 #   dplyr::arrange(year_month) |>
 #   View()
 # |>
-#   write_csv("month_with_numbers_gom_22.csv")
+#   write_csv("month_with_numbers_gom_23.csv")
 
 # count_weeks_per_vsl_permit_year_compl_m_p_nc |>
-#   filter(year_month == "May 2022" &
-#            year_permit == "2022 gom_dual" &
+#   filter(year_month == "May 2023" &
+#            year_permit == "2023 sa_dual" &
 #            percent_compl_m < 50) |>
 #   View()
-
 
