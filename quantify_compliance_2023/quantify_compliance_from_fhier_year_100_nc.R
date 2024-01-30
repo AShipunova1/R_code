a899d37c1b784e9e0f1f3ddc463a28fff5f307d2
AShipunova1 <anna.shipunova@noaa.gov>
2024-01-30 09:05:17 -0500

    Update quantify_compliance_from_fhier_year_100_nc.R
    clean up, num are ok

--- quantify_compliance_2023/quantify_compliance_from_fhier_year_100_nc.R
+++ quantify_compliance_2023/quantify_compliance_from_fhier_year_100_nc.R
@@ -78,9 +78,6 @@ sa_23_vessels <-
 # Calculate the percentage of never compliant entries from all non-compliant entries
 
 # Calculate the total number of non-compliant entries that are never compliant
-# by multiplying the number of rows in 'count_weeks_per_vsl_permit_year_compl_p_sa_23__non_c_100'
-# with 100 and dividing it by the number of rows in 'sa_23_non_c_vessels'
-
 sa_only_vessels_non_compl_total <-
   nrow(sa_23_non_c_vessels)
 # 1545
@@ -133,30 +130,26 @@ never_reported_filter <-
   rlang::quo(perc_nc_100_gr == 2 &
                tolower(compliant_) == "no")
 
-# count_weeks_per_vsl_permit_year_compl_p_sa_23__tot_perc <- 
+count_weeks_per_vsl_permit_year_compl_p_sa_23__tot_perc <-
   count_weeks_per_vsl_permit_year_compl_p_sa_23 |>
-    mutate(
-    perc_nc_100_gr = base::findInterval(percent_compl, c(1, 100))) |> 
-  mutate(perc_nc_100_gr_name =
+  mutate(perc_nc_100_gr = base::findInterval(percent_compl, c(1, 100))) |>
+  mutate(
+    perc_nc_100_gr_name =
       case_when(!!never_reported_filter ~
                   "Never Reported",
                 .default = "Reported At Least 1 Time")
   ) |>
   mutate(group_100_vs_rest =
-      case_when(!!never_reported_filter ~
-                  1,
-                .default = 2)
-  ) |> 
+           case_when(!!never_reported_filter ~
+                       1,
+                     .default = 2)) |>
   group_by(perc_nc_100_gr_name) |>
   mutate(group_vsl_cnt = n_distinct(vessel_official_number)) |>
-  dplyr::mutate(
-    perc_of_perc =
-          group_vsl_cnt * 100 / total_vsl_y_by_year_perm
-  ) |>
+  dplyr::mutate(perc_of_perc =
+                  group_vsl_cnt * 100 / total_vsl_y_by_year_perm) |>
   dplyr::ungroup()
 
-  
-  
+View(count_weeks_per_vsl_permit_year_compl_p_sa_23__tot_perc)
 
 count_weeks_per_vsl_permit_year_compl_p_short_count__not_compl__sa_tot_perc <-
   count_weeks_per_vsl_permit_year_compl_p_short_count__not_compl__sa_tot |>
