77e08ee707206a644e01f36987be013184625bd0
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-01 11:08:44 -0500

    Create vessels_from_pims_fix_port.R
    bad port names

---
+++ non_compliant_areas/vessels_from_pims_fix_port.R
@@ -0,0 +1,50 @@
+vessels_from_pims_fix_port__space <-
+  vessels_from_pims |>
+  select(official__, vessel_id, owner, hin, hailing_port) |>
+  filter(grepl(" ,", hailing_port)) |> 
+  distinct()
+nrow(vessels_from_pims_fix_port__space)
+# 349   
+
+write_csv(
+  vessels_from_pims_fix_port__space,
+  file.path(current_output_dir,
+            "vessels_from_pims_fix_port__space.csv")
+)
+
+vessels_from_pims_fix_port__digit <-
+  vessels_from_pims |>
+  select(official__, vessel_id, owner, hin, hailing_port) |>
+  filter(grepl("\\d", hailing_port)) |> 
+  distinct() |> 
+  filter(!official__ %in% c("NC6421AU",
+                            "1301930",
+                            "GA1769JL"))
+# OK names
+# FIGURE 8 ISLAND, NC
+# 22411 GENO LANE, AL
+# 117 HAWK LANDING LN, GA
+
+nrow(vessels_from_pims_fix_port__digit)
+# 53
+
+write_csv(
+  vessels_from_pims_fix_port__digit,
+  file.path(current_output_dir,
+            "vessels_from_pims_fix_port__digit.csv")
+)
+
+vessels_from_pims_fix_port__none <-
+  vessels_from_pims |>
+  select(official__, vessel_id, owner, hin, hailing_port) |>
+  filter(is.na(hailing_port)) |>
+  distinct()
+
+nrow(vessels_from_pims_fix_port__none)
+# 18
+
+write_csv(
+  vessels_from_pims_fix_port__none,
+  file.path(current_output_dir,
+            "vessels_from_pims_fix_port__none.csv")
+)
