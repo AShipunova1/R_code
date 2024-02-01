13e8b93471d4643bfd87a864805a4b0dfc4c7495
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-01 09:13:43 -0500

    Update non_compliant_areas_get_data.R
    get vessel info, start

--- non_compliant_areas/non_compliant_areas_get_data.R
+++ non_compliant_areas/non_compliant_areas_get_data.R
@@ -38,11 +38,11 @@ toc()
 compl_err_db_data <- 
   all_get_db_data_result_l$compl_err_db_data
 
-
+# get permit info from PIMS ----
 get_permit_data_from_PIMS <- function() {
   permit_names_file_path =
-    file.path(curr_proj_input_path,
-              r"(Permits - 2024-01-25_0904.xlsx)")
+    file.path(my_paths$inputs,
+              r"(from PIMS\Permits - 2024-01-25_0904.xlsx)")
   
   # file.exists(permit_names_file_path)
   
@@ -61,7 +61,8 @@ get_permit_data_from_PIMS <- function() {
 # Use the 'separate_wider_delim' function to split the 'permit__' column in the 'active_permits_from_pims_temp1' dataframe
 # based on the delimiter "-", creating new columns 'permit_code' and 'permit_num'.
 # The 'too_many' argument is set to "merge," which means any excess columns generated during the split will be merged.
-active_permits_from_pims_temp2 <- active_permits_from_pims_temp1 %>%
+active_permits_from_pims_temp2 <- 
+  active_permits_from_pims_temp1 %>%
     separate_wider_delim(permit__,
                          "-",                # Delimiter used for splitting
                          names = c("permit_code", "permit_num"),
@@ -109,6 +110,83 @@ active_permits_from_pims_temp2 <- active_permits_from_pims_temp1 %>%
   return(active_permits_from_pims)
 }
 
+permit_info <- get_permit_data_from_PIMS()
+# print_df_names(permit_info)
+# [1] 23575    13
+
+# get vessel (home port) info from PIMS ----
+get_vessel_data_pims <- 
+  function() {
+  
+  vessel_names_file_path =
+    file.path(my_paths$inputs,
+              r"(from PIMS\vessels - 2024-01-25_0904.xlsx)")
+  
+  # file.exists(vessel_names_file_path)
+  
+  active_vessels_from_pims_raw <- 
+    read_xlsx(vessel_names_file_path, skip = 5)
+  
+  dim(active_vessels_from_pims_raw)
+  # [1] 23575    11
+  
+  # clean_headers
+  active_vessels_from_pims_temp1 <-
+    active_vessels_from_pims_raw %>%
+    clean_headers()
+  
+  # separate columns
+# Use the 'separate_wider_delim' function to split the 'vessel__' column in the 'active_vessels_from_pims_temp1' dataframe
+# based on the delimiter "-", creating new columns 'vessel_code' and 'vessel_num'.
+# The 'too_many' argument is set to "merge," which means any excess columns generated during the split will be merged.
+active_vessels_from_pims_temp2 <- 
+  active_vessels_from_pims_temp1 %>%
+    separate_wider_delim(vessel__,
+                         "-",                # Delimiter used for splitting
+                         names = c("vessel_code", "vessel_num"),
+                         too_many = "merge") %>%
+
+    # Use the 'separate_wider_regex' function to split the 'vessel_or_dealer' column in the resulting dataframe.
+    # This function uses regular expressions to define patterns for creating new columns.
+    # In this case, it defines patterns for 'vessel_official_number' and 'vessel_name.'
+    # The 'too_few' argument is set to "align_start," which aligns any missing columns at the start.
+    separate_wider_regex(
+      cols = vessel_or_dealer,
+      patterns = c(
+        vessel_official_number = "[A-Za-z0-9]+",  # Regular expression for vessel official number (more than one alphanumeric character)
+        " */* ",                                  # Pattern for separating columns with slashes
+        vessel_name = "[A-Za-z0-9]+"              # Regular expression for vessel name (more than one alphanumeric character)
+      ),
+      too_few = "align_start"
+    )
+
+  # correct dates format
+
+  # get a list of field names with "_date"
+  # Use the 'grep' function to find and extract column names from the 'active_vessels_from_pims_temp2' dataframe
+  # that has "_date".
+  ends_with_date_fields <- grep("_date", # Pattern to search for in column names
+                                names(active_vessels_from_pims_temp2),  # Names of columns to search within
+                                value = TRUE)         # Return matching column names as values in the result.
+
+
+  # convert to the date format
+  active_vessels_from_pims <-
+    change_fields_arr_to_dates(active_vessels_from_pims_temp2,
+                               ends_with_date_fields,
+                               "%m/%d/%Y")
+
+  # test
+  active_vessels_from_pims %>%
+    dplyr::select(status_date) %>%
+    dplyr::arrange(dplyr::desc(status_date)) %>%
+    dplyr::distinct() %>% 
+    head()
+
+  return(active_vessels_from_pims)
+}
+
+"C:\Users\anna.shipunova\Documents\R_files_local\my_inputs\from PIMS\Vessels - 2024-02-01_0909.xlsx"
 
 # OLD ----
 ## use metricks only vessels ----
