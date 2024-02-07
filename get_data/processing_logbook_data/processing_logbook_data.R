65df8284ea52484bdb4131977cd4a71247984b37
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-07 13:40:32 -0500

    Update processing_logbook_data.R
    # Start the log ----

--- get_data/processing_logbook_data/processing_logbook_data.R
+++ get_data/processing_logbook_data/processing_logbook_data.R
@@ -92,6 +92,10 @@ if (Path == annas_path) {
 
 source(auxiliary_methods_file_path)
 
+# Start the log ----
+my_tee(date(),
+       my_title = str_glue("Start logbooks processing for {my_year}"))
+
 # Get data ----
 
 # This section is needed to pull data from Oracle database
