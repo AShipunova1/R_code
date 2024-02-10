8f6a48fd6ffd5f66e98ff48916883e49a779b335
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-10 10:08:29 -0500

    Update dual_landing_2023_get_data.R
    waters_shape_prep

--- dual_landing_2023/dual_landing_2023_get_data.R
+++ dual_landing_2023/dual_landing_2023_get_data.R
@@ -7,9 +7,12 @@ processed_input_data_path <-
 dir.exists(processed_input_data_path)
 # T  
 
+# Upload
 # 1) Processed Logbooks
 # SEFHIER_permitted_vessels_nonSRHS_{my_year}
 # "~\R_files_local\my_inputs\processing_logbook_data\Outputs\SEFHIER_processed_Logbooks_2022.rds"
+
+# 2) maps
 
 # get processed logbook data ----
 # For all years
@@ -30,7 +33,36 @@ names(processed_logbooks) <-
   names(processed_logbooks) |>
   tolower()
 
-results <-
-  c("processed_logbooks")
+# waters_shape_prep ----
+waters_shape_prep_path <- 
+  file.path(my_paths$git_r,
+            "get_data",
+            "waters_shape_prep.R")
+# file.exists(waters_shape_prep_path)
+# T
 
-cat(results, sep = "\n")
+source(waters_shape_prep_path)
+
+result_names <-
+  c("processed_logbooks",
+    "south_east_coast_states",
+    "east_coast_states",
+    "sa_council_states",
+    "south_atlantic_states",
+    "fl_counties",
+    "my_state_abb",
+    "my_state_name",
+    "GOMsf",
+    "world_state_and_fed_waters_path",
+    "fl_state_w_counties_shp",
+    "GOM_s_fl_state_waters_only",
+    "shp_4326_list: ",
+    "east_coast_sa_state_waters_shp",
+    "gom_fl_state_w_counties_shp",
+    "sa_fl_state_w_counties_shp",
+    "sa_shp",
+    "gom_states_shp",
+    "sa_states_shp")
+
+title_message_print(result_names)
+
