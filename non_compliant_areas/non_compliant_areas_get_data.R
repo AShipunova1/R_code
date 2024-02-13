66dd027d80f1e7b8938aafb1f43f62a47532a0eb
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-13 14:59:04 -0500

    Update non_compliant_areas_get_data.R
    processed_metrics_tracking_file_names for all years

--- non_compliant_areas/non_compliant_areas_get_data.R
+++ non_compliant_areas/non_compliant_areas_get_data.R
@@ -278,10 +278,9 @@ dir.exists(processed_input_data_path)
 
 # [1] "~\\R_files_local\\my_inputs\\processing_logbook_data//Outputs//SEFHIER_permitted_vessels_nonSRHS_2023.rds"
 # file names for all years
-# file name for 2023 only
 processed_metrics_tracking_file_names <-
   list.files(path = processed_input_data_path,
-             pattern = str_glue("SEFHIER_permitted_vessels_nonSRHS_{my_year2}"),
+             pattern = str_glue("SEFHIER_permitted_vessels_nonSRHS_*"),
              recursive = TRUE,
              full.names = TRUE)
 
@@ -587,13 +586,6 @@ compl_err_db_data_metrics_2022_23_clean__ports_short__comb_col_list |>
 
 # 1 NO          1363
 # 2 YES         1818
-
-# $`2023 sa_only`
-# # A tibble: 2 × 2
-#   compliant_     n
-#   <chr>      <int>
-# 1 NO          1343
-# 2 YES         1787
 
 map(compl_err_db_data_metrics_2022_23_clean__ports_short__comb_col_list,
     \(reg_df) {
