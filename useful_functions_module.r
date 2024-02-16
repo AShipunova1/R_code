e0c437a5b7195528a3573fce6ecd8262b103561b
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-16 10:50:16 -0500

    Update useful_functions_module.r
      print(myfiles)

--- useful_functions_module.r
+++ useful_functions_module.r
@@ -178,6 +178,9 @@ load_csv_names <- function(csv_files_paths, csv_names_list) {
  # Use 'lapply' to add the 'my_inputs' directory path in front of each file name in 'csv_names_list'
   # This creates a list of full file paths for the CSV files
   myfiles <- lapply(csv_names_list, function(x) file.path(csv_files_paths, x))
+  
+  # browser()
+  print(myfiles)
 
   # Use 'lapply' again to read all CSV files listed in 'myfiles'
   # The 'read_csv' function from the 'readr' package is used, specifying default column types as 'c' ('character')
