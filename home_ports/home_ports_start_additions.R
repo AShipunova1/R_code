3873b2d32e3fd7ce04ea8a0f366dca9b470a87db
AShipunova1 <anna.shipunova@noaa.gov>
2024-02-29 14:21:03 -0500

    Update home_ports_start_additions.R
    # Jenny's check ----

--- home_ports/home_ports_start_additions.R
+++ home_ports/home_ports_start_additions.R
@@ -232,3 +232,76 @@ vessels_from_pims_split_addr__city_state__fix2_ok_more_ports_more_ports |>
 cat("vessels_from_pims_split_addr__city_state__fix2_ok_more_ports_more_ports",
     sep = "\n")
 
+# Jenny's check ----
+#just keep vessel official number and state
+ports_vsls_cnts <- 
+  vessels_from_pims_split_addr__city_state__fix2_ok_short |> 
+  select(vessel_official_number, state_fixed) |> 
+  distinct() |> 
+  count(vessel_official_number)
+
+double_ports <-
+  ports_vsls_cnts |>
+  filter(n > 1)
+
+dim(double_ports)
+# 17 no uniqued
+# 5 uniqued
+   # vessel_official_number     n
+#    <chr>                  <int>
+#  1 1112053                    2
+#  2 1166732                    2
+#  3 596153                     2
+#  4 671353                     2
+#  5 FL                         8
+#  6 FL1862SU                   2
+#  7 FL5262LD                   2
+#  8 FL8000NR                   2
+#  9 FLORIDA                    2
+# 10 LA4017BH                   2
+# 11 LA6968EP                   2
+# 12 MD                         2
+# 13 N/A                        2
+# 14 NA                         9
+# 15 NC6164CW                   2
+# 16 NO                         2
+# 17 NONE                       3
+
+# double_ports
+# 1 1112053                    2 NEW BERN, NC
+# 2 596153                     2 NEW BERN, NC
+# 3 671353                     2 SWANSBORO, NC
+# 4 N/A                        2
+# 5 NA                         5
+
+# vessels_from_pims_split_addr__city_state__fix2_ok_short |> 
+#   filter(vessel_official_number %in% double_ports$vessel_official_number) |> 
+#   View()
+
+# check empty vessel ids ----
+is_empty <- c(NA, "NA", "", "UN", "N/A")
+
+vessels_from_pims_split_addr__city_state__fix2_ok_short__good_ids <-
+  vessels_from_pims_split_addr__city_state__fix2_ok_short |>
+  filter(!vessel_official_number %in% is_empty)
+  
+dim(vessels_from_pims_split_addr__city_state__fix2_ok_short)
+# [1] 23073     3
+dim(vessels_from_pims_split_addr__city_state__fix2_ok_short__good_ids)
+# [1] 23062     3
+
+# vessels_from_pims |> 
+#   filter(grepl("LIGHTHOUSE", hailing_port)) |>
+#   filter(!grepl("NOVESID", official__)) |> 
+#   View()
+# LIGHTHOUSE
+
+# grep("NOVES", vessels_from_pims_split_addr__city_state__fix2_ok_short$vessel_official_number, value = T)
+# [1] "NOVESSEL"
+
+vessels_from_pims_split_addr__city_state__fix2_ok_short__good_ids |> 
+  select(vessel_official_number, state_fixed) |> 
+  distinct() |> 
+  count(vessel_official_number) |> 
+  filter(n > 1)
+# 0! fixed
