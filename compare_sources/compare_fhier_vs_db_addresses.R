5180c2ed67bd3917f5e64815e258436ad7f4cba1
AShipunova1 <anna.shipunova@noaa.gov>
2024-03-02 14:12:18 -0500

    Update compare_fhier_vs_db_addresses.R
    use clean func

--- compare_sources/compare_fhier_vs_db_addresses.R
+++ compare_sources/compare_fhier_vs_db_addresses.R
@@ -1,5 +1,27 @@
-# compare addresses from fhier and db ----
+library(arsenal)
 # get_data from from egregious violators
+
+# aux functions ----
+subdf_prep <-
+  function(my_df,
+           field_names_from,
+           field_names_to,
+           sort_by = "vessel_official_number") {
+    my_df_renamed <-
+      my_df |>
+      rename_with( ~ field_names_to,
+                   .cols = all_of(field_names_from))
+    
+    my_df_renamed_cleaned_sorted <-
+      my_df_renamed |>
+      mutate(across(where(is.character), str_squish)) |>
+      arrange(sort_by)
+    
+    return(my_df_renamed_cleaned_sorted)
+  }
+
+# compare addresses from fhier and db ----
+## prepare sub dfs to have the same columns ----
 names(fhier_addr_short) |> 
   cat(sep = '", "')
 
@@ -70,9 +92,9 @@ fhier_addr_short_arr <-
   mutate(across(where(is.character), str_squish)) |> 
   arrange(vessel_official_number)
 
+## actual compare fhier and erv ----
 
 # diffdf::diffdf(fhier_addr_short_arr, db_participants_asddress_short)
-library(arsenal)
 
 address_compare <-
   summary(
@@ -87,12 +109,12 @@ address_compare <-
 # all.equal(address_compare,
 #                address_compare2)
 
-aa <-
+res_fhier__db_erv <-
   address_compare$diffs.table |>
   filter(vessel_official_number == "1020822") |>
   select(values.x, values.y)
 
-setdiff(aa[[1]], aa[[2]])
+# setdiff(aa[[1]], aa[[2]])
 # 0
 setdiff(aa[[2]], aa[[1]])
 # [1] "JUDY LYNN HELMEY"
@@ -106,7 +128,7 @@ setdiff(aa[[2]], aa[[1]])
 # r"(compare\address_compare__fhier_vs_db.csv)")
 
 # compare erv and erb addresses in db ----
-
+## prepare sub dfs to have the same col names ----
 # print_df_names(db_participants_asddress)
 # ser_id, official_number, uscg_documentation, state_registration, vchar_hull_id_number, vchar_vessel_name, is_primary, is_mail_rec, erv_ser_id, erv_entity_type, erv_entity_name, erv_ph_is_primary, erv_ph_area, erv_ph_number, erv_primary_email, erv_physical_address1, erv_physical_address2, erv_physical_city, erv_physical_county, erv_physical_state, erv_physical_zip_code, erv_mailing_address1, erv_mailing_address2, erv_mailing_city, erv_mailing_county, erv_mailing_country, erv_mailing_state, erv_mailing_zip_code, association_start_dt, relationship, erb_ser_id, erb_entity_type, erb_entity_name, erb_ph_is_primary, erb_ph_area, erb_ph_number, erb_primary_email
 
@@ -158,11 +180,47 @@ dim(db_participants_asddress_erv)
 dim(db_participants_asddress_2)
 # [1] 35462    12
 
-db_participants_asddress_2_renamed <-
-  db_participants_asddress_2 |>
-  rename_with(~ erv_fields,
-              .cols = all_of(second_field_set))
+db_participants_asddress_erv_cleaned <- 
+  subdf_prep(db_participants_asddress_erv,
+             erv_fields,
+             erv_fields,
+             sort_by = "official_number")
+
+db_participants_asddress_2_cleaned <- 
+  subdf_prep(db_participants_asddress_2,
+             second_field_set,
+             erv_fields,
+             sort_by = "official_number")
+
+  # db_participants_asddress_2 |>
+  # rename_with(~ erv_fields,
+  #             .cols = all_of(second_field_set))
+
+# all.equal(db_participants_asddress_2_renamed,
+#           db_participants_asddress_2_renamed__1)
+# F (trimmed)
 
 identical(names(db_participants_asddress_erv),
           names(db_participants_asddress_2_renamed))
 # T
+
+## compare w arsenal ----
+
+address_compare__in_db <-
+  summary(
+    comparedf(
+      db_participants_asddress_erv_cleaned,
+      db_participants_asddress_2_cleaned,
+      by = "official_number"
+    ),
+    max.print.diffs.per.var = NA,
+    max.print.diffs	= NA,
+    tol.char = "both"
+  )
+
+address_compare__in_db$diffs.table |>
+  filter(!grepl("entity_type", var.x)) |>
+  write_csv(r"(compare\address_compare__db_erv_vs_db_other.csv)")
+
+
+# entity_type - buisness vs. individual
