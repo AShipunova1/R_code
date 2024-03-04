864893a74f9bd61e61858f968efabe97198954e0
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-03-04 11:09:35 -0500

    Update useful_functions_module.r
    clean_names_and_addresses <- function(

--- useful_functions_module.r
+++ useful_functions_module.r
@@ -1344,3 +1344,43 @@ add_cnt_in_gr <-
       ungroup() %>%
       return()
   }
+
+# ---
+clean_names_and_addresses <- function(my_df) {
+
+  my_df_cleaned <-
+    my_df |>
+    mutate(
+      across(where(is.character),
+             ~ str_squish(.x)),
+      across(where(is.character),
+             ~ replace_na(.x, "")),
+      across(where(is.character),
+             ~ str_replace_all(.x, ", ;", ";")),
+      across(where(is.character),
+             ~ str_replace_all(.x, "\\s+[,;]", ",")),
+      across(where(is.character),
+             ~ str_replace_all(.x, ";,+", ";")),
+      across(where(is.character),
+             ~ str_replace_all(.x, ";;+", ";")),
+      across(where(is.character),
+             ~ str_replace_all(.x, ",,+", ",")),
+      across(where(is.character),
+             ~ str_replace_all(.x, "[,;] *\\bUN\\b *", "")),
+      across(where(is.character),
+                          ~ str_replace_all(.x, "\\bUN\\b", "")),
+      across(where(is.character),
+             ~ str_replace_all(.x, "\\s*\\bUN\\b\\s*", "")),
+      across(where(is.character),
+             ~ str_replace_all(.x, "^[,;] ", "")),
+      across(where(is.character),
+             ~ str_replace_all(.x, "^[,;]$", "")),
+      across(where(is.character),
+             ~ str_replace_all(.x, "[,;]$", "")),
+      across(where(is.character),
+             ~ str_squish(.x))
+    )
+
+  return(my_df_cleaned)
+}
+
