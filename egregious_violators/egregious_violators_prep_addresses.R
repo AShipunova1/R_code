eac162e761cbfba8b9429ac8b4235b1c75ea079b
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-03-08 14:45:17 -0500

    Update egregious_violators_prep_addresses.R
    clean up

--- egregious_violators/egregious_violators_prep_addresses.R
+++ egregious_violators/egregious_violators_prep_addresses.R
@@ -108,7 +108,6 @@ col_part_names <-
     "ph_is_primary",
     "ph_area",
     "ph_number",
-    "entity_name",
     "physical_city",
     "physical_county",
     "physical_state",
@@ -146,8 +145,6 @@ db_participants_address__needed_short <-
   ) |>
   distinct()
 
-# ser_id, official_number, uscg_documentation, state_registration, vchar_hull_id_number, vchar_vessel_name, is_primary, is_mail_rec, erv_ser_id, erv_entity_type, erv_entity_name, erv_ph_is_primary, erv_ph_area, erv_ph_number, erv_primary_email, erv_physical_address1, erv_physical_address2, erv_physical_city, erv_physical_county, erv_physical_state, erv_physical_zip_code, erv_mailing_address1, erv_mailing_address2, erv_mailing_city, erv_mailing_county, erv_mailing_country, erv_mailing_state, erv_mailing_zip_code, association_start_dt, relationship, erb_ser_id, erb_entity_type, erb_entity_name, erb_ph_is_primary, erb_ph_area, erb_ph_number, erb_primary_email
-
 nrow(compl_corr_to_investigation__corr_date__hailing_port__fhier_addr)
 # 199
 n_distinct(compl_corr_to_investigation__corr_date__hailing_port__fhier_addr$vessel_official_number)
@@ -160,100 +157,21 @@ dim(db_participants_address__needed_short)
 n_distinct(db_participants_address__needed_short$official_number)
 # 71
 
-# compl_corr_to_investigation__corr_date__hailing_port__fhier_addr |> 
-#   print_df_names()
-
-# db_participants_address__needed_short |> 
-  # filter(!erv_ph_number == erb_ph_number)
-# 0
-# erv_ph_area	erb_ph_area
-# erv_ph_number	erb_ph_number
-
+## combine area and phone numbers ----
 db_participants_address__needed_short__phone0 <- 
   db_participants_address__needed_short |> 
   mutate(erv_phone = paste0(erv_ph_area, erv_ph_number),
          erb_phone = paste0(erb_ph_area, erb_ph_number))
 
-# db_participants_address__needed_short__phone1 <-
-#   db_participants_address__needed_short__phone0 |>
-#   group_by(official_number) |>
-#   summarise(across(ends_with("_phone"), ~list(sort(unique(.x))))) |> 
-#   ungroup() |> 
-#   as.data.frame()
-
 ## make erv and erb combinations ----
 
-list_sort_uniq <- function(my_lists) {
-  # browser()
-  list(sort(unique(str_trim(my_lists)))) |> 
-    flatten() %>%
-    return()
-}
-
-# print_df_names(db_participants_address__needed_short__phone0)
-
-db_participants_address__needed_short__phone2 <-
-  db_participants_address__needed_short__phone0 |>
-  group_by(official_number) |>
-  mutate(db_phone = pmap(across(ends_with("_phone")),
-                         ~ list_sort_uniq(.))) |>
-  ungroup()
-
-# View(db_participants_address__needed_short__phone2)
-# c("3364239470", 
-#   "3365282062", 
-#   "3367239470")
-
-  # mutate(db_phone = paste(sort(unique(
-  #   erv_phone, erb_phone
-  # )),
-  # collapse = ", "))
-#   filter(grepl("&", active_or_expired)) |>
-  
-
-# View(db_participants_address__needed_short__phone1)
-
-
-# rr2 <- 
-  # col_part_names |>
-#      map_df(~ df %>%
-#            transmute(!! str_c(.x, '3') :=  !! rlang::sym(str_c(.x, '1'))  + 
-#          !! rlang::sym(str_c(.x, 2)))) %>%
-#      bind_cols(df, .)
-# list_cbind
-
-col_pre %>%
-     map_dfc(~ df %>%
-           transmute(!! str_c(.x, '3') :=  !! rlang::sym(str_c(.x, '1'))  + 
-         !! rlang::sym(str_c(.x, 2)))) %>%
-     bind_cols(df, .)
-
-
-df <- db_participants_address__needed_short__phone0
-res3 <-   
-col_part_names |>
-     map_dfc(~ df %>%
-      # group_by(official_number) |>
-      mutate(pmap(across(ends_with(.x)),
-                                                    ~ list_sort_uniq(.)),
-             .keep = "none",
-             )) |>
-      # ungroup()) |>
-         #                  transmute(!! str_c(.x, '3') :=  !! rlang::sym(str_c(.x, '1'))  + 
-         # !! rlang::sym(str_c(.x, 2)))) %>%
-     bind_cols(df, .)
-
-new_df <- db_participants_address__needed_short__phone0 |>
-  select(official_number)
-
 tic("map all pairs 3")
-rr3 <-
+db_participants_address__needed_short__erv_erb_combined <-
   col_part_names |>
   map(\(curr_col_part)  {
     new_col_name <- str_glue("db_{curr_col_part}")
-    cat(new_col_name, sep = "\n")
+    # cat(new_col_name, sep = "\n")
     
-    res <-
       db_participants_address__needed_short__phone0 |>
       group_by(official_number) |>
       mutate(!!new_col_name := pmap(across(ends_with(curr_col_part)),
@@ -263,7 +181,6 @@ rr3 <-
       select(-official_number)
     
   }) %>%
-  # set_names(paste0("sum_", nm1)) %>%
   bind_cols(db_participants_address__needed_short__phone0, .)
 toc()
 
