534e592b964a5050db38c5efee6fecbe4c6fc1ad
AShipunova1 <anna.shipunova@noaa.gov>
2024-03-25 16:55:52 -0400

    Update rselenum_test.R
    clean up

--- rselenium_test/rselenum_test.R
+++ rselenium_test/rselenum_test.R
@@ -20,23 +20,20 @@ remote_driver$navigate("https://grunt.sefsc.noaa.gov/apex/f?p=162:LOGIN_DESKTOP:
 username = keyring::key_list("SECPR")[1, 2]
 password = keyring::key_get("SECPR", keyring::key_list("SECPR")[1, 2])
 
-fhier_username <- rstudioapi::askForSecret("fhier_username")
-fhier_password <- rstudioapi::askForSecret("fhier_password")
-
-# key_set_with_value("R-test-service", "donaldduck", password = "secret",
-#                    keyring = "foobar")
-
-keyring::keyring_list()
-  key_get("SECPR", keyring::key_list("SECPR")[1, 2])
+# fhier_username <- rstudioapi::askForSecret("fhier_username")
+# fhier_password <- rstudioapi::askForSecret("fhier_password")
 
 remote_driver$getTitle()
+
+# login ----
 P101_USERNAME <- remote_driver$findElement(using = "id", "P101_USERNAME")
 P101_USERNAME$clearElement()
-P101_USERNAME$sendKeysToElement(list(fhier_username))
+# P101_USERNAME$sendKeysToElement(list(fhier_username))
+P101_USERNAME$sendKeysToElement(list(rstudioapi::askForSecret("fhier_username")))
 
 P101_PASSWORD <- remote_driver$findElement(using = "id", "P101_PASSWORD")
 P101_PASSWORD$clearElement()
-P101_PASSWORD$sendKeysToElement(list(fhier_password))
+P101_PASSWORD$sendKeysToElement(list(rstudioapi::askForSecret("fhier_password")))
 
 login_button <-
   remote_driver$findElement(using = "id",
@@ -46,3 +43,6 @@ login_button$clickElement()
 
 # //*[@id="B705187520008063447"]
 # <button class="t-Button t-Button--icon t-Button--iconLeft t-Button--hot" onclick="apex.submit({request:'LOGIN'});" type="button" id="B705187520008063447"><span class="t-Icon t-Icon--left fa fa-sign-in" aria-hidden="true"></span><span class="t-Button-label">Log In</span><span class="t-Icon t-Icon--right fa fa-sign-in" aria-hidden="true"></span></button>
+
+# download compliance ----
+
