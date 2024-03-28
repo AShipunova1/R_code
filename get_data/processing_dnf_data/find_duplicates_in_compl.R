8d6cedfccf0506c1e9e889bee828a8266eb6e21a
AShipunova1 <anna.shipunova@noaa.gov>
2024-03-28 14:24:30 +0100

    # Find all duplicates:

--- get_data/processing_dnf_data/find_duplicates_in_compl.R
+++ get_data/processing_dnf_data/find_duplicates_in_compl.R
@@ -16,7 +16,8 @@ compl_override_data__renamed__this_year |>
 # ℹ Row 22748 of `y` matches multiple rows in `x`.
 # We need the many to many relationship because the DNFs represent a single day in a 7 day week, while the compliance represents a single week. So the relationship between DNFs to Compliance is 7 to 1.
 
-# All duplicates:
+# Find all duplicates:
+# 1)
 duplicates_in_compl <-
   compl_override_data__renamed__this_year |>
   select(VESSEL_OFFICIAL_NUMBER,
@@ -28,21 +29,41 @@ duplicates_in_compl <-
   filter(n > 1) |>
   ungroup()
 
+# 2)
+dupe2 <-
+  compl_override_data__renamed__this_year |>
+  select(VESSEL_OFFICIAL_NUMBER,
+         COMP_YEAR,
+         COMP_WEEK,
+         SRH_VESSEL_COMP_ID) |>
+  group_by(VESSEL_OFFICIAL_NUMBER, COMP_YEAR, COMP_WEEK) |>
+  mutate(dups = any(duplicated(VESSEL_OFFICIAL_NUMBER, COMP_YEAR, COMP_WEEK))) %>%
+  ungroup()
 
-dup_SRH_VESSEL_COMP_ID <-
+compl_override_data__renamed__this_year |>
+  filter(VESSEL_OFFICIAL_NUMBER == "694424" &
+           COMP_YEAR == 2022 &
+           COMP_WEEK == 33) |> glimpse()
+
+# In FHIER only one entry without overridden
+
+dupe_entries <-
+  dupe2 |> filter(dups == TRUE) |> select(SRH_VESSEL_COMP_ID)
+
+compl_override_data__renamed__this_year |>
+  filter(SRH_VESSEL_COMP_ID %in% dupe_entries$SRH_VESSEL_COMP_ID) |>
+  arrange(VESSEL_OFFICIAL_NUMBER, COMP_WEEK) |>
+  # filter(VESSEL_OFFICIAL_NUMBER == "694424") |>
+  glimpse()
+
+# 3)
+compl_override_data__renamed__this_year_dups <-
   compl_override_data__renamed__this_year |>
   group_by(VESSEL_OFFICIAL_NUMBER, COMP_YEAR, COMP_WEEK) |>
-  mutate(dup_entries_in_compl = list(toString(unique(sort(SRH_VESSEL_COMP_ID))))) |>
-  # mutate(SRH_VESSEL_COMP_IDs = paste(SRH_VESSEL_COMP_ID)) |>
-  ungroup()
-# |>
-  # mutate_if(is.list, ~paste(unlist(.), collapse = ', ')) %>%
+  mutate(duplicated = n() > 1) |>
+  filter(duplicated == TRUE)
 
-# data[duplicated(data[,2:3]),]
-
-# str(dup_SRH_VESSEL_COMP_ID)
-dup_SRH_VESSEL_COMP_ID |>
-  mutate(dup_entries_in_compl_len =
-           length(dup_entries_in_compl)) |>
-  filter(dup_entries_in_compl_len > 1) |>
+compl_override_data__renamed__this_year_dups |>
+  arrange(VESSEL_OFFICIAL_NUMBER, COMP_WEEK) |>
+  filter(VESSEL_OFFICIAL_NUMBER == "694424") |>
   glimpse()
