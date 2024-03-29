023e76d7600724e096e2d84f10c561a17bccb37a
AShipunova1 <anna.shipunova@noaa.gov>
2024-03-29 18:00:53 +0100

    search_perm

--- rselenium_test/reselenium_download_pims_permit_by_vessel.R
+++ rselenium_test/reselenium_download_pims_permit_by_vessel.R
@@ -1,3 +1,4 @@
+library('XML')
 source("reselenium_download_fhier.R")
 
 # go to PIMS search ----
@@ -16,11 +17,24 @@ permits_menu_item <-
 
 permits_menu_item$clickElement()
 
-# ----
+# Search by vessel ----
 # $ VESSEL_OFFICIAL_NUMBER <chr> "FL8151TE", "1214856", "NC9069EA", "FL9693LY", "92321…
 # <input id="14a393859d6e0ef83ad3900b7b3b91fe" type="text" class="TextInput---text TextInput---align_start TextInput---inSideBySideItem TextInput---has_clear_link" placeholder="Search Permits" value="FL8151TE">
 
-library('XML')
+# "//*[contains(text(), 'Any')]/../../..")
+search_perm <-
+  remote_driver$findElements("xpath",
+                             "//*[contains(text(), 'Search Permits')]")
+
+search_perm[[1]]$clickElement()
+
+search_perm[[1]]$getElementTagName()
+
+search_perm[[1]]$getElementAttribute("id")
+search_perm[[1]]$
+FL8151TE
+
+# get the table ----
 
 doc <- htmlParse(remote_driver$getPageSource()[[1]])
 my_table_list <- readHTMLTable(doc)
