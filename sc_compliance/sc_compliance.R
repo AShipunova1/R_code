90f2fc16341f71343d2f4645947446d8a5fcf1f2
AShipunova1 <anna.shipunova@noaa.gov>
2024-04-05 12:56:28 -0400

    Update sc_compliance.R
    err with writeData

--- sc_compliance/sc_compliance.R
+++ sc_compliance/sc_compliance.R
@@ -728,6 +728,30 @@ for (i in seq_along(readme_text)) {
   writeDataTable(wb,
                  "Readme",
                  one_df,
+                 colNames = FALSE,
+                 startCol = 1,
+                 startRow = curr_row + 1)
+  curr_row <- curr_row + one_df_size + 2
+}
+
+
+for (i in seq_along(readme_text)) {
+
+  # browser()
+  one_df <- readme_text[[i]]
+  one_df_size <- nrow(one_df)
+  if (is.null(one_df_size)) {one_df_size <- length(one_df)}
+
+  # if (!any(grepl("data.frame", class(one_df))))
+  # {
+  #   one_df_size <- length(one_df)
+  #   one_df <- as.data.frame(one_df)
+  # }
+
+  writeData(wb,
+                 "Readme",
+                 one_df,
+                 # colNames = FALSE,
                  startCol = 1,
                  startRow = curr_row + 1)
   curr_row <- curr_row + one_df_size + 2
