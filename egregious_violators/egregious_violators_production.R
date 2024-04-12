83283f9049bba3ae85b682abe529dce0a1093210
AShipunova1 <anna.shipunova@noaa.gov>
2024-04-12 10:35:33 -0400

    Update egregious_violators_production.R
    clean up

--- egregious_violators/egregious_violators_production.R
+++ egregious_violators/egregious_violators_production.R
@@ -22,7 +22,8 @@ library(tictoc)
 # Load the 'openxlsx' library, which simplifies the creation of Excel .xlsx files by providing a high level interface to writing, styling and editing worksheets. 
 library(openxlsx)
 
-# library(zoo)
+# S3 Infrastructure for Regular and Irregular Time Series (Z's Ordered Observations)
+library(zoo)
 
 # Auxiliary
 # Set up paths ----
@@ -30,52 +31,6 @@ library(openxlsx)
 # function that will return current user name
 get_username <- function(){
     return(as.character(Sys.info()["user"]))
-}
-
-# change main_r_dir, in_dir, out_dir, git_r_dir to your local environment
-  # then you can use it in the code like my_paths$input etc.
-set_work_dir <- function() {
-  # Set the working directory to the user's home directory (~)
-  setwd("~/")
-  base_dir <- getwd()
-
-  # Initialize 'add_dir' as an empty string (for others)
-  add_dir <- ""
-
-  # Check if the username is "anna.shipunova" (Anna's computer)
-  if (get_username() == "anna.shipunova") {
-    # Set 'add_dir' to a specific directory path for Anna
-    add_dir <- "R_files_local/test_dir"
-  }
-
-  # Construct the path to the main R directory
-  main_r_dir <- file.path(add_dir, "SEFHIER/R code")
-
-  # Define directory names for 'Inputs' and 'Outputs'
-  in_dir <- "Inputs"
-  out_dir <- "Outputs"
-
-  # Construct full paths to 'Inputs' and 'Outputs' directories using 'file.path'
-  # file.path is a function used to create platform-independent file paths by joining its arguments using the appropriate path separator (e.g., "\" on Windows, "/" on Unix-like systems).
-  #
-  # base_dir is the base directory obtained from the user's home directory.
-  #
-  # main_r_dir is the path to the main R directory, which may vary depending on whether the user is Anna or not.
-  #
-  # in_dir is the name of the 'Inputs' directory.
-  #
-  # So, this line effectively combines these components to create the full path to the 'Inputs' directory, ensuring that the path is correctly formatted for the user's operating system.
-
-  full_path_to_in_dir <- file.path(base_dir, main_r_dir, in_dir)
-  full_path_to_out_dir <- file.path(base_dir, main_r_dir, out_dir)
-
-  # Change the working directory to the main R directory
-  setwd(file.path(base_dir, main_r_dir))
-
-  # Create a list of directory paths for 'inputs' and 'outputs'
-  my_paths <- list("inputs" = full_path_to_in_dir,
-                   "outputs" = full_path_to_out_dir)
-  return(my_paths)
 }
 
 # Define a function named 'set_work_dir_local'
