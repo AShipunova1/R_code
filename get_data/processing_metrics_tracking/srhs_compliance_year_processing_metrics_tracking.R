48638c2eebefa4d55b41deabad5d8213f05fad8f
AShipunova1 <anna.shipunova@noaa.gov>
2024-04-19 11:44:11 -0400

    Update srhs_compliance_year_processing_metrics_tracking.R
    srhs_22_23_diff

--- get_data/processing_metrics_tracking/srhs_compliance_year_processing_metrics_tracking.R
+++ get_data/processing_metrics_tracking/srhs_compliance_year_processing_metrics_tracking.R
@@ -1,1 +1,18 @@
 # check srhs vessels for compliance vs. calendar year
+
+srhs_22_23_filepath <-
+  file.path(my_paths$inputs,
+            r"(SRHS_headboat_survey\srhs_compare_years.xlsx)")
+
+# file.exists(srhs_22_23_filepath)
+
+srhs_22_23_raw <-
+  read.xlsx(srhs_22_23_filepath) |> clean_headers()
+# print_df_names(srhs_22_23_raw)
+
+srhs_22_23_diff <-
+  srhs_22_23_raw |>
+  filter(diff == TRUE) |>
+  select(-diff)
+
+srhs_22_23_diff
