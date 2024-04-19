88ce769c60149d342df1bab655b8d4935ba0baa4
AShipunova1 <anna.shipunova@noaa.gov>
2024-04-19 15:21:19 -0400

    Update srhs_compliance_year_processing_metrics_tracking.R
    clean up

--- get_data/processing_metrics_tracking/srhs_compliance_year_processing_metrics_tracking.R
+++ get_data/processing_metrics_tracking/srhs_compliance_year_processing_metrics_tracking.R
@@ -83,29 +83,13 @@ all_files <-
   all_file_names |>
   imap(\(curr_names, i)
        {
-         current_name <- map(curr_names, read_rds)
+         current_name_res <- map(curr_names, read_rds)
 
-         names(current_name) <- years
-         return(current_name)
+         names(current_name_res) <- years
+         return(current_name_res)
   })
 
-## read all metrics ----
-all_metrics <-
-  file_names_to_read_metrics |>
-  map(read_rds)
-
-names(all_metrics) <- years
-
-# View(all_metrics)
-
-# check if vessels in questions are in the results
-# 2022 ---
-
-
 # Compare with 2022 result of metrics tracking processing ----
-
-processed_metrics_permit_info_short_2022 <-
-  processed_metrics_permit_info_short_this_year
 
 srhs_22_23_diff_as_char__2022 <-
   srhs_22_23_diff_as_char |>
@@ -124,72 +108,4 @@ srhs_22_23_diff_as_char__2023 <-
   filter(uscg_2022 %in% processed_metrics_permit_info_short_2023$VESSEL_OFFICIAL_NUMBER)
 # 2
 
-# Check if in processed logbooks or dnfs ----
-## get all results ----
-processed_data_dir <-
-  file.path(my_paths$inputs,
-            r"(processing_logbook_data\Outputs)")
-
-# \SEFHIER_permitted_vessels_nonSRHS_2023.rds
-
-# dir.exists(processed_data_dir)
-
-file_names_all <-
-  list.files(processed_data_dir,
-             "process.*rds", full.names = T)
-
-file_names_to_read_ok <-
-  grep(
-    "calendar",
-    file_names_all,
-    invert = T,
-    ignore.case = T,
-    value = T
-  )
-
-file_names_to_read_dnf <-
-  grep(
-    "dnf",
-    file_names_to_read_ok,
-    ignore.case = T,
-    value = T
-  )
-
-file_names_to_read_logbooks_0 <-
-  grep("logbook",
-       file_names_to_read_ok,
-       ignore.case = T,
-       value = T)
-# incl. "C:/Users/anna.shipunova/Documents/R_files_local/my_inputs/processing_logbook_data\\Outputs/SEFHIER_processed_dnfs_2022.rds"
-
-file_names_to_read_logbooks <-
-  grep(
-    "dnf",
-    file_names_to_read_logbooks_0,
-    invert = T,
-    ignore.case = T,
-    value = T
-  )
-
-# check if vessels in questions are in the results
-# 2022 ---
-
-one_year <- "2022"
-file_name_list <- file_names_to_read_logbooks
-
-read_one_year_rds <-
-  function(one_year, file_name_list) {
-    file_name_to_read <-
-      grep(one_year, file_name_list, value = T)
-
-    this_year_result <-
-      read_rds(file_name_to_read)
-
-    return(this_year_result)
-  }
-
-### 2022 ----
-processed_logbooks_2022 <-
-  read_one_year_rds("2022", file_names_to_read_logbooks)
-
-# glimpse(processed_logbooks_2022)
+# check if vessels in questions are in processed logbooks or dnfs ----
