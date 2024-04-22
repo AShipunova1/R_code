fd156a5ddcc362e6be01a6e021cbe0b062ad328b
AShipunova1 <anna.shipunova@noaa.gov>
2024-04-22 11:06:24 -0400

    Update sc_compliance__check_20_vessels.R
    # Join sc unlicensed with compliance info ----

--- sc_compliance/sc_compliance__check_20_vessels.R
+++ sc_compliance/sc_compliance__check_20_vessels.R
@@ -14,3 +14,26 @@ sc_unlicensed_fed_charters <-
             sep.names = "_") |>
   clean_headers()
 # View(sc_unlicensed_fed_charters)
+
+
+# glimpse(compl_override_data__renamed_m_short__m_compl)
+
+# Join sc unlicensed with compliance info ----
+
+sc_unlicensed_fed_charters__compliance__join <-
+  left_join(
+    sc_unlicensed_fed_charters,
+    compl_override_data__renamed_m_short__m_compl,
+    join_by(vessel_id == vessel_official_number)
+  )
+
+sc_unlicensed_fed_charters__compliance__join |>
+  filter(!is.na(is_comp)) |>
+  select(vessel_id) |>
+  distinct() |>
+  count()
+# 20
+
+# Join sc unlicensed with logbooks ----
+
+logbooks |> View()
