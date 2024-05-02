42bfe82d59d8502b2b5a002cec952f4b2c479ff5
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-02 08:16:36 -0400

    Update useful_functions_module.r
    openxlsx

--- useful_functions_module.r
+++ useful_functions_module.r
@@ -39,8 +39,8 @@ library(tidyverse)
 # Load the 'magrittr' library, which provides piping data and functions.
 library(magrittr)
 
-# Load the 'readxl' library, used for reading Excel (.xlsx) files.
-library(readxl)
+# Load the 'openxlsx' library, used for reading and writing Excel (.xlsx) files.
+library(openxlsx)
 
 # Load the 'rbenchmark' library, which is used for benchmarking code performance.
 library(rbenchmark)
