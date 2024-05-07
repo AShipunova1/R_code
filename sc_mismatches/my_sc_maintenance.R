93a26c7c62f5c4cfeba6c653fb3144e18c537745
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-07 08:40:32 -0400

    Update my_sc_maintenance.R
    clean up

--- sc_mismatches/my_sc_maintenance.R
+++ sc_mismatches/my_sc_maintenance.R
@@ -14,17 +14,18 @@ SC_vessels_FHIERData_0 <- load_csv_names(my_paths$inputs, csv_names_list)[[1]]
 SC_vessels_FHIERData <- clean_headers(SC_vessels_FHIERData_0)
 
 glimpse(SC_vessels_FHIERData)
-str(SC_vessels_FHIERData)
+# str(SC_vessels_FHIERData)
 
 # get enabled only
 SC_vessels_FHIERData_enabled <-
   SC_vessels_FHIERData %>%
   filter(tolower(enabled) == "yes")
+
 dim(SC_vessels_FHIERData_enabled)
 # 188
 # 214
 
-#create new dataframe with just enabled vessel official # for analysis
+# create new dataframe with just enabled vessel official # for analysis
 FHIER_vessel_officialnumber <-
   data.frame(Official_number = tolower(SC_vessels_FHIERData_enabled$vessel_official_number))
 
@@ -51,33 +52,21 @@ names(SC_permittedVessels)[1]
 SC_vessel_officalnumber <-
   data.frame(Official_number = tolower(SC_permittedVessels$vessel_reg_uscg_))
 
-# #check for mistmatching fields using dplyr packages anti_join function
-# mismatched_officialnumbers_FHIERvsSC <-
-#   anti_join(FHIER_vessel_officialnumber,
-#             SC_vessel_officalnumber,
-#             by = "Official_number") #to ID rows that exist in FHIER but not in SC file
-#
-# NOTE - now do in reverse to ensure no vessels are in SC list that are not in FHIER list
-# mismatched_officialnumbers_SCvsFHIER <-
-#   anti_join(SC_vessel_officalnumber,
-#             FHIER_vessel_officialnumber,
-#             by = "Official_number") #to ID rows that exist in SC but not in FHIER
+# check for mismatching fields
+mismatched_officialnumbers_FHIERvsSC <-
+  setdiff(
+    FHIER_vessel_officialnumber$Official_number,
+    SC_vessel_officalnumber$Official_number
+  )
 
-#---
-# identical?
-mismatched_officialnumbers_FHIERvsSC <- setdiff(FHIER_vessel_officialnumber$Official_number, SC_vessel_officalnumber$Official_number)
-
+# Now do in reverse to ensure no vessels are in SC list that are not in FHIER list
 mismatched_officialnumbers_SCvsFHIER <-
   setdiff(
     SC_vessel_officalnumber$Official_number,
     FHIER_vessel_officialnumber$Official_number
   )
 
-# identical(sort(mismatched_officialnumbers_SCvsFHIER$Official_number),
-#           sort(sc_to_fh_diff))
-# T
-
-#create output files - use these to update FHIER maintenance list ----
+# create output files - use these to update FHIER maintenance list ----
 ## make output file names ----
 project_dir_name <- "mismatched_fields_sc"
 output_file_names <- c("remove_from_FHIER",
