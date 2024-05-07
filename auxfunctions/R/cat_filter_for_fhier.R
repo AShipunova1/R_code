f0d8e7b3a2544303ed89c8eb1bc9913b2aca7bbd
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-07 10:35:13 -0400

    rm unused cat_filter_for_fhier

--- auxfunctions/R/cat_filter_for_fhier.R
+++
@@ -1,8 +0,0 @@
-cat_filter_for_fhier <-
-function(my_characters) {
-  # Concatenate the elements of 'my_characters' using a comma and space as the separator.
-  # Output the concatenated string to a text file named "cat_out.txt" in the outputs directory.
-  cat(my_characters,
-      sep = ', ',
-      file = file.path(my_paths$outputs, "cat_out.txt"))
-}
