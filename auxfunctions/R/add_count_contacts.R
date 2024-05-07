2726ab4ed76739a564c2b2885778f00093a4d110
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-07 10:19:52 -0400

    Update add_count_contacts.R

--- auxfunctions/R/add_count_contacts.R
+++ auxfunctions/R/add_count_contacts.R
@@ -1,22 +1,20 @@
-add_count_contacts <-
-function(all_data_df_clean) {
-  # Find the column name for 'contactdate' and 'vesselnumber' in 'all_data_df_clean'
-  contactdate_field_name <-
+# Define a function to add contact frequency count to a dataframe
+add_count_contacts <- function(all_data_df_clean) {
+  # Find the column names for contact date and vessel number
+  contactdate_field_name <- 
     find_col_name(all_data_df_clean, "contact", "date")[1]
-  vessel_id_field_name <-
+  vessel_id_field_name <- 
     find_col_name(all_data_df_clean, "vessel", "number")[1]
-
-  # Apply a series of transformations to 'all_data_df_clean'
+  
+  # Mutate a new column 'was_contacted' based on the presence of contact date
+  # Convert the result to a factor with levels 'yes' and 'no'
   result_df <- all_data_df_clean %>%
-
-    # Add a new column 'was_contacted' with "yes" if 'contactdate' is not NA, or "no" if it is NA
-    # TODO: as.factor
-    dplyr::mutate(was_contacted =
-                    dplyr::if_else(is.na(contactdate_field_name), "no", "yes")) %>%
-
-    # Group the data by 'vesselofficialnumber' and 'was_contacted', and count the occurrences, saving it in 'contact_freq' column
-    dplyr::add_count(!!dplyr::sym(vessel_id_field_name), was_contacted, name = "contact_freq")
-
-  # Return the modified data frame with the added 'was_contacted' and 'contact_freq' columns
+    mutate(was_contacted = if_else(is.na(!!sym(contactdate_field_name)), "no", "yes")) %>%
+  
+    # Add count of contacts per vessel, considering whether the vessel was contacted or not
+    add_count(!!sym(vessel_id_field_name), 
+              was_contacted, 
+              name = "contact_freq")
+  
   return(result_df)
 }
