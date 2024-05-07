7f0f04a7296d1d22e770c4be987760dc4f0b5215
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-07 09:12:54 -0400

    Update my_sc_maintenance.R
    write out by # Map over my_outputs and output_file_path_list; rename vars

--- sc_mismatches/my_sc_maintenance.R
+++ sc_mismatches/my_sc_maintenance.R
@@ -53,27 +53,30 @@ SC_vessel_officalnumber <-
   data.frame(Official_number = tolower(SC_permittedVessels[[vessel_id_field_name]]))
 
 # check for mismatching fields (in FHIER, but not in SC)
-mismatched_officialnumbers_FHIERvsSC <-
+mismatched_officialnumbers_inFHIER_notSC <-
   setdiff(
     FHIER_vessel_officialnumber$Official_number,
     SC_vessel_officalnumber$Official_number
   )
 
-length(mismatched_officialnumbers_FHIERvsSC)
+length(mismatched_officialnumbers_inFHIER_notSC)
 # 0
 
 # Now do in reverse to ensure no vessels are in SC list that are not in FHIER list
-mismatched_officialnumbers_SCvsFHIER <-
+mismatched_officialnumbers_inSC_notFHIER <-
   setdiff(
     SC_vessel_officalnumber$Official_number,
     FHIER_vessel_officialnumber$Official_number
   )
 
-length(mismatched_officialnumbers_SCvsFHIER)
+length(mismatched_officialnumbers_inSC_notFHIER)
 # 5
 
 # create output files - use these to update FHIER maintenance list ----
 ## make output file names ----
+my_outputs <- list(mismatched_officialnumbers_inFHIER_notSC,
+                mismatched_officialnumbers_inSC_notFHIER)
+
 project_dir_name <- "mismatched_fields_sc"
 output_file_names <- c("remove_from_FHIER",
                        "add_or_enable_in_FHIER")
@@ -90,19 +93,24 @@ output_file_path_list <-
   setNames(output_file_path_list, output_file_names)
 
 ## write files ----
-write_csv(
-  mismatched_officialnumbers_FHIERvsSC,
-  output_file_path_list$remove_from_FHIER
-) #remove from FHIER, bc no longer permitted
 
-write_csv(
-  mismatched_officialnumbers_SCvsFHIER,
-  output_file_path_list$enable_in_FHIER
-) #enable in FHIER, as newly permitted
+# Map over my_outputs and output_file_path_list
 
-write_csv(
-  as.data.frame(not_it_fhier_sc_report),
-  output_file_path_list$add_to_FHIER
-) #add to FHIER, as newly permitted
+# Explanation:
+# - This code snippet uses `map2` to iterate over two lists simultaneously: `my_outputs` and `output_file_path_list`.
+# - For each pair of elements `(x, y)` in the lists:
+#   - It checks if the length of `x` is greater than 0 to ensure there is data to write.
+#   - If there is data:
+#     - It converts `x` to a data frame.
+#     - Sets the column names of the data frame based on the file name without extension using `tools::file_path_sans_ext`.
+#     - Writes the data frame to a CSV file specified by the path `y` using `write_csv`.
+# - This process is repeated for each pair of elements in the lists.
 
-
+map2(my_outputs, output_file_path_list, \(x, y) {
+  if (length(x) > 0) {
+    output_df <- as.data.frame(x)
+    names(output_df) <-
+      tools::file_path_sans_ext(basename(y))
+    write_csv(output_df, y)
+  }
+})
