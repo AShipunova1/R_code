890af89c2aeca49c80c40e39ea4774594aa00989
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 16:33:12 -0400

    Update add_cnt_in_gr.R
    add ::

--- auxfunctions/R/add_cnt_in_gr.R
+++ auxfunctions/R/add_cnt_in_gr.R
@@ -4,14 +4,14 @@ function(my_df,
            cnt_col_name = "total_vsl_m_by_year_perm") {
     my_df %>%
       # group by per month and permit
-      group_by_at(group_by_col) %>%
+      dplyr::group_by_at(group_by_col) %>%
       # cnt distinct vessels in each group
-      mutate({
+      dplyr::mutate({
         {
           cnt_col_name
         }
       } :=
-        n_distinct(vessel_official_number)) %>%
-      ungroup() %>%
+        dplyr::n_distinct(vessel_official_number)) %>%
+      dplyr::ungroup() %>%
       return()
   }
