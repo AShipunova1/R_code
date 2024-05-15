4d767e7d9f60ad3254f6f860471d92c6ea1b6a41
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 17:02:56 -0400

    Update vessels_permits_id_clean.R
    add ::

--- auxfunctions/R/vessels_permits_id_clean.R
+++ auxfunctions/R/vessels_permits_id_clean.R
@@ -2,8 +2,8 @@ vessels_permits_id_clean <-
 function(my_df) {
     # Create a new dataframe 'vessels_permits' by renaming two specific columns.
     vessels_permits <- my_df |>
-        rename("PERMIT_VESSEL_ID" = "QCSJ_C000000000300000") |>
-        rename("VESSEL_VESSEL_ID" = "QCSJ_C000000000300001")
+        dplyr::rename("PERMIT_VESSEL_ID" = "QCSJ_C000000000300000") |>
+        dplyr::rename("VESSEL_VESSEL_ID" = "QCSJ_C000000000300001")
 
     # Return the cleaned dataframe.
     return(vessels_permits)
