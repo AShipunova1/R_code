d4e1d426102fd7c3f4dc25920e5305df61ceaa57
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 17:03:01 -0400

    Update separate_permits_into_3_groups.R
    add ::

--- auxfunctions/R/separate_permits_into_3_groups.R
+++ auxfunctions/R/separate_permits_into_3_groups.R
@@ -2,7 +2,7 @@ separate_permits_into_3_groups <-
 function(my_df, permit_group_field_name = "permitgroup") {
     my_df %>%
       # Use 'mutate' to create a new column 'permit_sa_gom' with categories based on permit group
-      mutate(permit_sa_gom =
+      dplyr::mutate(permit_sa_gom =
                dplyr::case_when(
                  # Check if 'permit_group_field_name' doesn't contain 'RCG', 'HRCG', 'CHG', or 'HCHG'; assign "sa_only" if true
                  !grepl("RCG|HRCG|CHG|HCHG", !!sym(permit_group_field_name)) ~ "sa_only",
