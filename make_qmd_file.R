a3d256a859ee8f04e7de62bff143e6c925d05fac
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 10:04:51 -0400

    use a param in split_one_line_text_back

--- make_qmd_file.R
+++ make_qmd_file.R
@@ -42,7 +42,7 @@ to_one_line <-
 split_one_line_text_back <-
   function(my_text_with_at, split_by = my_split_newline_char) {
     my_text_with_at |>
-      str_split("@@@") |>
+      str_split(split_by) |>
       unlist()
   }
 
