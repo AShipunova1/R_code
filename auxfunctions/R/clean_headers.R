7385cf21814ce11224de8d5b11a942556081f40b
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 17:18:49 -0400

    Update clean_headers.R
    rm %<>%

--- auxfunctions/R/clean_headers.R
+++ auxfunctions/R/clean_headers.R
@@ -1,9 +1,13 @@
 clean_headers <-
 function(my_df) {
     # Use the 'fix_names' function to clean and fix the column names of the dataframe.
-    colnames(my_df) %<>%
+    new_names <-
+        colnames(my_df) |>
         fix_names()
 
+    colnames(my_df) <- 
+        new_names
+    
     # Return the dataframe with cleaned and fixed column names.
     return(my_df)
 }
