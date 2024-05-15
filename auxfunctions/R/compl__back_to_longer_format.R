5412ab979194587ff8ffdbe6b49377961dd2d2bf
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 16:34:55 -0400

    Update compl__back_to_longer_format.R
    add ::

--- auxfunctions/R/compl__back_to_longer_format.R
+++ auxfunctions/R/compl__back_to_longer_format.R
@@ -5,7 +5,7 @@ function(my_df,
       # turn back to a longer format, vessel ids in one column
       tidyr::pivot_longer(
         # all other columns are vessel ids, use them as names
-        cols = !any_of(cols_names),
+        cols = !tidyselect::any_of(cols_names),
         values_to = "is_compl_or_both",
         names_to = "vessel_official_number"
       ) %>%
