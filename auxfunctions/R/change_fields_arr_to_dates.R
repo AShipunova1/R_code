07139ea75d91e4f6e3e4206343a8698b31372903
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 16:33:38 -0400

    Update change_fields_arr_to_dates.R
    add ::

--- auxfunctions/R/change_fields_arr_to_dates.R
+++ auxfunctions/R/change_fields_arr_to_dates.R
@@ -3,7 +3,7 @@ function(my_df, field_names_arr, date_format) {
     # Use 'mutate' and 'across' to convert all specified columns in 'field_names_arr' to POSIXct date format
     result_df <- my_df %>%
       dplyr::mutate(dplyr::across(
-        all_of(field_names_arr),
+        dplyr::all_of(field_names_arr),
         ~ as.POSIXct(.x, format = date_format)  # Apply 'as.POSIXct' to each column with the provided 'date_format'
       ))
 
