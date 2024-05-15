33366c44b0bb669f76057ab752a786315f8f000e
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 16:34:26 -0400

    Update clean_names_and_addresses.R
    add ::

--- auxfunctions/R/clean_names_and_addresses.R
+++ auxfunctions/R/clean_names_and_addresses.R
@@ -1,38 +1,57 @@
 clean_names_and_addresses <-
-function(my_df) {
-
-  my_df_cleaned <-
-    my_df |>
-    mutate(
-      across(where(is.character),
-             ~ str_squish(.x)),
-      across(where(is.character),
-             ~ replace_na(.x, "")),
-      across(where(is.character),
-             ~ str_replace_all(.x, ", ;", ";")),
-      across(where(is.character),
-             ~ str_replace_all(.x, "\\s+[,;]", ",")),
-      across(where(is.character),
-             ~ str_replace_all(.x, ";,+", ";")),
-      across(where(is.character),
-             ~ str_replace_all(.x, ";;+", ";")),
-      across(where(is.character),
-             ~ str_replace_all(.x, ",,+", ",")),
-      across(where(is.character),
-             ~ str_replace_all(.x, "[,;] *\\bUN\\b *", "")),
-      across(where(is.character),
-                          ~ str_replace_all(.x, "\\bUN\\b", "")),
-      across(where(is.character),
-             ~ str_replace_all(.x, "\\s*\\bUN\\b\\s*", "")),
-      across(where(is.character),
-             ~ str_replace_all(.x, "^[,;] ", "")),
-      across(where(is.character),
-             ~ str_replace_all(.x, "^[,;]$", "")),
-      across(where(is.character),
-             ~ str_replace_all(.x, "[,;]$", "")),
-      across(where(is.character),
-             ~ str_squish(.x))
-    )
+  function(my_df) {
+    my_df_cleaned <-
+      my_df |>
+      dplyr::mutate(
+        dplyr::across(dplyr::where(is.character), ~ stringr::str_squish(.x)),
+        dplyr::across(dplyr::where(is.character), ~ tidyr::replace_na(.x, "")),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, ", ;", ";")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "\\s+[,;]", ",")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, ";,+", ";")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, ";;+", ";")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, ",,+", ",")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "[,;] *\\bUN\\b *", "")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "\\bUN\\b", "")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "\\s*\\bUN\\b\\s*", "")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "^[,;] ", "")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "^[,;]$", "")
+        ),
+        dplyr::across(
+          dplyr::where(is.character),
+          ~ stringr::str_replace_all(.x, "[,;]$", "")
+        ),
+        dplyr::across(dplyr::where(is.character), 
+                      ~ stringr::str_squish(.x))
+      )
 
   return(my_df_cleaned)
 }
