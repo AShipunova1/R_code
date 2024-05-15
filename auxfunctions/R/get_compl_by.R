e3b6efc8d565e5895ec06104ae579d82ec221a7d
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-15 17:00:01 -0400

    Update get_compl_by.R
    add ::

--- auxfunctions/R/get_compl_by.R
+++ auxfunctions/R/get_compl_by.R
@@ -3,14 +3,14 @@ function(my_df,
            group_by_for_compl =
              vars(-c("vessel_official_number", "compliant_", "overridden_")),
            names_from_list = c("vessel_official_number")) {
-    browser()
+    # browser()
     my_df %>%
     dplyr::group_by_at(group_by_for_compl) %>%
     # can unique, because we are looking at vessels, not weeks
     unique() %>%
     # more columns, a column per vessel
     tidyr::pivot_wider(
-      names_from = all_of(names_from_list),
+      names_from = tidyselect::all_of(names_from_list),
       values_from = c("compliant_", "overridden_"),
         # compliant_,
       # make it "NO_YES" if both
