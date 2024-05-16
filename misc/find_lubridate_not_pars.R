f13ac0dd8ef970feba46da894ec1354205bba25f
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-16 12:33:30 -0400

    Update find_lubridate_not_pars.R
    works

--- misc/find_lubridate_not_pars.R
+++ misc/find_lubridate_not_pars.R
@@ -1,16 +1,24 @@
 # From https://stackoverflow.com/questions/35369897/find-dates-that-fail-to-parse-in-r-lubridate
 
-parse_ymd = function(x) {
-  d = lubridate::ymd(x, quiet = TRUE)
-  errors = x[!is.na(x) & is.na(d)]
+library(lubridate)
+
+parse_mdy = function(x) {
+  parsed_dates <- 
+    lubridate::parse_date_time(x, orders = c("m/d/Y"), quiet = TRUE)
+  
+  errors = x[!is.na(x) & is.na(parsed_dates)]
   if (length(errors) > 0) {
     cli::cli_warn("Failed to parse some dates: {.val {errors}}")
   }
-  d
+  parsed_dates
 }
 
-x = c("2014/20/21", "2014/01/01", NA, "2014/01/02", "foobar")
-my_date = lubridate::ymd(permits_from_pims__split1$statusdate)
+all_dates <- 
+  # c("10/13/2006", "05/16/2024")
+  unique(permits_from_pims__split1$statusdate)
+  # c("2014/20/21", "2014/01/01", NA, "2014/01/02", "foobar")
+my_date = lubridate::mdy(all_dates,
+                         c("m/d/Y"))
 #> Warning: 2 failed to parse.
-my_date = parse_ymd(permits_from_pims__split1$statusdate)
+my_date = parse_mdy(all_dates)
 #> Warning: Failed to parse some dates: "2014/20/21" and "foobar"
