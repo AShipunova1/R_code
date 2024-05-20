5361a6fbc99e35e22094d54bda0d6ef1fa948cc3
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-20 10:32:34 -0400

    Update processing_logbook_data.R
    insert aux functions

--- get_data/processing_logbook_data/processing_logbook_data.R
+++ get_data/processing_logbook_data/processing_logbook_data.R
@@ -89,15 +89,130 @@ my_year <- "2022"
 db_year_1 <- "2021"
 db_year_2 <- "2024"
 
+# ---
+# Explanations:
+# - This function, `get_the_dates`, generates a list of date strings and date objects based on a specified year and a start day for weeks.
+# - It takes two parameters: `my_year`, which defaults to "2023", and `week_start_day`, which defaults to "Monday".
+# - The function returns a list (`lst`) of the start and end dates for the calendar year, as well as the start and end dates for compliance based on week boundaries. lst is used to keep entries names.
+# The compliance dates include the "fringe" weeks if needed.
+#
+# 1. **Generating Calendar Dates**:
+    )
 
 source(auxiliary_methods_file_path)
+  }
 
 curr_dates <- get_the_dates(my_year)
 my_compliance_date_beg <- curr_dates$my_compliance_date_beg
 my_compliance_date_end <- curr_dates$my_compliance_date_end
 
-# Create a log of stats for the processing file, various parameters are tallied throughout the code, and saved here. ----
+# ---
+# Pretty message print
+function_message_print <- function(text_msg) {
+  cat(crayon::bgCyan$bold(text_msg),
+      sep = "\n")
+}
 
+# Define a helper function 'title_message_print' to print the title message in blue.
+title_message_print <- function(title_msg) {
+  cat(crayon::blue(title_msg), sep = "\n")
+}
+
+# Define a helper function 'my_tee' to print the message to the console and a file.
+my_tee <- function(my_text,
+                   my_title = NA,
+                   stat_log_file_path = NA,
+                   date_range = my_year) {
+
+  the_end = "---"
+
+  # Print out to console
+  title_message_print(my_title)
+  cat(c(my_text, the_end),
+      sep = "\n")
+
+  # Create a new file every day
+  if (is.na(stat_log_file_path)) {
+    stat_log_file_path <-
+      file.path(Path,
+                Outputs,
+                str_glue("processing_stats_{date_range}_run_{today()}.log"))
+  }
+
+  # Write to a log file
+  cat(c(my_title, my_text, the_end),
+      file = stat_log_file_path,
+      sep = "\n",
+      append = TRUE)
+}
+
+# ---
+# A function to print out stats.
+# Usage: my_stats(Logbooks)
+
+# Explanation:
+#
+# 1. **Define Function with Optional Parameter:**
+#    - `my_stats <- function(my_df, title_msg = NA) { ... }`: Define a function named 'my_stats' that takes a dataframe 'my_df' as input and an optional 'title_msg' parameter with a default value of NA.
+#
+# 2. **Check and Assign Default Title Message:**
+#    - `if (is.na(title_msg))  { ... }`: Check if 'title_msg' is NA, and if so, assign the dataframe name as the default title message using 'deparse(substitute(my_df))'.
+#
+# 3. **Extract Statistics:**
+#    - `rows_n_columns <- dim(my_df)`: Extract the number of rows and columns in the dataframe.
+#    - `uniq_vessels_num <- n_distinct(my_df[["VESSEL_OFFICIAL_NUMBER"]])`: Count the number of distinct vessel numbers.
+#    - `uniq_trips_num <- n_distinct(my_df[["TRIP_ID"]])`: Count the number of distinct trip IDs.
+#
+# 4. **Create Formatted Text with Statistics:**
+#    - `stat_text <- str_glue("rows: {rows_n_columns[[1]]} ... Unique trips (logbooks): {uniq_trips_num}")`: Use 'str_glue' to format the statistics into a text string.
+#
+# 5. **Print Title Message and Statistics to Console:**
+#    - `title_message_print(title_msg)`: Use the helper function 'title_message_print' to print the title message in blue.
+#    - `print(stat_text)`: Print the formatted statistics to the console.
+#
+# 6. **Write Statistics to Log File:**
+#    - `stat_log_file_path <- file.path(Path, Outputs, str_glue("stat_info_{today()}.log"))`: Define the file path for the log file, including the date.
+#    - `cat(c(title_msg, stat_text), file = stat_log_file_path, sep = "\n", append = TRUE)`: Write the title message and formatted statistics to the log file, appending to the file if it already exists.
+
+my_stats <- function(my_df, title_msg = NA) {
+
+  # A title
+  if (is.na(title_msg))  {
+    df_name = deparse(substitute(my_df))
+    title_msg <- df_name
+  }
+
+  # Extract statistics
+  rows_n_columns <- dim(my_df)
+  uniq_vessels_num <- n_distinct(my_df[["VESSEL_OFFICIAL_NUMBER"]])
+  uniq_trips_num <- n_distinct(my_df[["TRIP_ID"]])
+
+  # Create a formatted text with statistics
+  # include trips, only if > 0
+  trip_cnts <-
+    if (uniq_trips_num > 0) {
+      str_glue("Unique trips: {uniq_trips_num}")
+    }
+  else {
+    ""
+  }
+
+  stat_text <- str_glue(
+    "
+rows: {rows_n_columns[[1]]}
+columns: {rows_n_columns[[2]]}
+Unique vessels: {uniq_vessels_num}
+{trip_cnts}
+"
+  )
+
+  # Print out to console and to the log file
+  my_tee(stat_text,
+         my_title = title_msg,
+         stat_log_file_path = NA)
+}
+
+# Create a log of stats for the processing file, various parameters are tallied throughout the code, and saved here. ----
 my_tee(date(),
        my_title = str_glue("Start logbook processing for {my_year}"))
 
@@ -114,6 +229,25 @@ my_tee(date(),
 Sys.setenv(TZ = Sys.timezone())
 Sys.setenv(ORA_SDTZ = Sys.timezone())
 
+connect_to_secpr <- function() {
+  # Retrieve the username associated with the "SECPR" database from the keyring.
+  my_username <- keyring::key_list("SECPR")[1, 2]
+
+  # Use 'dbConnect' to establish a database connection with the specified credentials.
+  con <- dbConnect(
+    dbDriver("Oracle"),
+    # Use the Oracle database driver.
+    username = my_username,
+    # Use the retrieved username.
+    password = keyring::key_get("SECPR", my_username),
+    # Retrieve the password from the keyring.
+    dbname = "SECPR"  # Specify the name of the database as "SECPR."
+  )
+
+  # Return the established database connection.
+  return(con)
+}
+
 # test if you are connected to VPN
 tic("try_connection")
 try(con <- connect_to_secpr())
@@ -129,7 +263,6 @@ compl_override_data_file_path <-
   file.path(Path,
             Outputs,
             str_glue("Raw_Oracle_Downloaded_compliance_2021_plus.rds"))
-# File: Raw_Oracle_Downloaded_compliance_2021_plus.rds modified 2024-02-05 09:52:06.996529
 
 # Check if the file path is correct, optional
 # file.exists(compl_override_data_file_path)
@@ -149,6 +282,74 @@ WHERE
 # using the function pre-defined above to check if there is a file saved already,
 # read it
 # or run the query and write the file for future use
+
+# ---
+# A function to use every time we want to read a ready file or query the database if no files exist.
+
+# The read_rds_or_run_query function is designed to read data from an RDS file if it exists or run an SQL query to pull the data from Oracle db if the file doesn't exist.
+# See usage below at the `Grab compliance file from Oracle` section
+read_rds_or_run_query <- function(my_file_path,
+                                  my_query,
+                                  force_from_db = NULL) {
+
+  if (file.exists(my_file_path)) {
+    modif_time <- file.info(my_file_path)$mtime
+  }
+
+    # Check if the file specified by 'my_file_path' exists and 'force_from_db' is not set.
+    if (file.exists(my_file_path) &
+        is.null(force_from_db)) {
+        # If the file exists and 'force_from_db' is not set, read the data from the RDS file.
+
+        function_message_print("File already exists, reading.")
+
+        my_result <- readr::read_rds(my_file_path)
+
+    } else {
+
+      # If the file doesn't exist or 'force_from_db' is set, perform the following steps:
+
+      # 0. Print this message.
+      function_message_print(c(
+        "File",
+        my_file_path,
+        "doesn't exists, pulling data from database.",
+        "Must be on VPN."
+      ))
+
+      # 1. Generate a message indicating the date and the purpose of the run for "tic".
+      msg_text <-
+        paste(today(), "run for", basename(my_file_path))
+      tictoc::tic(msg_text)  # Start timing the operation.
+
+      # 2. Run the specified function 'my_function' on the provided 'my_data' to generate the result. I.e. download data from the Oracle database. Must be on VPN.
+
+      my_result <- dbGetQuery(con, my_query)
+      # my_result <- my_function(my_data)
+
+      tictoc::toc()  # Stop timing the operation.
+
+      # 3. Save the result as an RDS binary file to 'my_file_path' for future use.
+      # try is a wrapper to run an expression that might fail and allow the user's code to handle error-recovery.
+
+      # 4. Print this message.
+      function_message_print(c("Saving new data into a file here: ",
+                       my_file_path))
+
+      try(readr::write_rds(my_result,
+                           my_file_path))
+
+      modif_time <- date()
+    }
+
+  # Print out the formatted string with the file name ('my_file_name') and the modification time ('modif_time') to keep track of when the data were downloaded.
+  my_file_name <- basename(my_file_path)
+  function_message_print(
+    str_glue("File: {my_file_name} modified {modif_time}"))
+
+    # Return the generated or read data.
+    return(my_result)
+}
 
 # use to force the DB download
 # force_from_db = TRUE
@@ -173,7 +374,6 @@ my_stats(compl_override_data__renamed)
 # rows: 460724
 # columns: 23
 # Unique vessels: 4390
-
 
 # stats (check the min date in the data)
 min(compl_override_data__renamed$COMP_WEEK_START_DT)
@@ -395,7 +595,6 @@ my_stats(Logbooks_raw_renamed__to_date_time4__my_year,
 # Unique vessels: 1885
 # Unique trips: 94733
 
-
 # check the min and max start dates, after filtering DF to just my analysis year
 min(Logbooks_raw_renamed__to_date_time4__my_year$TRIP_START_DATE)
 # [1] "2022-01-01"
@@ -507,7 +706,6 @@ my_tee(vessels_not_in_metrics,
        "Vessels removed if a vessel is not in Metrics tracking")
 # 244 (2022)
 
-
 ## add compliance/override data to logbooks ----
 # We add data from the compliance module to the DNF data frame to associate weeks where compliance was overridden with the corresponding DNFs.
 # Depending on the analysis question, we may want to remove DNFs for weeks that
@@ -525,7 +723,6 @@ my_stats(SEFHIER_compl_override_data__renamed__this_year,
 # rows: 141557
 # columns: 23
 # Unique vessels: 3382
-
 
 ### join the data frames ----
 
@@ -570,7 +767,6 @@ in_logbooks_not_in_compl <-
 # check the number of rows in DF
 nrow(in_logbooks_not_in_compl)
 
-
 ### Remove rows with NA logbooks and entries in Compliance ----
 # this just means that no logbooks were submitted for that compliance week, i.e. the vessel submitted a DNF instead
 
@@ -591,10 +787,8 @@ my_stats(logbooks_join_overr__all_logbooks)
 # Unique vessels: 3426
 # Unique trips: 94734
 
-
 ### Add a compliant_after_override column ----
 # This is needed so that we can easily filter out compliant or non-compliant vessels in the dataset, by adding an extra column that states yes or no regarding compliance. The NA in IS_COMP represents one of two possible scenarios: 1) a logbook was submitted for a vessel that is missing from the compliance module but is in metrics tracking, or 2) a logbook was submitted for a week when the vessel was not permitted. It is not simple to determine which. Deciding what to do with these DNFs will depend on the individual analysis question, and so is not addressed here, but simply left as NA.
-
 
 # Explanations:
 # 1. 'logbooks_join_overr__compl' is created as an extension of 'logbooks_join_overr__all_logbooks'.
@@ -621,7 +815,6 @@ logbooks_join_overr__all_logbooks |>
 # 5       0          0
 
 ## NOTE: IF “Is_Overriden == 1 & is_Comp == 0, then the vessel should be considered compliant in any compliance analyses
-
 
 tic("Add a compliant_after_override column")
 logbooks_join_overr__compl <-
