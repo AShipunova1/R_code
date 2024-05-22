252db808f5a1188e9d258ced17347aaaeeed55f9
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-22 14:46:32 -0400

    Update home_ports_manual_corrrections.R
    make city_state_typo_space

--- home_ports/home_ports_manual_corrrections.R
+++ home_ports/home_ports_manual_corrrections.R
@@ -1,3 +1,4 @@
+# check typos ----
 # vessels_from_pims_split_addr__city_state__fix2_ok__good_ids_short |> 
 #   dplyr::filter(vessel_official_number == "558651") |> 
 #   View()
@@ -104,14 +105,12 @@ get_vessel_id_2 <-
   dplyr::mutate(city = stringr::str_replace(city, "CATAÃ‘O", "CATAÑO")) |>
   mutate(
     city_state_typo = paste(city, state_from, sep = ", "),
-    city_state_change_to = paste(city_change_to, state_to, sep = ", ")
+    city_state_change_to = paste(city_change_to, state_to, sep = ", "),
+    city_state_typo_space = paste(city, state_from, sep = " , ")
   )
 
-  # stringr::str_replace("RINCÃ“N", "RINCÓN") |> 
-  # stringr::str_replace("CATAÃ‘O", "CATAÑO")
-  
 # find ids
-get_vessel_id_3 <-
+vessels_w_ports_0 <-
   vessels_from_pims_short_ok |>
   dplyr::filter(hailingport %in% get_vessel_id_2$city_state_typo) |>
   dplyr::arrange(hailingport)
@@ -121,38 +120,48 @@ get_vessel_id_3 <-
 # head()
   # auxfunctions::see_res_in_outfile()
 
-n_distinct(get_vessel_id_3$hailingport)
+n_distinct(vessels_w_ports_0$hailingport)
 # 200
 n_distinct(get_vessel_id_2$city_state_typo)
 # 207
 
 not_found <-
   dplyr::setdiff(unique(get_vessel_id_2$city_state_typo),
-                 unique(get_vessel_id_3$hailingport)) |> 
+                 unique(vessels_w_ports_0$hailingport)) |> 
   sort()
+
 # [1] "NEW SYMNRA, FL"               "OCEAN SPRIGS, MS"            
 # [3] "PORT CANAVERAL CANAVERAL, FL" "PORT ST. JOHN, FL"           
 # [5] "RINCÓN, PR"                   "ROCK PORT, TX"               
 # [7] "SOUTH PORT, NC"              
+
+dplyr::setdiff(unique(vessels_w_ports_0$hailingport),
+               unique(get_vessel_id_2$city_state_typo)) |> 
+  sort()
+# 0
+
+View(get_vessel_id_2)
 
 not_found_1 <-
   not_found |>
   stringr::str_split(pattern = ", ") |>
   purrr::map( ~ stringr::str_c(., collapse = " , "))
 
-get_vessel_id_4 <-
+vessels_w_ports_1 <-
   vessels_from_pims_short_ok |>
   dplyr::filter(hailingport %in% not_found_1) |>
   dplyr::arrange(hailingport)
 
-get_vessel_id_5 <- 
-  rbind(get_vessel_id_3, get_vessel_id_4)
+
+# get_vessel_id_5 <- 
+#   rbind(get_vessel_id_3, get_vessel_id_4)
 
 # check missing again
 # not_found_2 <-
-#   dplyr::setdiff(unique(get_vessel_id_2$city_state_typo),
-#                  unique(get_vessel_id_5$hailingport)) |> 
-#   sort()
+
+  dplyr::setdiff(unique(stringr::str_squish(get_vessel_id_2$city_state_typo)),
+                 unique(stringr::str_squish(vessels_w_ports_1$hailingport))) |>
+  sort()
   
   
 # dplyr::left_join(get_vessel_id_2,
@@ -161,3 +170,4 @@ get_vessel_id_5 <-
   # auxfunctions::see_res_in_outfile()
 
 get_vessel_id_5
+  
