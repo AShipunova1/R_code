23c15dc34aed5bb2379ff733d7448fb2bd822841
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-22 11:35:48 -0400

    Update see_res_in_outfile.R
    print instead of cat to handle dfs

--- auxfunctions/R/see_res_in_outfile.R
+++ auxfunctions/R/see_res_in_outfile.R
@@ -1,8 +1,12 @@
-see_res_in_outfile <- function(text_to_output) {
+see_res_in_outfile <- function(var_to_output) {
   # Creates a temporary file with the extension ".txt" and assigns its path to the variable `outfile`.
   outfile <- tempfile(fileext = ".txt")
+  sink(file = outfile)
   # Writes the content of `text_to_output` to the temporary file specified by `outfile`.
-  cat(text_to_output, file = outfile)
+  print(var_to_output, 
+        na.print = "",
+        n = nrow(var_to_output))
   # Opens the temporary file in the default text editor for viewing.
   file.show(outfile)
+  sink()
 }
