7918ee48ea9f04392f5ef4cec67d3b040cb2bd3d
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-24 10:50:06 -0400

    Update see_res_in_outfile.R
    write_csv or print

--- auxfunctions/R/see_res_in_outfile.R
+++ auxfunctions/R/see_res_in_outfile.R
@@ -4,14 +4,28 @@
 #' @param extension 
 #'
 see_res_in_outfile <- function(var_to_output, extension = ".txt") {
-  # Creates a temporary file and assigns its path to the variable `outfile`.
+  
+  # Creates a temporary file with the specified extension and assigns its path to `outfile`.
   outfile <- tempfile(fileext = extension)
-  sink(file = outfile)
-  # Writes the content of `text_to_output` to the temporary file specified by `outfile`.
-  print(var_to_output, 
-        na.print = "",
-        n = nrow(var_to_output))
+  
+  # Checks if the specified extension is ".csv".
+  if (extension == ".csv") {
+    # Writes the content of `var_to_output` to the temporary CSV file using `write_csv` from the `readr` package.
+    readr::write_csv(var_to_output, file = outfile)
+  }
+  else {
+    # Redirects R output to the file specified by `outfile` if the extension is not ".csv".
+    sink(file = outfile)
+    
+    # Writes the content of `var_to_output` to the temporary file specified by `outfile`.
+    print(var_to_output,
+          na.print = "",  # Specifies that NA values should be printed as an empty string.
+          n = nrow(var_to_output))  # Ensures all rows of `var_to_output` are printed.
+    
+    # Restores the normal R output to the console.
+    sink()
+  }
+  
   # Opens the temporary file in the default text editor for viewing.
   file.show(outfile)
-  sink()
 }
