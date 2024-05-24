f1972f3fbaece207ec3da02f8590d3031a34c30d
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-24 09:04:27 -0400

    Update see_res_in_outfile.R
    add an ext param

--- auxfunctions/R/see_res_in_outfile.R
+++ auxfunctions/R/see_res_in_outfile.R
@@ -1,6 +1,11 @@
-see_res_in_outfile <- function(var_to_output) {
-  # Creates a temporary file with the extension ".txt" and assigns its path to the variable `outfile`.
-  outfile <- tempfile(fileext = ".txt")
+#' Show the Long Text in an Outfile
+#'
+#' @param var_to_output 
+#' @param extension 
+#'
+see_res_in_outfile <- function(var_to_output, extension = ".txt") {
+  # Creates a temporary file and assigns its path to the variable `outfile`.
+  outfile <- tempfile(fileext = extension)
   sink(file = outfile)
   # Writes the content of `text_to_output` to the temporary file specified by `outfile`.
   print(var_to_output, 
