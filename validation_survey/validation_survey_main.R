16e02379709b9960a6a5bec6b3999cf5b7e19632
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-25 11:17:20 -0400

    Update validation_survey_main.R
    set up paths

--- validation_survey/validation_survey_main.R
+++ validation_survey/validation_survey_main.R
@@ -1,4 +1,23 @@
+# Set up ----
+
 install.packages("devtools")
 library(devtools)
 devtools::install_github("AShipunova1/R_code/auxfunctions@development")
 library(auxfunctions)
+
+my_paths <- set_work_dir()
+
+# get this project name
+current_project_dir_name <- this.path::this.dir()
+
+# find its base name
+current_project_name <-
+  basename(current_project_dir_name)
+
+# use current_project_name to create input and output path
+curr_proj_input_path <- file.path(my_paths$inputs,
+                         current_project_name)
+
+curr_proj_output_path <- file.path(my_paths$outputs,
+                         current_project_name)
+
