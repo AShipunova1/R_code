a180ee516abf66c13a266451034592f7680db139
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-29 15:37:39 -0400

    Update validation_survey_main.R
    trip_int_diff_vsl_3m_plot + text

--- validation_survey/validation_survey_main.R
+++ validation_survey/validation_survey_main.R
@@ -535,7 +535,7 @@ big_diff_times <-
   lgb_join_i1__t_diff_short__w_int_all_dup_rm__int_dup_rm_short |> 
   filter(big_diff_time == "yes") |> 
   mutate(int_before_trip = trip_end_interview_diff < 1,
-         diff_as_num = as.numeric(trip_end_interview_diff))
+         diff_as_num = as.numeric(trip_end_interview_diff) / 60)
 
 # View(big_diff_times)
 # 145
@@ -580,15 +580,24 @@ trip_int_diff_vsl_plot <-
 trip_int_diff_vsl_3m_plot <-
   big_diff_times |>
   filter(month(trip_end_date_time) == 7) |>
-  ggplot(aes(x = trip_end_date_time, y = VESSEL_OFFICIAL_NBR, colour = VESSEL_OFFICIAL_NBR)) +
-  geom_segment(aes(xend = interview_date_time, yend = VESSEL_OFFICIAL_NBR),
+  ggplot(aes(x = trip_end_date_time, 
+             y = VESSEL_OFFICIAL_NBR, 
+             colour = VESSEL_OFFICIAL_NBR,
+             label = diff_as_num)) +
+  geom_segment(aes(xend = interview_date_time, 
+                   yend = VESSEL_OFFICIAL_NBR),
                colour = "black") +
   geom_point(size = 3) +
   geom_point(aes(x = interview_date_time), size = 3) +
   theme_bw() +
   theme(legend.position = "none")
 
-trip_int_diff_vsl_3m_plot
+trip_int_diff_vsl_3m_plot +
+  geom_text(check_overlap = TRUE,
+            vjust = 0,
+            nudge_y = 0.3)
+# 
+#             vjust = 0)
 
 # Catch ----
 
