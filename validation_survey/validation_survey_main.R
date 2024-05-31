63ab0a9dcf44036cdc738433ade96afc27752dab
AShipunova1 <anna.shipunova@noaa.gov>
2024-05-31 13:19:42 -0400

    Update validation_survey_main.R
    rename source pathes

--- validation_survey/validation_survey_main.R
+++ validation_survey/validation_survey_main.R
@@ -32,13 +32,13 @@ curr_proj_output_path <- file.path(my_paths$outputs,
 auxfunctions::create_dir_if_not(curr_proj_output_path)
 
 # get data ----
-get_data_file_path <-
+get_data_path <-
   file.path(current_project_dir_name,
             paste0(current_project_name, "_", "get_data.R"))
 
-file.exists(get_data_file_path)
+file.exists(get_data_path)
 
-source(get_data_file_path)
+source(get_data_path)
 
 # Data are in:
 # survey_data_l_2022
@@ -47,7 +47,15 @@ source(get_data_file_path)
 
 # prepare data ----
 
-source(file.path(current_project_dir_name, "validation_survey_prepare_data.R"))
+prepare_data_path <-
+  file.path(current_project_dir_name,
+            paste0(current_project_name, "_", "prepare_data.R"))
+
+file.exists(prepare_data_path)
+
+source(prepare_data_path)
+
+
 
 glimpse(lgb_join_i1__t_diff_short__w_int_all_dup_rm__int_dup_rm_short)
 
