2bea9448bb211734ebfed4f1c327ac835b4e834b
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-06-03 14:51:21 -0400

    Update sc_compliance_get_data.R
    ## prep the compliance/override data ----

--- sc_compliance/sc_compliance_get_data.R
+++ sc_compliance/sc_compliance_get_data.R
@@ -49,3 +49,78 @@ compl_override_data$COMP_WEEK_START_DT |> min()
 # [1] "2023-01-01 23:00:00 EST"
 compl_override_data$COMP_WEEK_START_DT |> max()
 # [1] "2024-12-22 23:00:00 EST"
+
+## prep the compliance/override data ----
+
+### Change column names for consistency with other datasets ----
+compl_override_data__renamed <-
+  compl_override_data |>
+  dplyr::rename(vessel_official_number =
+                  "VESSEL_OFFICIAL_NBR",
+                overridden = "IS_COMP_OVERRIDE") |>
+  clean_headers()
+
+### change data type of this column if needed ----
+if (!class(compl_override_data__renamed$vessel_official_number) == "character") {
+  compl_override_data__renamed$vessel_official_number <-
+    as.character(compl_override_data__renamed$vessel_official_number)
+}
+
+#### Add both months for straddle weeks ----
+
+# Explanations:
+# 1. 'compl_override_data__renamed_interv' is created from 'compl_override_data__renamed'.
+# 2. It groups the data by the start and end dates of the compliance weeks.
+# 3. It calculates the minimum month value between the 'comp_week_start_dt' and 'comp_week_end_dt' dates, storing it in a new column called 'comp_month_min'.
+# 4. It calculates the maximum month value between the 'comp_week_start_dt' and 'comp_week_end_dt' dates, storing it in a new column called 'comp_month_max'.
+# 5. The data is ungrouped after the calculations are done.
+
+compl_override_data__renamed_interv <-
+  compl_override_data__renamed |>
+  dplyr::group_by(comp_week_start_dt, comp_week_end_dt) |>
+  dplyr::mutate(comp_month_min =
+           min(lubridate::month(comp_week_start_dt),
+               lubridate::month(comp_week_end_dt))) |>
+  dplyr::mutate(comp_month_max =
+           max(lubridate::month(comp_week_start_dt),
+               lubridate::month(comp_week_end_dt))) |>
+  dplyr::ungroup()
+
+# check
+compl_override_data__renamed_interv |>
+  dplyr::filter(!comp_month_min == comp_month_max) |>
+  dplyr::select(
+    comp_year,
+    comp_week,
+    comp_week_start_dt,
+    comp_week_end_dt,
+    comp_month_min,
+    comp_month_max
+  ) |>
+  dplyr::distinct() |>
+  dplyr::glimpse()
+
+## keep fewer fields from compliance info ----
+# Keep only entries for "my_year" defined earlier and the previous year. Could be changed depending on the data provided by SC.
+
+compl_override_data__renamed_m_short <-
+  compl_override_data__renamed_interv |>
+  select(
+    vessel_official_number,
+    permit_group,
+    prm_grp_exp_date,
+    comp_year,
+    comp_week,
+    comp_week_start_dt,
+    comp_week_end_dt,
+    is_comp,
+    overridden,
+    comp_month_min,
+    comp_month_max
+  ) |>
+  distinct() |>
+  filter(comp_year %in% c(my_year, as.integer(my_year) - 1))
+
+dim(compl_override_data__renamed_m_short)
+# [1] 279379     11
+
