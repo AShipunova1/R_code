8931e28d1d013f6a3164d233b2d1a5f40f80e89e
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-06-24 15:55:28 -0400

    Update validation_survey_interview_no_lgb.R
    check

--- validation_survey/validation_survey_interview_no_lgb.R
+++ validation_survey/validation_survey_interview_no_lgb.R
@@ -10,11 +10,7 @@ lgb_join_i1__t_diff_short_has_no_trip_not_empty <-
 dim(lgb_join_i1__t_diff_short_has_no_trip_not_empty)
 # 833
 
-# Prep data for interviews w no logbooks ----
-
-## prep lgb info ----
-db_logbooks_2022_vsl_t_end <-
-  db_logbooks_2022_vsl_t_end_all |>
-  dplyr::select(VESSEL_OFFICIAL_NBR, TRIP_END_DATE, TRIP_ID) |>
-  dplyr::distinct()
+lgb_join_i1__t_diff_short_has_no_trip_not_empty |> 
+  head() |> 
+  glimpse()
 
