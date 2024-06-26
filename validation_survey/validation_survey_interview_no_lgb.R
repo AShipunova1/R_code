a7f5b642987481eca2802894dbdc3816a8f5822d
AShipunova1 <anna.shipunova@noaa.gov>
2024-06-26 18:44:53 -0400

    Update validation_survey_interview_no_lgb.R
    start plotting

--- validation_survey/validation_survey_interview_no_lgb.R
+++ validation_survey/validation_survey_interview_no_lgb.R
@@ -28,12 +28,14 @@ lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts <-
     join_by(id_code, VESSEL_OFFICIAL_NBR == vsl_num)
   )
 
-dim(lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts)
+View(lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts)
 # [1] 833  9
 
-lgb_join_i1__t_diff_short_has_no_trip_not_empty
-  select(id_code, vsl_num, st_2, cnty_3, fips) |> 
-  count(st_2)
+lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts <- 
+lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts |> 
+  # select(id_code, VESSEL_OFFICIAL_NBR, st_2, cnty_3, fips) |> 
+  add_count(st_2, name = "no_lgb_int_by_state") |> 
+  add_count(fips, name = "no_lgb_int_by_st_county")
 #   st_2      n
 #   <chr> <int>
 # 1 00       35
@@ -43,7 +45,25 @@ lgb_join_i1__t_diff_short_has_no_trip_not_empty
 # 5 28      108
 # 6 48       78
 
-count_interviews <-
-  function(my_df,
-           cnt_field = "st_2") {
-    
+lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts_perc <-
+  lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts |>
+  mutate(
+    percent_no_lgb_cnty = no_lgb_int_by_st_county * 100 / total_int_by_st_county       ,
+    percent_no_lgb_st = no_lgb_int_by_state * 100 / total_int_by_state
+  ) |> 
+  mutate(percent_cnty_round = round(percent_no_lgb_cnty, 0),
+         percent_st_round = round(percent_no_lgb_st, 0))
+
+lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts_perc |> 
+  head() |> 
+  glimpse()
+
+int_no_lgb_by_state_to_plot_w_labels <- 
+  make_state_labels_w_cnts(lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts_perc,
+           "percent_st_round")
+   
+
+# int_no_lgb_by_state_to_plot_w_labels
+
+lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts_perc |> 
+plot_counties(county_cnt_col_name = "percent_cnty_round") 
