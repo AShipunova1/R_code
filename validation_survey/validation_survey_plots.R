803547811205dbb064375a38594712f7a989fbe4
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-01 15:53:58 -0400

    mv to plot Interview no logbooks plot

--- validation_survey/validation_survey_plots.R
+++ validation_survey/validation_survey_plots.R
@@ -123,3 +123,187 @@ plot_counties_res_with_labels <-
 #| out-width: 100%
 
 plot_counties_res_with_labels
+
+# Interview no logbooks plot ----
+
+plot_states <- usmap::plot_usmap(include = c(gulf_states, "FL")) 
+
+plot_cnties_only <-
+  plot_usmap(regions = "counties",
+             include = c(florida_gulf_counties))
+
+### prep state info for plotting ----
+selected_states_df <- usmap::us_map(include = c(gulf_states, "FL"))
+
+#' Get centroids
+centroid_labels <- usmapdata::centroid_labels("states")
+
+#' Join centroids to data
+old_names <- names(centroid_labels)
+
+names(centroid_labels) <- c("st_2", "abbr", "full", "geom")
+
+make_state_labels <- function(my_df, state_field_name = "st_2") {
+  temp_df <- 
+  dplyr::inner_join(my_df, 
+                   centroid_labels, 
+                   dplyr::join_by(!!state_field_name == st_2))
+  
+  temp_df |> 
+    dplyr::select(!!state_field_name,
+                  abbr,
+                  full,
+                  total_int_no_lgb_by_state,
+                  geom) |>
+    dplyr::distinct() |>
+    dplyr::mutate(label_st_cnt = 
+                    paste(abbr, total_int_no_lgb_by_state)) |> 
+    dplyr::arrange(full)
+}
+
+# my_dfs_to_plot <- Hmisc::llist(
+#   intv_w_no_lgb_join_by_day_vsl_cnt,
+#   intv_w_no_lgb_join_by_day_vsl_restored_cnt,
+#   intv_w_no_lgb_join_by_day_vsl__minus_same_cptn_cnt,
+#   intv_w_no_lgb_join_by_day_vsl__minus_same_cptn__restored_states_short__fips_cnt
+# )
+# 
+# fields_to_join_by <- 
+#   list("st_2", "restored_st", "st_2", "st_2")
+# 
+# my_dfs_to_plot_w_labels <-
+#   purrr::map2(my_dfs_to_plot,
+#               fields_to_join_by,
+#               make_state_labels)
+
+### interviews w no lgb plot ----
+
+plot_counties <- function(my_df) {
+  usmap::plot_usmap(
+    regions = "counties",
+    include = c(gulf_states, "FL"),
+    data = my_df,
+    values = "num_int_no_lgb_by_fips",
+    color = "lightgrey"
+  ) +
+    ggplot2::scale_fill_gradient(
+      # name = "Interviews w/o lgbks",
+      name = "",
+      high = "blue",
+      low = "yellow",
+      na.value = "transparent"
+      # ,
+      # guide = guide_legend()
+    ) +
+    ggplot2::theme(legend.position = "right",
+                   # legend.position = c(.95, .95),
+                   # legend.justification = c("right", "top"),
+                   # legend.box.just = "right",
+                   legend.margin = ggplot2::margin(0, 0, 0, 0)
+    )
+          # ,
+          # legend.spacing.x = unit(0, 'cm'))
+  # +
+    # guides(fill = guide_legend(label.position = "bottom"))
+}
+
+plot_cnties_4 <-
+  my_dfs_to_plot |>
+  purrr::map(plot_counties)
+
+#' check
+no_state_interview_no_lgb_num <- 
+  intv_w_no_lgb_join_by_day_vsl_cnt |>
+  dplyr::filter(st_2 == "00") |> 
+  dplyr::select(total_int_no_lgb_by_state) |> 
+  dplyr::distinct()
+#' 192
+
+intv_w_no_lgb_join_by_day_vsl__minus_same_cptn_cnt__no_st <-
+  intv_w_no_lgb_join_by_day_vsl__minus_same_cptn_cnt |> 
+  dplyr::filter(st_2 == "00") |> 
+  dplyr::select(total_int_no_lgb_by_state) |> 
+  dplyr::distinct()
+#' 189
+
+add_state_labels <-
+  function(usmap_plot, labels_by_state = state_labels_short) {
+    usmap_plot +
+      ggplot2::geom_sf_text(data = labels_by_state, 
+                            ggplot2::aes(geometry = geom, label = label_st_cnt)) +
+      ggplot2::geom_sf(data = selected_states_df,
+              color = "green",
+              fill = NA)
+  }
+
+#' add_state_labels
+
+all_4_plots <- 
+  purrr::map2(plot_cnties_4,
+              my_dfs_to_plot_w_labels,
+              add_state_labels)
+
+all_4_plots_together <-
+  gridExtra::grid.arrange(
+    grobs = all_4_plots,
+    top =
+      ggpubr::text_grob(
+        "Interview with no logbooks, \n (1) Raw, (1) With restored states, (2) With removed by captain names, (2) With restored states",
+        rot = 0,
+        vjust = 1
+      ),
+    ncol = 1,
+    heights = c(5, 10, 5, 10)
+  )
+
+# plot_restored_all <- 
+#   add_state_labels(plot_cnties_restored, state_labels_restored) +
+#   ggplot2::labs(title = "Number of interviews without logbooks by state/county")
+# 
+# plot_cnties_state_lbls <-
+#   add_state_labels(plot_cnties, state_labels_short) +
+#   ggplot2::labs(
+#     title = "Number of interviews without logbooks by state/county",
+#     caption = stringr::str_glue(
+#       "Number of interviews without logbooks with no state info is {no_state_interview_no_lgb_num$total_int_no_lgb_by_state}."
+#     )
+#   )
+
+#### interview wo lgb plot show ----
+# #| column: screen
+#| out-width: 100%
+
+purrr::map2(
+  all_4_plots,
+  names(all_4_plots),
+  \(p, p_name) {
+    p +
+      ggplot2::labs(title = "Number of interviews without logbooks by state/county",
+                    caption = p_name)
+  })
+    
+int_no_lgb_by_state_to_plot_w_labels <- 
+  make_state_labels_w_cnts(lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts_perc,
+           "percent_st_label")
+   
+
+no_state_lgb_num <- 
+lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts |> 
+  filter(st_2 == '00') |> 
+  select(no_lgb_int_by_state) |> 
+  distinct()
+
+
+plot_counties_res_with_labels <-
+  lgb_join_i1__t_diff_short_has_no_trip_not_empty__with_total_cnts__with_no_lgb_cnts_perc |> 
+  plot_counties(county_cnt_col_name = "percent_cnty_round")  +
+  add_state_labels(int_no_lgb_by_state_to_plot_w_labels) +
+  add_st_boundaries() +
+  ggplot2::labs(
+    title = "Percent of interviews without logbooks by state/county",
+    caption = stringr::str_glue(
+      "{no_state_lgb_num$no_lgb_int_by_state} interviews have no state info and are not included."
+    )
+  )
+
+plot_counties_res_with_labels
