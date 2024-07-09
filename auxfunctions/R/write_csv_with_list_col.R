67ef48bb5b7add68e3dd0d5436ca4443480e23b6
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-07-09 15:53:26 -0400

    write_csv_with_list_col

---
+++ auxfunctions/R/write_csv_with_list_col.R
@@ -0,0 +1,11 @@
+#' Write a df with List Columns to a csv
+#'
+#' @param my_df 
+#' @param csv_out_file_path
+#'
+write_csv_with_list_col <- function(my_df, csv_out_file_path) {
+  my_df |>
+    rowwise() |>
+    mutate_if(is.list, ~ paste(unlist(.), collapse = ', ')) |>
+    readr::write_csv(csv_out_file_path)
+}
