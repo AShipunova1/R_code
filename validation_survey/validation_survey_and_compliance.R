ae8dedcd4b5c10a4b90a05c5d84311375246e68f
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-10 16:49:48 -0400

    Update validation_survey_and_compliance.R
    cnts and take fhier compliance data out

--- validation_survey/validation_survey_and_compliance.R
+++ validation_survey/validation_survey_and_compliance.R
@@ -231,7 +231,7 @@ db_compliance_2022__comp_after_overr__short_m__interv <-
   filter(tolower(VESSEL_OFFICIAL_NBR) %in% tolower(vessels_in_survey_no_lgb))
 
 dim(db_compliance_2022__comp_after_overr__short_m__interv)
-# [1] 7600    6
+# [1] 7305    6
 
 vessels_in_survey_no_lgb__n__compl <- 
   unique(db_compliance_2022__comp_after_overr__short_m__interv$VESSEL_OFFICIAL_NBR)
@@ -239,11 +239,12 @@ vessels_in_survey_no_lgb__n__compl <-
 glimpse(vessels_in_survey_no_lgb__n__compl)
 
 n_distinct(db_compliance_2022__comp_after_overr__short_m__interv$VESSEL_OFFICIAL_NBR)
-# 192
+# 184
 
 #' Why there are vessels in survey_no_lgb with no compliance info?
 setdiff(vessels_in_survey_no_lgb,
         vessels_in_survey_no_lgb__n__compl)
+# 86
 
 # get compl, no compl, or both per year ----
 db_compliance_2022__comp_after_overr__short_m__interv__wide <-
@@ -261,8 +262,8 @@ db_compliance_2022__comp_after_overr__short_m__interv__wide <-
   ) |>
   dplyr::ungroup()
 
-# dim(db_compliance_2022__comp_after_overr__short_m__interv__wide)
-# 1 192
+dim(db_compliance_2022__comp_after_overr__short_m__interv__wide)
+# 1 184
 
 ## turn back year compliance to the long format ----
 db_compliance_2022__comp_after_overr__short_m__interv__wide_long <-
@@ -273,7 +274,7 @@ db_compliance_2022__comp_after_overr__short_m__interv__wide_long <-
   rename("is_compl_or_both_year" = V1)
 
 dim(db_compliance_2022__comp_after_overr__short_m__interv__wide_long)
-# [1] 192   2
+# [1] 184   2
 
 # check
 db_compliance_2022__comp_after_overr__short_m__interv__wide_long |> 
@@ -281,7 +282,7 @@ db_compliance_2022__comp_after_overr__short_m__interv__wide_long |>
 #  is_compl_or_both_year   n
 #                     no   2
 #                 no_yes   3
-#                    yes 187
+#                    yes 179
 
 # get compliance per month ----
 db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider <-
@@ -296,7 +297,7 @@ db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider <-
   dplyr::ungroup()
 
 dim(db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider)
-# [1]  12 193
+# [1]  12 185
 
 db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider |> 
   arrange(year_month) |> 
@@ -313,10 +314,10 @@ db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long <-
   )
 
 dim(db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long)
-# [1] 2304    3
+# [1] 2208    3
 
 n_distinct(db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long$vessel_official_number)
-# [1] 192, ok, as above
+# [1] 184, ok, as above
 
 ## count compliance per month ----
 db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long_cnts <-
@@ -326,55 +327,11 @@ db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long_cnts 
                    name = "compl_or_not_cnt_m") |> 
   unique()
   
-# View(db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long_cnts)
+db_compliance_2022__comp_after_overr__short_m__interv__compl_m_wider__long_cnts |> 
+  filter(!is.na(is_compl_or_both)) |> 
+  arrange(year_month, desc(is_compl_or_both)) |> 
+  glimpse()
 
 # Result: these vessels are mostly compliant.
-
-# Survey comparison with FHIER compliance ----
-# fhier_compliance_2022__comp_after_overr__short__clean_weeeks__clean_vsl_id__m
-
-# get fhier compliance information for vessels from survey no lgb ----
-fhier_compliance_2022__comp_after_overr__short__clean_weeeks__clean_vsl_id__m__interv <-
-  fhier_compliance_2022__comp_after_overr__short__clean_weeeks__clean_vsl_id__m |>
-  filter(tolower(vessel_official_number) %in% tolower(lgb_join_i1__no_lgb__short$VESSEL_OFFICIAL_NBR))
-
-dim(fhier_compliance_2022__comp_after_overr__short__clean_weeeks__clean_vsl_id__m__interv)
-# [1] 7600    9
-
-vessels_in_survey_no_lgb__n__compl_fhier <- 
-  unique(fhier_compliance_2022__comp_after_overr__short__clean_weeeks__clean_vsl_id__m__interv$vessel_official_number)
-
-setdiff(tolower(vessels_in_survey_no_lgb__n__compl), vessels_in_survey_no_lgb__n__compl_fhier)
-# 0
-
-n_distinct(fhier_compliance_2022__comp_after_overr__short__clean_weeeks__clean_vsl_id__m__interv$vessel_official_number)
-# 192
-
-#' Why there are vessels in survey_no_lgb with no compliance info?
-
-vessels_in_survey_not_in_compl <-
-  setdiff(vessels_in_survey_no_lgb,
-          vessels_in_survey_no_lgb__n__compl_fhier)
-
-length(vessels_in_survey_not_in_compl)
-# 38
-
-#' To use as a filter in FHIER/compliance
-vessels_in_survey_not_in_compl |> 
-  paste(collapse = ", ") |> 
-  print()
-#' nothing found for 2022
-
-#' check these vessels in original survey, found 8, the rest is from the fuzzy join with PIMS
-vessels_in_survey_and_in_compl <-
-  survey_data_l_2022_i1_w_dates_clean_vsl_no_na_vsl_num__short |>
-  filter(survey_vessel_id %in% vessels_in_survey_not_in_compl) |>
-  select(survey_vessel_id) |>
-  distinct()
-#' Manually checked in FHIER, no compliance  found for 2022
-
-vessels_in_survey_and_in_compl
-
-#' stop repeating here, the vessel ids are the same as from db.
 
 # Look at days in survey and compliance?
