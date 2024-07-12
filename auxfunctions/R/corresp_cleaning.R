c18b3026557587e919537b87800cf39dac8db0cc
AShipunova1 <77342640+AShipunova1@users.noreply.github.com>
2024-07-12 10:28:44 -0400

    Update corresp_cleaning.R
    rename corresp_arr, rm csvs_clean1[[1]]

--- auxfunctions/R/corresp_cleaning.R
+++ auxfunctions/R/corresp_cleaning.R
@@ -1,27 +1,24 @@
 corresp_cleaning <-
-function(csvs_clean1) {
-
-  # Extract the first element (correspondence data) from the cleaned CSV list.
-  corresp_arr <- csvs_clean1[[1]]
+function(corresp_df) {
 
   # Add a new column 'contact_freq' with "yes" if there is a 'contactdate' (and "no" if not).
   # Group the data by 'vesselofficialnumber' and count how many "contacts" there are for each. Save in the "contact_freq" column.
-  corresp_arr_contact_cnts <- add_count_contacts(corresp_arr)
+  corresp_df_contact_cnts <- auxfunctions::add_count_contacts(corresp_df)
 
   # Find the column names for 'createdon' and 'contactdate'.
   createdon_field_name <-
-    find_col_name(corresp_arr, "created", "on")[1]
+    auxfunctions::find_col_name(corresp_df, "created", "on")[1]
   contactdate_field_name <-
-    find_col_name(corresp_arr, "contact", "date")[1]
+    auxfunctions::find_col_name(corresp_df, "contact", "date")[1]
 
   # Change the data types of 'createdon' and 'contactdate' columns to POSIXct.
-  corresp_arr_contact_cnts <-
-    change_to_dates(corresp_arr_contact_cnts,
+  corresp_df_contact_cnts <-
+    auxfunctions::change_to_dates(corresp_df_contact_cnts,
                     createdon_field_name)
-  corresp_arr_contact_cnts <-
-    change_to_dates(corresp_arr_contact_cnts,
+  corresp_df_contact_cnts <-
+    auxfunctions::change_to_dates(corresp_df_contact_cnts,
                     contactdate_field_name)
 
   # Return the cleaned and processed correspondence data.
-  return(corresp_arr_contact_cnts)
+  return(corresp_df_contact_cnts)
 }
