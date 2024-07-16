e0633f652f4db070153279dfaed742d859437928
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-16 15:32:15 -0400

    Update get_one_function_text_help.R
    comments

--- auxfunctions/R/get_one_function_text_help.R
+++ auxfunctions/R/get_one_function_text_help.R
@@ -2,45 +2,52 @@ get_one_function_text_help <-
   function(function_name,
            my_split_newline_char = "@@@") {
     
-    
+    # Create a vector containing the function name
     my_used_function_names <- c(function_name)
-    # E.g. my_used_function_names <- c("trim_all_vessel_ids_simple")
+    # Example: my_used_function_names <- c("trim_all_vessel_ids_simple")
     
-    # get all my_used_function_texts ----
+    # Get the text for all the functions used
     my_used_function_texts <-
       auxfunctions::get_my_used_function_texts(my_used_function_names)
     
-    # get all my used function helps ----
+    # Get the help documentation for all the functions used
     my_used_function_helps <-
       auxfunctions::get_my_used_function_helps(my_used_function_names)
     
+    # Get the length of the function names vector
     idx <- length(my_used_function_names)
-    # 1
+    # Example: 1
     
-    my_fun_name_to_search <-
+    # Create a pattern to search for the function name in the text
+    my_function_name_to_search <-
       str_glue("(",
                my_split_newline_char,
                ".+{my_used_function_names[[idx]]})")
     
-    my_fun_name_w_txt <-
+    # Replace function names with their definitions in the text
+    my_function_name_w_txt <-
       purrr::reduce(seq_len(length(my_used_function_names)),
                     \(acc, nxt) auxfunctions::replace_function_with_def(acc, nxt),
-                    .init = my_fun_name_to_search)
+                    .init = my_function_name_to_search)
     
-    my_fun_name_w_txt_split <-
-      auxfunctions::split_one_line_text_back(my_fun_name_w_txt)
+    # Split the single line text back into multiple lines
+    my_function_name_w_txt_split <-
+      auxfunctions::split_one_line_text_back(my_function_name_w_txt)
     
-    my_fun_name_w_txt_split_rmd_text <-
-      knitr::spin(text = my_fun_name_w_txt_split,
+    # Convert the split text into R Markdown format using knitr::spin
+    my_function_name_w_txt_split_rmd_text <-
+      knitr::spin(text = my_function_name_w_txt_split,
                   knit = FALSE,
                   format = 'qmd')
     
     # Change back to \s and \b in functions
-    my_fun_name_w_txt_split_rmd_text <-
-      my_fun_name_w_txt_split_rmd_text |>
+    my_function_name_w_txt_split_rmd_text <-
+      my_function_name_w_txt_split_rmd_text |>
       str_replace_all(my_slash_replacement, "\\\\")
     
-    cat(my_fun_name_w_txt_split_rmd_text, sep = "\n")
+    # Print the final R Markdown formatted text
+    cat(my_function_name_w_txt_split_rmd_text, sep = "\n")
     
-    return(my_fun_name_w_txt_split_rmd_text)
+    # Return the final R Markdown formatted text
+    return(my_function_name_w_txt_split_rmd_text)
   }
