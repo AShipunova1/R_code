d19fe1856953cef8a3517d32760df03875ca791d
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-16 15:21:55 -0400

    Update get_my_used_function_helps.R
    comments and         paste0(., collapse = "")

--- auxfunctions/R/get_my_used_function_helps.R
+++ auxfunctions/R/get_my_used_function_helps.R
@@ -1,29 +1,39 @@
-## a function to get a function help as a text ----
 get_my_used_function_helps <- function(function_name) {
+  # Un-comment to enable browser() for debugging
   # browser()
+  
+  # Define the tags to look for in the help file
   used_tags <- c("description", "details")
-  help_text <-
+  
+  # Get the help file for the given function from the "auxfunctions" package
+  help_text <- 
     help(function_name, "auxfunctions") |>
     utils:::.getHelpFile()
   
-  used_tags_help_list <-
+  # Extract the relevant sections of the help file based on the defined tags
+  used_tags_help_list <- 
     map(used_tags, \(one_tag) {
       help_text |>
-        purrr::keep( ~ attr(.x, "Rd_tag") == paste0("\\", one_tag)) |>
-        purrr::map(as.character) %>%
-        purrr::flatten_chr() %>%
-        paste0(., collapse = "")
+        purrr::keep(~ attr(.x, "Rd_tag") == paste0("\\", one_tag)) |>
+        purrr::map(as.character) |>
+        purrr::flatten_chr() |>
+        paste0(collapse = "")
+        # paste0(., collapse = "")
+
     }) |>
     setNames(used_tags)
   
+  # Combine the extracted sections into a single string
   used_tags_help <- 
-    paste(used_tags_help_list[[1]],
-          "\n",
+    paste(used_tags_help_list[[1]], 
+          "\n", 
           used_tags_help_list[[2]])
   
+  # Add comment symbols to each line of the help text
   used_tags_help_commented <- 
     used_tags_help |> 
     str_replace_all("\n", "\n# ")
   
+  # Return the commented help text
   return(used_tags_help_commented)
 }
