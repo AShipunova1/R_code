9fdad746d563bde9ba7d94d51543eac571d317f4
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-19 15:09:15 -0400

    Update make_qmd_file.R
    comment out

--- make_qmd_file.R
+++ make_qmd_file.R
@@ -675,19 +675,18 @@ title: {curent_project_name}
 # Don't use in the auxiliary file
 # Setup
 setup_text <- "
-Note: In the following code '<<<<' and '>>>>' mark the start and the end of definitions and help documents for helper functions.
-
-Quarto
-
-Quarto enables you to weave together content and executable code into a finished document.
-
-Running Code
-
-The **Run** button allows you to run individual or bunch of chunks as a regular R script.
-
-When you click the **Render** button a document will be generated that includes both content and the output of embedded code.
 
 ```{r no cache setup, results='hide', message=FALSE, warning=FALSE, cache=FALSE, include=FALSE}
+
+# Quarto
+ 
+# Quarto enables you to weave together content and executable code into a finished document.
+ 
+# Running Code
+ 
+# The **Run** button allows you to run individual or bunch of chunks as a regular R script.
+ 
+# When you click the **Render** button a document will be generated that includes both content and the output of embedded code.
 
 # Setup for Quarto
 
