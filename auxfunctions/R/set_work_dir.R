2409c4385740f4bdf2914cd526f3689b4dc2127f
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-19 16:01:42 -0400

    Update set_work_dir.R
    rm redundant inline comments

--- auxfunctions/R/set_work_dir.R
+++ auxfunctions/R/set_work_dir.R
@@ -1,39 +1,31 @@
 set_work_dir <-
-function() {
-
-  # Set the working directory to the user's home directory (~)
-  setwd("~/")
-  base_dir <- getwd()
-
-  # Define 'main_r_dir' as "R_files_local"
-  main_r_dir <- "R_files_local"
-
-  # Define 'in_dir' as "my_inputs"
-  in_dir <- "my_inputs"
-
-  # Construct the full path to 'my_inputs' directory
-  full_path_to_in_dir <- file.path(base_dir, main_r_dir, in_dir)
-
-  # Define 'out_dir' as "my_outputs"
-  out_dir <- "my_outputs"
-
-  # Construct the full path to 'my_outputs' directory
-  full_path_to_out_dir <- file.path(base_dir, main_r_dir, out_dir)
-
-  # Define 'git_r_dir' as "R_code_github"
-  git_r_dir <- "R_code_github"
-
-  # Construct the full path to 'R_code_github' directory
-  full_path_to_r_git_dir <- file.path(base_dir, git_r_dir)
-
-  # Change the working directory to 'R_files_local'
-  setwd(file.path(base_dir, main_r_dir))
-
-  # Create a list of directory paths for 'inputs,' 'outputs,' and 'git_r'
-  my_paths <- list("inputs" = full_path_to_in_dir,
-                   "outputs" = full_path_to_out_dir,
-                   "git_r" = full_path_to_r_git_dir)
-
-  # Return the list of directory paths
-  return(my_paths)
-}
+  function() {
+    # Set the working directory to the user's home directory (~)
+    setwd("~/")
+    base_dir <- getwd()
+    
+    main_r_dir <- "R_files_local"
+    
+    in_dir <- "my_inputs"
+    
+    full_path_to_in_dir <- file.path(base_dir, main_r_dir, in_dir)
+    
+    out_dir <- "my_outputs"
+    
+    full_path_to_out_dir <- file.path(base_dir, main_r_dir, out_dir)
+    
+    git_r_dir <- "R_code_github"
+    
+    full_path_to_r_git_dir <- file.path(base_dir, git_r_dir)
+    
+    # Change the working directory
+    setwd(file.path(base_dir, main_r_dir))
+    
+    # Create a list of directory paths for 'inputs,' 'outputs,' and 'git_r'
+    my_paths <- list("inputs" = full_path_to_in_dir,
+                     "outputs" = full_path_to_out_dir,
+                     "git_r" = full_path_to_r_git_dir)
+    
+    # Return the list of directory paths
+    return(my_paths)
+  }
