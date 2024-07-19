c8ad7e1798d54cdedf964f113e92f25875c3a3f3
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-19 13:51:48 -0400

    Update fix_names.R
    clean up

--- auxfunctions/R/fix_names.R
+++ auxfunctions/R/fix_names.R
@@ -1,17 +1,13 @@
 fix_names <-
-function(x) {
-  # Use the pipe operator %>%
-  x %>%
-
-    # Remove dots from column names
-    stringr::str_replace_all("\\.", "") %>%
-
-    # Replace all characters that are not letters or numbers with underscores
-    stringr::str_replace_all("[^A-z0-9]", "_") %>%
-
-    # Ensure that letters are only in the beginning of the column name
-    stringr::str_replace_all("^(_*)(.+)", "\\2\\1") %>%
-
-    # Convert column names to lowercase using 'my_headers_case_function'
-    my_headers_case_function()
-}
+  function(x) {
+    x |>
+      
+      stringr::str_replace_all("\\.", "") |>
+      
+      stringr::str_replace_all("[^A-z0-9]", "_") |>
+      
+      stringr::str_replace_all("^(_*)(.+)", "\\2\\1") |>
+      
+      # Convert column names to lowercase using 'my_headers_case_function'
+      my_headers_case_function()
+  }
