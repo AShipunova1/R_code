d897903359981391f9cd853644c20d5472ff0cac
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-23 13:16:08 -0400

    Update clean_all_csvs.R
    ::

--- auxfunctions/R/clean_all_csvs.R
+++ auxfunctions/R/clean_all_csvs.R
@@ -1,7 +1,7 @@
 clean_all_csvs <-
 function(csvs, vessel_id_field_name = NA) {
   # Clean headers of all CSVs using the 'clean_headers' function
-  csvs_clean0 <- lapply(csvs, clean_headers)
+  csvs_clean0 <- lapply(csvs, auxfunctions::clean_headers)
 
   # Trim 'vesselofficialnumber' column (if specified) in all cleaned CSVs
   csvs_clean1 <-
