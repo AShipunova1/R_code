b1db91411920e9c4958edbe262198081d1d395e7
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-24 09:16:20 -0400

    Update write_r_script_to_google_drive.R
    get name to copy

--- write_r_script_to_google_drive.R
+++ write_r_script_to_google_drive.R
@@ -15,7 +15,7 @@ output_egr_violators_googledrive_folder_name <- "Code"
 # Define the current result Google Sheets name
 current_google_doc_name <- "egregious_violators_start.qmd"
 
-### Set up Google Drive paths ----
+## Set up Google Drive paths ----
 
 #'
 #' Get the path to the main Egregious violators folder on Google Drive
@@ -69,34 +69,25 @@ my_current_google_doc <-
 
 # An example of my_current_google_doc:
 #   name                        id                                           drive_resource
-# 1 Egregious Violators Current ...--o6BpLWpb4-... <named list [36]>
+# 1 egregious_violators_start.qmd ...--o6BpLWpb4-... <named list [36]>
+
+# Get my_current_google_doc created date
+# View(my_current_google_doc$drive_resource)
+my_current_google_doc_more <-
+  my_current_google_doc |>
+  mutate(
+    modified = map_chr(drive_resource, "modifiedTime"),
+    created = map_chr(drive_resource, "createdTime")
+  )
+
+# Make a name to rename to
+previous_current_google_doc_name <- 
+  str_glue("{current_google_doc_name}_{my_current_google_doc_more$modified}")
+
 
 # Next:
-# 1) load it to R;
-# 2) create a new spreadsheet with the date of the loaded worksheet and dump the content into it;
-# 3) create a new worksheet in the current spreadsheet with today's date;
-# 4) write the code output into it;
-# 5) check in browser.
-
-# 1) load it to R
-previous_current_content <- googlesheets4::read_sheet(my_current_google_doc)
-
-# 2) create a new spread sheet with the date of loaded worksheet and dump the content into it
-# a) get the previous spreadsheet name
-
-# ss_info contains detailed information about the current spreadsheet, including sheet names
-ss_info <- googlesheets4::gs4_get(my_current_google_doc)
-
-# grep for the pattern in case there are additional tabs
-previous_current_spread_sheet_name <-
-  grep(
-    "egregious_violators_to_investigate_20\\d\\d-\\d\\d-\\d\\d",
-    ss_info$sheets$name,
-    value = T
-  )
-# E.g. "egregious_violators_to_investigate_2024-06-18"
-
-# Rename the file from "current" to the previous_current_spread_sheet_name with the previous date.
+# Save a copy by renaming it
+# Rename the file from "current" to the previous_current_google_doc_name with the previous date.
 # In case of an error print the message and keep going.
 # If there is a file with this name this code will create another one with the same name.
 
@@ -106,7 +97,7 @@ tryCatch({
   googledrive::drive_mv(
     my_current_google_doc,
     path = googledrive::as_id(output_egr_violators_googledrive_folder_path),
-    name = previous_current_spread_sheet_name
+    name = previous_current_google_doc_name
   )
   # E.g.
   # Original file:
@@ -117,7 +108,7 @@ tryCatch({
 }, error = function(cond) {
   message(paste(
     "Failed to rename this file: ",
-    previous_current_spread_sheet_name
+    previous_current_google_doc_name
   ))
   
   message("Here's the original error message:")
