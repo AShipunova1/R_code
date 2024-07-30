d8aef96741de0bd1ea5e325ee4b3d7266ca52b5a
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-30 15:14:23 -0400

    Update home_port_main.R
    ## Set up paths ----
    start

--- home_ports/home_port_main.R
+++ home_ports/home_port_main.R
@@ -99,3 +99,115 @@ my_year_dates <-
 names(my_year_dates) <- my_years
 
 # str(my_year_dates)
+
+## Set up paths ----
+#
+# Different methods are used based on the user to accommodate different directory structure.
+# 
+# This allows the script to run correctly on multiple systems without manual path changes.
+# 
+# In the code in this section all user provided values have the word "manually" in the description. Everything else is created automatically.
+#
+# Manually: Change the following 2 lists (**my_paths** and **current_in_out_paths**) to your environment if needed. The variable _names_ are used throughout the code, so please change only the quoted _values_ inside the lists.
+# 
+
+# Check if the current username is not "anna.shipunova"
+if (!auxfunctions::get_username() == "anna.shipunova") {
+  auxfunctions::function_message_print(
+    "Please CHANGE the following 2 lists values to your environment if needed. Use full path to your directories in quotes."
+  )
+  
+  # 1) General directories (to look up additional files, e.g. processed data). It can be left as is if you don't have it. You can provide path to individual files later.
+  my_paths <- list(inputs  = "~/my_inputs",
+                   outputs = "~/my_outputs",
+                   git_r   = "~/R_code")
+  
+  # 2) Current project code, input and output directories
+  current_in_out_paths <-
+    list(
+      project_name = "home_port",
+      code = "~/home_port/code",
+      input = "~/home_port/input",
+      output = "~/home_port/output"
+    )
+  
+} else {
+  # If the username is "anna.shipunova", use Anna's directory structure.
+  my_paths <- auxfunctions::set_work_dir()
+  project_name = "home_port"
+  
+  current_project_paths <- function(current_project_name = NULL) {
+  
+  my_paths <- auxfunctions::set_work_dir()
+  
+  # check
+  'this.path' %in% rownames(installed.packages())
+
+  # get this project name
+  if (is.null(current_project_name)) {
+    current_project_dir_name <- this.path::this.dir()
+    
+    # find its base name
+    current_project_name <-
+      basename(current_project_dir_name)
+  } else {
+    current_project_dir_name <- 
+      file.path(my_paths$git_r, current_project_name)
+  }
+  
+  #' use current_project_name to create input and output paths
+  curr_proj_input_path <- 
+    file.path(my_paths$inputs, current_project_name)
+  
+  auxfunctions::create_dir_if_not(curr_proj_input_path)
+  
+  curr_proj_output_path <- file.path(my_paths$outputs, current_project_name)
+  
+  auxfunctions::create_dir_if_not(curr_proj_output_path)
+  
+  current_proj_paths <-
+    list(
+      "project_name" = current_project_name,
+      "code" = current_project_dir_name,
+      "input" = curr_proj_input_path,
+      "output" = curr_proj_output_path
+    )
+  
+  return(current_proj_paths)
+}
+
+  
+  # current_in_out_paths <- auxfunctions::current_project_paths(project_name)
+}
+
+current_project_dir_path <- this.path::this.dir()
+
+current_project_basename <- basename(current_project_dir_path)
+
+current_output_dir <-
+  file.path(my_paths$outputs,
+            current_project_basename)
+
+#
+# The following section uses provided directory names lists to automatically create separate variables for future use and create current input/output directories if they do not exists.
+# 
+# 
+# Create variables to store shortcuts to project directories
+# 
+# This is usually the current directory name.
+current_project_name <- current_in_out_paths$project_name
+
+current_project_path <- current_in_out_paths$code
+            
+current_project_input_path <- current_in_out_paths$input
+
+current_project_output_path <- current_in_out_paths$output
+
+# Create input and output directories if they don't exist
+auxfunctions::create_dir_if_not(current_project_input_path)
+
+auxfunctions::create_dir_if_not(current_project_output_path)
+
+### Additional individual paths to data files ----
+# This section sets up paths for specific data files used in the project
+# 
