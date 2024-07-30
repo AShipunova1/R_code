85717a3499e12ba7b2bdbf3a634435b42f953977
AShipunova1 <anna.shipunova@noaa.gov>
2024-07-30 14:20:50 -0400

    Create home_port_main.R

---
+++ home_ports/home_port_main.R
@@ -0,0 +1,12 @@
+# 1 Setup
+# 1.1 Install packages if needed
+# 1.2 Define dates
+# 1.3 Set up paths
+# 1.4 Other setup
+
+# 2 Prepare data
+# 2.1 Load data
+# 2.2 Data cleanup
+
+# 3 Clean home ports
+# 3.1 
