307992fe7d1a848157a54041bbfbd5418a58772d
AShipunova1 <anna.shipunova@noaa.gov>
2024-08-02 10:18:25 -0400

    Update get_google_drive_subdir_path.R
    clean up

--- auxfunctions/R/get_google_drive_subdir_path.R
+++ auxfunctions/R/get_google_drive_subdir_path.R
@@ -12,14 +12,3 @@ get_google_drive_subdir_path <-
     
     return(google_drive_subdir_info)
   }
-
-
-function(google_drive_project_name = "Egregious violators") {
-  google_drive_project_path_info <-
-    googledrive::drive_find(pattern =
-                              google_drive_project_name,
-                            type = "folder",
-                            n_max = 1)
-  
-  return(google_drive_project_path_info)
-}
