f580177e0fc871c80ee15f9c4166cc28024d14f1
AShipunova1 <anna.shipunova@noaa.gov>
2024-08-05 16:21:57 -0400

    Update home_port_main.R
    clean up

--- home_ports/home_port_main.R
+++ home_ports/home_port_main.R
@@ -1,16 +1,4 @@
 # This code cleans homeport city and state from PIMS
-# 1 Setup
-# 1.1 Install packages if needed
-# 1.2 Define dates
-# 1.3 Set up paths
-# 1.4 Other setup
-
-# 2 Prepare data
-# 2.1 Load data
-# 2.2 Data cleanup
-
-# 3 Clean home ports
-# 3.1 
 
 # Setup ----
 
